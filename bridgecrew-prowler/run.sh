fd993435301d924ad30156b9ab8f49a6f233eebc
Nimrod Kor <nimrodkor@gmail.com>
2019-11-19 22:22:15 -0800

    Create a script which assumes the role, runs prowler, and uploads to s3

---
+++ bridgecrew-prowler/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Assuming role for ${ACCOUNT_ID}"
+creds=$(aws sts assume-role --role-arn ${ROLE_ARN} --external-id ${EXTERNAL_ID} --role-session-name prowler | jq '.Credentials')
+export AWS_ACCESS_KEY_ID=$(echo $creds | jq ".AccessKeyId" | sed 's/\"//g')
+export AWS_SECRET_ACCESS_KEY=$(echo $creds | jq ".SecretAccessKey" | sed 's/\"//g')
+export AWS_SESSION_TOKEN=$(echo $creds | jq ".SessionToken" | sed 's/\"//g')
+
+echo "Running prowler on ${ACCOUNT_ID}"
+./prowler -c extra741,extra742,extra759 -M json > output.json
+
+echo "Results:"
+cat output.json
+
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN
+
+echo "Uploading result to s3://${BUCKET}/prowler/${ACCOUNT_ID}/output.json"
+aws s3api put-object --bucket ${BUCKET} --key prowler/$ACCOUNT_ID/output.json --body output.json
