c6009dd348c32cfebb66b9b931ede2feb0ff0d49
n4ch04 <nachor1992@gmail.com>
2022-06-15 16:48:38 +0200

    chore(creds renewal): Added support to credential renewal

--- providers/aws/aws_provider.py
+++ providers/aws/aws_provider.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 from arnparse import arnparse
 from boto3 import session
+from botocore.credentials import RefreshableCredentials
+from botocore.session import get_session
 
 from lib.logger import logger
 
@@ -11,12 +14,7 @@ class AWS_Credentials:
     aws_access_key_id: str
     aws_session_token: str
     aws_secret_access_key: str
-
-
-@dataclass
-class AWS_Session_Info:
-    profile: str
-    credentials: AWS_Credentials
+    expiration: datetime
 
 
 @dataclass
@@ -35,12 +33,21 @@ class AWS_Assume_Role:
     session_duration: int
     external_id: str
     sts_session: session
+    caller_identity: str
+
+
+@dataclass
+class AWS_Session_Info:
+    profile: str
+    credentials: AWS_Credentials
+    role_info: AWS_Assume_Role
 
 
 ################## AWS PROVIDER
 class AWS_Provider:
     def __init__(self, session_info):
         self.aws_session = self.set_session(session_info)
+        self.role_info = session_info.role_info
 
     def get_session(self):
         return self.aws_session
@@ -48,17 +55,43 @@ class AWS_Provider:
     def set_session(self, session_info):
         try:
             if session_info.credentials:
+                logger.info("Creating session for assumed role ...")
+                assumed_refreshable_credentials = RefreshableCredentials(
+                    access_key=session_info.credentials.aws_access_key_id,
+                    secret_key=session_info.credentials.aws_secret_access_key,
+                    token=session_info.credentials.aws_session_token,
+                    expiry_time=session_info.credentials.expiration,
+                    refresh_using=self.refresh,
+                    method="sts-assume-role",
+                )
+                assumed_botocore_session = get_session()
+                assumed_botocore_session._credentials = assumed_refreshable_credentials
+                assumed_botocore_session.set_config_variable("region", "us-east-1")
+
                 return session.Session(
                     profile_name=session_info.profile,
-                    aws_access_key_id=session_info.credentials.aws_access_key_id,
-                    aws_secret_access_key=session_info.credentials.aws_secret_access_key,
-                    aws_session_token=session_info.credentials.aws_session_token,
+                    botocore_session=assumed_botocore_session,
                 )
             else:
+                logger.info("Creating session for not assumed identity ...")
                 return session.Session(profile_name=session_info.profile)
         except Exception as error:
             logger.critical(f"{error.__class__.__name__} -- {error}")
             quit()
+
+    def refresh(self):
+        logger.info("Refreshing assumed credentials...")
+
+        response = assume_role(self.role_info)
+        refreshed_credentials = dict(
+            access_key=response["Credentials"]["AccessKeyId"],
+            secret_key=response["Credentials"]["SecretAccessKey"],
+            token=response["Credentials"]["SessionToken"],
+            expiry_time=response["Credentials"]["Expiration"].isoformat(),
+        )
+        logger.info("Refreshed Credentials:")
+        logger.info(refreshed_credentials)
+        return refreshed_credentials
 
 
 def validate_credentials(validate_session):
@@ -77,19 +110,23 @@ def provider_set_session(session_input):
     global aws_session
     global original_session
     session_info = AWS_Session_Info(
-        session_input.profile, AWS_Credentials(None, None, None)
-    )
-    role_info = AWS_Assume_Role(
-        session_input.role_name,
-        session_input.account_to_assume,
-        session_input.session_duration,
-        session_input.external_id,
+        session_input.profile,
+        None,
         None,
     )
 
     original_session = AWS_Provider(session_info).get_session()
     logger.info("Validating credentials ...")
     caller_identity = validate_credentials(original_session)
+
+    role_info = AWS_Assume_Role(
+        session_input.role_name,
+        session_input.account_to_assume,
+        session_input.session_duration,
+        session_input.external_id,
+        original_session,
+        caller_identity,
+    )
     logger.info("Credentials validated")
     logger.info(f"Original caller identity UserId : {caller_identity['UserId']}")
     logger.info(f"Original caller identity ARN : {caller_identity['Arn']}")
@@ -98,27 +135,29 @@ def provider_set_session(session_input):
         logger.info(
             f"Assuming role {role_info.role_name} in account {role_info.account_to_assume}"
         )
-        role_info.sts_session = original_session
-        assumed_role_response = assume_role(role_info, caller_identity)
+        assumed_role_response = assume_role(role_info)
         logger.info("Role assumed")
-        session_info.credentials.assumed_access_key_id = assumed_role_response[
-            "Credentials"
-        ]["AccessKeyId"]
-        session_info.credentials.assumed_secret_access_key = assumed_role_response[
-            "Credentials"
-        ]["SecretAccessKey"]
-        session_info.credentials.assumed_session_token = assumed_role_response[
-            "Credentials"
-        ]["SessionToken"]
+        session_info = AWS_Session_Info(
+            session_input.profile,
+            AWS_Credentials(
+                aws_access_key_id=assumed_role_response["Credentials"]["AccessKeyId"],
+                aws_session_token=assumed_role_response["Credentials"]["SessionToken"],
+                aws_secret_access_key=assumed_role_response["Credentials"][
+                    "SecretAccessKey"
+                ],
+                expiration=assumed_role_response["Credentials"]["Expiration"],
+            ),
+            role_info,
+        )
 
     aws_session = AWS_Provider(session_info).get_session()
 
 
-def assume_role(role_info, caller_identity):
+def assume_role(role_info):
 
     try:
         sts_client = role_info.sts_session.client("sts")
-        arn_caller_identity = arnparse(caller_identity["Arn"])
+        arn_caller_identity = arnparse(role_info.caller_identity["Arn"])
         role_arn = f"arn:{arn_caller_identity.partition}:iam::{role_info.account_to_assume}:role/{role_info.role_name}"
         if role_info.external_id:
             assumed_credentials = sts_client.assume_role(
