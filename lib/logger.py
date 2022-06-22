97080c84f3ffff3af49fc42dc13318ad9ba42329
Pepe Fagoaga <pepe@verica.io>
2022-06-22 13:16:58 +0200

    feat(logger): Include JSON formatter to file

--- lib/logger.py
+++ lib/logger.py
@@ -11,15 +11,29 @@ logging_levels = {
 
 
 def set_logging_config(log_file: str = None, log_level: str = "ERROR"):
+    # Logs formatter
+    stream_formatter = logging.Formatter(
+        "%(asctime)s [File: %(filename)s:%(lineno)d] \t[Module: %(module)s]\t %(levelname)s: %(message)s"
+    )
+    log_file_formatter = logging.Formatter(
+        '{"timestamp": "%(asctime)s", "filename": "%(filename)s:%(lineno)d", "level": "%(levelname)s", "module": "%(module)s", "message": "%(message)s"}'
+    )
+
     # Where to put logs
     logging_handlers = []
-    # Include stdout by default
-    logging_handlers.append(logging.StreamHandler())
 
-    # When logging to file the log level is DEBUG to store every log
+    # Include stdout by default
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(stream_formatter)
+    logging_handlers.append(stream_handler)
+
+    # Log to file configuration
     if log_file:
         # Set log to file handler
-        logging_handlers.append(logging.FileHandler(log_file))
+        log_file_handler = logging.FileHandler(log_file)
+        log_file_handler.setFormatter(log_file_formatter)
+        # Append the log formatter
+        logging_handlers.append(log_file_handler)
 
     # Configure Logger
     # Initialize you log configuration using the base class
