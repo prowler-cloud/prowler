6b705e5a4c07e6e0f429a8b3eb3fb0ebcfcda165
sergargar <sergio@verica.io>
2022-07-15 10:18:42 +0200

    feat(checks): add extra748, extra749, extra72

--- providers/aws/services/ec2/ec2_ebs_public_snapshot/ec2_ebs_public_snapshot.py
+++ providers/aws/services/ec2/ec2_ebs_public_snapshot/ec2_ebs_public_snapshot.py
@@ -2,7 +2,7 @@ from lib.check.models import Check, Check_Report
 from providers.aws.services.ec2.ec2_service import ec2_client
 
 
-class ec2_ebs_snapshots_encrypted(Check):
+class ec2_ebs_public_snapshot(Check):
     def execute(self):
         findings = []
         for regional_client in ec2_client.regional_clients:
@@ -11,16 +11,16 @@ class ec2_ebs_snapshots_encrypted(Check):
                 for snapshot in regional_client.snapshots:
                     report = Check_Report(self.metadata)
                     report.region = region
-                    if snapshot.encrypted:
+                    if not snapshot.public:
                         report.status = "PASS"
                         report.status_extended = (
-                            f"EBS Snapshot {snapshot.id} is encrypted"
+                            f"EBS Snapshot {snapshot.id} is not Public"
                         )
                         report.resource_id = snapshot.id
                     else:
                         report.status = "FAIL"
                         report.status_extended = (
-                            f"EBS Snapshot {snapshot.id} is unencrypted"
+                            f"EBS Snapshot {snapshot.id} is currently Public"
                         )
                         report.resource_id = snapshot.id
                     findings.append(report)
--- providers/aws/services/ec2/ec2_service.py
+++ providers/aws/services/ec2/ec2_service.py
@@ -1,4 +1,5 @@
 import threading
+from dataclasses import dataclass
 
 from lib.logger import logger
 from providers.aws.aws_provider import current_audit_info, generate_regional_clients
@@ -11,7 +12,9 @@ class EC2:
         self.session = audit_info.audit_session
         self.audited_account = audit_info.audited_account
         self.regional_clients = generate_regional_clients(self.service, audit_info)
+        self.__threading_call__(self.__describe_security_groups__)
         self.__threading_call__(self.__describe_snapshots__)
+        self.__threading_call__(self.__get_snapshot_public__)
 
     def __get_session__(self):
         return self.session
@@ -19,34 +22,102 @@ class EC2:
     def __threading_call__(self, call):
         threads = []
         for regional_client in self.regional_clients:
-            threads.append(
-                threading.Thread(
-                    target=call, args=(regional_client, self.audited_account)
-                )
-            )
+            threads.append(threading.Thread(target=call, args=(regional_client,)))
         for t in threads:
             t.start()
         for t in threads:
             t.join()
 
-    def __describe_snapshots__(self, regional_client, audited_account):
+    def __describe_security_groups__(self, regional_client):
+        logger.info("EC2 - Describing Security Groups...")
+        try:
+            describe_security_groups_paginator = regional_client.get_paginator(
+                "describe_security_groups"
+            )
+            security_groups = []
+            for page in describe_security_groups_paginator.paginate():
+                for sg in page["SecurityGroups"]:
+                    security_groups.append(
+                        SecurityGroup(
+                            sg["GroupName"],
+                            sg["GroupId"],
+                            sg["IpPermissions"],
+                            sg["IpPermissionsEgress"],
+                        )
+                    )
+        except Exception as error:
+            logger.error(
+                f"{regional_client.region} -- {error.__class__.__name__}: {error}"
+            )
+            regional_client.security_groups = []
+        else:
+            regional_client.security_groups = security_groups
+
+    def __describe_snapshots__(self, regional_client):
         logger.info("EC2 - Describing Snapshots...")
         try:
             describe_snapshots_paginator = regional_client.get_paginator(
                 "describe_snapshots"
             )
             snapshots = []
+            encrypted = False
             for page in describe_snapshots_paginator.paginate(
-                OwnerIds=[audited_account]
+                OwnerIds=[self.audited_account]
             ):
                 for snapshot in page["Snapshots"]:
-                    snapshots.append(snapshot)
+                    if snapshot["Encrypted"]:
+                        encrypted = True
+                    snapshots.append(Snapshot(snapshot["SnapshotId"], encrypted))
         except Exception as error:
             logger.error(
                 f"{regional_client.region} -- {error.__class__.__name__}: {error}"
             )
+            regional_client.snapshots = []
         else:
             regional_client.snapshots = snapshots
+
+    def __get_snapshot_public__(self, regional_client):
+        logger.info("EC2 - Get snapshots encryption...")
+        try:
+            if hasattr(regional_client, "snapshots"):
+                for snapshot in regional_client.snapshots:
+                    snapshot_public = regional_client.describe_snapshot_attribute(
+                        Attribute="createVolumePermission", SnapshotId=snapshot.id
+                    )
+                    for permission in snapshot_public["CreateVolumePermissions"]:
+                        if "Group" in permission:
+                            if permission["Group"] == "all":
+                                snapshot.public = True
+        except Exception as error:
+            logger.error(
+                f"{regional_client.region} -- {error.__class__.__name__}: {error}"
+            )
+
+
+@dataclass
+class Snapshot:
+    id: str
+    encrypted: bool
+    public: bool
+
+    def __init__(self, id, encrypted):
+        self.id = id
+        self.encrypted = encrypted
+        self.public = False
+
+
+@dataclass
+class SecurityGroup:
+    name: str
+    id: str
+    ingress_rules: list[dict]
+    egress_rules: list[dict]
+
+    def __init__(self, name, id, ingress_rules, egress_rules):
+        self.name = name
+        self.id = id
+        self.ingress_rules = ingress_rules
+        self.egress_rules = egress_rules
 
 
 ec2_client = EC2(current_audit_info)
