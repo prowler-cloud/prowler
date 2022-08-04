f30245bb1534ea9c231f72d77893709decb5d35a
Pepe Fagoaga <pepe@verica.io>
2022-08-04 16:33:16 +0200

    fix(nacls): Handle IPv6 source ingress (#1319)

--- providers/aws/services/ec2/ec2_networkacl_allow_ingress_tcp_port_22/ec2_networkacl_allow_ingress_tcp_port_22.py
+++ providers/aws/services/ec2/ec2_networkacl_allow_ingress_tcp_port_22/ec2_networkacl_allow_ingress_tcp_port_22.py
@@ -1,29 +1,26 @@
 from lib.check.models import Check, Check_Report
-from providers.aws.services.ec2.ec2_service import ec2_client
+from providers.aws.services.ec2.ec2_service import check_network_acl, ec2_client
 
 
 class ec2_networkacl_allow_ingress_tcp_port_22(Check):
     def execute(self):
         findings = []
+        tcp_protocol = "6"
         check_port = 22
         for network_acl in ec2_client.network_acls:
             public = False
             report = Check_Report(self.metadata)
             report.region = network_acl.region
             for entry in network_acl.entries:
-                if (
-                    entry["CidrBlock"] == "0.0.0.0/0"
-                    and entry["RuleAction"] == "allow"
-                    and not entry["Egress"]
-                ):
-                    if entry["Protocol"] == "-1":
-                        public = True
-                    elif (
-                        entry["PortRange"]["From"] == check_port
-                        and entry["PortRange"]["To"] == check_port
-                        and entry["Protocol"] == "6"
-                    ):
-                        public = True
+                # For IPv4
+                if "CidrBlock" in entry:
+                    public = check_network_acl(entry, tcp_protocol, check_port, "IPv4")
+                # For IPv6
+                if "Ipv6CidrBlock" in entry:
+                    public = check_network_acl(entry, tcp_protocol, check_port, "IPv6")
+                # If some entry allows it, that ACL is not securely configured
+                if public:
+                    break
             if not public:
                 report.status = "PASS"
                 report.status_extended = f"Network ACL {network_acl.id} has not SSH port 22 open to the Internet."
