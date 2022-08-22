d18b430c16c0a6efd38d5f6d71ecbaa3b3a9f068
Sergio Garcia <38561120+sergargar@users.noreply.github.com>
2022-08-22 09:33:43 +0100

    feat(new_checks): add check for ec2 and iam (#1337)
    * fix(key_error): remove KeyError.
    
    * feat(ftp_check): add ec2_securitygroup_allow_ingress_from_internet_to_tcp_ftp_port_20_21 check.
    
    * feat(password_check): iam_password_policy_expires_passwords_within_15_days_or_less added.
    
    * change days to 90
    
    Co-authored-by: sergargar <sergio@verica.io>

---
+++ providers/aws/services/ec2/ec2_securitygroup_allow_ingress_from_internet_to_tcp_ftp_port_20_21/ec2_securitygroup_allow_ingress_from_internet_to_tcp_ftp_port_20_21.py
@@ -0,0 +1,27 @@
+from lib.check.models import Check, Check_Report
+from providers.aws.services.ec2.ec2_service import check_security_group, ec2_client
+
+
+class ec2_securitygroup_allow_ingress_from_internet_to_tcp_ftp_port_20_21(Check):
+    def execute(self):
+        findings = []
+        check_ports = [20, 21]
+        for security_group in ec2_client.security_groups:
+            public = False
+            report = Check_Report(self.metadata)
+            report.region = security_group.region
+            # Loop through every security group's ingress rule and check it
+            for ingress_rule in security_group.ingress_rules:
+                public = check_security_group(ingress_rule, "tcp", check_ports)
+                # Check
+                if public:
+                    report.status = "FAIL"
+                    report.status_extended = f"Security group {security_group.name} ({security_group.id}) has FTP ports 20 and 21 open to the Internet."
+                    report.resource_id = security_group.id
+                else:
+                    report.status = "PASS"
+                    report.status_extended = f"Security group {security_group.name} ({security_group.id}) has not FPT ports 20 and 21 open to the Internet."
+                    report.resource_id = security_group.id
+                findings.append(report)
+
+        return findings
