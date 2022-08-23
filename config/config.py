60cb23ee498083ac084349eeb744c9e314330cad
n4ch04 <nachor1992@gmail.com>
2022-08-23 11:09:45 +0200

    chore(tests): added tests to outputs

--- config/config.py
+++ config/config.py
@@ -14,6 +14,7 @@ aws_services_json_file = "providers/aws/aws_regions_by_service.json"
 default_output_directory = getcwd() + "/output"
 
 output_file_timestamp = timestamp.strftime("%Y%m%d%H%M%S")
+timestamp_iso = timestamp.isoformat()
 csv_file_suffix = f"{output_file_timestamp}.csv"
 json_file_suffix = f"{output_file_timestamp}.json"
 json_asff_file_suffix = f"{output_file_timestamp}.asff.json"
