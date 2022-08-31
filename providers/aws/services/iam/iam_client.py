df24e7b563ef7d905d9ae62df1698c6be421875d
Pepe Fagoaga <pepe@verica.io>
2022-08-31 07:56:07 +0200

    fix(client): Point to audit info

--- providers/aws/services/iam/iam_client.py
+++ providers/aws/services/iam/iam_client.py
@@ -1,4 +1,4 @@
-from providers.aws.aws_provider import current_audit_info
+from providers.aws.lib.audit_info.audit_info import current_audit_info
 from providers.aws.services.iam.iam_service import IAM
 
 iam_client = IAM(current_audit_info)
