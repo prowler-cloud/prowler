be74db15701237dacf02f412fba4b59237765094
Toni de la Fuente <toni@blyx.com>
2022-09-02 12:48:42 +0200

    feat(accessanalyzer) Check accessanalyzer_enabled_without_findings

---
+++ providers/aws/services/accessanalyzer/accessanalyzer_service_test.py
@@ -0,0 +1,35 @@
+from boto3 import client, resource, session
+from moto import mock_ec2
+
+from providers.aws.lib.audit_info.models import AWS_Audit_Info
+from providers.aws.services.accessanalyzer.accessanalyzer_service import ACCESSANALYZER
+
+AWS_ACCOUNT_NUMBER = 123456789012
+AWS_REGION = "us-east-1"
+
+class Test_ACCESSANALYZER_Service:
+    # Mocked Audit Info
+    def set_mocked_audit_info(self):
+        audit_info = AWS_Audit_Info(
+            original_session=None,
+            audit_session=session.Session(
+                profile_name=None,
+                botocore_session=None,
+            ),
+            audited_account=AWS_ACCOUNT_NUMBER,
+            audited_user_id=None,
+            audited_partition="aws",
+            audited_identity_arn=None,
+            profile=None,
+            profile_region=None,
+            credentials=None,
+            assumed_role_info=None,
+            audited_regions=None,
+            organizations_metadata=None,
+        )
+        return audit_info
+
+    # Test ACCESSANALYZER Service
+    # not covered by moto
+    # more info here https://github.com/spulec/moto/blob/a2a1967ef869091d747da74ffb4f4f05bd3535cd/IMPLEMENTATION_COVERAGE.md
+    # lol 
