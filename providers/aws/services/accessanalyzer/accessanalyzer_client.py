ad0e111fdeddbe83a8b9b72ad226c4b5f4213521
Sergio Garcia <38561120+sergargar@users.noreply.github.com>
2022-09-12 09:53:38 +0200

    Update providers/aws/services/accessanalyzer/accessanalyzer_client.py
    Co-authored-by: Pepe Fagoaga <pepe@verica.io>

--- providers/aws/services/accessanalyzer/accessanalyzer_client.py
+++ providers/aws/services/accessanalyzer/accessanalyzer_client.py
@@ -1,4 +1,4 @@
 from providers.aws.lib.audit_info.audit_info import current_audit_info
 from providers.aws.services.accessanalyzer.accessanalyzer_service import ACCESSANALYZER
 
-accessanalyzer_client = ACCESSANALYZER(current_audit_info)
+accessanalyzer_client = AccessAnalyzer(current_audit_info)
