8187c478090003197f96c4be6b0f345a38fd4557
Nacho Rivera <59198746+n4ch04@users.noreply.github.com>
2022-10-18 16:52:55 +0200

    fix(comment): Deleted comment
    Co-authored-by: Pepe Fagoaga <pepe@verica.io>

--- providers/aws/services/iam/iam_user_no_setup_initial_access_key/iam_user_no_setup_initial_access_key_test.py
+++ providers/aws/services/iam/iam_user_no_setup_initial_access_key/iam_user_no_setup_initial_access_key_test.py
@@ -1,9 +1,3 @@
-# user;arn;user_creation_time;password_enabled;password_last_used;password_last_changed;password_next_rotation;mfa_active;access_key_1_active;access_key_1_last_rotated;access_key_1_last_used_date;access_key_1_last_used_region;access_key_1_last_used_service;access_key_2_active;access_key_2_last_rotated;access_key_2_last_used_date;access_key_2_last_used_region;access_key_2_last_used_service;cert_1_active;cert_1_last_rotated;cert_2_active;cert_2_last_rotated
-# <root_account>;arn:aws:iam::106908755756:root;2022-02-17T14:59:38+00:00;not_supported;no_information;not_supported;not_supported;false;false;N/A;N/A;N/A;N/A;false;N/A;N/A;N/A;N/A;false;N/A;false;N/A
-# test_false_access_key_1;arn:aws:iam::106908755756:test_false_access_key_1;2022-04-17T14:59:38+00:00;true;no_information;not_supported;not_supported;false;true;N/A;N/A;N/A;N/A;false;N/A;N/A;N/A;N/A;false;N/A;false;N/A
-# test_false_access_key_2;arn:aws:iam::106908755756:test_false_access_key_2;2022-04-17T14:59:38+00:00;true;no_information;not_supported;not_supported;false;false;N/A;N/A;N/A;N/A;true;N/A;N/A;N/A;N/A;false;N/A;false;N/A
-# test_pass;arn:aws:iam::106908755756:test_pass;2022-02-17T14:59:38+00:00;not_supported;no_information;not_supported;not_supported;false;false;N/A;N/A;N/A;N/A;false;N/A;N/A;N/A;N/A;false;N/A;false;N/A
-
 from csv import DictReader
 from re import search
 from unittest import mock
