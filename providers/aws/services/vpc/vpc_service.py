bddfc0cf2bc8967a3bc42ba47748dd7df17c9d96
sergargar <sergio@verica.io>
2022-10-25 13:17:12 +0200

    add check44 and tests

--- providers/aws/services/vpc/vpc_service.py
+++ providers/aws/services/vpc/vpc_service.py
@@ -13,8 +13,11 @@ class VPC:
         self.audited_account = audit_info.audited_account
         self.regional_clients = generate_regional_clients(self.service, audit_info)
         self.vpcs = []
+        self.vpc_peering_connections = []
         self.__threading_call__(self.__describe_vpcs__)
+        self.__threading_call__(self.__describe_vpc_peering_connections__)
         self.__describe_flow_logs__()
+        self.__describe_route_tables__()
 
     def __get_session__(self):
         return self.session
@@ -40,6 +43,61 @@ class VPC:
                             vpc["IsDefault"],
                             vpc["CidrBlock"],
                             regional_client.region,
+                        )
+                    )
+        except Exception as error:
+            logger.error(
+                f"{regional_client.region} -- {error.__class__.__name__}: {error}"
+            )
+
+    def __describe_vpc_peering_connections__(self, regional_client):
+        logger.info("VPC - Describing VPC Peering Connections...")
+        try:
+            describe_vpc_peering_connections_paginator = regional_client.get_paginator(
+                "describe_vpc_peering_connections"
+            )
+            for page in describe_vpc_peering_connections_paginator.paginate():
+                for conn in page["VpcPeeringConnections"]:
+                    self.vpc_peering_connections.append(
+                        VpcPeeringConnection(
+                            conn["VpcPeeringConnectionId"],
+                            conn["AccepterVpcInfo"]["VpcId"],
+                            conn["AccepterVpcInfo"]["CidrBlock"],
+                            conn["RequesterVpcInfo"]["VpcId"],
+                            conn["RequesterVpcInfo"]["CidrBlock"],
+                            regional_client.region,
+                        )
+                    )
+        except Exception as error:
+            logger.error(
+                f"{regional_client.region} -- {error.__class__.__name__}: {error}"
+            )
+
+    def __describe_route_tables__(self):
+        logger.info("VPC - Describing Peering Route Tables...")
+        try:
+            for conn in self.vpc_peering_connections:
+                regional_client = self.regional_clients[conn.region]
+                for route_table in regional_client.describe_route_tables(
+                    Filters=[
+                        {
+                            "Name": "route.vpc-peering-connection-id",
+                            "Values": [
+                                conn.id,
+                            ],
+                        },
+                    ]
+                )["RouteTables"]:
+                    destination_cidrs = []
+                    for route in route_table["Routes"]:
+                        if (
+                            route["Origin"] != "CreateRouteTable"
+                        ):  # avoid default route table
+                            destination_cidrs.append(route["DestinationCidrBlock"])
+                    conn.route_tables.append(
+                        Route(
+                            route_table["RouteTableId"],
+                            destination_cidrs,
                         )
                     )
         except Exception as error:
@@ -87,4 +145,46 @@ class VPCs:
         self.default = default
         self.cidr_block = cidr_block
         self.flow_log = False
+        self.region = region
+
+
+@dataclass
+class Route:
+    id: str
+    destination_cidrs: list[str]
+
+    def __init__(
+        self,
+        id,
+        destination_cidrs,
+    ):
+        self.id = id
+        self.destination_cidrs = destination_cidrs
+
+
+@dataclass
+class VpcPeeringConnection:
+    id: str
+    accepter_vpc: str
+    accepter_cidr: str
+    requester_vpc: str
+    requester_cidr: str
+    route_tables: list[Route]
+    region: str
+
+    def __init__(
+        self,
+        id,
+        accepter_vpc,
+        accepter_cidr,
+        requester_vpc,
+        requester_cidr,
+        region,
+    ):
+        self.id = id
+        self.accepter_vpc = accepter_vpc
+        self.accepter_cidr = accepter_cidr
+        self.requester_vpc = requester_vpc
+        self.requester_cidr = requester_cidr
+        self.route_tables = []
         self.region = region
