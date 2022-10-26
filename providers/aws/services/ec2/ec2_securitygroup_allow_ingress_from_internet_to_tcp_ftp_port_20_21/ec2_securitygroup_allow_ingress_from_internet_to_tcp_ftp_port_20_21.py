ec25768ac64714d45c1ab2c95222485656f1a1ff
Pepe Fagoaga <pepe@verica.io>
2022-10-26 08:54:24 +0200

    fix(typo): FPT -> FTP

--- providers/aws/services/ec2/ec2_securitygroup_allow_ingress_from_internet_to_tcp_ftp_port_20_21/ec2_securitygroup_allow_ingress_from_internet_to_tcp_ftp_port_20_21.py
+++ providers/aws/services/ec2/ec2_securitygroup_allow_ingress_from_internet_to_tcp_ftp_port_20_21/ec2_securitygroup_allow_ingress_from_internet_to_tcp_ftp_port_20_21.py
@@ -22,7 +22,7 @@ class ec2_securitygroup_allow_ingress_from_internet_to_tcp_ftp_port_20_21(Check)
                     report.resource_id = security_group.id
                 else:
                     report.status = "PASS"
-                    report.status_extended = f"Security group {security_group.name} ({security_group.id}) has not FPT ports 20 and 21 open to the Internet."
+                    report.status_extended = f"Security group {security_group.name} ({security_group.id}) has not FTP ports 20 and 21 open to the Internet."
                     report.resource_id = security_group.id
                 findings.append(report)
 
