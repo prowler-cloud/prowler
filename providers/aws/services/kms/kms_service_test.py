d29d6929df8f1cb6a3a05e1453b06695df7039e2
sergargar <sergio@verica.io>
2022-10-28 10:52:57 +0200

    add services tests

--- providers/aws/services/kms/kms_service_test.py
+++ providers/aws/services/kms/kms_service_test.py
@@ -1,3 +1,5 @@
+import json
+
 from boto3 import client, session
 from moto import mock_kms
 
@@ -77,3 +79,84 @@ class Test_ACM_Service:
         assert len(kms.keys) == 2
         assert kms.keys[0].arn == key1["Arn"]
         assert kms.keys[1].arn == key2["Arn"]
+
+    # Test KMS Describe Keys
+    @mock_kms
+    def test__describe_key__(self):
+        # Generate KMS Client
+        kms_client = client("kms", region_name=AWS_REGION)
+        # Create KMS keys
+        key1 = kms_client.create_key()["KeyMetadata"]
+        # KMS client for this test class
+        audit_info = self.set_mocked_audit_info()
+        kms = KMS(audit_info)
+        assert len(kms.keys) == 1
+        assert kms.keys[0].arn == key1["Arn"]
+        assert kms.keys[0].state == key1["KeyState"]
+        assert kms.keys[0].origin == key1["Origin"]
+        assert kms.keys[0].manager == key1["KeyManager"]
+
+    # Test KMS Get rotation status
+    @mock_kms
+    def test__get_key_rotation_status__(self):
+        # Generate KMS Client
+        kms_client = client("kms", region_name=AWS_REGION)
+        # Create KMS keys
+        key1 = kms_client.create_key()["KeyMetadata"]
+        key2 = kms_client.create_key()["KeyMetadata"]
+        kms_client.enable_key_rotation(KeyId=key2["KeyId"])
+        # KMS client for this test class
+        audit_info = self.set_mocked_audit_info()
+        kms = KMS(audit_info)
+        assert len(kms.keys) == 2
+        assert kms.keys[0].arn == key1["Arn"]
+        assert kms.keys[0].rotation_enabled == False
+        assert kms.keys[1].arn == key2["Arn"]
+        assert kms.keys[1].rotation_enabled == True
+
+    # Test KMS Key policy
+    @mock_kms
+    def test__get_key_policy__(self):
+        public_policy = json.dumps(
+            {
+                "Version": "2012-10-17",
+                "Id": "key-default-1",
+                "Statement": [
+                    {
+                        "Sid": "Enable IAM User Permissions",
+                        "Effect": "Allow",
+                        "Principal": "*",
+                        "Action": "kms:*",
+                        "Resource": "*",
+                    }
+                ],
+            }
+        )
+        default_policy = json.dumps(
+            {
+                "Version": "2012-10-17",
+                "Id": "key-default-1",
+                "Statement": [
+                    {
+                        "Sid": "Enable IAM User Permissions",
+                        "Effect": "Allow",
+                        "Principal": {"AWS": "arn:aws:iam::123456789012:root"},
+                        "Action": "kms:*",
+                        "Resource": "*",
+                    }
+                ],
+            }
+        )
+        # Generate KMS Client
+        kms_client = client("kms", region_name=AWS_REGION)
+        # Create KMS keys
+        key1 = kms_client.create_key(Policy=default_policy)["KeyMetadata"]
+        key2 = kms_client.create_key(Policy=public_policy)["KeyMetadata"]
+        # KMS client for this test class
+        audit_info = self.set_mocked_audit_info()
+        kms = KMS(audit_info)
+        assert len(kms.keys) == 2
+        assert kms.keys[0].arn == key1["Arn"]
+        assert kms.keys[0].policy == json.loads(default_policy)
+        assert kms.keys[1].arn == key2["Arn"]
+        assert kms.keys[1].policy == json.loads(public_policy)
