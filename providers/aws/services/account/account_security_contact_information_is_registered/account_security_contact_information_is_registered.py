1ae0e9bd7b0acc2b52ebfcd0a739dbe2e3e6a24a
sergargar <sergio@verica.io>
2022-11-02 10:28:37 +0100

    add account service

---
+++ providers/aws/services/account/account_security_contact_information_is_registered/account_security_contact_information_is_registered.py
@@ -0,0 +1,13 @@
+from colorama import Fore, Style
+
+from lib.check.models import Check
+
+### This check has no findings since it is manual
+
+
+class account_security_contact_information_is_registered(Check):
+    def execute(self):
+        print(
+            f"\t{Fore.YELLOW}INFO{Style.RESET_ALL} Manual check: Login to the AWS Console. Choose your account name on the top right of the window -> My Account -> Alternate Contacts -> Security Section."
+        )
+        return []
