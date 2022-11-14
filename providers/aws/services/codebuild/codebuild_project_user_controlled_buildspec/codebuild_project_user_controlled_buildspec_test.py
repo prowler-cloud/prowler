2876ad1823fd4272965013d5d9102d4d1034b4e3
Pepe Fagoaga <pepe@verica.io>
2022-11-14 16:55:12 +0100

    fix(codebuild_project_user_controlled_buildspec): regex

--- providers/aws/services/codebuild/codebuild_project_user_controlled_buildspec/codebuild_project_user_controlled_buildspec_test.py
+++ providers/aws/services/codebuild/codebuild_project_user_controlled_buildspec/codebuild_project_user_controlled_buildspec_test.py
@@ -9,7 +9,10 @@ class Test_codebuild_project_user_controlled_buildspec:
         codebuild_client = mock.MagicMock
         codebuild_client.projects = [
             CodebuildProject(
-                name="test", region="eu-west-1", last_invoked_time=None, buildspec=None
+                name="test",
+                region="eu-west-1",
+                last_invoked_time=None,
+                buildspec=None,
             )
         ]
         with mock.patch(
@@ -26,7 +29,8 @@ class Test_codebuild_project_user_controlled_buildspec:
             assert len(result) == 1
             assert result[0].status == "FAIL"
             assert search(
-                "does not use a user controlled buildspec", result[0].status_extended
+                "does not use a user controlled buildspec",
+                result[0].status_extended,
             )
             assert result[0].resource_id == "test"
             assert result[0].resource_arn == ""
@@ -55,7 +59,8 @@ class Test_codebuild_project_user_controlled_buildspec:
             assert len(result) == 1
             assert result[0].status == "FAIL"
             assert search(
-                "does not use a user controlled buildspec", result[0].status_extended
+                "does not use a user controlled buildspec",
+                result[0].status_extended,
             )
             assert result[0].resource_id == "test"
             assert result[0].resource_arn == ""
@@ -84,5 +89,35 @@ class Test_codebuild_project_user_controlled_buildspec:
             assert len(result) == 1
             assert result[0].status == "PASS"
             assert search("uses a user controlled buildspec", result[0].status_extended)
+            assert result[0].resource_id == "test"
+            assert result[0].resource_arn == ""
+
+    def test_project_invalid_buildspec_without_extension(self):
+        codebuild_client = mock.MagicMock
+        codebuild_client.projects = [
+            CodebuildProject(
+                name="test",
+                region="eu-west-1",
+                last_invoked_time=None,
+                buildspec="arn:aws:s3:::my-codebuild-sample2/buildspecyaml",
+            )
+        ]
+        with mock.patch(
+            "providers.aws.services.codebuild.codebuild_service.Codebuild",
+            codebuild_client,
+        ):
+            from providers.aws.services.codebuild.codebuild_project_user_controlled_buildspec.codebuild_project_user_controlled_buildspec import (
+                codebuild_project_user_controlled_buildspec,
+            )
+
+            check = codebuild_project_user_controlled_buildspec()
+            result = check.execute()
+
+            assert len(result) == 1
+            assert result[0].status == "FAIL"
+            assert search(
+                "does not use a user controlled buildspec",
+                result[0].status_extended,
+            )
             assert result[0].resource_id == "test"
             assert result[0].resource_arn == ""
