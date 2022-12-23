2b73bcf853609a230e18956b5222b2818982a417
sergargar <sergio@verica.io>
2022-12-23 10:59:21 +0100

    fix trustedadvisor service

--- prowler/providers/aws/services/trustedadvisor/trustedadvisor_service.py
+++ prowler/providers/aws/services/trustedadvisor/trustedadvisor_service.py
@@ -4,7 +4,6 @@ from botocore.client import ClientError
 from pydantic import BaseModel
 
 from prowler.lib.logger import logger
-from prowler.providers.aws.aws_provider import generate_regional_clients
 
 
 ################################ TrustedAdvisor
@@ -12,14 +11,20 @@ class TrustedAdvisor:
     def __init__(self, audit_info):
         self.service = "support"
         self.session = audit_info.audit_session
-        global_client = generate_regional_clients(
-            self.service, audit_info, global_service=True
-        )
+        self.account = audit_info.audited_account
         self.checks = []
+        self.enabled = True
         # Support API is not available in China Partition
-        if global_client and audit_info.audited_partition != "aws-cn":
-            self.client = list(global_client.values())[0]
-            self.region = self.client.region
+        # But only in us-east-1 or us-gov-west-1 https://docs.aws.amazon.com/general/latest/gr/awssupport.html
+        if audit_info.audited_partition != "aws-cn":
+            if audit_info.audited_partition == "aws":
+                support_region = "us-east-1"
+            else:
+                support_region = "us-gov-west-1"
+            self.client = audit_info.audit_session.client(
+                self.service, region_name=support_region
+            )
+            self.client.region = self.region = support_region
             self.__describe_trusted_advisor_checks__()
             self.__describe_trusted_advisor_check_result__()
 
@@ -40,7 +45,9 @@ class TrustedAdvisor:
                     )
                 )
         except ClientError as error:
-            if error.response["Error"]["Code"] != "SubscriptionRequiredException":
+            if error.response["Error"]["Code"] == "SubscriptionRequiredException":
+                self.enabled = False
+            else:
                 logger.error(
                     f"{self.client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
                 )
