88c7df1c800cb628b06f6859def2a1c565a50e1c
Pepe Fagoaga <pepe@verica.io>
2022-12-23 10:49:01 +0100

    fix(regions): Include subservices in the regions json

--- prowler/providers/aws/aws_provider.py
+++ prowler/providers/aws/aws_provider.py
@@ -113,54 +113,9 @@ def generate_regional_clients(
         f = open_file(f"{actual_directory}/{aws_services_json_file}")
         data = parse_json_file(f)
         # Check if it is a subservice
-        if service == "accessanalyzer":
-            json_regions = data["services"]["iam"]["regions"][
-                audit_info.audited_partition
-            ]
-        elif service == "apigatewayv2":
-            json_regions = data["services"]["apigateway"]["regions"][
-                audit_info.audited_partition
-            ]
-        elif service == "macie2":
-            json_regions = data["services"]["macie"]["regions"][
-                audit_info.audited_partition
-            ]
-        elif service == "logs":
-            json_regions = data["services"]["cloudwatch"]["regions"][
-                audit_info.audited_partition
-            ]
-        elif service == "dax":
-            json_regions = data["services"]["dynamodb"]["regions"][
-                audit_info.audited_partition
-            ]
-        elif service == "glacier":
-            json_regions = data["services"]["s3"]["regions"][
-                audit_info.audited_partition
-            ]
-        elif service == "opensearch":
-            json_regions = data["services"]["es"]["regions"][
-                audit_info.audited_partition
-            ]
-        elif service == "elbv2":
-            json_regions = data["services"]["elb"]["regions"][
-                audit_info.audited_partition
-            ]
-        elif service == "route53domains":
-            json_regions = data["services"]["route53"]["regions"][
-                audit_info.audited_partition
-            ]
-        elif service == "s3control":
-            json_regions = data["services"]["s3"]["regions"][
-                audit_info.audited_partition
-            ]
-        elif service == "wafv2" or service == "waf-regional":
-            json_regions = data["services"]["waf"]["regions"][
-                audit_info.audited_partition
-            ]
-        else:
-            json_regions = data["services"][service]["regions"][
-                audit_info.audited_partition
-            ]
+        json_regions = data["services"][service]["regions"][
+            audit_info.audited_partition
+        ]
         if audit_info.audited_regions:  # Check for input aws audit_info.audited_regions
             regions = list(
                 set(json_regions).intersection(audit_info.audited_regions)
