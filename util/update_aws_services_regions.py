a78c6a08e7d269e6a0cf2e1c02b5dfd23a53ca46
Sergio Garcia <38561120+sergargar@users.noreply.github.com>
2023-01-03 12:24:37 +0100

    typo
    Co-authored-by: Pepe Fagoaga <pepe@verica.io>

--- util/update_aws_services_regions.py
+++ util/update_aws_services_regions.py
@@ -50,7 +50,6 @@ regions_by_service["services"]["opensearch"] = regions_by_service["services"]["e
 regions_by_service["services"]["elbv2"] = regions_by_service["services"]["elb"]
 # wafv2 --> waf
 regions_by_service["services"]["wafv2"] = regions_by_service["services"]["waf"]
-print(regions_by_service)
 
 # Write to file
 parsed_matrix_regions_aws = f"{os.path.dirname(os.path.realpath(__name__))}/prowler/providers/aws/aws_regions_by_service.json"
