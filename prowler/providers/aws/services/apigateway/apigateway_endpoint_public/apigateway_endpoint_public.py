7eef8acb556ff7a01c4d61c4cdac8c46ea21cab4
Gabriel Alejandro Soltz <thegaby@gmail.com>
2023-01-14 13:25:27 +0100

    apigateway_endpoint_public_arn

--- prowler/providers/aws/services/apigateway/apigateway_endpoint_public/apigateway_endpoint_public.py
+++ prowler/providers/aws/services/apigateway/apigateway_endpoint_public/apigateway_endpoint_public.py
@@ -10,16 +10,16 @@ class apigateway_endpoint_public(Check):
         for rest_api in apigateway_client.rest_apis:
             report = Check_Report_AWS(self.metadata())
             report.region = rest_api.region
+            report.resource_id = rest_api.name
+            report.resource_arn = rest_api.arn
             if rest_api.public_endpoint:
                 report.status = "FAIL"
                 report.status_extended = f"API Gateway {rest_api.name} ID {rest_api.id} is internet accesible."
-                report.resource_id = rest_api.name
             else:
                 report.status = "PASS"
                 report.status_extended = (
                     f"API Gateway {rest_api.name} ID {rest_api.id} is private."
                 )
-                report.resource_id = rest_api.name
             findings.append(report)
 
         return findings
