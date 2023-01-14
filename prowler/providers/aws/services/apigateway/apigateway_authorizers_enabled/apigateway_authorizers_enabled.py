efa4f69dd31733278abd6dac66685e254c29b862
Gabriel Alejandro Soltz <thegaby@gmail.com>
2023-01-14 13:26:04 +0100

    apigateway_authorizers_enabled_arn

--- prowler/providers/aws/services/apigateway/apigateway_authorizers_enabled/apigateway_authorizers_enabled.py
+++ prowler/providers/aws/services/apigateway/apigateway_authorizers_enabled/apigateway_authorizers_enabled.py
@@ -10,14 +10,14 @@ class apigateway_authorizers_enabled(Check):
         for rest_api in apigateway_client.rest_apis:
             report = Check_Report_AWS(self.metadata())
             report.region = rest_api.region
+            report.resource_id = rest_api.name
+            report.resource_arn = rest_api.arn
             if rest_api.authorizer:
                 report.status = "PASS"
                 report.status_extended = f"API Gateway {rest_api.name} ID {rest_api.id} has authorizer configured."
-                report.resource_id = rest_api.name
             else:
                 report.status = "FAIL"
                 report.status_extended = f"API Gateway {rest_api.name} ID {rest_api.id} has not authorizer configured."
-                report.resource_id = rest_api.name
             findings.append(report)
 
         return findings
