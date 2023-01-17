52d65ee4e84f5318a151ef78dd9329c806f21a07
Sergio Garcia <38561120+sergargar@users.noreply.github.com>
2023-01-17 17:53:08 +0100

    feat(pypi): replicate PyPi package (#1727)
    Co-authored-by: sergargar <sergio@verica.io>

---
+++ util/replicate_pypi_package.py
@@ -0,0 +1,10 @@
+import toml
+
+data = toml.load("pyproject.toml")
+# Modify field
+data["project"]["name"] = "prowler"
+
+# To use the dump function, you need to open the file in 'write' mode
+f = open("pyproject.toml", "w")
+toml.dump(data, f)
+f.close()
