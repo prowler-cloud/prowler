656425714108bf66cdb544e615531953a5bad017
sergargar <sergio@verica.io>
2023-01-17 17:09:16 +0100

    test version

--- util/replicate_pypi_package.py
+++ util/replicate_pypi_package.py
@@ -3,8 +3,9 @@ import toml
 data = toml.load("pyproject.toml")
 # Modify field
 data["project"]["name"] = "prowler"
+#data["tool"]["setuptools"]["py-modules"]  = []
 
 # To use the dump function, you need to open the file in 'write' mode
 f = open("pyproject.toml", "w")
-toml.dumps(data)
+toml.dump(data, f)
 f.close()
