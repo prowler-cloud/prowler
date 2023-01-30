7941f6a0376fd76c942dceb05c2c2cf8a873dec1
Sergio Garcia <38561120+sergargar@users.noreply.github.com>
2023-01-30 16:51:07 +0100

    fix(ec2_securitygroup_not_used): ignore default security groups (#1800)
    Co-authored-by: sergargar <sergio@verica.io>

--- prowler/providers/aws/services/ec2/ec2_securitygroup_not_used/ec2_securitygroup_not_used.py
+++ prowler/providers/aws/services/ec2/ec2_securitygroup_not_used/ec2_securitygroup_not_used.py
@@ -6,16 +6,18 @@ class ec2_securitygroup_not_used(Check):
     def execute(self):
         findings = []
         for security_group in ec2_client.security_groups:
-            report = Check_Report_AWS(self.metadata())
-            report.region = security_group.region
-            report.resource_id = security_group.id
-            report.resource_arn = security_group.arn
-            report.status = "PASS"
-            report.status_extended = f"Security group {security_group.name} ({security_group.id}) it is being used."
-            if len(security_group.network_interfaces) == 0:
-                report.status = "FAIL"
-                report.status_extended = f"Security group {security_group.name} ({security_group.id}) it is not being used."
+            # Default security groups can not be deleted, so ignore them
+            if security_group.name != "default":
+                report = Check_Report_AWS(self.metadata())
+                report.region = security_group.region
+                report.resource_id = security_group.id
+                report.resource_arn = security_group.arn
+                report.status = "PASS"
+                report.status_extended = f"Security group {security_group.name} ({security_group.id}) it is being used."
+                if len(security_group.network_interfaces) == 0:
+                    report.status = "FAIL"
+                    report.status_extended = f"Security group {security_group.name} ({security_group.id}) it is not being used."
 
-            findings.append(report)
+                findings.append(report)
 
         return findings
