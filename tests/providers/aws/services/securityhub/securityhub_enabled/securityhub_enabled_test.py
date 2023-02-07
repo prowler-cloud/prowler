d3f6e09f872c158bacdb8401632dbd49f9ade31c
sergargar <sergargar@users.noreply.github.com>
2023-02-07 10:55:45 +0100

    add tests

--- tests/providers/aws/services/securityhub/securityhub_enabled/securityhub_enabled_test.py
+++ tests/providers/aws/services/securityhub/securityhub_enabled/securityhub_enabled_test.py
@@ -14,6 +14,7 @@ class Test_accessanalyzer_enabled_without_findings:
                 "Security Hub",
                 "NOT_AVAILABLE",
                 "",
+                "",
                 "eu-west-1",
             )
         ]
@@ -33,7 +34,7 @@ class Test_accessanalyzer_enabled_without_findings:
             assert result[0].status_extended == "Security Hub is not enabled"
             assert result[0].resource_id == "Security Hub"
 
-    def test_securityhub_hub_active(self):
+    def test_securityhub_hub_active_with_standards(self):
         securityhub_client = mock.MagicMock
         securityhub_client.securityhubs = [
             SecurityHubHub(
@@ -41,6 +42,7 @@ class Test_accessanalyzer_enabled_without_findings:
                 "default",
                 "ACTIVE",
                 "cis-aws-foundations-benchmark/v/1.2.0",
+                "",
                 "eu-west-1",
             )
         ]
@@ -59,6 +61,68 @@ class Test_accessanalyzer_enabled_without_findings:
             assert result[0].status == "PASS"
             assert (
                 result[0].status_extended
-                == "Security Hub is enabled with standards cis-aws-foundations-benchmark/v/1.2.0"
+                == "Security Hub is enabled with standards: cis-aws-foundations-benchmark/v/1.2.0"
+            )
+            assert result[0].resource_id == "default"
+
+    def test_securityhub_hub_active_with_integrations(self):
+        securityhub_client = mock.MagicMock
+        securityhub_client.securityhubs = [
+            SecurityHubHub(
+                "arn:aws:securityhub:us-east-1:0123456789012:hub/default",
+                "default",
+                "ACTIVE",
+                "",
+                "prowler",
+                "eu-west-1",
+            )
+        ]
+        with mock.patch(
+            "prowler.providers.aws.services.securityhub.securityhub_service.SecurityHub",
+            new=securityhub_client,
+        ):
+            # Test Check
+            from prowler.providers.aws.services.securityhub.securityhub_enabled.securityhub_enabled import (
+                securityhub_enabled,
+            )
+
+            check = securityhub_enabled()
+            result = check.execute()
+
+            assert result[0].status == "PASS"
+            assert (
+                result[0].status_extended
+                == "Security Hub is enabled without standards but with integrations: prowler"
+            )
+            assert result[0].resource_id == "default"
+
+    def test_securityhub_hub_active_without_integrations_or_standards(self):
+        securityhub_client = mock.MagicMock
+        securityhub_client.securityhubs = [
+            SecurityHubHub(
+                "arn:aws:securityhub:us-east-1:0123456789012:hub/default",
+                "default",
+                "ACTIVE",
+                "",
+                "",
+                "eu-west-1",
+            )
+        ]
+        with mock.patch(
+            "prowler.providers.aws.services.securityhub.securityhub_service.SecurityHub",
+            new=securityhub_client,
+        ):
+            # Test Check
+            from prowler.providers.aws.services.securityhub.securityhub_enabled.securityhub_enabled import (
+                securityhub_enabled,
+            )
+
+            check = securityhub_enabled()
+            result = check.execute()
+
+            assert result[0].status == "FAIL"
+            assert (
+                result[0].status_extended
+                == "Security Hub is enabled but without any standard or integration"
             )
             assert result[0].resource_id == "default"
