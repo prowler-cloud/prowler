5eeb227f398fad944ed3ae452a03ddde44d73d75
sergargar <sergargar@users.noreply.github.com>
2023-02-07 10:50:07 +0100

    improve logic

--- prowler/providers/aws/services/securityhub/securityhub_service.py
+++ prowler/providers/aws/services/securityhub/securityhub_service.py
@@ -1,6 +1,8 @@
 import threading
 from dataclasses import dataclass
 
+from botocore.client import ClientError
+
 from prowler.lib.logger import logger
 from prowler.lib.scan_filters.scan_filters import is_resource_filtered
 from prowler.providers.aws.aws_provider import generate_regional_clients
@@ -32,67 +34,73 @@ class SecurityHub:
     def __describe_hub__(self, regional_client):
         logger.info("SecurityHub - Describing Hub...")
         try:
-            get_enabled_standards_paginator = regional_client.get_paginator(
-                "get_enabled_standards"
-            )
-            standards = ""
-            for page in get_enabled_standards_paginator.paginate():
-                for standard in page["StandardsSubscriptions"]:
-                    standards += f" {standard['StandardsArn'].split('/')[1]}"
-            # Security Hub is not enabled in region
-            if standards == "":
-                self.securityhubs.append(
-                    SecurityHubHub(
-                        "",
-                        "Security Hub",
-                        "NOT_AVAILABLE",
-                        "",
-                        regional_client.region,
-                    )
-                )
-            else:
-                # SecurityHub is active so get HubArn
+            # Check if SecurityHub is active
+            try:
                 hub_arn = regional_client.describe_hub()["HubArn"]
-                if not self.audit_resources or (
-                    is_resource_filtered(hub_arn, self.audit_resources)
-                ):
-                    hub_id = hub_arn.split("/")[1]
-                    self.securityhubs.append(
-                        SecurityHubHub(
-                            hub_arn,
-                            hub_id,
-                            "ACTIVE",
-                            standards,
-                            regional_client.region,
-                        )
-                    )
-                else:
+            except ClientError as e:
+                # Check if Account is subscribed to Security Hub
+                if e.response["Error"]["Code"] == "InvalidAccessException":
                     self.securityhubs.append(
                         SecurityHubHub(
                             "",
                             "Security Hub",
                             "NOT_AVAILABLE",
                             "",
+                            "",
+                            regional_client.region,
+                        )
+                    )
+            else:
+                if not self.audit_resources or (
+                    is_resource_filtered(hub_arn, self.audit_resources)
+                ):
+                    hub_id = hub_arn.split("/")[1]
+                    get_enabled_standards_paginator = regional_client.get_paginator(
+                        "get_enabled_standards"
+                    )
+                    standards = ""
+                    for page in get_enabled_standards_paginator.paginate():
+                        for standard in page["StandardsSubscriptions"]:
+                            standards += f"{standard['StandardsArn'].split('/')[1]} "
+                    list_enabled_products_for_import_paginator = (
+                        regional_client.get_paginator(
+                            "list_enabled_products_for_import"
+                        )
+                    )
+                    integrations = ""
+                    for page in list_enabled_products_for_import_paginator.paginate():
+                        for integration in page["ProductSubscriptions"]:
+                            if (
+                                "/aws/securityhub" not in integration
+                            ):  # ignore Security Hub integration with itself
+                                integrations += f"{integration.split('/')[-1]} "
+                    self.securityhubs.append(
+                        SecurityHubHub(
+                            hub_arn,
+                            hub_id,
+                            "ACTIVE",
+                            standards,
+                            integrations,
+                            regional_client.region,
+                        )
+                    )
+                else:
+                    # SecurityHub is filtered
+                    self.securityhubs.append(
+                        SecurityHubHub(
+                            "",
+                            "Security Hub",
+                            "NOT_AVAILABLE",
+                            "",
+                            "",
                             regional_client.region,
                         )
                     )
 
         except Exception as error:
-            # Check if Account is subscribed to Security Hub
-            if "InvalidAccessException" in str(error):
-                self.securityhubs.append(
-                    SecurityHubHub(
-                        "",
-                        "Security Hub",
-                        "NOT_AVAILABLE",
-                        "",
-                        regional_client.region,
-                    )
-                )
-            else:
-                logger.error(
-                    f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
-                )
+            logger.error(
+                f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+            )
 
 
 @dataclass
@@ -101,6 +109,7 @@ class SecurityHubHub:
     id: str
     status: str
     standards: str
+    integrations: str
     region: str
 
     def __init__(
@@ -109,10 +118,12 @@ class SecurityHubHub:
         id,
         status,
         standards,
+        integrations,
         region,
     ):
         self.arn = arn
         self.id = id
         self.status = status
         self.standards = standards
+        self.integrations = integrations
         self.region = region
