5ac7cde5771efb1039239f2ce2ffbee50d6121e1
Sergio Garcia <38561120+sergargar@users.noreply.github.com>
2023-02-21 15:20:33 +0100

    chore(iam_disable_N_days_credentials): improve checks logic (#1923)

--- prowler/providers/aws/services/iam/iam_disable_90_days_credentials/iam_disable_90_days_credentials.py
+++ prowler/providers/aws/services/iam/iam_disable_90_days_credentials/iam_disable_90_days_credentials.py
@@ -9,13 +9,11 @@ maximum_expiration_days = 90
 class iam_disable_90_days_credentials(Check):
     def execute(self) -> Check_Report_AWS:
         findings = []
-        response = iam_client.users
-
-        for user in response:
+        for user in iam_client.users:
             report = Check_Report_AWS(self.metadata())
-            report.region = iam_client.region
             report.resource_id = user.name
             report.resource_arn = user.arn
+            report.region = iam_client.region
             if user.password_last_used:
                 time_since_insertion = (
                     datetime.datetime.now()
@@ -25,17 +23,65 @@ class iam_disable_90_days_credentials(Check):
                 )
                 if time_since_insertion.days > maximum_expiration_days:
                     report.status = "FAIL"
-                    report.status_extended = f"User {user.name} has not logged in to the console in the past 90 days."
+                    report.status_extended = f"User {user.name} has not logged in to the console in the past {maximum_expiration_days} days."
                 else:
                     report.status = "PASS"
-                    report.status_extended = f"User {user.name} has logged in to the console in the past 90 days."
+                    report.status_extended = f"User {user.name} has logged in to the console in the past {maximum_expiration_days} days."
             else:
                 report.status = "PASS"
-
                 report.status_extended = (
                     f"User {user.name} does not have a console password or is unused."
                 )
+
             # Append report
+            findings.append(report)
+
+        for user in iam_client.credential_report:
+            report = Check_Report_AWS(self.metadata())
+            report.region = iam_client.region
+            report.resource_id = user["user"]
+            report.resource_arn = user["arn"]
+            if (
+                user["access_key_1_active"] != "true"
+                and user["access_key_2_active"] != "true"
+            ):
+                report.status = "PASS"
+                report.status_extended = (
+                    f"User {user['user']} does not have access keys."
+                )
+            else:
+                old_access_keys = False
+                if user["access_key_1_active"] == "true":
+                    if user["access_key_1_last_used_date"] != "N/A":
+                        access_key_1_last_used_date = (
+                            datetime.datetime.now()
+                            - datetime.datetime.strptime(
+                                user["access_key_1_last_used_date"],
+                                "%Y-%m-%dT%H:%M:%S+00:00",
+                            )
+                        )
+                        if access_key_1_last_used_date.days > maximum_expiration_days:
+                            old_access_keys = True
+                            report.status = "FAIL"
+                            report.status_extended = f"User {user['user']} has not used access key 1 in the last {maximum_expiration_days} days ({access_key_1_last_used_date.days} days)."
+
+                if user["access_key_2_active"] == "true":
+                    if user["access_key_2_last_used_date"] != "N/A":
+                        access_key_2_last_used_date = (
+                            datetime.datetime.now()
+                            - datetime.datetime.strptime(
+                                user["access_key_2_last_used_date"],
+                                "%Y-%m-%dT%H:%M:%S+00:00",
+                            )
+                        )
+                        if access_key_2_last_used_date.days > maximum_expiration_days:
+                            old_access_keys = True
+                            report.status = "FAIL"
+                            report.status_extended = f"User {user['user']} has not used access key 2 in the last {maximum_expiration_days} days ({access_key_2_last_used_date.days} days)."
+
+                if not old_access_keys:
+                    report.status = "PASS"
+                    report.status_extended = f"User {user['user']} does not have unused access keys for {maximum_expiration_days} days."
             findings.append(report)
 
         return findings
