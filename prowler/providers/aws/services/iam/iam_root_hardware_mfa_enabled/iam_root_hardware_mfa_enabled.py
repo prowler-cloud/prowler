e760e3302e48520174bf85e446c24cf67888b990
n4ch04 <nachor1992@gmail.com>
2023-03-01 16:40:49 +0100

    fix(iam): pydantic migration and reformat logic service

--- prowler/providers/aws/services/iam/iam_root_hardware_mfa_enabled/iam_root_hardware_mfa_enabled.py
+++ prowler/providers/aws/services/iam/iam_root_hardware_mfa_enabled/iam_root_hardware_mfa_enabled.py
@@ -7,28 +7,29 @@ class iam_root_hardware_mfa_enabled(Check):
         findings = []
         # This check is only avaible in Commercial Partition
         if iam_client.partition == "aws":
-            virtual_mfa = False
-            report = Check_Report_AWS(self.metadata())
-            report.region = iam_client.region
-            report.resource_id = "root"
-            report.resource_arn = f"arn:aws:iam::{iam_client.account}:root"
+            if iam_client.account_summary:
+                virtual_mfa = False
+                report = Check_Report_AWS(self.metadata())
+                report.region = iam_client.region
+                report.resource_id = "root"
+                report.resource_arn = f"arn:aws:iam::{iam_client.account}:root"
 
-            if iam_client.account_summary["SummaryMap"]["AccountMFAEnabled"] > 0:
-                virtual_mfas = iam_client.virtual_mfa_devices
-                for mfa in virtual_mfas:
-                    if "root" in mfa["SerialNumber"]:
-                        virtual_mfa = True
-                        report.status = "FAIL"
-                        report.status_extended = "Root account has a virtual MFA instead of a hardware MFA device enabled."
-                if not virtual_mfa:
-                    report.status = "PASS"
-                    report.status_extended = (
-                        "Root account has a hardware MFA device enabled."
-                    )
-            else:
-                report.status = "FAIL"
-                report.status_extended = "MFA is not enabled for root account."
+                if iam_client.account_summary["SummaryMap"]["AccountMFAEnabled"] > 0:
+                    virtual_mfas = iam_client.virtual_mfa_devices
+                    for mfa in virtual_mfas:
+                        if "root" in mfa["SerialNumber"]:
+                            virtual_mfa = True
+                            report.status = "FAIL"
+                            report.status_extended = "Root account has a virtual MFA instead of a hardware MFA device enabled."
+                    if not virtual_mfa:
+                        report.status = "PASS"
+                        report.status_extended = (
+                            "Root account has a hardware MFA device enabled."
+                        )
+                else:
+                    report.status = "FAIL"
+                    report.status_extended = "MFA is not enabled for root account."
 
-            findings.append(report)
+                findings.append(report)
 
         return findings
