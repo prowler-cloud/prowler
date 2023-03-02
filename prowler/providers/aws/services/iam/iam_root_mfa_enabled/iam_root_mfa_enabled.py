eed7ab979370fb3cfdca89c838946fe929f907ed
Nacho Rivera <nachor1992@gmail.com>
2023-03-02 11:16:05 +0100

    fix(iam): refactor IAM service (#2010)

--- prowler/providers/aws/services/iam/iam_root_mfa_enabled/iam_root_mfa_enabled.py
+++ prowler/providers/aws/services/iam/iam_root_mfa_enabled/iam_root_mfa_enabled.py
@@ -5,19 +5,19 @@ from prowler.providers.aws.services.iam.iam_client import iam_client
 class iam_root_mfa_enabled(Check):
     def execute(self) -> Check_Report_AWS:
         findings = []
-
-        for user in iam_client.credential_report:
-            if user["user"] == "<root_account>":
-                report = Check_Report_AWS(self.metadata())
-                report.region = iam_client.region
-                report.resource_id = user["user"]
-                report.resource_arn = user["arn"]
-                if user["mfa_active"] == "false":
-                    report.status = "FAIL"
-                    report.status_extended = "MFA is not enabled for root account."
-                else:
-                    report.status = "PASS"
-                    report.status_extended = "MFA is enabled for root account."
-                findings.append(report)
+        if iam_client.credential_report:
+            for user in iam_client.credential_report:
+                if user["user"] == "<root_account>":
+                    report = Check_Report_AWS(self.metadata())
+                    report.region = iam_client.region
+                    report.resource_id = user["user"]
+                    report.resource_arn = user["arn"]
+                    if user["mfa_active"] == "false":
+                        report.status = "FAIL"
+                        report.status_extended = "MFA is not enabled for root account."
+                    else:
+                        report.status = "PASS"
+                        report.status_extended = "MFA is enabled for root account."
+                    findings.append(report)
 
         return findings
