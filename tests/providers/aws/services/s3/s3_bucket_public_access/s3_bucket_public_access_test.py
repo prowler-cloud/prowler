7fd11b2f0eb451a5811e788442b5c4f477811223
Pepe Fagoaga <pepe@verica.io>
2023-03-27 17:00:20 +0200

    fix(s3): handle if ignore_public_acls is None (#2128)

--- tests/providers/aws/services/s3/s3_bucket_public_access/s3_bucket_public_access_test.py
+++ tests/providers/aws/services/s3/s3_bucket_public_access/s3_bucket_public_access_test.py
@@ -422,3 +422,47 @@ class Test_s3_bucket_public_access:
                         == f"arn:{audit_info.audited_partition}:s3:::{bucket_name_us}"
                     )
                     assert result[0].region == AWS_REGION
+
+    @mock_s3
+    @mock_s3control
+    def test_bucket_can_not_retrieve_public_access_block(self):
+        s3_client = client("s3", region_name=AWS_REGION)
+        bucket_name_us = "bucket_test_us"
+        s3_client.create_bucket(Bucket=bucket_name_us)
+        s3_client.put_public_access_block(
+            Bucket=bucket_name_us,
+            PublicAccessBlockConfiguration={
+                "BlockPublicAcls": True,
+                "IgnorePublicAcls": True,
+                "BlockPublicPolicy": True,
+                "RestrictPublicBuckets": True,
+            },
+        )
+        from prowler.providers.aws.services.s3.s3_service import S3, S3Control
+
+        audit_info = self.set_mocked_audit_info()
+
+        with mock.patch(
+            "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
+            new=audit_info,
+        ):
+            # To test this behaviour we need to set public_access_block to None
+            s3 = S3(audit_info)
+            s3.buckets[0].public_access_block = None
+            with mock.patch(
+                "prowler.providers.aws.services.s3.s3_bucket_public_access.s3_bucket_public_access.s3_client",
+                new=s3,
+            ):
+                with mock.patch(
+                    "prowler.providers.aws.services.s3.s3_bucket_public_access.s3_bucket_public_access.s3control_client",
+                    new=S3Control(audit_info),
+                ):
+                    # Test Check
+                    from prowler.providers.aws.services.s3.s3_bucket_public_access.s3_bucket_public_access import (
+                        s3_bucket_public_access,
+                    )
+
+                    check = s3_bucket_public_access()
+                    result = check.execute()
+
+                    assert len(result) == 0
