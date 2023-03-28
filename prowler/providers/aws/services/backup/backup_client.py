efa3430bcf6bcd0a250acfbab53c00b9b5085945
Gabriel Alejandro Soltz <thegaby@gmail.com>
2023-03-28 20:24:52 +0200

    remove_non_related_files

--- prowler/providers/aws/services/backup/backup_client.py
+++
@@ -1,4 +0,0 @@
-from prowler.providers.aws.lib.audit_info.audit_info import current_audit_info
-from prowler.providers.aws.services.backup.backup_service import Backup
-
-organizations_client = Backup(current_audit_info)
