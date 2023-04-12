b0eb8ae114990a135e4586d93d64f4e6f7a76a12
Gabriel Alejandro Soltz <thegaby@gmail.com>
2023-04-12 07:38:11 +0200

    New Check and Service: resourceexplorer2_indexes_found

---
+++ prowler/providers/aws/services/resourceexplorer2/resourceexplorer2_indexes_found/resourceexplorer2_indexes_found.py
@@ -0,0 +1,22 @@
+from prowler.lib.check.models import Check, Check_Report_AWS
+from prowler.providers.aws.services.resourceexplorer2.resourceexplorer2_client import resource_explorer_2_client
+
+
+class resourceexplorer2_indexes_found(Check):
+    def execute(self):
+        findings = []
+        report = Check_Report_AWS(self.metadata())
+        report.status = "FAIL"
+        report.status_extended = "No Resource Explorer Indexes found"
+        report.region = resource_explorer_2_client.region
+        report.resource_arn = "NoResourceExplorer"
+        if resource_explorer_2_client.indexes:
+            report.region = resource_explorer_2_client.indexes[0].region
+            report.resource_arn = resource_explorer_2_client.indexes[0].arn
+            report.status = "PASS"
+            report.status_extended = (
+                f"Resource Explorer Indexes found: {len(resource_explorer_2_client.indexes)}"
+            )
+        findings.append(report)
+
+        return findings
---
+++ prowler/providers/aws/services/resourceexplorer2/resourceexplorer2_service.py
@@ -0,0 +1,61 @@
+import threading
+from typing import Optional
+
+from pydantic import BaseModel
+
+from prowler.lib.logger import logger
+from prowler.lib.scan_filters.scan_filters import is_resource_filtered
+from prowler.providers.aws.aws_provider import generate_regional_clients
+
+
+################################ ResourceExplorer2
+class ResourceExplorer2:
+    def __init__(self, audit_info):
+        self.service = "resource-explorer-2"
+        self.session = audit_info.audit_session
+        self.audit_resources = audit_info.audit_resources
+        self.regional_clients = generate_regional_clients(self.service, audit_info)
+        self.region = audit_info.profile_region
+        self.indexes = []
+        self.__threading_call__(self.__list_indexes__)
+
+    def __get_session__(self):
+        return self.session
+
+    def __threading_call__(self, call):
+        threads = []
+        for regional_client in self.regional_clients.values():
+            threads.append(threading.Thread(target=call, args=(regional_client,)))
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+
+    def __list_indexes__(self, regional_client):
+        logger.info("ResourceExplorer - list indexes...")
+        try:
+            list_indexes_paginator = regional_client.get_paginator("list_indexes")
+            for page in list_indexes_paginator.paginate():
+                for index in page.get("Indexes"):
+                    if not self.audit_resources or (
+                        is_resource_filtered(
+                            index["Arn"], self.audit_resources
+                        )
+                    ):
+                        self.indexes.append(
+                            Indexes(
+                                arn=index["Arn"],
+                                region=index["Region"],
+                                type=index["Type"],
+                            )
+                        )
+                        print (self.indexes)
+        except Exception as error:
+            logger.error(
+                f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+            )
+
+class Indexes(BaseModel):
+    arn: str
+    region: str
+    type: str
