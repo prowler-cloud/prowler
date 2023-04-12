79a18ed86f614d107674958810415017c2b54d41
Gabriel Alejandro Soltz <thegaby@gmail.com>
2023-04-12 12:49:31 +0200

    remove_unused_func

--- tests/providers/aws/services/cloudtrail/cloudtrail_bucket_requires_mfa_delete/cloudtrail_bucket_requires_mfa_delete_test.py
+++ tests/providers/aws/services/cloudtrail/cloudtrail_bucket_requires_mfa_delete/cloudtrail_bucket_requires_mfa_delete_test.py
@@ -10,7 +10,6 @@ from prowler.providers.aws.services.cloudtrail.cloudtrail_service import Cloudtr
 from prowler.providers.aws.services.s3.s3_service import S3
 
 AWS_ACCOUNT_NUMBER = 123456789012
-AWS_ACCOUNT_NUMBER_2 = 123456789013
 
 # Mocking Backup Calls
 make_api_call = botocore.client.BaseClient._make_api_call
@@ -26,28 +25,6 @@ class Test_cloudtrail_bucket_requires_mfa_delete:
                 botocore_session=None,
             ),
             audited_account=AWS_ACCOUNT_NUMBER,
-            audited_user_id=None,
-            audited_partition="aws",
-            audited_identity_arn=None,
-            profile=None,
-            profile_region=None,
-            credentials=None,
-            assumed_role_info=None,
-            audited_regions=["us-east-1", "eu-west-1"],
-            organizations_metadata=None,
-            audit_resources=None,
-        )
-        return audit_info
-
-    def set_mocked_audit_info_cross(self):
-        audit_info = AWS_Audit_Info(
-            session_config=None,
-            original_session=None,
-            audit_session=session.Session(
-                profile_name=None,
-                botocore_session=None,
-            ),
-            audited_account=AWS_ACCOUNT_NUMBER_2,
             audited_user_id=None,
             audited_partition="aws",
             audited_identity_arn=None,
