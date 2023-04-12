ae7f4501e337c9a68ee51891e47d5b379562bf54
Gabriel Alejandro Soltz <thegaby@gmail.com>
2023-04-12 05:26:12 +0200

    New Guardduty check: guarduty_centrally_managed

--- prowler/providers/aws/services/guardduty/guardduty_service.py
+++ prowler/providers/aws/services/guardduty/guardduty_service.py
@@ -21,6 +21,8 @@ class GuardDuty:
         self.__threading_call__(self.__list_detectors__)
         self.__get_detector__(self.regional_clients)
         self.__list_findings__(self.regional_clients)
+        self.__list_members__(self.regional_clients)
+        self.__get_administrator_account__(self.regional_clients)
         self.__list_tags_for_resource__()
 
     def __get_session__(self):
@@ -64,6 +66,57 @@ class GuardDuty:
                 if "Status" in detector_info and detector_info["Status"] == "ENABLED":
                     detector.status = True
 
+        except Exception as error:
+            logger.error(
+                f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+            )
+
+    def __get_administrator_account__(self, regional_clients):
+        logger.info("GuardDuty - getting administrator account...")
+        try:
+            for detector in self.detectors:
+                try:
+                    regional_client = regional_clients[detector.region]
+                    detector_administrator = regional_client.get_administrator_account(
+                        DetectorId=detector.id
+                    )
+                    detector_administrator_account = detector_administrator.get(
+                        "Administrator"
+                    )
+                    if detector_administrator_account:
+                        detector.administrator_account = (
+                            detector_administrator_account.get("AccountId")
+                        )
+                except Exception as error:
+                    logger.error(
+                        f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+                    )
+                    continue
+
+        except Exception as error:
+            logger.error(
+                f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+            )
+
+    def __list_members__(self, regional_clients):
+        logger.info("GuardDuty - listing members...")
+        try:
+            for detector in self.detectors:
+                try:
+                    regional_client = regional_clients[detector.region]
+                    list_members_paginator = regional_client.get_paginator(
+                        "list_members"
+                    )
+                    for page in list_members_paginator.paginate(
+                        DetectorId=detector.id,
+                    ):
+                        for member in page["Members"]:
+                            detector.member_accounts.append(member.get("AccountId"))
+                except Exception as error:
+                    logger.error(
+                        f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+                    )
+                    continue
         except Exception as error:
             logger.error(
                 f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
@@ -121,4 +174,6 @@ class Detector(BaseModel):
     region: str
     status: bool = None
     findings: list = []
+    member_accounts: list = []
+    administrator_account: str = None
     tags: Optional[list] = []
