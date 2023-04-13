77eba3a063e9031da791f49cfe01cf9c9f08eac1
Pepe Fagoaga <pepe@verica.io>
2023-04-13 11:11:48 +0200

    fix(test): Use Mock.MagicMock to set the service client

--- tests/providers/aws/services/guardduty/guardduty_centrally_managed/guardduty_centrally_managed_test.py
+++ tests/providers/aws/services/guardduty/guardduty_centrally_managed/guardduty_centrally_managed_test.py
@@ -1,220 +1,133 @@
 from unittest import mock
-from unittest.mock import patch
+from uuid import uuid4
 
-import botocore
-from boto3 import client, session
-from moto import mock_guardduty
+from prowler.providers.aws.services.guardduty.guardduty_service import Detector
 
-from prowler.providers.aws.lib.audit_info.models import AWS_Audit_Info
-from prowler.providers.aws.services.guardduty.guardduty_service import GuardDuty
-
+AWS_REGION = "eu-west-1"
 AWS_ACCOUNT_NUMBER = "123456789012"
 AWS_ACCOUNT_NUMBER_ADMIN = "123456789013"
-
-
-# Mocking Backup Calls
-make_api_call = botocore.client.BaseClient._make_api_call
-
-
-def mock_make_api_call_central_managed(self, operation_name, kwarg):
-    """
-    Mock unsoportted AWS API call
-    """
-    if operation_name == "ListMembers":
-        return {
-            "Members": [],
-        }
-    if operation_name == "GetAdministratorAccount":
-        return {
-            "Administrator": {
-                "AccountId": AWS_ACCOUNT_NUMBER_ADMIN,
-                "InvitationId": "string",
-                "RelationshipStatus": "string",
-                "InvitedAt": "string",
-            }
-        }
-    return make_api_call(self, operation_name, kwarg)
-
-
-def mock_make_api_call_administrator(self, operation_name, kwarg):
-    """
-    Mock unsoportted AWS API call
-    """
-    if operation_name == "ListMembers":
-        return {
-            "Members": [
-                {
-                    "AccountId": "string",
-                    "DetectorId": "string",
-                    "MasterId": "string",
-                    "Email": "string",
-                    "RelationshipStatus": "string",
-                    "InvitedAt": "string",
-                    "UpdatedAt": "string",
-                    "AdministratorId": "string",
-                },
-            ],
-        }
-    if operation_name == "GetAdministratorAccount":
-        return {}
-    return make_api_call(self, operation_name, kwarg)
+DETECTOR_ID = str(uuid4())
+DETECTOR_ARN = (
+    f"arn:aws:guardduty:{AWS_REGION}:{AWS_ACCOUNT_NUMBER}:detector/{DETECTOR_ID}"
+)
 
 
 class Test_guardduty_centrally_managed:
-    def set_mocked_audit_info(self):
-        audit_info = AWS_Audit_Info(
-            session_config=None,
-            original_session=None,
-            audit_session=session.Session(
-                profile_name=None,
-                botocore_session=None,
-            ),
-            audited_account=AWS_ACCOUNT_NUMBER,
-            audited_user_id=None,
-            audited_partition="aws",
-            audited_identity_arn=None,
-            profile=None,
-            profile_region="us-east-1",
-            credentials=None,
-            assumed_role_info=None,
-            audited_regions=None,
-            organizations_metadata=None,
-            audit_resources=None,
-        )
-        return audit_info
-
-    @mock_guardduty
     def test_no_detectors(self):
-        current_audit_info = self.set_mocked_audit_info()
-
+        guardduty_client = mock.MagicMock
+        guardduty_client.detectors = []
         with mock.patch(
-            "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
-            new=current_audit_info,
+            "prowler.providers.aws.services.guardduty.guardduty_service.GuardDuty",
+            guardduty_client,
         ):
-            with mock.patch(
-                "prowler.providers.aws.services.guardduty.guardduty_centrally_managed.guardduty_centrally_managed.guardduty_client",
-                new=GuardDuty(current_audit_info),
-            ):
-                # Test Check
-                from prowler.providers.aws.services.guardduty.guardduty_centrally_managed.guardduty_centrally_managed import (
-                    guardduty_centrally_managed,
-                )
+            from prowler.providers.aws.services.guardduty.guardduty_no_high_severity_findings.guardduty_no_high_severity_findings import (
+                guardduty_no_high_severity_findings,
+            )
 
-                check = guardduty_centrally_managed()
-                result = check.execute()
-                assert len(result) == 0
+            check = guardduty_no_high_severity_findings()
+            result = check.execute()
+            assert len(result) == 0
 
-    @mock_guardduty
     def test_detector_no_centralized_managed(self):
-        current_audit_info = self.set_mocked_audit_info()
-
-        guardduty_client = client("guardduty", region_name="us-east-1")
-        detector_id = guardduty_client.create_detector(Enable=True)["DetectorId"]
+        guardduty_client = mock.MagicMock
+        guardduty_client.detectors = []
+        guardduty_client.detectors.append(
+            Detector(
+                id=DETECTOR_ID,
+                region=AWS_REGION,
+                arn=DETECTOR_ARN,
+                status=False,
+                findings=[str(uuid4())],
+            )
+        )
 
         with mock.patch(
-            "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
-            new=current_audit_info,
+            "prowler.providers.aws.services.guardduty.guardduty_service.GuardDuty",
+            guardduty_client,
         ):
-            with mock.patch(
-                "prowler.providers.aws.services.guardduty.guardduty_centrally_managed.guardduty_centrally_managed.guardduty_client",
-                new=GuardDuty(current_audit_info),
-            ):
-                # Test Check
-                from prowler.providers.aws.services.guardduty.guardduty_centrally_managed.guardduty_centrally_managed import (
-                    guardduty_centrally_managed,
-                )
+            # Test Check
+            from prowler.providers.aws.services.guardduty.guardduty_centrally_managed.guardduty_centrally_managed import (
+                guardduty_centrally_managed,
+            )
 
-                check = guardduty_centrally_managed()
-                result = check.execute()
-                assert len(result) == 1
-                assert result[0].status == "FAIL"
-                assert (
-                    result[0].status_extended
-                    == f"GuardDuty detector {detector_id} is not centrally managed"
-                )
-                assert result[0].resource_id == detector_id
-                assert result[0].region == "us-east-1"
-                assert (
-                    result[0].resource_arn
-                    == f"arn:aws:guardduty:us-east-1:{AWS_ACCOUNT_NUMBER}:detector/{detector_id}"
-                )
+            check = guardduty_centrally_managed()
+            result = check.execute()
+            assert len(result) == 1
+            assert result[0].status == "FAIL"
+            assert (
+                result[0].status_extended
+                == f"GuardDuty detector {DETECTOR_ID} is not centrally managed"
+            )
+            assert result[0].resource_id == DETECTOR_ID
+            assert result[0].region == AWS_REGION
+            assert result[0].resource_arn == DETECTOR_ARN
 
-    @mock_guardduty
-    # Patch with mock_make_api_call_central_managed:
-    @patch(
-        "botocore.client.BaseClient._make_api_call",
-        new=mock_make_api_call_central_managed,
-    )
     def test_detector_centralized_managed(self):
-        current_audit_info = self.set_mocked_audit_info()
-
-        guardduty_client = client("guardduty", region_name="us-east-1")
-        detector_id = guardduty_client.create_detector(Enable=True)["DetectorId"]
+        guardduty_client = mock.MagicMock
+        guardduty_client.detectors = []
+        guardduty_client.detectors.append(
+            Detector(
+                id=DETECTOR_ID,
+                region=AWS_REGION,
+                arn=DETECTOR_ARN,
+                status=False,
+                findings=[str(uuid4())],
+                administrator_account=AWS_ACCOUNT_NUMBER_ADMIN,
+            )
+        )
 
         with mock.patch(
-            "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
-            new=current_audit_info,
+            "prowler.providers.aws.services.guardduty.guardduty_service.GuardDuty",
+            guardduty_client,
         ):
-            with mock.patch(
-                "prowler.providers.aws.services.guardduty.guardduty_centrally_managed.guardduty_centrally_managed.guardduty_client",
-                new=GuardDuty(current_audit_info),
-            ):
-                # Test Check
-                from prowler.providers.aws.services.guardduty.guardduty_centrally_managed.guardduty_centrally_managed import (
-                    guardduty_centrally_managed,
-                )
+            # Test Check
+            from prowler.providers.aws.services.guardduty.guardduty_centrally_managed.guardduty_centrally_managed import (
+                guardduty_centrally_managed,
+            )
 
-                check = guardduty_centrally_managed()
-                result = check.execute()
-                assert len(result) == 1
-                assert result[0].status == "PASS"
-                assert (
-                    result[0].status_extended
-                    == f"GuardDuty detector {detector_id} is centrally managed by account {AWS_ACCOUNT_NUMBER_ADMIN}"
-                )
-                assert result[0].resource_id == detector_id
-                assert result[0].region == "us-east-1"
-                assert (
-                    result[0].resource_arn
-                    == f"arn:aws:guardduty:us-east-1:{AWS_ACCOUNT_NUMBER}:detector/{detector_id}"
-                )
+            check = guardduty_centrally_managed()
+            result = check.execute()
+            assert len(result) == 1
+            assert result[0].status == "PASS"
+            assert (
+                result[0].status_extended
+                == f"GuardDuty detector {DETECTOR_ID} is centrally managed by account {AWS_ACCOUNT_NUMBER_ADMIN}"
+            )
+            assert result[0].resource_id == DETECTOR_ID
+            assert result[0].region == AWS_REGION
+            assert result[0].resource_arn == DETECTOR_ARN
 
-    @mock_guardduty
-    # Patch with mock_make_api_call_administrator:
-    @patch(
-        "botocore.client.BaseClient._make_api_call",
-        new=mock_make_api_call_administrator,
-    )
     def test_detector_administrator(self):
-        current_audit_info = self.set_mocked_audit_info()
-
-        guardduty_client = client("guardduty", region_name="us-east-1")
-        detector_id = guardduty_client.create_detector(Enable=True)["DetectorId"]
+        guardduty_client = mock.MagicMock
+        guardduty_client.detectors = []
+        guardduty_client.detectors.append(
+            Detector(
+                id=DETECTOR_ID,
+                region=AWS_REGION,
+                arn=DETECTOR_ARN,
+                status=False,
+                findings=[str(uuid4())],
+                member_accounts=[AWS_ACCOUNT_NUMBER_ADMIN],
+            )
+        )
 
         with mock.patch(
-            "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
-            new=current_audit_info,
+            "prowler.providers.aws.services.guardduty.guardduty_service.GuardDuty",
+            guardduty_client,
         ):
-            with mock.patch(
-                "prowler.providers.aws.services.guardduty.guardduty_centrally_managed.guardduty_centrally_managed.guardduty_client",
-                new=GuardDuty(current_audit_info),
-            ):
-                # Test Check
-                from prowler.providers.aws.services.guardduty.guardduty_centrally_managed.guardduty_centrally_managed import (
-                    guardduty_centrally_managed,
-                )
+            # Test Check
+            from prowler.providers.aws.services.guardduty.guardduty_centrally_managed.guardduty_centrally_managed import (
+                guardduty_centrally_managed,
+            )
 
-                check = guardduty_centrally_managed()
-                result = check.execute()
-                assert len(result) == 1
-                assert result[0].status == "PASS"
-                assert (
-                    result[0].status_extended
-                    == f"GuardDuty detector {detector_id} is administrator account with 1 member accounts"
-                )
-                assert result[0].resource_id == detector_id
-                assert result[0].region == "us-east-1"
-                assert (
-                    result[0].resource_arn
-                    == f"arn:aws:guardduty:us-east-1:{AWS_ACCOUNT_NUMBER}:detector/{detector_id}"
-                )
+            check = guardduty_centrally_managed()
+            result = check.execute()
+            assert len(result) == 1
+            assert result[0].status == "PASS"
+            assert (
+                result[0].status_extended
+                == f"GuardDuty detector {DETECTOR_ID} is administrator account with 1 member accounts"
+            )
+            assert result[0].resource_id == DETECTOR_ID
+            assert result[0].region == AWS_REGION
+            assert result[0].resource_arn == DETECTOR_ARN
