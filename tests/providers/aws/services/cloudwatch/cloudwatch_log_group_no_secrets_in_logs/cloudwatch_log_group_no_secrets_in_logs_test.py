180e82c42e8f1eb1cb58f53ecd57fb3ba512c462
Pepe Fagoaga <pepe@verica.io>
2023-04-17 09:51:46 +0200

    fix(test): Mock audit into in CloudWatch

--- tests/providers/aws/services/cloudwatch/cloudwatch_log_group_no_secrets_in_logs/cloudwatch_log_group_no_secrets_in_logs_test.py
+++ tests/providers/aws/services/cloudwatch/cloudwatch_log_group_no_secrets_in_logs/cloudwatch_log_group_no_secrets_in_logs_test.py
@@ -1,20 +1,47 @@
 from re import search
 from unittest import mock
 
-from boto3 import client
+from boto3 import client, session
 from moto import mock_logs
 from moto.core.utils import unix_time_millis
 
+from prowler.providers.aws.lib.audit_info.models import AWS_Audit_Info
+
 AWS_REGION = "us-east-1"
+AWS_ACCOUNT_NUMBER = "123456789012"
 
 
 class Test_cloudwatch_log_group_no_secrets_in_logs:
+    def set_mocked_audit_info(self):
+        audit_info = AWS_Audit_Info(
+            session_config=None,
+            original_session=None,
+            audit_session=session.Session(
+                profile_name=None,
+                botocore_session=None,
+            ),
+            audited_account=AWS_ACCOUNT_NUMBER,
+            audited_user_id=None,
+            audited_partition="aws",
+            audited_identity_arn=None,
+            profile=None,
+            profile_region=None,
+            credentials=None,
+            assumed_role_info=None,
+            audited_regions=["us-east-1", "eu-west-1"],
+            organizations_metadata=None,
+            audit_resources=None,
+        )
+
+        return audit_info
+
     def test_cloudwatch_no_log_groups(self):
-        from prowler.providers.aws.lib.audit_info.audit_info import current_audit_info
         from prowler.providers.aws.services.cloudwatch.cloudwatch_service import Logs
+
+        current_audit_info = self.set_mocked_audit_info()
+
         from prowler.providers.common.models import Audit_Metadata
 
-        current_audit_info.audited_partition = "aws"
         current_audit_info.audit_metadata = Audit_Metadata(
             services_scanned=0,
             # We need to set this check to call __describe_log_groups__
@@ -24,6 +51,9 @@ class Test_cloudwatch_log_group_no_secrets_in_logs:
         )
 
         with mock.patch(
+            "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
+            new=current_audit_info,
+        ), mock.patch(
             "prowler.providers.aws.services.cloudwatch.cloudwatch_log_group_no_secrets_in_logs.cloudwatch_log_group_no_secrets_in_logs.logs_client",
             new=Logs(current_audit_info),
         ):
@@ -49,11 +79,12 @@ class Test_cloudwatch_log_group_no_secrets_in_logs:
             logStreamName="test stream",
             logEvents=[{"timestamp": 0, "message": "line"}],
         )
-        from prowler.providers.aws.lib.audit_info.audit_info import current_audit_info
         from prowler.providers.aws.services.cloudwatch.cloudwatch_service import Logs
+
+        current_audit_info = self.set_mocked_audit_info()
+
         from prowler.providers.common.models import Audit_Metadata
 
-        current_audit_info.audited_partition = "aws"
         current_audit_info.audit_metadata = Audit_Metadata(
             services_scanned=0,
             # We need to set this check to call __describe_log_groups__
@@ -63,6 +94,9 @@ class Test_cloudwatch_log_group_no_secrets_in_logs:
         )
 
         with mock.patch(
+            "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
+            new=current_audit_info,
+        ), mock.patch(
             "prowler.providers.aws.services.cloudwatch.cloudwatch_log_group_no_secrets_in_logs.cloudwatch_log_group_no_secrets_in_logs.logs_client",
             new=Logs(current_audit_info),
         ):
@@ -96,11 +130,12 @@ class Test_cloudwatch_log_group_no_secrets_in_logs:
                 }
             ],
         )
-        from prowler.providers.aws.lib.audit_info.audit_info import current_audit_info
         from prowler.providers.aws.services.cloudwatch.cloudwatch_service import Logs
+
+        current_audit_info = self.set_mocked_audit_info()
+
         from prowler.providers.common.models import Audit_Metadata
 
-        current_audit_info.audited_partition = "aws"
         current_audit_info.audit_metadata = Audit_Metadata(
             services_scanned=0,
             # We need to set this check to call __describe_log_groups__
@@ -110,6 +145,9 @@ class Test_cloudwatch_log_group_no_secrets_in_logs:
         )
 
         with mock.patch(
+            "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
+            new=current_audit_info,
+        ), mock.patch(
             "prowler.providers.aws.services.cloudwatch.cloudwatch_log_group_no_secrets_in_logs.cloudwatch_log_group_no_secrets_in_logs.logs_client",
             new=Logs(current_audit_info),
         ):
