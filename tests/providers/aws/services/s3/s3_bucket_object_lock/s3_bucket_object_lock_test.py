2a99c3d2bfb7cdde1b4c0a5879c78982641da601
Sergio Garcia <sergargar1@gmail.com>
2023-04-26 13:41:11 +0200

    resolve comments

--- tests/providers/aws/services/s3/s3_bucket_object_lock/s3_bucket_object_lock_test.py
+++ tests/providers/aws/services/s3/s3_bucket_object_lock/s3_bucket_object_lock_test.py
@@ -36,6 +36,30 @@ class Test_s3_bucket_object_lock:
         return audit_info
 
     @mock_s3
+    def test_no_buckets(self):
+        from prowler.providers.aws.services.s3.s3_service import S3
+
+        audit_info = self.set_mocked_audit_info()
+
+        with mock.patch(
+            "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
+            new=audit_info,
+        ):
+            with mock.patch(
+                "prowler.providers.aws.services.s3.s3_bucket_object_lock.s3_bucket_object_lock.s3_client",
+                new=S3(audit_info),
+            ):
+                # Test Check
+                from prowler.providers.aws.services.s3.s3_bucket_object_lock.s3_bucket_object_lock import (
+                    s3_bucket_object_lock,
+                )
+
+                check = s3_bucket_object_lock()
+                result = check.execute()
+
+                assert len(result) == 0
+
+    @mock_s3
     def test_bucket_no_object_lock(self):
         s3_client_us_east_1 = client("s3", region_name="us-east-1")
         bucket_name_us = "bucket_test_us"
@@ -73,6 +97,7 @@ class Test_s3_bucket_object_lock:
                     == f"arn:{audit_info.audited_partition}:s3:::{bucket_name_us}"
                 )
                 assert result[0].region == "us-east-1"
+                assert result[0].resource_tags == []
 
     @mock_s3
     def test_bucket_object_lock_enabled(self):
@@ -116,3 +141,4 @@ class Test_s3_bucket_object_lock:
                     == f"arn:{audit_info.audited_partition}:s3:::{bucket_name_us}"
                 )
                 assert result[0].region == "us-east-1"
+                assert result[0].resource_tags == []
