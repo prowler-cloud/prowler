292918f958558512782889478c35c7db6d236d6c
Pepe Fagoaga <pepe@verica.io>
2023-05-18 15:36:40 +0200

    chore(aws-crendentials): Rename argument

--- prowler/providers/aws/lib/credentials/credentials.py
+++ prowler/providers/aws/lib/credentials/credentials.py
@@ -9,12 +9,12 @@ from prowler.providers.aws.lib.audit_info.models import AWS_Audit_Info
 AWS_STS_GLOBAL_ENDPOINT_REGION = "us-east-1"
 
 
-def validate_aws_credentials(validate_session: session, input_regions: list) -> dict:
+def validate_aws_credentials(session: session, input_regions: list) -> dict:
     try:
         # For a valid STS GetCallerIdentity we have to use the right AWS Region
         if input_regions is None or len(input_regions) == 0:
-            if validate_session.region_name is not None:
-                aws_region = validate_session.region_name
+            if session.region_name is not None:
+                aws_region = session.region_name
             else:
                 # If there is no region set passed with -f/--region
                 # we use the Global STS Endpoint Region, us-east-1
@@ -22,7 +22,7 @@ def validate_aws_credentials(validate_session: session, input_regions: list) -> 
         else:
             # Get the first region passed to the -f/--region
             aws_region = input_regions[0]
-        validate_credentials_client = validate_session.client("sts", aws_region)
+        validate_credentials_client = session.client("sts", aws_region)
         caller_identity = validate_credentials_client.get_caller_identity()
         # Include the region where the caller_identity has validated the credentials
         caller_identity["region"] = aws_region
