4f320e98a3c5df84be5c2fdaa069880572bdf357
Sergio Garcia <sergargar1@gmail.com>
2023-05-24 13:21:39 +0200

    add logging service

--- prowler/providers/gcp/services/logging/logging_log_metric_filter_and_alert_for_sql_instance_configuration_changes_enabled/logging_log_metric_filter_and_alert_for_sql_instance_configuration_changes_enabled.py
+++ prowler/providers/gcp/services/logging/logging_log_metric_filter_and_alert_for_sql_instance_configuration_changes_enabled/logging_log_metric_filter_and_alert_for_sql_instance_configuration_changes_enabled.py
@@ -10,32 +10,34 @@ class logging_log_metric_filter_and_alert_for_sql_instance_configuration_changes
 ):
     def execute(self) -> Check_Report_GCP:
         findings = []
-        report = Check_Report_GCP(self.metadata())
-        report.project_id = logging_client.default_project_id
-        report.resource_id = logging_client.default_project_id
-        report.resource_name = ""
-        report.location = logging_client.region
-        report.status = "FAIL"
-        report.status_extended = "There are no log metric filters or alerts associated."
-        if logging_client.metrics:
-            for metric in logging_client.metrics:
-                if (
-                    'protoPayload.methodName="cloudsql.instances.update"'
-                    in metric.filter
-                ):
-                    report = Check_Report_GCP(self.metadata())
-                    report.project_id = metric.project_id
-                    report.resource_id = metric.name
-                    report.resource_name = metric.name
-                    report.location = logging_client.region
-                    report.status = "FAIL"
-                    report.status_extended = f"Log metric filter {metric.name} found but no alerts associated."
-                    for alert_policy in monitoring_client.alert_policies:
-                        for filter in alert_policy.filters:
-                            if metric.name in filter:
-                                report.status = "PASS"
-                                report.status_extended = f"Log metric filter {metric.name} found with alert policy {alert_policy.display_name} associated."
-                                break
-        findings.append(report)
+        projects_with_metric = set()
+        for metric in logging_client.metrics:
+            if 'protoPayload.methodName="cloudsql.instances.update"' in metric.filter:
+                report = Check_Report_GCP(self.metadata())
+                projects_with_metric.add(metric.project_id)
+                report.project_id = metric.project_id
+                report.resource_id = metric.name
+                report.resource_name = metric.name
+                report.location = logging_client.region
+                report.status = "FAIL"
+                report.status_extended = f"Log metric filter {metric.name} found but no alerts associated in project {metric.project_id}."
+                for alert_policy in monitoring_client.alert_policies:
+                    for filter in alert_policy.filters:
+                        if metric.name in filter:
+                            report.status = "PASS"
+                            report.status_extended = f"Log metric filter {metric.name} found with alert policy {alert_policy.display_name} associated in project {metric.project_id}."
+                            break
+                findings.append(report)
+
+        for project in logging_client.project_ids:
+            if project not in projects_with_metric:
+                report = Check_Report_GCP(self.metadata())
+                report.project_id = project
+                report.resource_id = project
+                report.resource_name = ""
+                report.location = logging_client.region
+                report.status = "FAIL"
+                report.status_extended = f"There are no log metric filters or alerts associated in project {project}."
+                findings.append(report)
 
         return findings
