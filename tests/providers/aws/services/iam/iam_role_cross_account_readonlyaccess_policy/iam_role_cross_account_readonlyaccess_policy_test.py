275d125f594326c84d978071cea4b86af62037ec
Pepe Fagoaga <pepe@verica.io>
2023-06-13 08:18:43 +0200

    test(iam): Fix test_only_aws_service_linked_roles

--- tests/providers/aws/services/iam/iam_role_cross_account_readonlyaccess_policy/iam_role_cross_account_readonlyaccess_policy_test.py
+++ tests/providers/aws/services/iam/iam_role_cross_account_readonlyaccess_policy/iam_role_cross_account_readonlyaccess_policy_test.py
@@ -272,7 +272,10 @@ class Test_iam_role_cross_account_readonlyaccess_policy:
         )
 
         with mock.patch(
-            "prowler.providers.aws.services.iam.iam_role_cross_account_readonlyaccess_policy.iam_role_cross_account_readonlyaccess_policy.iam_client",
+            "prowler.providers.aws.services.iam.iam_service.IAM",
+            new=iam_client,
+        ), mock.patch(
+            "prowler.providers.aws.services.iam.iam_client.iam_client",
             new=iam_client,
         ):
             # Test Check
