0e167b80fc816715f47650283b23a61a887ce51b
Sergio Garcia <sergargar1@gmail.com>
2023-07-05 10:30:22 +0200

    apikeys_key_exists

---
+++ prowler/providers/gcp/services/apikeys/apikeys_key_exists/apikeys_key_exists.py
@@ -0,0 +1,21 @@
+from prowler.lib.check.models import Check, Check_Report_GCP
+from prowler.providers.gcp.services.apikeys.apikeys_client import apikeys_client
+
+
+class apikeys_key_exists(Check):
+    def execute(self) -> Check_Report_GCP:
+        findings = []
+        for project in apikeys_client.project_ids:
+            report = Check_Report_GCP(self.metadata())
+            report.project_id = project
+            report.resource_id = project
+            report.status = "PASS"
+            report.status_extended = f"Project {project} does not have active API Keys."
+            for key in apikeys_client.keys:
+                if key.project_id == project:
+                    report.status = "FAIL"
+                    report.status_extended = f"Project {project} has active API Keys."
+                    break
+            findings.append(report)
+
+        return findings
