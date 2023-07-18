6f4659d71329db04650fb1ff2b9a55532c011a9d
Pepe Fagoaga <pepe@verica.io>
2023-07-18 08:41:11 +0200

    fix(iam): iam_policy_attached_only_to_group_or_roles

--- prowler/providers/aws/services/iam/iam_policy_attached_only_to_group_or_roles/iam_policy_attached_only_to_group_or_roles.py
+++ prowler/providers/aws/services/iam/iam_policy_attached_only_to_group_or_roles/iam_policy_attached_only_to_group_or_roles.py
@@ -7,10 +7,6 @@ class iam_policy_attached_only_to_group_or_roles(Check):
         findings = []
         if iam_client.users:
             for user in iam_client.users:
-                report = Check_Report_AWS(self.metadata())
-                report.region = iam_client.region
-                report.resource_id = user.name
-                report.resource_arn = user.arn
                 if user.attached_policies or user.inline_policies:
                     if user.attached_policies:
                         for policy in user.attached_policies:
@@ -19,6 +15,7 @@ class iam_policy_attached_only_to_group_or_roles(Check):
                             report.status = "FAIL"
                             report.status_extended = f"User {user.name} has the policy {policy['PolicyName']} attached."
                             report.resource_id = user.name
+                            report.resource_arn = user.arn
                             findings.append(report)
                     if user.inline_policies:
                         for policy in user.inline_policies:
@@ -27,12 +24,17 @@ class iam_policy_attached_only_to_group_or_roles(Check):
                             report.status = "FAIL"
                             report.status_extended = f"User {user.name} has the inline policy {policy} attached."
                             report.resource_id = user.name
+                            report.resource_arn = user.arn
                             findings.append(report)
 
                 else:
+                    report = Check_Report_AWS(self.metadata())
+                    report.region = iam_client.region
+                    report.resource_id = user.name
+                    report.resource_arn = user.arn
                     report.status = "PASS"
                     report.status_extended = (
-                        f"User {user.name} has no inline or attached policies"
+                        f"User {user.name} has no inline or attached policies."
                     )
                     findings.append(report)
         return findings
