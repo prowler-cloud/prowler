6cdcbd9513b3c672e90c9f204e690e05578e14f6
Sergio Garcia <sergargar1@gmail.com>
2023-08-01 10:38:43 +0200

    fix tests

--- tests/providers/aws/services/s3/s3_bucket_level_public_access_block/s3_bucket_level_public_access_block_test.py
+++ tests/providers/aws/services/s3/s3_bucket_level_public_access_block/s3_bucket_level_public_access_block_test.py
@@ -5,6 +5,7 @@ from boto3 import client, session
 from moto import mock_s3, mock_s3control
 
 from prowler.providers.aws.lib.audit_info.models import AWS_Audit_Info
+from prowler.providers.aws.services.s3.s3_service import S3Control
 from prowler.providers.common.models import Audit_Metadata
 
 AWS_ACCOUNT_NUMBER = "123456789012"
@@ -59,15 +60,19 @@ class Test_s3_bucket_level_public_access_block:
                 "prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block.s3_client",
                 new=S3(audit_info),
             ):
-                # Test Check
-                from prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block import (
-                    s3_bucket_level_public_access_block,
-                )
+                with mock.patch(
+                    "prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block.s3control_client",
+                    new=S3Control(audit_info),
+                ):
+                    # Test Check
+                    from prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block import (
+                        s3_bucket_level_public_access_block,
+                    )
 
-                check = s3_bucket_level_public_access_block()
-                result = check.execute()
+                    check = s3_bucket_level_public_access_block()
+                    result = check.execute()
 
-                assert len(result) == 0
+                    assert len(result) == 0
 
     @mock_s3
     @mock_s3control
@@ -106,26 +111,30 @@ class Test_s3_bucket_level_public_access_block:
                 "prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block.s3_client",
                 new=S3(audit_info),
             ):
-                # Test Check
-                from prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block import (
-                    s3_bucket_level_public_access_block,
-                )
+                with mock.patch(
+                    "prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block.s3control_client",
+                    new=S3Control(audit_info),
+                ):
+                    # Test Check
+                    from prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block import (
+                        s3_bucket_level_public_access_block,
+                    )
 
-                check = s3_bucket_level_public_access_block()
-                result = check.execute()
+                    check = s3_bucket_level_public_access_block()
+                    result = check.execute()
 
-                assert len(result) == 1
-                assert result[0].status == "FAIL"
-                assert search(
-                    "Block Public Access is not configured",
-                    result[0].status_extended,
-                )
-                assert result[0].resource_id == bucket_name_us
-                assert (
-                    result[0].resource_arn
-                    == f"arn:{audit_info.audited_partition}:s3:::{bucket_name_us}"
-                )
-                assert result[0].region == AWS_REGION
+                    assert len(result) == 1
+                    assert result[0].status == "FAIL"
+                    assert search(
+                        "Block Public Access is not configured",
+                        result[0].status_extended,
+                    )
+                    assert result[0].resource_id == bucket_name_us
+                    assert (
+                        result[0].resource_arn
+                        == f"arn:{audit_info.audited_partition}:s3:::{bucket_name_us}"
+                    )
+                    assert result[0].region == AWS_REGION
 
     @mock_s3
     @mock_s3control
@@ -164,26 +173,30 @@ class Test_s3_bucket_level_public_access_block:
                 "prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block.s3_client",
                 new=S3(audit_info),
             ):
-                # Test Check
-                from prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block import (
-                    s3_bucket_level_public_access_block,
-                )
+                with mock.patch(
+                    "prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block.s3control_client",
+                    new=S3Control(audit_info),
+                ):
+                    # Test Check
+                    from prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block import (
+                        s3_bucket_level_public_access_block,
+                    )
 
-                check = s3_bucket_level_public_access_block()
-                result = check.execute()
+                    check = s3_bucket_level_public_access_block()
+                    result = check.execute()
 
-                assert len(result) == 1
-                assert result[0].status == "PASS"
-                assert search(
-                    "Block Public Access is configured",
-                    result[0].status_extended,
-                )
-                assert result[0].resource_id == bucket_name_us
-                assert (
-                    result[0].resource_arn
-                    == f"arn:{audit_info.audited_partition}:s3:::{bucket_name_us}"
-                )
-                assert result[0].region == AWS_REGION
+                    assert len(result) == 1
+                    assert result[0].status == "PASS"
+                    assert search(
+                        "Block Public Access is configured",
+                        result[0].status_extended,
+                    )
+                    assert result[0].resource_id == bucket_name_us
+                    assert (
+                        result[0].resource_arn
+                        == f"arn:{audit_info.audited_partition}:s3:::{bucket_name_us}"
+                    )
+                    assert result[0].region == AWS_REGION
 
     @mock_s3
     @mock_s3control
@@ -222,26 +235,30 @@ class Test_s3_bucket_level_public_access_block:
                 "prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block.s3_client",
                 new=S3(audit_info),
             ):
-                # Test Check
-                from prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block import (
-                    s3_bucket_level_public_access_block,
-                )
+                with mock.patch(
+                    "prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block.s3control_client",
+                    new=S3Control(audit_info),
+                ):
+                    # Test Check
+                    from prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block import (
+                        s3_bucket_level_public_access_block,
+                    )
 
-                check = s3_bucket_level_public_access_block()
-                result = check.execute()
+                    check = s3_bucket_level_public_access_block()
+                    result = check.execute()
 
-                assert len(result) == 1
-                assert result[0].status == "PASS"
-                assert search(
-                    f"Block Public Access is configured for the S3 Bucket {bucket_name_us} at account {AWS_ACCOUNT_NUMBER} level.",
-                    result[0].status_extended,
-                )
-                assert result[0].resource_id == bucket_name_us
-                assert (
-                    result[0].resource_arn
-                    == f"arn:{audit_info.audited_partition}:s3:::{bucket_name_us}"
-                )
-                assert result[0].region == AWS_REGION
+                    assert len(result) == 1
+                    assert result[0].status == "PASS"
+                    assert search(
+                        f"Block Public Access is configured for the S3 Bucket {bucket_name_us} at account {AWS_ACCOUNT_NUMBER} level.",
+                        result[0].status_extended,
+                    )
+                    assert result[0].resource_id == bucket_name_us
+                    assert (
+                        result[0].resource_arn
+                        == f"arn:{audit_info.audited_partition}:s3:::{bucket_name_us}"
+                    )
+                    assert result[0].region == AWS_REGION
 
     @mock_s3
     @mock_s3control
@@ -284,12 +301,16 @@ class Test_s3_bucket_level_public_access_block:
                 "prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block.s3_client",
                 new=s3,
             ):
-                # Test Check
-                from prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block import (
-                    s3_bucket_level_public_access_block,
-                )
+                with mock.patch(
+                    "prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block.s3control_client",
+                    new=S3Control(audit_info),
+                ):
+                    # Test Check
+                    from prowler.providers.aws.services.s3.s3_bucket_level_public_access_block.s3_bucket_level_public_access_block import (
+                        s3_bucket_level_public_access_block,
+                    )
 
-                check = s3_bucket_level_public_access_block()
-                result = check.execute()
+                    check = s3_bucket_level_public_access_block()
+                    result = check.execute()
 
-                assert len(result) == 0
+                    assert len(result) == 0
