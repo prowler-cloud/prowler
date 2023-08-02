5eba5d725f8ea4c22b72bb47832cc73f3a4e46c9
Sergio Garcia <sergargar1@gmail.com>
2023-08-02 11:11:45 +0200

    fix test

--- tests/providers/aws/services/sns/sns_topics_not_publicly_accessible/sns_topics_not_publicly_accessible_test.py
+++ tests/providers/aws/services/sns/sns_topics_not_publicly_accessible/sns_topics_not_publicly_accessible_test.py
@@ -27,7 +27,7 @@ test_policy_restricted_condition = {
             "Principal": {"AWS": "*"},
             "Action": ["sns:Publish"],
             "Resource": f"arn:aws:sns:{AWS_REGION}:{AWS_ACCOUNT_NUMBER}:{topic_name}",
-            "Condition": {"StringEquals": {"sns:Protocol": "https"}},
+            "Condition": {"StringEquals": {"aws:SourceAccount": AWS_ACCOUNT_NUMBER}},
         }
     ]
 }
@@ -121,6 +121,7 @@ class Test_sns_topics_not_publicly_accessible:
 
     def test_topic_public_with_condition(self):
         sns_client = mock.MagicMock
+        sns_client.audited_account = AWS_ACCOUNT_NUMBER
         sns_client.topics = []
         sns_client.topics.append(
             Topic(
@@ -141,10 +142,10 @@ class Test_sns_topics_not_publicly_accessible:
             check = sns_topics_not_publicly_accessible()
             result = check.execute()
             assert len(result) == 1
-            assert result[0].status == "FAIL"
+            assert result[0].status == "PASS"
             assert (
                 result[0].status_extended
-                == f"SNS topic {topic_name} is public because its policy allows public access"
+                == f"SNS topic {topic_name} is not public because its policy only allows access from the same account"
             )
             assert result[0].resource_id == topic_name
             assert result[0].resource_arn == topic_arn
