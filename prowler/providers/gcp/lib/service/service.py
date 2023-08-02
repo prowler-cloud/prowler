5627d396e7625eea50d3d58e0eb941f8143f5b61
Sergio Garcia <sergargar1@gmail.com>
2023-08-02 13:13:15 +0200

    check if API is active

--- prowler/providers/gcp/lib/service/service.py
+++ prowler/providers/gcp/lib/service/service.py
@@ -1,3 +1,6 @@
+from googleapiclient import discovery
+
+from prowler.lib.logger import logger
 from prowler.providers.gcp.gcp_provider import generate_client
 
 
@@ -8,11 +11,32 @@ class GCPService:
         self.service = service.lower() if not service.islower() else service
 
         self.api_version = api_version
-        self.project_ids = audit_info.project_ids
         self.default_project_id = audit_info.default_project_id
 
         self.region = region
         self.client = generate_client(service, api_version, audit_info)
+        self.project_ids = self.__is_api_active__(audit_info.project_ids)
 
     def __get_client__(self):
         return self.client
+
+    def __is_api_active__(self, audited_project_ids):
+        project_ids = []
+        try:
+            for project_id in audited_project_ids:
+                client = discovery.build("serviceusage", "v1")
+                request = client.services().get(
+                    name=f"projects/{project_id}/services/{self.service}.googleapis.com"
+                )
+                response = request.execute()
+                if response.get("state") != "DISABLED":
+                    project_ids.append(project_id)
+                else:
+                    logger.error(
+                        f"{self.service} API has not been used in project {project_id} before or it is disabled. Enable it by visiting https://console.developers.google.com/apis/api/dataproc.googleapis.com/overview?project={project_id} then retry."
+                    )
+        except Exception as error:
+            logger.error(
+                f"{error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+            )
+        return project_ids
