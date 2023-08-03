514500c71c930385fe4b79ac0e23bb964c1d3c6a
Pepe Fagoaga <pepe@verica.io>
2023-08-03 09:16:58 +0200

    fix(vpc_endpoint_connections_trust_boundaries): Handle AWS Account ID as Principal (#2611)

--- prowler/providers/aws/lib/policy_condition_parser/policy_condition_parser.py
+++ prowler/providers/aws/lib/policy_condition_parser/policy_condition_parser.py
@@ -1,39 +1,45 @@
+# lista de cuentas y te devuelva las válidas
 def is_account_only_allowed_in_condition(
     condition_statement: dict, source_account: str
 ):
     is_condition_valid = False
     valid_condition_options = {
-        "StringEquals": "aws:SourceAccount",
-        "ArnLike": "aws:SourceArn",
-        "ArnEquals": "aws:SourceArn",
+        "StringEquals": [
+            "aws:SourceAccount",
+            "s3:ResourceAccount",
+            "aws:PrincipalAccount",
+        ],
+        "StringLike": ["aws:SourceArn", "aws:PrincipalArn"],
+        "ArnLike": ["aws:SourceArn", "aws:PrincipalArn"],
+        "ArnEquals": ["aws:SourceArn", "aws:PrincipalArn"],
     }
+
     for condition_operator, condition_operator_key in valid_condition_options.items():
         if condition_operator in condition_statement:
-            if condition_operator_key in condition_statement[condition_operator]:
-                # values are a list
-                if isinstance(
-                    condition_statement[condition_operator][condition_operator_key],
-                    list,
-                ):
-                    # if there is an arn/account without the source account -> we do not consider it safe
-                    # here by default we assume is true and look for false entries
-                    is_condition_valid = True
-                    for item in condition_statement[condition_operator][
-                        condition_operator_key
-                    ]:
-                        if source_account not in item:
-                            is_condition_valid = False
-                            break
-                # value is a string
-                elif isinstance(
-                    condition_statement[condition_operator][condition_operator_key], str
-                ):
-                    if (
-                        source_account
-                        in condition_statement[condition_operator][
-                            condition_operator_key
-                        ]
+            for value in condition_operator_key:
+                if value in condition_statement[condition_operator]:
+                    # values are a list
+                    if isinstance(
+                        condition_statement[condition_operator][value],
+                        list,
                     ):
+                        # if there is an arn/account without the source account -> we do not consider it safe
+                        # here by default we assume is true and look for false entries
                         is_condition_valid = True
+                        for item in condition_statement[condition_operator][value]:
+                            if source_account not in item:
+                                is_condition_valid = False
+                                break
+
+                    # value is a string
+                    elif isinstance(
+                        condition_statement[condition_operator][value],
+                        str,
+                    ):
+                        if (
+                            source_account
+                            in condition_statement[condition_operator][value]
+                        ):
+                            is_condition_valid = True
 
     return is_condition_valid
