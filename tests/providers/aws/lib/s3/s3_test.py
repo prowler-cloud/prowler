73cbd7f9eb0f0cb240aeea86ed4df150da5f8131
Pepe Fagoaga <pepe@verica.io>
2023-08-08 17:03:31 +0200

    fix: s3 test

--- tests/providers/aws/lib/s3/s3_test.py
+++ tests/providers/aws/lib/s3/s3_test.py
@@ -12,17 +12,14 @@ AWS_ACCOUNT_ID = "123456789012"
 
 
 class TestS3:
-    @mock_s3
-    def test_send_to_s3_bucket(self):
-        # Create mock session
-        session = boto3.session.Session(
-            region_name="us-east-1",
-        )
-        # Create mock audit_info
-        input_audit_info = AWS_Audit_Info(
+    def set_mocked_audit_info(self):
+        audit_info = AWS_Audit_Info(
             session_config=None,
             original_session=None,
-            audit_session=session,
+            audit_session=boto3.session.Session(
+                profile_name=None,
+                botocore_session=None,
+            ),
             audited_account=AWS_ACCOUNT_ID,
             audited_account_arn=f"arn:aws:iam::{AWS_ACCOUNT_ID}:root",
             audited_identity_arn="test-arn",
@@ -43,23 +40,34 @@ class TestS3:
                 audit_progress=0,
             ),
         )
+
+        return audit_info
+
+    @mock_s3
+    def test_send_to_s3_bucket(self):
+        # Create mock audit_info
+        audit_info = self.set_mocked_audit_info()
+
         # Create mock bucket
         bucket_name = "test_bucket"
         client = boto3.client("s3")
         client.create_bucket(Bucket=bucket_name)
+
         # Create mock csv output file
         fixtures_dir = "tests/lib/outputs/fixtures"
         output_directory = getcwd() + "/" + fixtures_dir
         output_mode = "csv"
-        filename = f"prowler-output-{input_audit_info.audited_account}"
+        filename = f"prowler-output-{audit_info.audited_account}"
+
         # Send mock csv file to mock S3 Bucket
         send_to_s3_bucket(
             filename,
             output_directory,
             output_mode,
             bucket_name,
-            input_audit_info.audit_session,
+            audit_info.audit_session,
         )
+
         # Check if the file has been sent by checking its content type
         assert (
             client.get_object(
@@ -71,51 +79,27 @@ class TestS3:
 
     @mock_s3
     def test_send_to_s3_bucket_compliance(self):
-        # Create mock session
-        session = boto3.session.Session(
-            region_name="us-east-1",
-        )
         # Create mock audit_info
-        input_audit_info = AWS_Audit_Info(
-            session_config=None,
-            original_session=None,
-            audit_session=session,
-            audited_account=AWS_ACCOUNT_ID,
-            audited_account_arn=f"arn:aws:iam::{AWS_ACCOUNT_ID}:root",
-            audited_identity_arn="test-arn",
-            audited_user_id="test",
-            audited_partition="aws",
-            profile="default",
-            profile_region="eu-west-1",
-            credentials=None,
-            assumed_role_info=None,
-            audited_regions=["eu-west-2", "eu-west-1"],
-            organizations_metadata=None,
-            audit_resources=None,
-            mfa_enabled=False,
-            audit_metadata=Audit_Metadata(
-                services_scanned=0,
-                expected_checks=[],
-                completed_checks=0,
-                audit_progress=0,
-            ),
-        )
+        audit_info = self.set_mocked_audit_info()
+
         # Create mock bucket
         bucket_name = "test_bucket"
         client = boto3.client("s3")
         client.create_bucket(Bucket=bucket_name)
+
         # Create mock csv output file
         fixtures_dir = "tests/lib/outputs/fixtures"
         output_directory = getcwd() + "/" + fixtures_dir
         output_mode = "cis_1.4_aws"
-        filename = f"prowler-output-{input_audit_info.audited_account}"
+        filename = f"prowler-output-{audit_info.audited_account}"
+
         # Send mock csv file to mock S3 Bucket
         send_to_s3_bucket(
             filename,
             output_directory,
             output_mode,
             bucket_name,
-            input_audit_info.audit_session,
+            audit_info.audit_session,
         )
         # Check if the file has been sent by checking its content type
         assert (
@@ -135,51 +119,27 @@ class TestS3:
 
     @mock_s3
     def test_send_to_s3_bucket_custom_directory(self):
-        # Create mock session
-        session = boto3.session.Session(
-            region_name="us-east-1",
-        )
         # Create mock audit_info
-        input_audit_info = AWS_Audit_Info(
-            session_config=None,
-            original_session=None,
-            audit_session=session,
-            audited_account=AWS_ACCOUNT_ID,
-            audited_account_arn=f"arn:aws:iam::{AWS_ACCOUNT_ID}:root",
-            audited_identity_arn="test-arn",
-            audited_user_id="test",
-            audited_partition="aws",
-            profile="default",
-            profile_region="eu-west-1",
-            credentials=None,
-            assumed_role_info=None,
-            audited_regions=["eu-west-2", "eu-west-1"],
-            organizations_metadata=None,
-            audit_resources=None,
-            mfa_enabled=False,
-            audit_metadata=Audit_Metadata(
-                services_scanned=0,
-                expected_checks=[],
-                completed_checks=0,
-                audit_progress=0,
-            ),
-        )
+        audit_info = self.set_mocked_audit_info()
+
         # Create mock bucket
         bucket_name = "test_bucket"
         client = boto3.client("s3")
         client.create_bucket(Bucket=bucket_name)
+
         # Create mock csv output file
         fixtures_dir = "fixtures"
         output_directory = f"tests/lib/outputs/{fixtures_dir}"
         output_mode = "csv"
-        filename = f"prowler-output-{input_audit_info.audited_account}"
+        filename = f"prowler-output-{audit_info.audited_account}"
+
         # Send mock csv file to mock S3 Bucket
         send_to_s3_bucket(
             filename,
             output_directory,
             output_mode,
             bucket_name,
-            input_audit_info.audit_session,
+            audit_info.audit_session,
         )
         # Check if the file has been sent by checking its content type
         assert (
