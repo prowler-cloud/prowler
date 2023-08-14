76082ad43655d3ee1f85ecc8126e030d97ea79fb
Sergio Garcia <38561120+sergargar@users.noreply.github.com>
2023-08-14 09:18:23 +0200

    fix (test): Context name on conditions are case-insensitive

--- prowler/providers/aws/lib/policy_condition_parser/policy_condition_parser.py
+++ prowler/providers/aws/lib/policy_condition_parser/policy_condition_parser.py
@@ -5,49 +5,55 @@ def is_account_only_allowed_in_condition(
     is_condition_valid = False
     valid_condition_options = {
         "StringEquals": [
-            "aws:SourceAccount",
-            "aws:SourceOwner",
-            "s3:ResourceAccount",
-            "aws:PrincipalAccount",
-            "aws:ResourceAccount",
+            "aws:sourceaccount",
+            "aws:sourceowner",
+            "s3:resourceaccount",
+            "aws:principalaccount",
+            "aws:resourceaccount",
         ],
         "StringLike": [
-            "aws:SourceAccount",
-            "aws:SourceOwner",
-            "aws:SourceArn",
-            "aws:PrincipalArn",
-            "aws:ResourceAccount",
-            "aws:PrincipalAccount",
+            "aws:sourceaccount",
+            "aws:sourceowner",
+            "aws:sourcearn",
+            "aws:principalarn",
+            "aws:resourceaccount",
+            "aws:principalaccount",
         ],
-        "ArnLike": ["aws:SourceArn", "aws:PrincipalArn"],
-        "ArnEquals": ["aws:SourceArn", "aws:PrincipalArn"],
+        "ArnLike": ["aws:sourcearn", "aws:principalarn"],
+        "ArnEquals": ["aws:sourcearn", "aws:principalarn"],
     }
 
     for condition_operator, condition_operator_key in valid_condition_options.items():
         if condition_operator in condition_statement:
             for value in condition_operator_key:
-                if value in condition_statement[condition_operator]:
+                condition_statement[condition_operator] = {
+                    k.lower(): v
+                    for k, v in condition_statement[condition_operator].items()
+                }
+                if value.lower() in condition_statement[condition_operator]:
                     # values are a list
                     if isinstance(
-                        condition_statement[condition_operator][value],
+                        condition_statement[condition_operator][value.lower()],
                         list,
                     ):
                         # if there is an arn/account without the source account -> we do not consider it safe
                         # here by default we assume is true and look for false entries
                         is_condition_valid = True
-                        for item in condition_statement[condition_operator][value]:
+                        for item in condition_statement[condition_operator][
+                            value.lower()
+                        ]:
                             if source_account not in item:
                                 is_condition_valid = False
                                 break
 
                     # value is a string
                     elif isinstance(
-                        condition_statement[condition_operator][value],
+                        condition_statement[condition_operator][value.lower()],
                         str,
                     ):
                         if (
                             source_account
-                            in condition_statement[condition_operator][value]
+                            in condition_statement[condition_operator][value.lower()]
                         ):
                             is_condition_valid = True
 
