f08906425a1f4ff00a2f4964d0d63dcbd8236a32
vysakh-devopspace <135362393+vysakh-devopspace@users.noreply.github.com>
2023-08-16 11:04:03 +0400

    Update ec2_detailed_monitoring_enabled.py
    Removed whitespaces

--- prowler/providers/aws/services/ec2/ec2_detailed_monitoring_enabled/ec2_detailed_monitoring_enabled.py
+++ prowler/providers/aws/services/ec2/ec2_detailed_monitoring_enabled/ec2_detailed_monitoring_enabled.py
@@ -6,18 +6,17 @@ class ec2_detailed_monitoring_enabled(Check):
     def execute(self):
         findings = []
         for instance in ec2_client.instances:
-                report = Check_Report_AWS(self.metadata())
-                report.region = instance.region
-                report.resource_id = instance.id
-                report.resource_arn = instance.arn
-                report.resource_tags = instance.tags 
-                report.status = "PASS"
-                report.status_extended = f"EC2 Instance {instance.id} has detailed monitoring enabled."
-                
-                if instance.monitoring_state != "enabled":
-                    report.status = "FAIL"
-                    report.status_extended = f"EC2 Instance {instance.id} does not have detailed monitoring enabled."
+            report = Check_Report_AWS(self.metadata())
+            report.region = instance.region
+            report.resource_id = instance.id
+            report.resource_arn = instance.arn
+            report.resource_tags = instance.tags
+            report.status = "PASS"
+            report.status_extended = f"EC2 Instance {instance.id} has detailed monitoring enabled."
+            if instance.monitoring_state != "enabled":
+               report.status = "FAIL"
+               report.status_extended = f"EC2 Instance {instance.id} does not have detailed monitoring enabled."
 
-                findings.append(report)
+            findings.append(report)
 
         return findings
