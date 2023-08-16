9de8a5120a1844cb64e7a99188ccba154244262d
Pepe Fagoaga <pepe@verica.io>
2023-08-16 09:42:42 +0200

    test(ec2_instance_detailed_monitoring_enabled)

--- prowler/providers/aws/services/ec2/ec2_detailed_monitoring_enabled/ec2_detailed_monitoring_enabled.py
+++ prowler/providers/aws/services/ec2/ec2_detailed_monitoring_enabled/ec2_detailed_monitoring_enabled.py
@@ -2,7 +2,7 @@ from prowler.lib.check.models import Check, Check_Report_AWS
 from prowler.providers.aws.services.ec2.ec2_client import ec2_client
 
 
-class ec2_detailed_monitoring_enabled(Check):
+class ec2_instance_detailed_monitoring_enabled(Check):
     def execute(self):
         findings = []
         for instance in ec2_client.instances:
