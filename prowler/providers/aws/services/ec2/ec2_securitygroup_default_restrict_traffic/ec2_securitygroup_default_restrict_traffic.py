276f6f9fb1746243d4c52d78aa28d5f3f83e9d6b
Nacho Rivera <nachor1992@gmail.com>
2023-08-25 12:42:26 +0200

    fix(ec2_securitygroup_default_restrict_traffic): fix check only allow empty rules (#2777)

--- prowler/providers/aws/services/ec2/ec2_securitygroup_default_restrict_traffic/ec2_securitygroup_default_restrict_traffic.py
+++ prowler/providers/aws/services/ec2/ec2_securitygroup_default_restrict_traffic/ec2_securitygroup_default_restrict_traffic.py
@@ -1,6 +1,5 @@
 from prowler.lib.check.models import Check, Check_Report_AWS
 from prowler.providers.aws.services.ec2.ec2_client import ec2_client
-from prowler.providers.aws.services.ec2.lib.security_groups import check_security_group
 
 
 class ec2_securitygroup_default_restrict_traffic(Check):
@@ -15,13 +14,14 @@ class ec2_securitygroup_default_restrict_traffic(Check):
             report.resource_tags = security_group.tags
             # Find default security group
             if security_group.name == "default":
-                report.status = "PASS"
-                report.status_extended = f"Default Security Group ({security_group.id}) is not open to the Internet."
-                for ingress_rule in security_group.ingress_rules:
-                    if check_security_group(ingress_rule, "-1", any_address=True):
-                        report.status = "FAIL"
-                        report.status_extended = f"Default Security Group ({security_group.id}) is open to the Internet."
-                        break
+                report.status = "FAIL"
+                report.status_extended = (
+                    f"Default Security Group ({security_group.id}) rules allow traffic."
+                )
+                if not security_group.ingress_rules and not security_group.egress_rules:
+                    report.status = "PASS"
+                    report.status_extended = f"Default Security Group ({security_group.id}) rules do not allow traffic."
+
                 findings.append(report)
 
         return findings
