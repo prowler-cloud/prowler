70fbf1676a4c9c6082754b77671de2c9b7c1490c
Sergio Garcia <38561120+sergargar@users.noreply.github.com>
2023-09-22 12:59:10 +0200

    fix(iam_inline_policy_no_administrative_privileges): set resource id as the entity name (#2820)

--- prowler/providers/aws/services/iam/iam_service.py
+++ prowler/providers/aws/services/iam/iam_service.py
@@ -398,6 +398,7 @@ class IAM(AWSService):
                                 Policy(
                                     name=policy,
                                     arn=user.arn,
+                                    entity=user.name,
                                     type="Inline",
                                     attached=True,
                                     version_id="v1",
@@ -438,6 +439,7 @@ class IAM(AWSService):
                                 Policy(
                                     name=policy,
                                     arn=group.arn,
+                                    entity=group.name,
                                     type="Inline",
                                     attached=True,
                                     version_id="v1",
@@ -478,6 +480,7 @@ class IAM(AWSService):
                                 Policy(
                                     name=policy,
                                     arn=role.arn,
+                                    entity=role.name,
                                     type="Inline",
                                     attached=True,
                                     version_id="v1",
@@ -536,6 +539,7 @@ class IAM(AWSService):
                             Policy(
                                 name=policy["PolicyName"],
                                 arn=policy["Arn"],
+                                entity=policy["PolicyId"],
                                 version_id=policy["DefaultVersionId"],
                                 type="Custom" if scope == "Local" else "AWS",
                                 attached=True
@@ -714,6 +718,7 @@ class Certificate(BaseModel):
 class Policy(BaseModel):
     name: str
     arn: str
+    entity: str
     version_id: str
     type: str
     attached: bool
