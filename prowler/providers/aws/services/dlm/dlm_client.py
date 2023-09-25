d55b221a7d3c4c59e8630b59228a283b4f15f280
Jit <133079410+jit-contrib@users.noreply.github.com>
2023-09-25 16:34:12 +0100

    Update prowler/providers/aws/services/dlm/dlm_client.py
    Co-authored-by: Pepe Fagoaga <pepe@verica.io>

--- prowler/providers/aws/services/dlm/dlm_client.py
+++ prowler/providers/aws/services/dlm/dlm_client.py
@@ -1,4 +1,4 @@
 from prowler.providers.aws.lib.audit_info.audit_info import current_audit_info
 from prowler.providers.aws.services.dlm.dlm_service import Dlm
 
-dlm_client = Dlm(current_audit_info)
+dlm_client = DLM(current_audit_info)
