4d6d58ef91d1d3f9f5c9fcd3fcdebabd7b3f77e5
Pepe Fagoaga <pepe@verica.io>
2023-09-28 17:13:17 +0200

    fix(autoscaling_find_secrets_ec2_launch_configuration): Fix UnicodeDecodeError (#2870)

--- tests/providers/aws/services/ec2/ec2_instance_secrets_user_data/ec2_instance_secrets_user_data_test.py
+++ tests/providers/aws/services/ec2/ec2_instance_secrets_user_data/ec2_instance_secrets_user_data_test.py
@@ -1,3 +1,5 @@
+from os import path
+from pathlib import Path
 from unittest import mock
 
 from boto3 import resource, session
@@ -9,6 +11,9 @@ from prowler.providers.common.models import Audit_Metadata
 AWS_REGION = "us-east-1"
 EXAMPLE_AMI_ID = "ami-12c6146b"
 AWS_ACCOUNT_NUMBER = "123456789012"
+
+ACTUAL_DIRECTORY = Path(path.dirname(path.realpath(__file__)))
+FIXTURES_DIR_NAME = "fixtures"
 
 
 class Test_ec2_instance_secrets_user_data:
@@ -154,7 +159,7 @@ class Test_ec2_instance_secrets_user_data:
     def test_one_ec2_file_with_secrets(self):
         # Include launch_configurations to check
         f = open(
-            "prowler/providers/aws/services/ec2/ec2_instance_secrets_user_data/fixtures/fixture",
+            f"{ACTUAL_DIRECTORY}/{FIXTURES_DIR_NAME}/fixture",
             "r",
         )
         secrets = f.read()
@@ -225,6 +230,51 @@ class Test_ec2_instance_secrets_user_data:
             assert (
                 result[0].status_extended
                 == f"No secrets found in EC2 instance {instance.id} since User Data is empty."
+            )
+            assert result[0].resource_id == instance.id
+            assert (
+                result[0].resource_arn
+                == f"arn:{current_audit_info.audited_partition}:ec2:{AWS_REGION}:{current_audit_info.audited_account}:instance/{instance.id}"
+            )
+            assert result[0].resource_tags is None
+            assert result[0].region == AWS_REGION
+
+    @mock_ec2
+    def test_one_ec2_file_with_secrets_gzip(self):
+        # Include launch_configurations to check
+        f = open(
+            f"{ACTUAL_DIRECTORY}/{FIXTURES_DIR_NAME}/fixture.gz",
+            "rb",
+        )
+        secrets = f.read()
+        ec2 = resource("ec2", region_name=AWS_REGION)
+        instance = ec2.create_instances(
+            ImageId=EXAMPLE_AMI_ID, MinCount=1, MaxCount=1, UserData=secrets
+        )[0]
+
+        from prowler.providers.aws.services.ec2.ec2_service import EC2
+
+        current_audit_info = self.set_mocked_audit_info()
+
+        with mock.patch(
+            "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
+            new=current_audit_info,
+        ), mock.patch(
+            "prowler.providers.aws.services.ec2.ec2_instance_secrets_user_data.ec2_instance_secrets_user_data.ec2_client",
+            new=EC2(current_audit_info),
+        ):
+            from prowler.providers.aws.services.ec2.ec2_instance_secrets_user_data.ec2_instance_secrets_user_data import (
+                ec2_instance_secrets_user_data,
+            )
+
+            check = ec2_instance_secrets_user_data()
+            result = check.execute()
+
+            assert len(result) == 1
+            assert result[0].status == "FAIL"
+            assert (
+                result[0].status_extended
+                == f"Potential secret found in EC2 instance {instance.id} User Data -> Secret Keyword on line 1, Hex High Entropy String on line 3, Secret Keyword on line 3, Secret Keyword on line 4."
             )
             assert result[0].resource_id == instance.id
             assert (
