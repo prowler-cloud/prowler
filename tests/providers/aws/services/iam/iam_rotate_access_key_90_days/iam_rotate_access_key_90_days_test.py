c36fa4bf39effbe0aa8fe4a52932c5f7a462864b
Nacho Rivera <nachor1992@gmail.com>
2023-10-02 16:27:44 +0200

    fix(iam creds checks): add missing tests and fix current ones (#2888)

--- tests/providers/aws/services/iam/iam_rotate_access_key_90_days/iam_rotate_access_key_90_days_test.py
+++ tests/providers/aws/services/iam/iam_rotate_access_key_90_days/iam_rotate_access_key_90_days_test.py
@@ -8,6 +8,7 @@ from prowler.providers.aws.lib.audit_info.models import AWS_Audit_Info
 from prowler.providers.common.models import Audit_Metadata
 
 AWS_ACCOUNT_NUMBER = "123456789012"
+AWS_REGION = "us-east-1"
 
 
 class Test_iam_rotate_access_key_90_days_test:
@@ -25,7 +26,7 @@ class Test_iam_rotate_access_key_90_days_test:
             audited_partition="aws",
             audited_identity_arn=None,
             profile=None,
-            profile_region=None,
+            profile_region=AWS_REGION,
             credentials=None,
             assumed_role_info=None,
             audited_regions=["us-east-1", "eu-west-1"],
@@ -68,12 +69,14 @@ class Test_iam_rotate_access_key_90_days_test:
 
             check = iam_rotate_access_key_90_days()
             result = check.execute()
+            assert len(result) == 1
             assert result[0].status == "PASS"
             assert (
                 result[0].status_extended == f"User {user} does not have access keys."
             )
             assert result[0].resource_id == user
             assert result[0].resource_arn == arn
+            assert result[0].region == AWS_REGION
 
     @mock_iam
     def test_user_access_key_1_not_rotated(self):
@@ -106,6 +109,7 @@ class Test_iam_rotate_access_key_90_days_test:
 
             check = iam_rotate_access_key_90_days()
             result = check.execute()
+            assert len(result) == 1
             assert result[0].status == "FAIL"
             assert (
                 result[0].status_extended
@@ -113,6 +117,7 @@ class Test_iam_rotate_access_key_90_days_test:
             )
             assert result[0].resource_id == user
             assert result[0].resource_arn == arn
+            assert result[0].region == AWS_REGION
 
     @mock_iam
     def test_user_access_key_2_not_rotated(self):
@@ -145,6 +150,7 @@ class Test_iam_rotate_access_key_90_days_test:
 
             check = iam_rotate_access_key_90_days()
             result = check.execute()
+            assert len(result) == 1
             assert result[0].status == "FAIL"
             assert (
                 result[0].status_extended
@@ -152,6 +158,7 @@ class Test_iam_rotate_access_key_90_days_test:
             )
             assert result[0].resource_id == user
             assert result[0].resource_arn == arn
+            assert result[0].region == AWS_REGION
 
     @mock_iam
     def test_user_both_access_keys_not_rotated(self):
@@ -189,6 +196,7 @@ class Test_iam_rotate_access_key_90_days_test:
 
             check = iam_rotate_access_key_90_days()
             result = check.execute()
+            assert len(result) == 2
             assert result[0].status == "FAIL"
             assert (
                 result[0].status_extended
@@ -196,7 +204,7 @@ class Test_iam_rotate_access_key_90_days_test:
             )
             assert result[0].resource_id == user
             assert result[0].resource_arn == arn
-
+            assert result[0].region == AWS_REGION
             assert result[1].status == "FAIL"
             assert (
                 result[1].status_extended
@@ -204,3 +212,51 @@ class Test_iam_rotate_access_key_90_days_test:
             )
             assert result[1].resource_id == user
             assert result[1].resource_arn == arn
+            assert result[1].region == AWS_REGION
+
+    @mock_iam
+    def test_user_both_access_keys_rotated(self):
+        credentials_last_rotated = (
+            datetime.datetime.now() - datetime.timedelta(days=10)
+        ).strftime("%Y-%m-%dT%H:%M:%S+00:00")
+        iam_client = client("iam")
+        user = "test-user"
+        arn = iam_client.create_user(UserName=user)["User"]["Arn"]
+
+        from prowler.providers.aws.services.iam.iam_service import IAM
+
+        audit_info = self.set_mocked_audit_info()
+
+        with mock.patch(
+            "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
+            new=audit_info,
+        ):
+            with mock.patch(
+                "prowler.providers.aws.services.iam.iam_rotate_access_key_90_days.iam_rotate_access_key_90_days.iam_client",
+                new=IAM(audit_info),
+            ) as service_client:
+                from prowler.providers.aws.services.iam.iam_rotate_access_key_90_days.iam_rotate_access_key_90_days import (
+                    iam_rotate_access_key_90_days,
+                )
+
+                service_client.credential_report[0]["access_key_1_active"] = "true"
+                service_client.credential_report[0][
+                    "access_key_1_last_rotated"
+                ] = credentials_last_rotated
+
+                service_client.credential_report[0]["access_key_2_active"] = "true"
+                service_client.credential_report[0][
+                    "access_key_2_last_rotated"
+                ] = credentials_last_rotated
+
+                check = iam_rotate_access_key_90_days()
+                result = check.execute()
+                assert len(result) == 1
+                assert result[0].status == "PASS"
+                assert (
+                    result[0].status_extended
+                    == f"User {user} does not have access keys older than 90 days."
+                )
+                assert result[0].resource_id == user
+                assert result[0].resource_arn == arn
+                assert result[0].region == AWS_REGION
