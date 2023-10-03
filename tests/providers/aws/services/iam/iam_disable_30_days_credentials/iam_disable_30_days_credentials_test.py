1df5e96c72718829f755858aa588baee341b273c
Sergio Garcia <38561120+sergargar@users.noreply.github.com>
2023-10-03 10:40:57 +0200

    Apply suggestions from code review

--- tests/providers/aws/services/iam/iam_disable_30_days_credentials/iam_disable_30_days_credentials_test.py
+++ tests/providers/aws/services/iam/iam_disable_30_days_credentials/iam_disable_30_days_credentials_test.py
@@ -275,9 +275,9 @@ class Test_iam_disable_30_days_credentials_test:
                     result[1].status_extended
                     == f"User {user} has not used access key 1 in the last 30 days (100 days)."
                 )
-                assert result[-1].resource_id == user + "/AccessKey1"
-                assert result[-1].resource_arn == arn
-                assert result[-1].region == AWS_REGION
+                assert result[1].resource_id == user + "/AccessKey1"
+                assert result[1].resource_arn == arn
+                assert result[1].region == AWS_REGION
 
     @mock_iam
     def test_user_access_key_2_not_used(self):
@@ -325,9 +325,9 @@ class Test_iam_disable_30_days_credentials_test:
                     result[1].status_extended
                     == f"User {user} has not used access key 2 in the last 30 days (100 days)."
                 )
-                assert result[-1].resource_id == user + "/AccessKey2"
-                assert result[-1].resource_arn == arn
-                assert result[-1].region == AWS_REGION
+                assert result[1].resource_id == user + "/AccessKey2"
+                assert result[1].resource_arn == arn
+                assert result[1].region == AWS_REGION
 
     @mock_iam
     def test_user_both_access_keys_not_used(self):
@@ -380,18 +380,18 @@ class Test_iam_disable_30_days_credentials_test:
                     result[1].status_extended
                     == f"User {user} has not used access key 1 in the last 30 days (100 days)."
                 )
-                assert result[-1].resource_id == user + "/AccessKey2"
-                assert result[-1].resource_arn == arn
-                assert result[-1].region == AWS_REGION
+                assert result[1].resource_id == user + "/AccessKey1"
+                assert result[1].resource_arn == arn
+                assert result[1].region == AWS_REGION
 
                 assert result[2].status == "FAIL"
                 assert (
                     result[2].status_extended
                     == f"User {user} has not used access key 2 in the last 30 days (100 days)."
                 )
-                assert result[-2].resource_id == user + "/AccessKey1"
-                assert result[-2].resource_arn == arn
-                assert result[-2].region == AWS_REGION
+                assert result[2].resource_id == user + "/AccessKey2"
+                assert result[2].resource_arn == arn
+                assert result[2].region == AWS_REGION
 
     @mock_iam
     def test_user_both_access_keys_used(self):
