7230d29ebc46749d5c49da3ead3cb1e5e8824210
Sergio Garcia <sergargar1@gmail.com>
2023-10-04 11:46:38 +0200

    add resource arn to elb

--- tests/providers/aws/services/elb/elb_logging_enabled/elb_logging_enabled_test.py
+++ tests/providers/aws/services/elb/elb_logging_enabled/elb_logging_enabled_test.py
@@ -9,6 +9,9 @@ from prowler.providers.common.models import Audit_Metadata
 
 AWS_REGION = "eu-west-1"
 AWS_ACCOUNT_NUMBER = "123456789012"
+elb_arn = (
+    f"arn:aws:elasticloadbalancing:{AWS_REGION}:{AWS_ACCOUNT_NUMBER}:loadbalancer/my-lb"
+)
 
 
 class Test_elb_logging_enabled:
@@ -108,6 +111,7 @@ class Test_elb_logging_enabled:
                 result[0].status_extended,
             )
             assert result[0].resource_id == "my-lb"
+            assert result[0].resource_arn == elb_arn
 
     @mock_ec2
     @mock_elb
@@ -165,3 +169,4 @@ class Test_elb_logging_enabled:
                 result[0].status_extended,
             )
             assert result[0].resource_id == "my-lb"
+            assert result[0].resource_arn == elb_arn
