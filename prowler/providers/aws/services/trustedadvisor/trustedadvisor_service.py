113709947b971e02f9f96ade0f73b0ded2636112
jit-contrib <contrib@jit.io>
2023-10-05 01:16:14 +0100

    Merge support service into trustadvisor service

--- prowler/providers/aws/services/trustedadvisor/trustedadvisor_service.py
+++ prowler/providers/aws/services/trustedadvisor/trustedadvisor_service.py
@@ -28,6 +28,8 @@ class TrustedAdvisor(AWSService):
             self.client.region = support_region
             self.__describe_trusted_advisor_checks__()
             self.__describe_trusted_advisor_check_result__()
+        self.support_services = []
+        self.__threading_call__(self.__describe_services__)
 
     def __describe_trusted_advisor_checks__(self):
         logger.info("TrustedAdvisor - Describing Checks...")
@@ -78,9 +80,41 @@ class TrustedAdvisor(AWSService):
                 f"{self.client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
             )
 
+    def __describe_services__(self, regional_client):
+        logger.info("Support - Describing Services...")
+        try:
+            try:
+                regional_client.describe_services()
+                self.support_services.append(
+                    SupportServices(region=regional_client.region, premium_support=True)
+                )
+            except ClientError as error:
+                if (
+                    error.response.get("Error", {}).get("Code")
+                    == "SubscriptionRequiredException"
+                ):
+                    self.support_services.append(
+                        SupportServices(
+                            region=regional_client.region, premium_support=False
+                        )
+                    )
+                else:
+                    raise error
+        except Exception as error:
+            logger.error(
+                f"{regional_client.region} --"
+                f" {error.__class__.__name__}[{error.__traceback__.tb_lineno}]:"
+                f" {error}"
+            )
+
 
 class Check(BaseModel):
     id: str
     name: str
     status: Optional[str]
     region: str
+
+
+class SupportServices(BaseModel):
+    region: str
+    premium_support: bool
--- prowler/providers/aws/services/trustedadvisor/trustedadvisor_support_plan_subscribed/trustedadvisor_support_plan_subscribed.py
+++ prowler/providers/aws/services/trustedadvisor/trustedadvisor_support_plan_subscribed/trustedadvisor_support_plan_subscribed.py
@@ -1,15 +1,19 @@
 from prowler.lib.check.models import Check, Check_Report_AWS
-from prowler.providers.aws.services.support.support_client import support_client
+from prowler.providers.aws.services.trustedadvisor.trustedadvisor_client import (
+    trustedadvisor_client,
+)
 
 
-class support_plan_subscribed(Check):
+class trustedadvisor_support_plan_subscribed(Check):
     def execute(self):
         findings = []
         report = Check_Report_AWS(self.metadata())
         report.status = "FAIL"
         report.status_extended = "Premium support plan isn't subscribed."
-        report.region = "global"
-        for support_service in support_client.support_services:
+        # report.region = "global"
+        report.resource_id = trustedadvisor_client.audited_account
+        report.resource_arn = trustedadvisor_client.audited_account_arn
+        for support_service in trustedadvisor_client.support_services:
             if support_service.premium_support:
                 report.status = "PASS"
                 report.status_extended = "Premium support plan is subscribed."
