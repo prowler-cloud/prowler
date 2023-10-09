139cfcd6717604d8f70759d878e312370e93d296
jit-contrib <contrib@jit.io>
2023-10-09 15:39:43 +0100

    Move Business logic from account_maintain_different_contact_details_to_security_billing_and_operations rule to account service

--- prowler/providers/aws/services/account/account_maintain_different_contact_details_to_security_billing_and_operations/account_maintain_different_contact_details_to_security_billing_and_operations.py
+++ prowler/providers/aws/services/account/account_maintain_different_contact_details_to_security_billing_and_operations/account_maintain_different_contact_details_to_security_billing_and_operations.py
@@ -2,7 +2,6 @@ from prowler.lib.check.models import Check, Check_Report_AWS
 from prowler.providers.aws.services.account.account_client import account_client
 
 
-
 class account_maintain_different_contact_details_to_security_billing_and_operations(
     Check
 ):
@@ -11,37 +10,14 @@ class account_maintain_different_contact_details_to_security_billing_and_operati
         report.region = account_client.region
         report.resource_id = account_client.audited_account
         report.resource_arn = account_client.audited_account_arn
-        root_contact = account_client.client.get_contact_information()[
-            "ContactInformation"
-        ]
-        try:
-            billing_contact = account_client.client.get_alternate_contact(
-                AlternateContactType="BILLING"
-            )
-            security_contact = account_client.client.get_alternate_contact(
-                AlternateContactType="SECURITY"
-            )
-            operations_contact = account_client.client.get_alternate_contact(
-                AlternateContactType="OPERATIONS"
-            )
-        except Exception:
-            billing_contact = None
-            security_contact = None
-            operations_contact = None
+
         if all(
-            [
-                contact is not None
-                for contact in [
-                    root_contact,
-                    billing_contact,
-                    security_contact,
-                    operations_contact,
-                ]
-            ]
+            [contact is not None for contact in account_client.contacts]
+            and len(set(account_client.contacts)) == len(account_client.contacts)
         ):
             report.status = "PASS"
             report.status_extended = "SECURITY, BILLING and OPERATIONS contacts found and they are different between each other and between ROOT contact."
         else:
             report.status = "FAIL"
-            report.status_extended = "SECURITY, BILLING and OPERATIONS contacts not found and they are different between each other and between ROOT contact."
+            report.status_extended = "SECURITY, BILLING and OPERATIONS contacts not found or they are not different between each other and between ROOT contact."
         return [report]
--- prowler/providers/aws/services/account/account_service.py
+++ prowler/providers/aws/services/account/account_service.py
@@ -6,6 +6,25 @@ class Account(AWSService):
     def __init__(self, audit_info):
         # Call AWSService's __init__
         super().__init__(__class__.__name__, audit_info)
+        self.contacts = []
+        self.__get_contacts__()
+
+    def __get_contacts__(self):
+        self.contacts.append(
+            self.client.get_contact_information()["ContactInformation"]
+        )
+        try:
+            self.contacts.append(
+                self.client.get_alternate_contact(AlternateContactType="BILLING")
+            )
+            self.contacts.append(
+                self.client.get_alternate_contact(AlternateContactType="SECURITY")
+            )
+            self.contacts.append(
+                self.client.get_alternate_contact(AlternateContactType="OPERATIONS")
+            )
+        except Exception:
+            self.contacts.append(None)
 
 
 ### This service don't need boto3 calls
