4ddfe7eafd2852cb3d4dcd46731d2da23fcf746b
Sergio Garcia <sergargar1@gmail.com>
2023-10-10 11:52:17 +0200

    add resourceexplorer2

--- prowler/providers/aws/services/resourceexplorer2/resourceexplorer2_indexes_found/resourceexplorer2_indexes_found.py
+++ prowler/providers/aws/services/resourceexplorer2/resourceexplorer2_indexes_found/resourceexplorer2_indexes_found.py
@@ -7,18 +7,22 @@ from prowler.providers.aws.services.resourceexplorer2.resourceexplorer2_client i
 class resourceexplorer2_indexes_found(Check):
     def execute(self):
         findings = []
-        report = Check_Report_AWS(self.metadata())
-        report.status = "FAIL"
-        report.status_extended = "No Resource Explorer Indexes found."
-        report.region = resource_explorer_2_client.region
-        report.resource_arn = "NoResourceExplorer"
-        report.resource_id = resource_explorer_2_client.audited_account
-        report.resource_arn = resource_explorer_2_client.audited_account_arn
         if resource_explorer_2_client.indexes:
+            report = Check_Report_AWS(self.metadata())
             report.region = resource_explorer_2_client.indexes[0].region
+            report.resource_id = resource_explorer_2_client.audited_account
             report.resource_arn = resource_explorer_2_client.indexes[0].arn
             report.status = "PASS"
             report.status_extended = f"Resource Explorer Indexes found: {len(resource_explorer_2_client.indexes)}."
-        findings.append(report)
+            findings.append(report)
+        else:
+            if not resource_explorer_2_client.audit_info.reduce_noise:
+                report = Check_Report_AWS(self.metadata())
+                report.status = "FAIL"
+                report.status_extended = "AWS Resource Explorer is not enabled."
+                report.region = resource_explorer_2_client.region
+                report.resource_id = resource_explorer_2_client.audited_account
+                report.resource_arn = resource_explorer_2_client.audited_account_arn
+                findings.append(report)
 
         return findings
