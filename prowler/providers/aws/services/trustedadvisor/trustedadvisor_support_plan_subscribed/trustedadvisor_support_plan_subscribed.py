787d0c42604b3927e1756350e334760de11a6980
jit-contrib <contrib@jit.io>
2023-10-15 15:37:12 +0100

    Fix trustedadvisor_support_plan_subscribed based on PR comments and discussions

--- prowler/providers/aws/services/trustedadvisor/trustedadvisor_support_plan_subscribed/trustedadvisor_support_plan_subscribed.py
+++ prowler/providers/aws/services/trustedadvisor/trustedadvisor_support_plan_subscribed/trustedadvisor_support_plan_subscribed.py
@@ -7,16 +7,17 @@ from prowler.providers.aws.services.trustedadvisor.trustedadvisor_client import 
 class trustedadvisor_support_plan_subscribed(Check):
     def execute(self):
         findings = []
-        report = Check_Report_AWS(self.metadata())
-        report.status = "FAIL"
-        report.status_extended = "Premium support plan isn't subscribed."
-        # report.region = "global"
-        report.resource_id = trustedadvisor_client.audited_account
-        report.resource_arn = trustedadvisor_client.audited_account_arn
-        for support_service in trustedadvisor_client.support_services:
-            if support_service.premium_support:
-                report.status = "PASS"
-                report.status_extended = "Premium support plan is subscribed."
-                break
-        findings.append(report)
+        if trustedadvisor_client.audit_config.get("verify_premium_support_plans"):
+            report = Check_Report_AWS(self.metadata())
+            report.status = "FAIL"
+            report.status_extended = "Premium support plan isn't subscribed."
+            report.region = trustedadvisor_client.region
+            report.resource_id = trustedadvisor_client.audited_account
+            report.resource_arn = trustedadvisor_client.audited_account_arn
+            for support_service in trustedadvisor_client.support_services:
+                if support_service.premium_support:
+                    report.status = "PASS"
+                    report.status_extended = "Premium support plan is subscribed."
+                    break
+            findings.append(report)
         return findings
