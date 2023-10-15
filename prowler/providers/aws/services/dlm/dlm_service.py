337ff9f7827e72d909a68eabc734acf3a44fb6cd
jit-contrib <contrib@jit.io>
2023-10-15 16:18:27 +0100

    Apply PR comments to discussions outcome to dlm_ebs_snapshot_lifecycle_policy_exists

--- prowler/providers/aws/services/dlm/dlm_service.py
+++ prowler/providers/aws/services/dlm/dlm_service.py
@@ -1,5 +1,3 @@
-import threading
-
 from prowler.lib.logger import logger
 from prowler.providers.aws.lib.service.service import AWSService
 
@@ -9,14 +7,16 @@ class DLM(AWSService):
     def __init__(self, audit_info):
         # Call AWSService's __init__
         super().__init__(__class__.__name__, audit_info)
-        self.lifecycle_policies = []
+        self.lifecycle_policies = {}
         self.__threading_call__(self.__get_lifecycle_policies__)
 
     def __get_lifecycle_policies__(self, regional_client):
         logger.info("DLM - Getting EBS Snapshots Lifecycle Policies...")
         try:
             lifecycle_policies = regional_client.get_lifecycle_policies()
-            self.lifecycle_policies += lifecycle_policies["Policies"]
+            self.lifecycle_policies[regional_client.region] = lifecycle_policies[
+                "Policies"
+            ]
         except Exception as error:
             logger.error(
                 f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
