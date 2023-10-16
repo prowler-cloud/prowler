2827ff719e496f566cade9d36325b895c2f440d5
jit-contrib <contrib@jit.io>
2023-10-17 00:40:56 +0100

    Apply PR comments to neptune_service and rule

--- prowler/providers/aws/services/neptune/neptune_service.py
+++ prowler/providers/aws/services/neptune/neptune_service.py
@@ -1,7 +1,10 @@
-from pydantic import BaseModel, typing
+from typing import Optional
+
+from pydantic import BaseModel
 
 from prowler.lib.logger import logger
 from prowler.providers.aws.lib.service.service import AWSService
+from prowler.providers.aws.services.vpc.vpc_client import vpc_client
 
 
 ################## NetworkFirewall
@@ -11,11 +14,21 @@ class Neptune(AWSService):
         super().__init__(__class__.__name__, audit_info)
         self.clusters = []
         self.__describe_clusters__()
+        self.__get_public_subnets__()
 
     def __describe_clusters__(self):
         logger.info("Neptune - Describing DB Clusters...")
         try:
-            for cluster in self.client.describe_db_clusters()["DBClusters"]:
+            for cluster in self.client.describe_db_clusters(
+                Filters=[
+                    {
+                        "Name": "engine",
+                        "Values": [
+                            "neptune",
+                        ],
+                    },
+                ],
+            )["DBClusters"]:
                 self.clusters.append(
                     Cluster(
                         arn=cluster["DBClusterArn"],
@@ -31,9 +44,21 @@ class Neptune(AWSService):
                 f"{error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
             )
 
+    def __get_public_subnets__(self):
+        for cluster in self.clusters:
+            public_subnets = []
+            for subnets in cluster.subnet_group:
+                for subnet in subnets["Subnets"]:
+                    if vpc_client.vpc_subnets[subnet["SubnetIdentifier"]].public:
+                        public_subnets.append(
+                            vpc_client.vpc_subnets[subnet["SubnetIdentifier"]].id
+                        )
+            cluster.public_subnets = public_subnets
+
 
 class Cluster(BaseModel):
     arn: str
     name: str
     id: str
-    subnet_group: typing.Any
+    subnet_group: list
+    public_subnets: Optional[list] = []
--- prowler/providers/aws/services/neptune/neptune_uses_a_public_subnet/neptune_uses_a_public_subnet.py
+++ prowler/providers/aws/services/neptune/neptune_uses_a_public_subnet/neptune_uses_a_public_subnet.py
@@ -1,30 +1,19 @@
 from prowler.lib.check.models import Check, Check_Report_AWS
 from prowler.providers.aws.services.neptune.neptune_client import neptune_client
-from prowler.providers.aws.services.vpc.vpc_client import vpc_client
 
 
 class neptune_uses_a_public_subnet(Check):
     def execute(self):
         findings = []
-        vpc_subnets = vpc_client.vpc_subnets
         for cluster in neptune_client.clusters:
             report = Check_Report_AWS(self.metadata())
             report.resource_id = cluster.id
             report.resource_arn = cluster.arn
             report.status = "PASS"
             report.status_extended = "Cluster isn't using public subnets."
-            public_subnets = []
-            for subnets in cluster.subnet_group:
-                for subnet in subnets["Subnets"]:
-                    if vpc_subnets[subnet["SubnetIdentifier"]].public:
-                        public_subnets.append(
-                            vpc_subnets[subnet["SubnetIdentifier"]].id
-                        )
-            if len(public_subnets) > 0:
+            if len(cluster.public_subnets) > 0:
                 report.status = "FAIL"
-                report.status_extended = (
-                    f"Cluster is using {', '.join(public_subnets)} public subnets."
-                )
+                report.status_extended = f"Cluster is using {', '.join(cluster.public_subnets)} public subnets."
 
             findings.append(report)
 
