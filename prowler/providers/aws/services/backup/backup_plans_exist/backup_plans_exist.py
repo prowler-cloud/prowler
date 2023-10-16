e17b42885b2f9b974960a8fe4ad2f613a1b9cab3
Sergio Garcia <sergargar1@gmail.com>
2023-10-16 15:41:05 +0200

    reduce backup noise

--- prowler/providers/aws/services/backup/backup_plans_exist/backup_plans_exist.py
+++ prowler/providers/aws/services/backup/backup_plans_exist/backup_plans_exist.py
@@ -5,18 +5,20 @@ from prowler.providers.aws.services.backup.backup_client import backup_client
 class backup_plans_exist(Check):
     def execute(self):
         findings = []
-        report = Check_Report_AWS(self.metadata())
-        report.status = "FAIL"
-        report.status_extended = "No Backup Plan exist."
-        report.resource_arn = backup_client.audited_account_arn
-        report.resource_id = backup_client.audited_account
-        report.region = backup_client.region
         if backup_client.backup_plans:
+            report = Check_Report_AWS(self.metadata())
             report.status = "PASS"
-            report.status_extended = f"At least one backup plan exists: {backup_client.backup_plans[0].name}."
+            report.status_extended = f"At least one Backup Plan exists: {backup_client.backup_plans[0].name}."
             report.resource_arn = backup_client.backup_plans[0].arn
             report.resource_id = backup_client.backup_plans[0].name
             report.region = backup_client.backup_plans[0].region
+        elif backup_client.backup_vaults:
+            report = Check_Report_AWS(self.metadata())
+            report.status = "FAIL"
+            report.status_extended = "No Backup Plan exist."
+            report.resource_arn = backup_client.audited_account_arn
+            report.resource_id = backup_client.audited_account
+            report.region = backup_client.region
 
         findings.append(report)
         return findings
