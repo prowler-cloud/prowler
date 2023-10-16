863016a3fac9e8bc75ad2fa743e4d5e928620ebc
Pepe Fagoaga <pepe@verica.io>
2023-10-16 17:18:43 +0200

    fix(account): Improve logic and add tests

--- prowler/providers/aws/services/account/account_maintain_different_contact_details_to_security_billing_and_operations/account_maintain_different_contact_details_to_security_billing_and_operations.py
+++ prowler/providers/aws/services/account/account_maintain_different_contact_details_to_security_billing_and_operations/account_maintain_different_contact_details_to_security_billing_and_operations.py
@@ -11,10 +11,13 @@ class account_maintain_different_contact_details_to_security_billing_and_operati
         report.resource_id = account_client.audited_account
         report.resource_arn = account_client.audited_account_arn
 
-        contacts_list = account_client.contacts.get_contacts_list()
-        if all(
-            [contact is not None for contact in contacts_list]
-            and len(set(contacts_list)) == len(contacts_list)
+        if (
+            len(account_client.contact_phone_numbers)
+            == account_client.number_of_contacts
+            and len(account_client.contact_names) == account_client.number_of_contacts
+            # This is because the primary contact has no email field
+            and len(account_client.contact_emails)
+            == account_client.number_of_contacts - 1
         ):
             report.status = "PASS"
             report.status_extended = "SECURITY, BILLING and OPERATIONS contacts found and they are different between each other and between ROOT contact."
--- prowler/providers/aws/services/account/account_service.py
+++ prowler/providers/aws/services/account/account_service.py
@@ -1,5 +1,6 @@
 ################## Account
 from typing import Optional
+from venv import logger
 
 from pydantic import BaseModel
 
@@ -10,34 +11,75 @@ class Account(AWSService):
     def __init__(self, audit_info):
         # Call AWSService's __init__
         super().__init__(__class__.__name__, audit_info)
-        self.__get_contacts__()
+        self.number_of_contacts = 4
+        self.contact_base = self.__get_contact_information__()
+        self.contacts_billing = self.__get_alternate_contact__("BILLING")
+        self.contacts_security = self.__get_alternate_contact__("SECURITY")
+        self.contacts_operations = self.__get_alternate_contact__("OPERATIONS")
 
-    def __get_contacts__(self):
+        # Set of contact phone numbers
+        self.contact_phone_numbers = {
+            self.contact_base.phone_number,
+            self.contacts_billing.phone_number,
+            self.contacts_security.phone_number,
+            self.contacts_operations.phone_number,
+        }
+
+        # Set of contact names
+        self.contact_names = {
+            self.contact_base.name,
+            self.contacts_billing.name,
+            self.contacts_security.name,
+            self.contacts_operations.name,
+        }
+
+        # Set of contact emails
+        self.contact_emails = {
+            self.contacts_billing.email,
+            self.contacts_security.email,
+            self.contacts_operations.email,
+        }
+
+    def __get_contact_information__(self):
         try:
-            self.contacts = Contacts(
-                base=self.client.get_contact_information()["ContactInformation"],
-                billing=self.client.get_alternate_contact(
-                    AlternateContactType="BILLING"
-                ),
-                security=self.client.get_alternate_contact(
-                    AlternateContactType="SECURITY"
-                ),
-                operations=self.client.get_alternate_contact(
-                    AlternateContactType="OPERATIONS"
-                ),
+            primary_account_contact = self.client.get_contact_information()[
+                "ContactInformation"
+            ]
+
+            return Contact(
+                type="PRIMARY",
+                name=primary_account_contact.get("FullName"),
+                phone_number=primary_account_contact.get("PhoneNumber"),
             )
-        except Exception:
-            self.contacts = Contacts()
+        except Exception as error:
+            logger.error(
+                f"{self.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+            )
+            return Contact(type="PRIMARY")
+
+    def __get_alternate_contact__(self, contact_type: str):
+        try:
+            account_contact = self.client.get_alternate_contact(
+                AlternateContactType=contact_type
+            )["AlternateContact"]
+
+            return Contact(
+                type=contact_type,
+                email=account_contact.get("EmailAddress"),
+                name=account_contact.get("Name"),
+                phone_number=account_contact.get("PhoneNumber"),
+            )
+        except Exception as error:
+            logger.error(
+                f"{self.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+            )
+            return Contact(
+                type=contact_type,
+            )
 
 
-class Contacts(BaseModel):
-    base: Optional[str]
-    billing: Optional[str]
-    security: Optional[str]
-    operations: Optional[str]
-
-    def get_contacts_list(self):
-        return [self.base, self.billing, self.security, self.operations]
-
-
-### This service don't need boto3 calls
+class Contact(BaseModel):
+    type: str
+    email: Optional[str]
+    name: Optional[str]
+    phone_number: Optional[str]
