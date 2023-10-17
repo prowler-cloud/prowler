f3bb12bc2bcebb3ac2fd21a4a76b7b87d97a93a6
Sergio Garcia <sergargar1@gmail.com>
2023-10-17 10:40:02 +0200

    reduce nacl noise

--- prowler/providers/aws/services/ec2/ec2_networkacl_allow_ingress_any_port/ec2_networkacl_allow_ingress_any_port.py
+++ prowler/providers/aws/services/ec2/ec2_networkacl_allow_ingress_any_port/ec2_networkacl_allow_ingress_any_port.py
@@ -9,17 +9,31 @@ class ec2_networkacl_allow_ingress_any_port(Check):
         tcp_protocol = "-1"
         check_port = 0
         for network_acl in ec2_client.network_acls:
-            report = Check_Report_AWS(self.metadata())
-            report.region = network_acl.region
-            report.resource_id = network_acl.id
-            report.resource_arn = network_acl.arn
-            report.resource_tags = network_acl.tags
-            report.status = "PASS"
-            report.status_extended = f"Network ACL {network_acl.name if network_acl.name else network_acl.id} does not have every port open to the Internet."
-            # If some entry allows it, that ACL is not securely configured
-            if check_network_acl(network_acl.entries, tcp_protocol, check_port):
-                report.status = "FAIL"
-                report.status_extended = f"Network ACL {network_acl.name if network_acl.name else network_acl.id} has every port open to the Internet."
-            findings.append(report)
+            if ec2_client.audit_info.reduce_noise:
+                sg_in_region = False
+                for sg in ec2_client.security_groups:
+                    if sg.region == network_acl.region and sg.name != "default":
+                        sg_in_region = True
+                        break
+            if not ec2_client.audit_info.reduce_noise or sg_in_region:
+                # If some entry allows it, that ACL is not securely configured
+                if check_network_acl(network_acl.entries, tcp_protocol, check_port):
+                    report = Check_Report_AWS(self.metadata())
+                    report.resource_id = network_acl.id
+                    report.region = network_acl.region
+                    report.resource_arn = network_acl.arn
+                    report.resource_tags = network_acl.tags
+                    report.status = "FAIL"
+                    report.status_extended = f"Network ACL {network_acl.id} has SSH port 22 open to the Internet."
+                    findings.append(report)
+                else:
+                    report = Check_Report_AWS(self.metadata())
+                    report.resource_id = network_acl.id
+                    report.region = network_acl.region
+                    report.resource_arn = network_acl.arn
+                    report.resource_tags = network_acl.tags
+                    report.status = "PASS"
+                    report.status_extended = f"Network ACL {network_acl.id} does not have SSH port 22 open to the Internet."
+                    findings.append(report)
 
         return findings
