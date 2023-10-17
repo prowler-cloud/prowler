3a92404e8c75f8758e82be41fad2692978ebe389
Sergio Garcia <sergargar1@gmail.com>
2023-10-17 15:03:12 +0200

    check dlm only if snapshots

--- prowler/providers/aws/services/dlm/dlm_ebs_snapshot_lifecycle_policy_exists/dlm_ebs_snapshot_lifecycle_policy_exists.py
+++ prowler/providers/aws/services/dlm/dlm_ebs_snapshot_lifecycle_policy_exists/dlm_ebs_snapshot_lifecycle_policy_exists.py
@@ -1,19 +1,26 @@
 from prowler.lib.check.models import Check, Check_Report_AWS
 from prowler.providers.aws.services.dlm.dlm_client import dlm_client
+from prowler.providers.aws.services.ec2.ec2_client import ec2_client
 
 
 class dlm_ebs_snapshot_lifecycle_policy_exists(Check):
     def execute(self):
         findings = []
         for region in dlm_client.lifecycle_policies:
-            report = Check_Report_AWS(self.metadata())
-            report.status = "FAIL"
-            report.status_extended = "No EBS Snapshot lifecycle policies found."
-            report.region = region
-            report.resource_id = dlm_client.audited_account
-            report.resource_arn = dlm_client.audited_account_arn
-            if dlm_client.lifecycle_policies[region]:
-                report.status = "PASS"
-                report.status_extended = "EBS snapshot lifecycle policies found."
-            findings.append(report)
+            snapshots_in_region = False
+            for snapshot in ec2_client.snapshots:
+                if snapshot.region == region:
+                    snapshots_in_region = True
+                    break
+            if snapshots_in_region:
+                report = Check_Report_AWS(self.metadata())
+                report.status = "FAIL"
+                report.status_extended = "No EBS Snapshot lifecycle policies found."
+                report.region = region
+                report.resource_id = dlm_client.audited_account
+                report.resource_arn = dlm_client.audited_account_arn
+                if dlm_client.lifecycle_policies[region]:
+                    report.status = "PASS"
+                    report.status_extended = "EBS snapshot lifecycle policies found."
+                findings.append(report)
         return findings
