1ee84c8bb20955678f7abdc83d92f31171463cff
Pepe Fagoaga <pepe@verica.io>
2023-10-18 16:01:41 +0200

    fix(apigw): Fix check and tests

--- prowler/providers/aws/services/apigateway/apigateway_endpoint_public_without_authorizer/apigateway_endpoint_public_without_authorizer.py
+++ prowler/providers/aws/services/apigateway/apigateway_endpoint_public_without_authorizer/apigateway_endpoint_public_without_authorizer.py
@@ -8,17 +8,20 @@ class apigateway_endpoint_public_without_authorizer(Check):
     def execute(self):
         findings = []
         for rest_api in apigateway_client.rest_apis:
-            report = Check_Report_AWS(self.metadata())
-            report.region = rest_api.region
-            report.resource_id = rest_api.name
-            report.resource_arn = rest_api.arn
-            report.resource_tags = rest_api.tags
-            if rest_api.public_endpoint and not rest_api.authorizer:
-                report.status = "FAIL"
-                report.status_extended = f"API Gateway {rest_api.name} ID {rest_api.id} is internet accesible without an authorizer."
-            elif rest_api.public_endpoint and rest_api.authorizer:
+            if rest_api.public_endpoint:
+                report = Check_Report_AWS(self.metadata())
+                report.region = rest_api.region
+                report.resource_id = rest_api.name
+                report.resource_arn = rest_api.arn
+                report.resource_tags = rest_api.tags
+
                 report.status = "PASS"
-                report.status_extended = f"API Gateway {rest_api.name} ID {rest_api.id} is {'internet accesible with an authorizer' if rest_api.public_endpoint else 'private'}."
-            findings.append(report)
+                report.status_extended = f"API Gateway REST API {rest_api.name} with ID {rest_api.id} has a public endpoint with an authorizer."
+
+                if not rest_api.authorizer:
+                    report.status = "FAIL"
+                    report.status_extended = f"API Gateway REST API {rest_api.name} with ID {rest_api.id} has a public endpoint without an authorizer."
+
+                findings.append(report)
 
         return findings
--- tests/providers/aws/services/apigateway/apigateway_endpoint_public_without_authorizer/apigateway_endpoint_public_without_authorizer_test.py
+++ tests/providers/aws/services/apigateway/apigateway_endpoint_public_without_authorizer/apigateway_endpoint_public_without_authorizer_test.py
@@ -9,6 +9,8 @@ from prowler.providers.common.models import Audit_Metadata
 AWS_REGION = "us-east-1"
 AWS_ACCOUNT_NUMBER = "123456789012"
 
+API_GW_NAME = "test-rest-api"
+
 
 class Test_apigateway_endpoint_public_without_authorizer:
     def set_mocked_audit_info(self):
@@ -54,8 +56,11 @@ class Test_apigateway_endpoint_public_without_authorizer:
             "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
             new=current_audit_info,
         ), mock.patch(
-            "prowler.providers.aws.services.apigateway.apigateway_endpoint_public_without_authorizer.apigateway_endpoint_public_without_authorizer.apigateway_client",
-            new=APIGateway(current_audit_info),
+            "prowler.providers.aws.services.apigateway.apigateway_service.APIGateway",
+            return_value=APIGateway(current_audit_info),
+        ) as apigateway_client, mock.patch(
+            "prowler.providers.aws.services.apigateway.apigateway_client.apigateway_client",
+            new=apigateway_client(),
         ):
             # Test Check
             from prowler.providers.aws.services.apigateway.apigateway_endpoint_public_without_authorizer.apigateway_endpoint_public_without_authorizer import (
@@ -68,111 +73,12 @@ class Test_apigateway_endpoint_public_without_authorizer:
             assert len(result) == 0
 
     @mock_apigateway
-    def test_apigateway_one_private_rest_api_without_authorizer(self):
-        # Create APIGateway Mocked Resources
-        apigateway_client = client("apigateway", region_name=AWS_REGION)
-        # Create APIGateway Deployment Stage
-        rest_api = apigateway_client.create_rest_api(
-            name="test-rest-api",
-            endpointConfiguration={
-                "types": [
-                    "PRIVATE",
-                ]
-            },
-        )
-        from prowler.providers.aws.services.apigateway.apigateway_service import (
-            APIGateway,
-        )
-
-        current_audit_info = self.set_mocked_audit_info()
-
-        with mock.patch(
-            "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
-            new=current_audit_info,
-        ), mock.patch(
-            "prowler.providers.aws.services.apigateway.apigateway_endpoint_public_without_authorizer.apigateway_endpoint_public_without_authorizer.apigateway_client",
-            new=APIGateway(current_audit_info),
-        ):
-            # Test Check
-            from prowler.providers.aws.services.apigateway.apigateway_endpoint_public_without_authorizer.apigateway_endpoint_public_without_authorizer import (
-                apigateway_endpoint_public_without_authorizer,
-            )
-
-            check = apigateway_endpoint_public_without_authorizer()
-            result = check.execute()
-
-            assert result[0].status == "PASS"
-            assert len(result) == 1
-            assert (
-                result[0].status_extended
-                == f"API Gateway test-rest-api ID {rest_api['id']} is private."
-            )
-            assert result[0].resource_id == "test-rest-api"
-            assert (
-                result[0].resource_arn
-                == f"arn:{current_audit_info.audited_partition}:apigateway:{AWS_REGION}::/restapis/{rest_api['id']}"
-            )
-            assert result[0].region == AWS_REGION
-            assert result[0].resource_tags == [{}]
-
-    @mock_apigateway
-    def test_apigateway_one_private_rest_api_with_authorizer(self):
-        # Create APIGateway Mocked Resources
-        apigateway_client = client("apigateway", region_name=AWS_REGION)
-        # Create APIGateway Deployment Stage
-        rest_api = apigateway_client.create_rest_api(
-            name="test-rest-api",
-            endpointConfiguration={
-                "types": [
-                    "PRIVATE",
-                ]
-            },
-        )
-        apigateway_client.create_authorizer(
-            restApiId=rest_api["id"], name="test-rest-api-with-authorizer", type="TOKEN"
-        )
-        from prowler.providers.aws.services.apigateway.apigateway_service import (
-            APIGateway,
-        )
-
-        current_audit_info = self.set_mocked_audit_info()
-
-        with mock.patch(
-            "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
-            new=current_audit_info,
-        ), mock.patch(
-            "prowler.providers.aws.services.apigateway.apigateway_endpoint_public_without_authorizer.apigateway_endpoint_public_without_authorizer.apigateway_client",
-            new=APIGateway(current_audit_info),
-        ):
-            # Test Check
-            from prowler.providers.aws.services.apigateway.apigateway_endpoint_public_without_authorizer.apigateway_endpoint_public_without_authorizer import (
-                apigateway_endpoint_public_without_authorizer,
-            )
-
-            check = apigateway_endpoint_public_without_authorizer()
-            result = check.execute()
-
-            assert result[0].status == "PASS"
-            assert len(result) == 1
-            assert (
-                result[0].status_extended
-                == f"API Gateway test-rest-api ID {rest_api['id']} is private."
-            )
-            assert result[0].resource_id == "test-rest-api"
-            assert (
-                result[0].resource_arn
-                == f"arn:{current_audit_info.audited_partition}:apigateway:{AWS_REGION}::/restapis/{rest_api['id']}"
-            )
-            assert result[0].region == AWS_REGION
-            assert result[0].resource_tags == [{}]
-
-    @mock_apigateway
     def test_apigateway_one_public_rest_api_without_authorizer(self):
         # Create APIGateway Mocked Resources
         apigateway_client = client("apigateway", region_name=AWS_REGION)
         # Create APIGateway Deployment Stage
         rest_api = apigateway_client.create_rest_api(
-            name="test-rest-api",
+            name=API_GW_NAME,
             endpointConfiguration={
                 "types": [
                     "EDGE",
@@ -189,8 +95,11 @@ class Test_apigateway_endpoint_public_without_authorizer:
             "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
             new=current_audit_info,
         ), mock.patch(
-            "prowler.providers.aws.services.apigateway.apigateway_endpoint_public_without_authorizer.apigateway_endpoint_public_without_authorizer.apigateway_client",
-            new=APIGateway(current_audit_info),
+            "prowler.providers.aws.services.apigateway.apigateway_service.APIGateway",
+            return_value=APIGateway(current_audit_info),
+        ) as apigateway_client, mock.patch(
+            "prowler.providers.aws.services.apigateway.apigateway_client.apigateway_client",
+            new=apigateway_client(),
         ):
             # Test Check
             from prowler.providers.aws.services.apigateway.apigateway_endpoint_public_without_authorizer.apigateway_endpoint_public_without_authorizer import (
@@ -200,13 +109,13 @@ class Test_apigateway_endpoint_public_without_authorizer:
             check = apigateway_endpoint_public_without_authorizer()
             result = check.execute()
 
-            assert result[0].status == "FAIL"
             assert len(result) == 1
+            assert result[0].status == "FAIL"
             assert (
                 result[0].status_extended
-                == f"API Gateway test-rest-api ID {rest_api['id']} is internet accesible without an authorizer."
+                == f"API Gateway REST API {API_GW_NAME} with ID {rest_api['id']} has a public endpoint without an authorizer."
             )
-            assert result[0].resource_id == "test-rest-api"
+            assert result[0].resource_id == API_GW_NAME
             assert (
                 result[0].resource_arn
                 == f"arn:{current_audit_info.audited_partition}:apigateway:{AWS_REGION}::/restapis/{rest_api['id']}"
@@ -240,8 +149,11 @@ class Test_apigateway_endpoint_public_without_authorizer:
             "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
             new=current_audit_info,
         ), mock.patch(
-            "prowler.providers.aws.services.apigateway.apigateway_endpoint_public_without_authorizer.apigateway_endpoint_public_without_authorizer.apigateway_client",
-            new=APIGateway(current_audit_info),
+            "prowler.providers.aws.services.apigateway.apigateway_service.APIGateway",
+            return_value=APIGateway(current_audit_info),
+        ) as apigateway_client, mock.patch(
+            "prowler.providers.aws.services.apigateway.apigateway_client.apigateway_client",
+            new=apigateway_client(),
         ):
             # Test Check
             from prowler.providers.aws.services.apigateway.apigateway_endpoint_public_without_authorizer.apigateway_endpoint_public_without_authorizer import (
@@ -255,9 +167,9 @@ class Test_apigateway_endpoint_public_without_authorizer:
             assert len(result) == 1
             assert (
                 result[0].status_extended
-                == f"API Gateway test-rest-api ID {rest_api['id']} is internet accesible with an authorizer."
+                == f"API Gateway REST API {API_GW_NAME} with ID {rest_api['id']} has a public endpoint with an authorizer."
             )
-            assert result[0].resource_id == "test-rest-api"
+            assert result[0].resource_id == API_GW_NAME
             assert (
                 result[0].resource_arn
                 == f"arn:{current_audit_info.audited_partition}:apigateway:{AWS_REGION}::/restapis/{rest_api['id']}"
