0184bac5bed141ab6ba88bb7ba4d334a73a69a8a
Pepe Fagoaga <pepe@verica.io>
2023-11-30 08:34:05 +0100

    fix(vpc_different_regions): Handle no VPCs and add tests

--- prowler/providers/aws/services/vpc/vpc_different_regions/vpc_different_regions.py
+++ prowler/providers/aws/services/vpc/vpc_different_regions/vpc_different_regions.py
@@ -5,25 +5,23 @@ from prowler.providers.aws.services.vpc.vpc_client import vpc_client
 class vpc_different_regions(Check):
     def execute(self):
         findings = []
-        vpc_regions = set()
-        for vpc in vpc_client.vpcs.values():
-            if not vpc.default:
-                vpc_regions.add(vpc.region)
+        if len(vpc_client.vpcs) > 0:
+            vpc_regions = set()
+            for vpc in vpc_client.vpcs.values():
+                if not vpc.default:
+                    vpc_regions.add(vpc.region)
 
-        report = Check_Report_AWS(self.metadata())
-        # This is a global check under the vpc service: region, resource_id and tags are not relevant here but we keep them for consistency
-        report.region = vpc_client.region
-        report.resource_id = vpc_client.audited_account
-        report.resource_arn = vpc_client.audited_account_arn
-        if len(vpc_regions) == 0:
-            report.status = "INFO"
-            report.status_extended = "No VPCs found."
-        elif len(vpc_regions) == 1:
-            report.status = "FAIL"
-            report.status_extended = "VPCs found only in one region."
-        else:
-            report.status = "PASS"
-            report.status_extended = "VPCs found in more than one region."
-        findings.append(report)
+            report = Check_Report_AWS(self.metadata())
+            report.region = vpc_client.region
+            report.resource_id = vpc_client.audited_account
+            report.resource_arn = vpc_client.audited_account_arn
+
+            if len(vpc_regions) == 1:
+                report.status = "FAIL"
+                report.status_extended = "VPCs found only in one region."
+            else:
+                report.status = "PASS"
+                report.status_extended = "VPCs found in more than one region."
+            findings.append(report)
 
         return findings
