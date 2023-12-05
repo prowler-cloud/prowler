e7f9360ae1ed528715e74053254bb4711b74ae94
n4ch04 <nachor1992@gmail.com>
2023-12-05 10:56:51 +0100

    test(audit_info): refactor shield

--- tests/providers/aws/services/shield/shield_advanced_protection_in_classic_load_balancers/shield_advanced_protection_in_classic_load_balancers_test.py
+++ tests/providers/aws/services/shield/shield_advanced_protection_in_classic_load_balancers/shield_advanced_protection_in_classic_load_balancers_test.py
@@ -1,48 +1,17 @@
 from unittest import mock
 
-from boto3 import client, resource, session
+from boto3 import client, resource
 from moto import mock_ec2, mock_elb
 from moto.core import DEFAULT_ACCOUNT_ID
 
-from prowler.providers.aws.lib.audit_info.models import AWS_Audit_Info
 from prowler.providers.aws.services.shield.shield_service import Protection
-from prowler.providers.common.models import Audit_Metadata
-
-AWS_REGION = "eu-west-1"
+from tests.providers.aws.audit_info_utils import (
+    AWS_REGION_EU_WEST_1,
+    set_mocked_aws_audit_info,
+)
 
 
 class Test_shield_advanced_protection_in_classic_load_balancers:
-    # Mocked Audit Info
-    def set_mocked_audit_info(self):
-        audit_info = AWS_Audit_Info(
-            session_config=None,
-            original_session=None,
-            audit_session=session.Session(
-                profile_name=None,
-                botocore_session=None,
-            ),
-            audited_account=DEFAULT_ACCOUNT_ID,
-            audited_account_arn=f"arn:aws:iam::{DEFAULT_ACCOUNT_ID}:root",
-            audited_user_id=None,
-            audited_partition="aws",
-            audited_identity_arn=None,
-            profile=None,
-            profile_region=AWS_REGION,
-            credentials=None,
-            assumed_role_info=None,
-            audited_regions=None,
-            organizations_metadata=None,
-            audit_resources=None,
-            mfa_enabled=False,
-            audit_metadata=Audit_Metadata(
-                services_scanned=0,
-                expected_checks=[],
-                completed_checks=0,
-                audit_progress=0,
-            ),
-        )
-        return audit_info
-
     @mock_elb
     @mock_ec2
     def test_no_shield_not_active(self):
@@ -57,10 +26,10 @@ class Test_shield_advanced_protection_in_classic_load_balancers:
             new=shield_client,
         ), mock.patch(
             "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
-            new=self.set_mocked_audit_info(),
+            new=set_mocked_aws_audit_info([AWS_REGION_EU_WEST_1]),
         ), mock.patch(
             "prowler.providers.aws.services.shield.shield_advanced_protection_in_classic_load_balancers.shield_advanced_protection_in_classic_load_balancers.elb_client",
-            new=ELB(self.set_mocked_audit_info()),
+            new=ELB(set_mocked_aws_audit_info([AWS_REGION_EU_WEST_1])),
         ):
             # Test Check
             from prowler.providers.aws.services.shield.shield_advanced_protection_in_classic_load_balancers.shield_advanced_protection_in_classic_load_balancers import (
@@ -76,8 +45,8 @@ class Test_shield_advanced_protection_in_classic_load_balancers:
     @mock_elb
     def test_shield_enabled_elb_protected(self):
         # ELB Client
-        elb = client("elb", region_name=AWS_REGION)
-        ec2 = resource("ec2", region_name=AWS_REGION)
+        elb = client("elb", region_name=AWS_REGION_EU_WEST_1)
+        ec2 = resource("ec2", region_name=AWS_REGION_EU_WEST_1)
 
         security_group = ec2.create_security_group(
             GroupName="sg01", Description="Test security group sg01"
@@ -89,16 +58,16 @@ class Test_shield_advanced_protection_in_classic_load_balancers:
                 {"Protocol": "tcp", "LoadBalancerPort": 80, "InstancePort": 8080},
                 {"Protocol": "http", "LoadBalancerPort": 81, "InstancePort": 9000},
             ],
-            AvailabilityZones=[f"{AWS_REGION}a"],
+            AvailabilityZones=[f"{AWS_REGION_EU_WEST_1}a"],
             Scheme="internet-facing",
             SecurityGroups=[security_group.id],
         )
-        elb_arn = f"arn:aws:elasticloadbalancing:{AWS_REGION}:{DEFAULT_ACCOUNT_ID}:loadbalancer/{elb_name}"
+        elb_arn = f"arn:aws:elasticloadbalancing:{AWS_REGION_EU_WEST_1}:{DEFAULT_ACCOUNT_ID}:loadbalancer/{elb_name}"
 
         # Shield Client
         shield_client = mock.MagicMock
         shield_client.enabled = True
-        shield_client.region = AWS_REGION
+        shield_client.region = AWS_REGION_EU_WEST_1
         protection_id = "test-protection"
         shield_client.protections = {
             protection_id: Protection(
@@ -106,7 +75,7 @@ class Test_shield_advanced_protection_in_classic_load_balancers:
                 name="",
                 resource_arn=elb_arn,
                 protection_arn="",
-                region=AWS_REGION,
+                region=AWS_REGION_EU_WEST_1,
             )
         }
 
@@ -117,10 +86,10 @@ class Test_shield_advanced_protection_in_classic_load_balancers:
             new=shield_client,
         ), mock.patch(
             "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
-            new=self.set_mocked_audit_info(),
+            new=set_mocked_aws_audit_info([AWS_REGION_EU_WEST_1]),
         ), mock.patch(
             "prowler.providers.aws.services.shield.shield_advanced_protection_in_classic_load_balancers.shield_advanced_protection_in_classic_load_balancers.elb_client",
-            new=ELB(self.set_mocked_audit_info()),
+            new=ELB(set_mocked_aws_audit_info([AWS_REGION_EU_WEST_1])),
         ):
             # Test Check
             from prowler.providers.aws.services.shield.shield_advanced_protection_in_classic_load_balancers.shield_advanced_protection_in_classic_load_balancers import (
@@ -131,7 +100,7 @@ class Test_shield_advanced_protection_in_classic_load_balancers:
             result = check.execute()
 
             assert len(result) == 1
-            assert result[0].region == AWS_REGION
+            assert result[0].region == AWS_REGION_EU_WEST_1
             assert result[0].resource_id == elb_name
             assert result[0].resource_arn == elb_arn
             assert result[0].status == "PASS"
@@ -144,8 +113,8 @@ class Test_shield_advanced_protection_in_classic_load_balancers:
     @mock_ec2
     def test_shield_enabled_elb_not_protected(self):
         # ELB Client
-        elb = client("elb", region_name=AWS_REGION)
-        ec2 = resource("ec2", region_name=AWS_REGION)
+        elb = client("elb", region_name=AWS_REGION_EU_WEST_1)
+        ec2 = resource("ec2", region_name=AWS_REGION_EU_WEST_1)
 
         security_group = ec2.create_security_group(
             GroupName="sg01", Description="Test security group sg01"
@@ -157,16 +126,16 @@ class Test_shield_advanced_protection_in_classic_load_balancers:
                 {"Protocol": "tcp", "LoadBalancerPort": 80, "InstancePort": 8080},
                 {"Protocol": "http", "LoadBalancerPort": 81, "InstancePort": 9000},
             ],
-            AvailabilityZones=[f"{AWS_REGION}a"],
+            AvailabilityZones=[f"{AWS_REGION_EU_WEST_1}a"],
             Scheme="internet-facing",
             SecurityGroups=[security_group.id],
         )
-        elb_arn = f"arn:aws:elasticloadbalancing:{AWS_REGION}:{DEFAULT_ACCOUNT_ID}:loadbalancer/{elb_name}"
+        elb_arn = f"arn:aws:elasticloadbalancing:{AWS_REGION_EU_WEST_1}:{DEFAULT_ACCOUNT_ID}:loadbalancer/{elb_name}"
 
         # Shield Client
         shield_client = mock.MagicMock
         shield_client.enabled = True
-        shield_client.region = AWS_REGION
+        shield_client.region = AWS_REGION_EU_WEST_1
         shield_client.protections = {}
 
         from prowler.providers.aws.services.elb.elb_service import ELB
@@ -176,10 +145,10 @@ class Test_shield_advanced_protection_in_classic_load_balancers:
             new=shield_client,
         ), mock.patch(
             "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
-            new=self.set_mocked_audit_info(),
+            new=set_mocked_aws_audit_info([AWS_REGION_EU_WEST_1]),
         ), mock.patch(
             "prowler.providers.aws.services.shield.shield_advanced_protection_in_classic_load_balancers.shield_advanced_protection_in_classic_load_balancers.elb_client",
-            new=ELB(self.set_mocked_audit_info()),
+            new=ELB(set_mocked_aws_audit_info([AWS_REGION_EU_WEST_1])),
         ):
             # Test Check
             from prowler.providers.aws.services.shield.shield_advanced_protection_in_classic_load_balancers.shield_advanced_protection_in_classic_load_balancers import (
@@ -190,7 +159,7 @@ class Test_shield_advanced_protection_in_classic_load_balancers:
             result = check.execute()
 
             assert len(result) == 1
-            assert result[0].region == AWS_REGION
+            assert result[0].region == AWS_REGION_EU_WEST_1
             assert result[0].resource_id == elb_name
             assert result[0].resource_arn == elb_arn
             assert result[0].status == "FAIL"
@@ -203,8 +172,8 @@ class Test_shield_advanced_protection_in_classic_load_balancers:
     @mock_ec2
     def test_shield_disabled_elb_not_protected(self):
         # ELB Client
-        elb = client("elb", region_name=AWS_REGION)
-        ec2 = resource("ec2", region_name=AWS_REGION)
+        elb = client("elb", region_name=AWS_REGION_EU_WEST_1)
+        ec2 = resource("ec2", region_name=AWS_REGION_EU_WEST_1)
 
         security_group = ec2.create_security_group(
             GroupName="sg01", Description="Test security group sg01"
@@ -216,16 +185,16 @@ class Test_shield_advanced_protection_in_classic_load_balancers:
                 {"Protocol": "tcp", "LoadBalancerPort": 80, "InstancePort": 8080},
                 {"Protocol": "http", "LoadBalancerPort": 81, "InstancePort": 9000},
             ],
-            AvailabilityZones=[f"{AWS_REGION}a"],
+            AvailabilityZones=[f"{AWS_REGION_EU_WEST_1}a"],
             Scheme="internet-facing",
             SecurityGroups=[security_group.id],
         )
-        _ = f"arn:aws:elasticloadbalancing:{AWS_REGION}:{DEFAULT_ACCOUNT_ID}:loadbalancer/{elb_name}"
+        _ = f"arn:aws:elasticloadbalancing:{AWS_REGION_EU_WEST_1}:{DEFAULT_ACCOUNT_ID}:loadbalancer/{elb_name}"
 
         # Shield Client
         shield_client = mock.MagicMock
         shield_client.enabled = False
-        shield_client.region = AWS_REGION
+        shield_client.region = AWS_REGION_EU_WEST_1
         shield_client.protections = {}
 
         from prowler.providers.aws.services.elb.elb_service import ELB
@@ -235,10 +204,10 @@ class Test_shield_advanced_protection_in_classic_load_balancers:
             new=shield_client,
         ), mock.patch(
             "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
-            new=self.set_mocked_audit_info(),
+            new=set_mocked_aws_audit_info([AWS_REGION_EU_WEST_1]),
         ), mock.patch(
             "prowler.providers.aws.services.shield.shield_advanced_protection_in_classic_load_balancers.shield_advanced_protection_in_classic_load_balancers.elb_client",
-            new=ELB(self.set_mocked_audit_info()),
+            new=ELB(set_mocked_aws_audit_info([AWS_REGION_EU_WEST_1])),
         ):
             # Test Check
             from prowler.providers.aws.services.shield.shield_advanced_protection_in_classic_load_balancers.shield_advanced_protection_in_classic_load_balancers import (
