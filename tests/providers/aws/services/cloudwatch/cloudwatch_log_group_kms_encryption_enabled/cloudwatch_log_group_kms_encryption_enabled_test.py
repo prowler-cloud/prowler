3d19e89710d81e5483f52091b9cd339ad439ae11
Pepe Fagoaga <pepe@verica.io>
2023-12-05 10:04:45 +0100

    test(audit_info): refactor cloudwatch (#3116)

--- tests/providers/aws/services/cloudwatch/cloudwatch_log_group_kms_encryption_enabled/cloudwatch_log_group_kms_encryption_enabled_test.py
+++ tests/providers/aws/services/cloudwatch/cloudwatch_log_group_kms_encryption_enabled/cloudwatch_log_group_kms_encryption_enabled_test.py
@@ -1,51 +1,22 @@
 from unittest import mock
 
-from boto3 import client, session
+from boto3 import client
 from moto import mock_logs
 
-from prowler.providers.aws.lib.audit_info.models import AWS_Audit_Info
-from prowler.providers.common.models import Audit_Metadata
-
-AWS_REGION = "us-east-1"
-AWS_ACCOUNT_NUMBER = "123456789012"
+from tests.providers.aws.audit_info_utils import (
+    AWS_REGION_EU_WEST_1,
+    AWS_REGION_US_EAST_1,
+    set_mocked_aws_audit_info,
+)
 
 
 class Test_cloudwatch_log_group_kms_encryption_enabled:
-    def set_mocked_audit_info(self):
-        audit_info = AWS_Audit_Info(
-            session_config=None,
-            original_session=None,
-            audit_session=session.Session(
-                profile_name=None,
-                botocore_session=None,
-            ),
-            audited_account=AWS_ACCOUNT_NUMBER,
-            audited_account_arn=f"arn:aws:iam::{AWS_ACCOUNT_NUMBER}:root",
-            audited_user_id=None,
-            audited_partition="aws",
-            audited_identity_arn=None,
-            profile=None,
-            profile_region=None,
-            credentials=None,
-            assumed_role_info=None,
-            audited_regions=["us-east-1", "eu-west-1"],
-            organizations_metadata=None,
-            audit_resources=None,
-            mfa_enabled=False,
-            audit_metadata=Audit_Metadata(
-                services_scanned=0,
-                expected_checks=[],
-                completed_checks=0,
-                audit_progress=0,
-            ),
-        )
-
-        return audit_info
-
     def test_cloudwatch_no_log_groups(self):
         from prowler.providers.aws.services.cloudwatch.cloudwatch_service import Logs
 
-        current_audit_info = self.set_mocked_audit_info()
+        current_audit_info = set_mocked_aws_audit_info(
+            [AWS_REGION_EU_WEST_1, AWS_REGION_US_EAST_1]
+        )
 
         from prowler.providers.common.models import Audit_Metadata
 
@@ -77,7 +48,7 @@ class Test_cloudwatch_log_group_kms_encryption_enabled:
     @mock_logs
     def test_cloudwatch_log_group_without_kms_key(self):
         # Generate Logs Client
-        logs_client = client("logs", region_name=AWS_REGION)
+        logs_client = client("logs", region_name=AWS_REGION_US_EAST_1)
         # Request Logs group
         logs_client.create_log_group(
             logGroupName="test",
@@ -85,7 +56,9 @@ class Test_cloudwatch_log_group_kms_encryption_enabled:
 
         from prowler.providers.aws.services.cloudwatch.cloudwatch_service import Logs
 
-        current_audit_info = self.set_mocked_audit_info()
+        current_audit_info = set_mocked_aws_audit_info(
+            [AWS_REGION_EU_WEST_1, AWS_REGION_US_EAST_1]
+        )
 
         from prowler.providers.common.models import Audit_Metadata
 
@@ -123,13 +96,15 @@ class Test_cloudwatch_log_group_kms_encryption_enabled:
     @mock_logs
     def test_cloudwatch_log_group_with_kms_key(self):
         # Generate Logs Client
-        logs_client = client("logs", region_name=AWS_REGION)
+        logs_client = client("logs", region_name=AWS_REGION_US_EAST_1)
         # Request Logs group
         logs_client.create_log_group(logGroupName="test", kmsKeyId="test_kms_id")
 
         from prowler.providers.aws.services.cloudwatch.cloudwatch_service import Logs
 
-        current_audit_info = self.set_mocked_audit_info()
+        current_audit_info = set_mocked_aws_audit_info(
+            [AWS_REGION_EU_WEST_1, AWS_REGION_US_EAST_1]
+        )
 
         from prowler.providers.common.models import Audit_Metadata
 
