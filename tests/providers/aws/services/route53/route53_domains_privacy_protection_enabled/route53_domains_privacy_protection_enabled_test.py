6ff864fc0405d50bcbf37f0ea4a8bca46aaec514
Nacho Rivera <nachor1992@gmail.com>
2023-12-05 12:28:12 +0100

    test(audit_info): refactor route53 (#3141)

--- tests/providers/aws/services/route53/route53_domains_privacy_protection_enabled/route53_domains_privacy_protection_enabled_test.py
+++ tests/providers/aws/services/route53/route53_domains_privacy_protection_enabled/route53_domains_privacy_protection_enabled_test.py
@@ -1,8 +1,7 @@
 from unittest import mock
 
 from prowler.providers.aws.services.route53.route53_service import Domain
-
-AWS_REGION = "us-east-1"
+from tests.providers.aws.audit_info_utils import AWS_REGION_US_EAST_1
 
 
 class Test_route53_domains_privacy_protection_enabled:
@@ -29,7 +28,7 @@ class Test_route53_domains_privacy_protection_enabled:
         domain_name = "test-domain.com"
         route53domains.domains = {
             domain_name: Domain(
-                name=domain_name, region=AWS_REGION, admin_privacy=False
+                name=domain_name, region=AWS_REGION_US_EAST_1, admin_privacy=False
             )
         }
 
@@ -47,7 +46,7 @@ class Test_route53_domains_privacy_protection_enabled:
 
             assert len(result) == 1
             assert result[0].resource_id == domain_name
-            assert result[0].region == AWS_REGION
+            assert result[0].region == AWS_REGION_US_EAST_1
             assert result[0].status == "FAIL"
             assert (
                 result[0].status_extended
@@ -58,7 +57,9 @@ class Test_route53_domains_privacy_protection_enabled:
         route53domains = mock.MagicMock
         domain_name = "test-domain.com"
         route53domains.domains = {
-            domain_name: Domain(name=domain_name, region=AWS_REGION, admin_privacy=True)
+            domain_name: Domain(
+                name=domain_name, region=AWS_REGION_US_EAST_1, admin_privacy=True
+            )
         }
 
         with mock.patch(
@@ -75,7 +76,7 @@ class Test_route53_domains_privacy_protection_enabled:
 
             assert len(result) == 1
             assert result[0].resource_id == domain_name
-            assert result[0].region == AWS_REGION
+            assert result[0].region == AWS_REGION_US_EAST_1
             assert result[0].status == "PASS"
             assert (
                 result[0].status_extended
