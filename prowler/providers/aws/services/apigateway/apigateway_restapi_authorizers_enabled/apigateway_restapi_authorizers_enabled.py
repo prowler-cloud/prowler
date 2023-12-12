37e7a7c6fae0889dcf0318dcd9eebee79c4f841b
n4ch04 <nachor1992@gmail.com>
2023-12-12 10:13:56 +0100

    fix(apigw_restapi_auth check): add method auth testing

--- prowler/providers/aws/services/apigateway/apigateway_restapi_authorizers_enabled/apigateway_restapi_authorizers_enabled.py
+++ prowler/providers/aws/services/apigateway/apigateway_restapi_authorizers_enabled/apigateway_restapi_authorizers_enabled.py
@@ -13,12 +13,28 @@ class apigateway_restapi_authorizers_enabled(Check):
             report.resource_id = rest_api.name
             report.resource_arn = rest_api.arn
             report.resource_tags = rest_api.tags
+            report.status = "FAIL"
+            report.status_extended = f"API Gateway {rest_api.name} ID {rest_api.id} does not have an authorizer configured at api or methods level."
             if rest_api.authorizer:
                 report.status = "PASS"
-                report.status_extended = f"API Gateway {rest_api.name} ID {rest_api.id} has an authorizer configured."
+                report.status_extended = f"API Gateway {rest_api.name} ID {rest_api.id} has an authorizer configured at api or methods scope."
             else:
-                report.status = "FAIL"
-                report.status_extended = f"API Gateway {rest_api.name} ID {rest_api.id} does not have an authorizer configured."
+                # we want to know if api has not authorizers and all the resources don't have methods configured
+                api_resources_with_all_methods_authorized = False
+                for resource in rest_api.resources:
+                    # if the resource has methods test if they have all configured authorizer
+                    if resource.resource_methods:
+                        if "NONE" not in resource.resource_methods.values():
+                            api_resources_with_all_methods_authorized = True
+                        else:
+                            # with only one unauthorized method -> return FAIL
+                            api_resources_with_all_methods_authorized = False
+                            break
+
+                if api_resources_with_all_methods_authorized:
+                    report.status = "PASS"
+                    report.status_extended = f"API Gateway {rest_api.name} ID {rest_api.id} has an authorizer configured at api or methods level."
+
             findings.append(report)
 
         return findings
