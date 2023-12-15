934e7e2d2fec84ac789909e8021562694791a3a5
Pepe Fagoaga <pepe@verica.io>
2023-12-15 13:49:47 +0100

    fix(iam): Handle NoSuchEntity

--- prowler/providers/aws/services/iam/iam_service.py
+++ prowler/providers/aws/services/iam/iam_service.py
@@ -494,6 +494,16 @@ class IAM(AWSService):
                                     document=inline_group_policy_doc,
                                 )
                             )
+                        except ClientError as error:
+                            if error.response["Error"]["Code"] == "NoSuchEntity":
+                                logger.warning(
+                                    f"{self.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+                                )
+                            else:
+                                logger.error(
+                                    f"{self.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+                                )
+
                         except Exception as error:
                             logger.error(
                                 f"{self.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
