1f139f79309a390e04d1a4c7a3586f9cc4261066
basapathitarun <101045826+basapathitarun@users.noreply.github.com>
2024-01-11 14:03:13 +0530

    Update __main__.py

--- prowler/__main__.py
+++ prowler/__main__.py
@@ -70,7 +70,7 @@ def prowler():
     checks_file = args.checks_file
     checks_folder = args.checks_folder
     severities = args.severity
-    compliance_framework = args.compliance
+    compliance_framework = 'cis_1.4_aws'
     custom_checks_metadata_file = args.custom_checks_metadata_file
 
     if not args.no_banner:
