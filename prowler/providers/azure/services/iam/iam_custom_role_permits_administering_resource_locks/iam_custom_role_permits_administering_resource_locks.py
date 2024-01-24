749ec75d43ef7ac60c343fd9f7d66c5004ede7be
pedrooot <pedromarting3@gmail.com>
2024-01-24 14:01:45 +0100

    feat(azure_checks) adds new check iam_custom_role_permits_administering_resource_locks

---
+++ prowler/providers/azure/services/iam/iam_custom_role_permits_administering_resource_locks/iam_custom_role_permits_administering_resource_locks.py
@@ -0,0 +1,31 @@
+from re import search
+
+from prowler.lib.check.models import Check, Check_Report_Azure
+from prowler.providers.azure.services.iam.iam_client import iam_client
+
+
+class iam_custom_role_permits_administering_resource_locks(Check):
+    def execute(self) -> Check_Report_Azure:
+        findings = []
+        for subscription, roles in iam_client.roles.items():
+            roles = [role for role in roles if role.type == "CustomRole"]
+            for role in roles:
+                report = Check_Report_Azure(self.metadata())
+                report.subscription = subscription
+                report.resource_id = role.id
+                report.resource_name = role.name
+                for scope in role.assignable_scopes:
+                    if search("^/.*", scope):
+                        for permission_item in role.permissions:
+                            for action in permission_item.actions:
+                                if search("^Microsoft.Authorization/locks/.*", action):
+                                    report.status = "PASS"
+                                    report.status_extended = f"Role {role.name} from subscription {subscription} has permission to administer resource locks."
+                                    break
+                                else:
+                                    report.status = "FAIL"
+                                    report.status_extended = f"Role {role.name} from subscription {subscription} has no permission to administer resource locks."
+                                    break
+
+                findings.append(report)
+        return findings
