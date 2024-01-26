13ee3a1be47c7c31550db63230a3ac01a5372132
puchy22 <rubendltv22@gmail.com>
2024-01-26 12:03:50 +0100

    fix(defender): Only checks default workspace and improve variable names

--- prowler/providers/azure/services/defender/defender_auto_provisioning_log_analytics_agent_vms_on/defender_auto_provisioning_log_analytics_agent_vms_on.py
+++ prowler/providers/azure/services/defender/defender_auto_provisioning_log_analytics_agent_vms_on/defender_auto_provisioning_log_analytics_agent_vms_on.py
@@ -6,18 +6,17 @@ class defender_auto_provisioning_log_analytics_agent_vms_on(Check):
     def execute(self) -> Check_Report_Azure:
         findings = []
 
-        for subscription, auto_provisioning_settings in defender_client.auto_provisioning_settings.items():
+        for subscription_name, auto_provisioning_settings in defender_client.auto_provisioning_settings.items():
             report = Check_Report_Azure(self.metadata())
             report.status = "PASS"
-            report.subscription = subscription
-            report.resource_name = "Defender Auto Provisioning Log Analytics Agents On"
+            report.subscription = subscription_name
+            report.resource_name = auto_provisioning_settings["default"].resource_name
             report.resource_id = auto_provisioning_settings["default"].resource_id
-            report.status_extended = f"Defender Auto Provisioning Log Analytics Agents from subscription {subscription} is set to ON."
+            report.status_extended = f"Defender Auto Provisioning Log Analytics Agents from subscription {subscription_name} is set to ON."
 
-            for ap in auto_provisioning_settings:
-                if auto_provisioning_settings[ap].auto_provision != "On":
-                    report.status = "FAIL"
-                    report.status_extended = f"Defender Auto Provisioning Log Analytics Agents from subscription {subscription} is set to OFF."
+            if auto_provisioning_settings["default"].auto_provision != "On":
+                report.status = "FAIL"
+                report.status_extended = f"Defender Auto Provisioning Log Analytics Agents from subscription {subscription_name} is set to OFF."
 
             findings.append(report)
 
