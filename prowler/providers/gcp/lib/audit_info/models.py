83c15b53f043a817f0d366f73bb22b399909f775
pedrooot <pedromarting3@gmail.com>
2024-01-26 12:31:09 +0100

    feat(azure): Remove all unnecessary init methods in @dataclass

--- prowler/providers/gcp/lib/audit_info/models.py
+++ prowler/providers/gcp/lib/audit_info/models.py
@@ -12,19 +12,3 @@ class GCP_Audit_Info:
     audit_resources: Optional[Any]
     audit_metadata: Optional[Any]
     audit_config: Optional[dict]
-
-    def __init__(
-        self,
-        credentials,
-        default_project_id,
-        project_ids,
-        audit_metadata,
-        audit_resources,
-        audit_config,
-    ):
-        self.credentials = credentials
-        self.default_project_id = default_project_id
-        self.project_ids = project_ids
-        self.audit_metadata = audit_metadata
-        self.audit_resources = audit_resources
-        self.audit_config = audit_config
