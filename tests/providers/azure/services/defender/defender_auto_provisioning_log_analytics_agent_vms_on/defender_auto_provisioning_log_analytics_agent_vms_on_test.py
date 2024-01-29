91980f44b8320f60f190a286d2d14d4a04a76235
puchy22 <rubendltv22@gmail.com>
2024-01-29 09:38:17 +0100

    test(defender) Add new test for Auto Provisioning Log to check that it works with multiple workspaces

--- tests/providers/azure/services/defender/defender_auto_provisioning_log_analytics_agent_vms_on/defender_auto_provisioning_log_analytics_agent_vms_on_test.py
+++ tests/providers/azure/services/defender/defender_auto_provisioning_log_analytics_agent_vms_on/defender_auto_provisioning_log_analytics_agent_vms_on_test.py
@@ -1,9 +1,11 @@
 from unittest import mock
 from uuid import uuid4
 
-from prowler.providers.azure.services.defender.defender_service import AutoProvisioningSetting
-
+from prowler.providers.azure.services.defender.defender_service import (
+    AutoProvisioningSetting,
+)
 from tests.providers.azure.azure_fixtures import AZURE_SUSCRIPTION
+
 
 class Test_defender_auto_provisioning_log_analytics_agent_vms_on:
     def test_defender_no_app_services(self):
@@ -49,7 +51,8 @@ class Test_defender_auto_provisioning_log_analytics_agent_vms_on:
             assert len(result) == 1
             assert result[0].status == "FAIL"
             assert (
-                result[0].status_extended == f"Defender Auto Provisioning Log Analytics Agents from subscription {AZURE_SUSCRIPTION} is set to OFF."
+                result[0].status_extended
+                == f"Defender Auto Provisioning Log Analytics Agents from subscription {AZURE_SUSCRIPTION} is set to OFF."
             )
             assert result[0].subscription == AZURE_SUSCRIPTION
             assert result[0].resource_name == "default"
@@ -82,8 +85,58 @@ class Test_defender_auto_provisioning_log_analytics_agent_vms_on:
             assert len(result) == 1
             assert result[0].status == "PASS"
             assert (
-                result[0].status_extended == f"Defender Auto Provisioning Log Analytics Agents from subscription {AZURE_SUSCRIPTION} is set to ON."
+                result[0].status_extended
+                == f"Defender Auto Provisioning Log Analytics Agents from subscription {AZURE_SUSCRIPTION} is set to ON."
             )
             assert result[0].subscription == AZURE_SUSCRIPTION
             assert result[0].resource_name == "default"
             assert result[0].resource_id == resource_id
+
+    def test_defender_auto_provisioning_log_analytics_on_and_off(self):
+        resource_id = str(uuid4())
+        defender_client = mock.MagicMock
+        defender_client.auto_provisioning_settings = {
+            AZURE_SUSCRIPTION: {
+                "default": AutoProvisioningSetting(
+                    resource_id=resource_id,
+                    resource_name="default",
+                    auto_provision="On",
+                    resource_type="Defender",
+                ),
+                "default2": AutoProvisioningSetting(
+                    resource_id=resource_id,
+                    resource_name="default2",
+                    auto_provision="Off",
+                    resource_type="Defender",
+                ),
+            }
+        }
+
+        with mock.patch(
+            "prowler.providers.azure.services.defender.defender_auto_provisioning_log_analytics_agent_vms_on.defender_auto_provisioning_log_analytics_agent_vms_on.defender_client",
+            new=defender_client,
+        ):
+            from prowler.providers.azure.services.defender.defender_auto_provisioning_log_analytics_agent_vms_on.defender_auto_provisioning_log_analytics_agent_vms_on import (
+                defender_auto_provisioning_log_analytics_agent_vms_on,
+            )
+
+            check = defender_auto_provisioning_log_analytics_agent_vms_on()
+            result = check.execute()
+            assert len(result) == 2
+            assert result[0].status == "PASS"
+            assert (
+                result[0].status_extended
+                == f"Defender Auto Provisioning Log Analytics Agents from subscription {AZURE_SUSCRIPTION} is set to ON."
+            )
+            assert result[0].subscription == AZURE_SUSCRIPTION
+            assert result[0].resource_name == "default"
+            assert result[0].resource_id == resource_id
+
+            assert result[1].status == "FAIL"
+            assert (
+                result[1].status_extended
+                == f"Defender Auto Provisioning Log Analytics Agents from subscription {AZURE_SUSCRIPTION} is set to OFF."
+            )
+            assert result[1].subscription == AZURE_SUSCRIPTION
+            assert result[1].resource_name == "default2"
+            assert result[1].resource_id == resource_id
