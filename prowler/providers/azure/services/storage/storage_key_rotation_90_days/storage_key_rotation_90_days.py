cffa04cea22f1a5db536b1039a71985c5b1aadaf
pedrooot <pedromarting3@gmail.com>
2024-01-29 11:09:02 +0100

    resolve comments

--- prowler/providers/azure/services/storage/storage_key_rotation_90_days/storage_key_rotation_90_days.py
+++ prowler/providers/azure/services/storage/storage_key_rotation_90_days/storage_key_rotation_90_days.py
@@ -9,18 +9,18 @@ class storage_key_rotation_90_days(Check):
             for storage_account in storage_accounts:
                 report = Check_Report_Azure(self.metadata())
                 report.subscription = subscription
-                report.status = "PASS"
-                report.status_extended = f"Storage account {storage_account.name} from subscription {subscription} has a key expiration period of {storage_account.key_expiration_period_in_days} days."
                 report.resource_name = storage_account.name
                 report.resource_id = storage_account.id
-                if storage_account.key_expiration_period_in_days is None:
+                if not storage_account.key_expiration_period_in_days:
                     report.status = "FAIL"
                     report.status_extended = f"Storage account {storage_account.name} from subscription {subscription} has no key expiration period set."
                 else:
                     if storage_account.key_expiration_period_in_days > 90:
                         report.status = "FAIL"
                         report.status_extended = f"Storage account {storage_account.name} from subscription {subscription} has an invalid key expiration period of {storage_account.key_expiration_period_in_days} days."
-
+                    else:
+                        report.status = "PASS"
+                        report.status_extended = f"Storage account {storage_account.name} from subscription {subscription} has a key expiration period of {storage_account.key_expiration_period_in_days} days."
                 findings.append(report)
 
         return findings
