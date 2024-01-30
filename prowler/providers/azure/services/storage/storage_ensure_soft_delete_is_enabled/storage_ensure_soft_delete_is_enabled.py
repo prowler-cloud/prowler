a9c3e60282189b862eb4c2e69312e7f4132d79b9
pedrooot <pedromarting3@gmail.com>
2024-01-30 11:26:19 +0100

    feat(azure): start implementing new check storage_ensure_soft_delete_is_enabled

---
+++ prowler/providers/azure/services/storage/storage_ensure_soft_delete_is_enabled/storage_ensure_soft_delete_is_enabled.py
@@ -0,0 +1,23 @@
+from prowler.lib.check.models import Check, Check_Report_Azure
+from prowler.providers.azure.services.storage.storage_client import storage_client
+
+
+class storage_ensure_soft_delete_is_enabled(Check):
+    def execute(self) -> Check_Report_Azure:
+        findings = []
+        for subscription, storage_accounts in storage_client.storage_accounts.items():
+            for storage_account in storage_accounts:
+                report = Check_Report_Azure(self.metadata())
+                report.subscription = subscription
+                report.resource_name = storage_account.name
+                report.resource_id = storage_account.id
+                print("oka")
+                if storage_account.private_endpoint_connections:
+                    report.status = "PASS"
+                    report.status_extended = f"Storage account {storage_account.name} from subscription {subscription} has private endpoint connections."
+                else:
+                    report.status = "FAIL"
+                    report.status_extended = f"Storage account {storage_account.name} from subscription {subscription} does not have private endpoint connections."
+                findings.append(report)
+
+        return findings
--- prowler/providers/azure/services/storage/storage_service.py
+++ prowler/providers/azure/services/storage/storage_service.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from azure.mgmt.storage import StorageManagementClient
 from azure.mgmt.storage.v2022_09_01.models import NetworkRuleSet
 from azure.mgmt.storage.v2023_01_01.models import PrivateEndpointConnection
+from azure.storage.common.models import DeleteRetentionPolicy
 
 from prowler.lib.logger import logger
 from prowler.providers.azure.lib.service.service import AzureService
@@ -13,6 +14,7 @@ class Storage(AzureService):
     def __init__(self, audit_info):
         super().__init__(StorageManagementClient, audit_info)
         self.storage_accounts = self.__get_storage_accounts__()
+        self.blob_properties = self.__get_blob_properties__()
 
     def __get_storage_accounts__(self):
         logger.info("Storage - Getting storage accounts...")
@@ -22,10 +24,17 @@ class Storage(AzureService):
                 storage_accounts.update({subscription: []})
                 storage_accounts_list = client.storage_accounts.list()
                 for storage_account in storage_accounts_list:
+                    parts = storage_account.id.split("/")
+                    if "resourceGroups" in parts:
+                        resouce_name_index = parts.index("resourceGroups") + 1
+                        resouce_group_name = parts[resouce_name_index]
+                    else:
+                        resouce_group_name = None
                     storage_accounts[subscription].append(
                         Storage_Account(
                             id=storage_account.id,
                             name=storage_account.name,
+                            resouce_group_name=resouce_group_name,
                             enable_https_traffic_only=storage_account.enable_https_traffic_only,
                             infrastructure_encryption=storage_account.encryption.require_infrastructure_encryption,
                             allow_blob_public_access=storage_account.allow_blob_public_access,
@@ -34,6 +43,7 @@ class Storage(AzureService):
                             minimum_tls_version=storage_account.minimum_tls_version,
                             private_endpoint_connections=storage_account.private_endpoint_connections,
                             key_expiration_period_in_days=storage_account.key_policy.key_expiration_period_in_days,
+                            blob_properties=self.__get_blob_properties__(),
                         )
                     )
             except Exception as error:
@@ -41,6 +51,30 @@ class Storage(AzureService):
                     f"Subscription name: {subscription} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
                 )
         return storage_accounts
+
+    def __get_blob_properties__(self):
+        logger.info("Storage - Getting blob properties...")
+        blob_properties = {}
+        for subscription, client in self.clients.items():
+            try:
+                blob_properties.update({subscription: []})
+                for account in self.storage_accounts:
+                    properties = client.blob_services.get_service_properties(
+                        account.resouce_group_name, account.name
+                    )
+                    blob_properties[subscription].append(
+                        Blob_Properties(
+                            id=properties.id,
+                            name=properties.name,
+                            default_service_version=properties.default_service_version,
+                            container_delete_retention_policy=properties.container_delete_retention_policy,
+                        )
+                    )
+            except Exception as error:
+                logger.error(
+                    f"Subscription name: {subscription} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+                )
+        return blob_properties
 
 
 @dataclass
@@ -55,3 +89,12 @@ class Storage_Account:
     minimum_tls_version: str
     private_endpoint_connections: PrivateEndpointConnection
     key_expiration_period_in_days: str
+    blob_properties: str = {}
+
+
+class Blob_Properties:
+    id: str
+    name: str
+    type: str
+    default_service_version: str
+    container_delete_retention_policy: DeleteRetentionPolicy
