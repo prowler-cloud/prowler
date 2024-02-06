2d5a7f4668a0e0491ff1e25dd695eb60e03afc9e
pedrooot <pedromarting3@gmail.com>
2024-02-06 13:28:11 +0100

    fix(azure): fix check sqlserver_auditing_retention_90_days

--- prowler/providers/azure/services/sqlserver/sqlserver_auditing_retention_90_days/sqlserver_auditing_retention_90_days.py
+++ prowler/providers/azure/services/sqlserver/sqlserver_auditing_retention_90_days/sqlserver_auditing_retention_90_days.py
@@ -7,27 +7,33 @@ class sqlserver_auditing_retention_90_days(Check):
         findings = []
         for subscription, sql_servers in sqlserver_client.sql_servers.items():
             for sql_server in sql_servers:
-                report = Check_Report_Azure(self.metadata())
-                report.subscription = subscription
-                report.resource_name = sql_server.name
-                report.resource_id = sql_server.id
-                has_failed = False
-                for policy in sql_server.auditing_policies:
-                    if has_failed:
-                        break
-                    if policy.state == "Enabled":
-                        if policy.retention_days <= 90:
-                            report.status = "FAIL"
-                            report.status_extended = f"SQL Server {sql_server.name} from subscription {subscription} has auditing retention less than 91 days."
-                            has_failed = True
+                auditing_policies = (
+                    sql_server.auditing_policies
+                    if sql_server.auditing_policies is not None
+                    else []
+                )
+                if len(auditing_policies) > 0:
+                    report = Check_Report_Azure(self.metadata())
+                    report.subscription = subscription
+                    report.resource_name = sql_server.name
+                    report.resource_id = sql_server.id
+                    has_failed = False
+                    for policy in sql_server.auditing_policies:
+                        if has_failed:
+                            break
+                        if policy.state == "Enabled":
+                            if policy.retention_days <= 90:
+                                report.status = "FAIL"
+                                report.status_extended = f"SQL Server {sql_server.name} from subscription {subscription} has auditing retention less than 91 days."
+                                has_failed = True
+                            else:
+                                report.status = "PASS"
+                                report.status_extended = f"SQL Server {sql_server.name} from subscription {subscription} has auditing retention greater than 90 days."
                         else:
-                            report.status = "PASS"
-                            report.status_extended = f"SQL Server {sql_server.name} from subscription {subscription} has auditing retention greater than 90 days."
-                    else:
-                        report.status = "FAIL"
-                        report.status_extended = f"SQL Server {sql_server.name} from subscription {subscription} has auditing disabled."
-                        has_failed = True
+                            report.status = "FAIL"
+                            report.status_extended = f"SQL Server {sql_server.name} from subscription {subscription} has auditing disabled."
+                            has_failed = True
 
-                findings.append(report)
+                    findings.append(report)
 
         return findings
