639e1446ca744a452f1d117885f0b7e9a71d9489
pedrooot <pedromarting3@gmail.com>
2024-02-06 17:01:17 +0100

    fix(azure): fix check sqlserver_auditing_retention_90_days

--- prowler/providers/azure/services/sqlserver/sqlserver_auditing_retention_90_days/sqlserver_auditing_retention_90_days.py
+++ prowler/providers/azure/services/sqlserver/sqlserver_auditing_retention_90_days/sqlserver_auditing_retention_90_days.py
@@ -7,29 +7,29 @@ class sqlserver_auditing_retention_90_days(Check):
         findings = []
         for subscription, sql_servers in sqlserver_client.sql_servers.items():
             for sql_server in sql_servers:
-                auditing_policies_list = list(sql_server.auditing_policies)
-                if auditing_policies_list != []:
-                    report = Check_Report_Azure(self.metadata())
-                    report.subscription = subscription
-                    report.resource_name = sql_server.name
-                    report.resource_id = sql_server.id
-                    has_failed = False
-                    for policy in auditing_policies_list:
-                        if has_failed:
-                            break
-                        if policy.state == "Enabled":
-                            if policy.retention_days <= 90:
-                                report.status = "FAIL"
-                                report.status_extended = f"SQL Server {sql_server.name} from subscription {subscription} has auditing retention less than 91 days."
-                                has_failed = True
-                            else:
-                                report.status = "PASS"
-                                report.status_extended = f"SQL Server {sql_server.name} from subscription {subscription} has auditing retention greater than 90 days."
-                        else:
+                report = Check_Report_Azure(self.metadata())
+                report.subscription = subscription
+                report.resource_name = sql_server.name
+                report.resource_id = sql_server.id
+                has_failed = False
+                has_policy = False
+                for policy in sql_server.auditing_policies:
+                    has_policy = True
+                    if has_failed:
+                        break
+                    if policy.state == "Enabled":
+                        if policy.retention_days <= 90:
                             report.status = "FAIL"
-                            report.status_extended = f"SQL Server {sql_server.name} from subscription {subscription} has auditing disabled."
+                            report.status_extended = f"SQL Server {sql_server.name} from subscription {subscription} has auditing retention less than 91 days."
                             has_failed = True
-
+                        else:
+                            report.status = "PASS"
+                            report.status_extended = f"SQL Server {sql_server.name} from subscription {subscription} has auditing retention greater than 90 days."
+                    else:
+                        report.status = "FAIL"
+                        report.status_extended = f"SQL Server {sql_server.name} from subscription {subscription} has auditing disabled."
+                        has_failed = True
+                if has_policy:
                     findings.append(report)
 
         return findings
