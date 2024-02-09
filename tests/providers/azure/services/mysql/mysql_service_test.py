66721ac1076f1e98aab93156f22461ddcd3d762a
puchy22 <rubendltv22@gmail.com>
2024-02-09 13:22:12 +0100

    test(mysql): Add test for configurations attribute in the service

--- tests/providers/azure/services/mysql/mysql_service_test.py
+++ tests/providers/azure/services/mysql/mysql_service_test.py
@@ -1,6 +1,10 @@
 from unittest.mock import patch
 
-from prowler.providers.azure.services.mysql.mysql_service import MySQL, Server
+from prowler.providers.azure.services.mysql.mysql_service import (
+    Configuration,
+    MySQL,
+    Server,
+)
 from tests.providers.azure.azure_fixtures import (
     AZURE_SUBSCRIPTION,
     set_mocked_azure_audit_info,
@@ -14,8 +18,20 @@ def mock_mysql_get_servers(_):
                 resource_id="/subscriptions/resource_id",
                 location="location",
                 version="version",
-                ssl_enforcement="Enabled",
-                minimal_tls_version="TLS1_2",
+                resource_group="resource_group",
+            )
+        }
+    }
+
+
+def mock_mysql_get_configurations(_):
+    return {
+        AZURE_SUBSCRIPTION: {
+            "test": Configuration(
+                resource_id="/subscriptions/resource_id",
+                server_name="test",
+                description="description",
+                value="value",
             )
         }
     }
@@ -24,6 +40,10 @@ def mock_mysql_get_servers(_):
 @patch(
     "prowler.providers.azure.services.mysql.mysql_service.MySQL.__get_servers__",
     new=mock_mysql_get_servers,
+)
+@patch(
+    "prowler.providers.azure.services.mysql.mysql_service.MySQL.__get_configurations__",
+    new=mock_mysql_get_configurations,
 )
 class Test_MySQL_Service:
     def test__get_client__(self):
@@ -46,5 +66,20 @@ class Test_MySQL_Service:
         )
         assert mysql.servers[AZURE_SUBSCRIPTION]["test"].location == "location"
         assert mysql.servers[AZURE_SUBSCRIPTION]["test"].version == "version"
-        assert mysql.servers[AZURE_SUBSCRIPTION]["test"].ssl_enforcement == "Enabled"
-        assert mysql.servers[AZURE_SUBSCRIPTION]["test"].minimal_tls_version == "TLS1_2"
+        assert (
+            mysql.servers[AZURE_SUBSCRIPTION]["test"].resource_group == "resource_group"
+        )
+
+    def test__get_configurations__(self):
+        mysql = MySQL(set_mocked_azure_audit_info())
+        assert len(mysql.configurations) == 1
+        assert (
+            mysql.configurations[AZURE_SUBSCRIPTION]["test"].resource_id
+            == "/subscriptions/resource_id"
+        )
+        assert mysql.configurations[AZURE_SUBSCRIPTION]["test"].server_name == "test"
+        assert (
+            mysql.configurations[AZURE_SUBSCRIPTION]["test"].description
+            == "description"
+        )
+        assert mysql.configurations[AZURE_SUBSCRIPTION]["test"].value == "value"
