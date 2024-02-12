d9a2d384d2caaefea71a8686346bfcd27f494e51
puchy22 <rubendltv22@gmail.com>
2024-02-12 14:22:21 +0100

    feat(mysql): New check for ensure audit_log_enabled is activated

---
+++ prowler/providers/azure/services/mysql/mysql_flexible_server_audit_log_enabled/mysql_flexible_server_audit_log_enabled.py
@@ -0,0 +1,34 @@
+from prowler.lib.check.models import Check, Check_Report_Azure
+from prowler.providers.azure.services.mysql.mysql_client import mysql_client
+
+
+class mysql_flexible_server_audit_log_enabled(Check):
+    def execute(self) -> Check_Report_Azure:
+        findings = []
+
+        for (
+            subscription_name,
+            servers,
+        ) in mysql_client.flexible_servers.items():
+            for (
+                server_name,
+                server,
+            ) in servers.items():
+
+                if "audit_log_enabled" in server.configurations:
+                    report = Check_Report_Azure(self.metadata())
+                    report.status = "PASS"
+                    report.subscription = subscription_name
+                    report.resource_name = server_name
+                    report.resource_id = server.configurations[
+                        "audit_log_enabled"
+                    ].resource_id
+                    report.status_extended = f"Audit log is enabled for server {server_name} in subscription {subscription_name}."
+
+                    if server.configurations["audit_log_enabled"].value != "ON":
+                        report.status = "FAIL"
+                        report.status_extended = f"Audit log is disabled for server {server_name} in subscription {subscription_name}."
+
+                    findings.append(report)
+
+        return findings
