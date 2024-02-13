aad76d241db8b88fac9c4fad98a1bae83a5ece66
puchy22 <rubendltv22@gmail.com>
2024-02-13 10:02:09 +0100

    fix(mysql): Fix name of mysql service Header

--- prowler/providers/azure/services/mysql/mysql_service.py
+++ prowler/providers/azure/services/mysql/mysql_service.py
@@ -6,7 +6,7 @@ from prowler.lib.logger import logger
 from prowler.providers.azure.lib.service.service import AzureService
 
 
-########################## SQLServer
+########################## MySQL
 class MySQL(AzureService):
     def __init__(self, audit_info):
         super().__init__(MySQLManagementClient, audit_info)
