e3406a0778e628e6cbb8035f4515fa8f00807a9a
puchy22 <rubendltv22@gmail.com>
2024-02-13 11:07:45 +0100

    test(appinsights): Unit tests for appinsights service

---
+++ tests/providers/azure/services/appinsights/appinsights_service_test.py
@@ -0,0 +1,50 @@
+from unittest.mock import patch
+
+from prowler.providers.azure.services.appinsights.appinsights_service import (
+    AppInsights,
+    Component,
+)
+from tests.providers.azure.azure_fixtures import (
+    AZURE_SUBSCRIPTION,
+    set_mocked_azure_audit_info,
+)
+
+
+def mock_appinsights_get_components(_):
+    return {
+        AZURE_SUBSCRIPTION: {
+            "app_id-1": Component(
+                resource_id="/subscriptions/resource_id",
+                resource_name="AppInsightsTest",
+            )
+        }
+    }
+
+
+@patch(
+    "prowler.providers.azure.services.appinsights.appinsights_service.AppInsights.__get_components__",
+    new=mock_appinsights_get_components,
+)
+class Test_AppInsights_Service:
+    def test__get_client__(self):
+        app_insights = AppInsights(set_mocked_azure_audit_info())
+        assert (
+            app_insights.clients[AZURE_SUBSCRIPTION].__class__.__name__
+            == "ApplicationInsightsManagementClient"
+        )
+
+    def test__get_subscriptions__(self):
+        app_insights = AppInsights(set_mocked_azure_audit_info())
+        assert app_insights.subscriptions.__class__.__name__ == "dict"
+
+    def test__get_componentes(self):
+        appinsights = AppInsights(set_mocked_azure_audit_info())
+        assert len(appinsights.components) == 1
+        assert (
+            appinsights.components[AZURE_SUBSCRIPTION]["app_id-1"].resource_id
+            == "/subscriptions/resource_id"
+        )
+        assert (
+            appinsights.components[AZURE_SUBSCRIPTION]["app_id-1"].resource_name
+            == "AppInsightsTest"
+        )
