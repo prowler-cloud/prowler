0af9f8511139220703b0b64e3340ad2a5089bf55
pedrooot <pedromarting3@gmail.com>
2024-02-15 09:59:29 +0100

    refactor(azure): change network service

--- prowler/providers/azure/services/network/network_service.py
+++ prowler/providers/azure/services/network/network_service.py
@@ -1,9 +1,15 @@
 from dataclasses import dataclass
 
+import requests
+from azure.identity import DefaultAzureCredential
 from azure.mgmt.network import NetworkManagementClient
+from azure.mgmt.network.models import NetworkWatcher
 
 from prowler.lib.logger import logger
 from prowler.providers.azure.lib.service.service import AzureService
+
+credential = DefaultAzureCredential()
+token = credential.get_token("https://management.azure.com/.default").token
 
 
 ########################## SQLServer
@@ -19,13 +25,25 @@ class Network(AzureService):
             try:
                 security_groups.update({subscription: []})
                 security_groups_list = client.network_security_groups.list_all()
+                available_locations = {}
                 for security_group in security_groups_list:
+                    subscription_id = security_group.id.split("/")[2]
+                    if subscription_id not in available_locations:
+                        available_locations[subscription_id] = (
+                            self.__get_subscription_locations__(subscription_id)
+                        )
+                for security_group in security_groups_list:
+                    subscription_locations = available_locations[subscription_id]
                     security_groups[subscription].append(
                         SecurityGroup(
                             id=security_group.id,
                             name=security_group.name,
                             location=security_group.location,
                             security_rules=security_group.security_rules,
+                            network_watchers=self.__get_network_watchers__(
+                                client, subscription
+                            ),
+                            subscription_locations=subscription_locations,
                         )
                     )
 
@@ -35,6 +53,28 @@ class Network(AzureService):
                 )
         return security_groups
 
+    def __get_network_watchers__(self, client, subscription):
+        logger.info("SQL Server - Getting Network Watchers...")
+        client = self.clients[subscription]
+        network_watchers = client.network_watchers.list_all()
+        return network_watchers
+
+    def __get_subscription_locations__(self, subscription_id):
+        logger.info("SQL Server - Getting Subscription Locations...")
+        subscription_locations = []
+        url = f"https://management.azure.com/subscriptions/{subscription_id}/locations?api-version=2022-12-01"
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+        }
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            for location in data["value"]:
+                subscription_locations.append(location["name"])
+
+        return subscription_locations
+
 
 @dataclass
 class SecurityGroup:
@@ -42,3 +82,5 @@ class SecurityGroup:
     name: str
     location: str
     security_rules: list
+    network_watchers: list[NetworkWatcher]
+    subscription_locations: list
