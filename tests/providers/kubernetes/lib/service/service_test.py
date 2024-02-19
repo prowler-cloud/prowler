f29cd0d8eaf5ea6cd32d7fb67980f11b10255766
Sergio Garcia <sergargar1@gmail.com>
2024-02-19 12:43:58 +0000

    solve comment

--- tests/providers/kubernetes/lib/service/service_test.py
+++ tests/providers/kubernetes/lib/service/service_test.py
@@ -1,29 +1,12 @@
 from kubernetes import client
 
-from prowler.providers.common.models import Audit_Metadata
-from prowler.providers.kubernetes.lib.audit_info.models import Kubernetes_Audit_Info
 from prowler.providers.kubernetes.lib.service.service import KubernetesService
+from tests.providers.kubernetes.audit_info_utils import set_mocked_audit_info
 
 
 class Test_KubernetesService:
-    # Mocked Audit Info
-    def set_mocked_audit_info(self):
-        audit_info = Kubernetes_Audit_Info(
-            api_client=client.ApiClient,
-            context=None,
-            audit_config=None,
-            audit_resources=[],
-            audit_metadata=Audit_Metadata(
-                services_scanned=0,
-                expected_checks=[],
-                completed_checks=0,
-                audit_progress=0,
-            ),
-        )
-        return audit_info
-
     def test_KubernetesService_init(self):
-        audit_info = self.set_mocked_audit_info()
+        audit_info = set_mocked_audit_info()
         service = KubernetesService(audit_info)
 
         assert service.context is None
