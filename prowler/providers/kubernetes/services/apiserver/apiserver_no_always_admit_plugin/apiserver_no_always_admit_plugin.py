e7ff70d62616e5a7b32522272b3c57d5de4c9e13
Sergio Garcia <sergargar1@gmail.com>
2024-02-22 09:40:44 +0000

    fix check

--- prowler/providers/kubernetes/services/apiserver/apiserver_no_always_admit_plugin/apiserver_no_always_admit_plugin.py
+++ prowler/providers/kubernetes/services/apiserver/apiserver_no_always_admit_plugin/apiserver_no_always_admit_plugin.py
@@ -16,12 +16,17 @@ class apiserver_no_always_admit_plugin(Check):
             report.status_extended = (
                 f"AlwaysAdmit admission control plugin is not set in pod {pod.name}."
             )
+            always_admit_plugin = True
             for container in pod.containers.values():
+                always_admit_plugin = True
                 for command in container.command:
                     if command.startswith("--enable-admission-plugins"):
                         if "AlwaysAdmit" in (command.split("=")[1]):
                             report.status = "FAIL"
                             report.status_extended = f"AlwaysAdmit admission control plugin is set in pod {pod.name}."
+                            always_admit_plugin = False
                             break
+                if not always_admit_plugin:
+                    break
             findings.append(report)
         return findings
