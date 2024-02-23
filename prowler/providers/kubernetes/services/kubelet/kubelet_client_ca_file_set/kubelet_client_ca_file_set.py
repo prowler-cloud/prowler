b811f557bd9463faab936559303a36af1b2060b4
Sergio Garcia <sergargar1@gmail.com>
2024-02-23 17:01:39 +0000

    solve comments

--- prowler/providers/kubernetes/services/kubelet/kubelet_client_ca_file_set/kubelet_client_ca_file_set.py
+++ prowler/providers/kubernetes/services/kubelet/kubelet_client_ca_file_set/kubelet_client_ca_file_set.py
@@ -6,17 +6,21 @@ class kubelet_client_ca_file_set(Check):
     def execute(self) -> Check_Report_Kubernetes:
         findings = []
         for cm in kubelet_client.kubelet_config_maps:
-            authentication = cm.kubelet_args.get("authentication", {})
+            authentication = cm.kubelet_args.get("authentication")
             report = Check_Report_Kubernetes(self.metadata())
             report.namespace = cm.namespace
             report.resource_name = cm.name
             report.resource_id = cm.uid
-            report.status = "FAIL"
-            report.status_extended = (
-                f"Kubelet is missing the client CA file in config file {cm.name}."
-            )
-            if "clientCAFile" in authentication.get("x509", {}):
-                report.status = "PASS"
-                report.status_extended = f"Kubelet has the client CA file configured appropriately in config file {cm.name}."
+            if not authentication:
+                report.status = "MANUAL"
+                report.status_extended = f"Kubelet does not have the argument `readOnlyPort` in config file {cm.name}, verify it in the node's arguments."
+            else:
+                report.status = "FAIL"
+                report.status_extended = (
+                    f"Kubelet is missing the client CA file in config file {cm.name}."
+                )
+                if "clientCAFile" in authentication.get("x509", {}):
+                    report.status = "PASS"
+                    report.status_extended = f"Kubelet has the client CA file configured appropriately in config file {cm.name}."
             findings.append(report)
         return findings
