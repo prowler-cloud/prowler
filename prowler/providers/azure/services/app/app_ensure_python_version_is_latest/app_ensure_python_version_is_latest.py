3321341459fec85ba2a24000be156745449a289f
puchy22 <rubendltv22@gmail.com>
2024-02-27 10:04:17 +0100

    chore(app): Change business logic checking python version

--- prowler/providers/azure/services/app/app_ensure_python_version_is_latest/app_ensure_python_version_is_latest.py
+++ prowler/providers/azure/services/app/app_ensure_python_version_is_latest/app_ensure_python_version_is_latest.py
@@ -15,18 +15,22 @@ class app_ensure_python_version_is_latest(Check):
 
                 if "python" in framework.lower():
                     report = Check_Report_Azure(self.metadata())
-                    report.status = "PASS"
+                    report.status = "FAIL"
                     report.subscription = subscription_name
                     report.resource_name = app_name
                     report.resource_id = app.resource_id
                     python_latest_version = app_client.audit_config.get(
                         "python_latest_version", "3.12"
                     )
-                    report.status_extended = f"Python version is set to {python_latest_version} for app '{app_name}' in subscription '{subscription_name}'."
+                    report.status_extended = f"Python version is '{framework}', the latest version that you could use is the '{python_latest_version}' version, for app '{app_name}' in subscription '{subscription_name}'."
 
-                    if python_latest_version not in framework:
-                        report.status = "FAIL"
-                        report.status_extended = f"Python version is not set to {python_latest_version} for app '{app_name}' in subscription '{subscription_name}'."
+                    if (
+                        python_latest_version in framework
+                        or getattr(app.configurations, "python_version", None)
+                        == python_latest_version
+                    ):
+                        report.status = "PASS"
+                        report.status_extended = f"Python version is set to '{python_latest_version}' for app '{app_name}' in subscription '{subscription_name}'."
 
                     findings.append(report)
 
