c4ab6e2e1cc4f98227a14381c1462c7f6e6a41b0
Sergio Garcia <sergargar1@gmail.com>
2024-02-28 10:59:58 +0100

    fix(gcp): remove Default Project ID requirement

--- prowler/providers/common/audit_info.py
+++ prowler/providers/common/audit_info.py
@@ -46,10 +46,8 @@ class Audit_Info:
         # Beautify audited profile, set "default" if there is no profile set
         try:
             getattr(audit_info.credentials, "_service_account_email")
-            profile = (
-                audit_info.credentials._service_account_email
-                if audit_info.credentials._service_account_email is not None
-                else "default"
+            profile = getattr(
+                audit_info.credentials, "_service_account_email", "default"
             )
         except AttributeError:
             profile = "default"
