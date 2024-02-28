c6d4a108d254929230c7168e7d3c954fa5b37fbf
Sergio Garcia <sergargar1@gmail.com>
2024-02-28 11:40:45 +0100

    solve comments

--- prowler/providers/common/audit_info.py
+++ prowler/providers/common/audit_info.py
@@ -44,13 +44,7 @@ class Audit_Info:
 
     def print_gcp_credentials(self, audit_info: GCP_Audit_Info):
         # Beautify audited profile, set "default" if there is no profile set
-        try:
-            getattr(audit_info.credentials, "_service_account_email")
-            profile = getattr(
-                audit_info.credentials, "_service_account_email", "default"
-            )
-        except AttributeError:
-            profile = "default"
+        profile = getattr(audit_info.credentials, "_service_account_email", "default")
 
         report = f"""
 This report is being generated using credentials below:
