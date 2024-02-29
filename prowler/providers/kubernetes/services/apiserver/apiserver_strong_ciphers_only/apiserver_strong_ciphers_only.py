97f927d9c1b38e19f37c3294b293bce95e87253d
Sergio Garcia <sergargar1@gmail.com>
2024-02-29 14:37:12 +0100

    chore(kubernetes): add strong ciphers config vars

--- prowler/providers/kubernetes/services/apiserver/apiserver_strong_ciphers_only/apiserver_strong_ciphers_only.py
+++ prowler/providers/kubernetes/services/apiserver/apiserver_strong_ciphers_only/apiserver_strong_ciphers_only.py
@@ -14,20 +14,22 @@ class apiserver_strong_ciphers_only(Check):
             report.resource_id = pod.uid
             report.status = "PASS"
             report.status_extended = f"API Server is configured with strong cryptographic ciphers in pod {pod.name}."
-            strong_ciphers_set = True
+            strong_ciphers_set = False
             for container in pod.containers.values():
-                strong_ciphers_set = True
+                strong_ciphers_set = False
                 # Check if strong ciphers are set in "--tls-cipher-suites"
                 for command in container.command:
                     if command.startswith("--tls-cipher-suites"):
-                        for cipher in command.split("=")[1].split(","):
-                            if cipher not in [
-                                "TLS_AES_128_GCM_SHA256",
-                                "TLS_AES_256_GCM_SHA384",
-                                "TLS_CHACHA20_POLY1305_SHA256",
-                            ]:
-                                strong_ciphers_set = False
-                                break
+                        if (
+                            command.split("=")[1]
+                            .split(",")
+                            .issubset(
+                                apiserver_client.audit_config.get(
+                                    "apiserver_strong_ciphers", []
+                                )
+                            )
+                        ):
+                            strong_ciphers_set = True
                 if not strong_ciphers_set:
                     break
 
