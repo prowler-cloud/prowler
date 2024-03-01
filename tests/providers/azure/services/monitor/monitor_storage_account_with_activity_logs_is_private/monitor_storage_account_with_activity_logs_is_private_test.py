3d8741306d11d055a3f00fb5c5ec32413b6b860f
Hugo Gálvez Ureña <hugogalvezu96@gmail.com>
2024-03-01 11:52:44 +0100

    fix(azure):account name as giagnostic setting attribute

--- tests/providers/azure/services/monitor/monitor_storage_account_with_activity_logs_is_private/monitor_storage_account_with_activity_logs_is_private_test.py
+++ tests/providers/azure/services/monitor/monitor_storage_account_with_activity_logs_is_private/monitor_storage_account_with_activity_logs_is_private_test.py
@@ -56,6 +56,7 @@ class Test_monitor_storage_account_with_activity_logs_is_private:
                         mock.MagicMock(category="Autoscale", enabled=False),
                     ],
                     storage_account_id="/subscriptions/1234a5-123a-123a-123a-1234567890ab/resourceGroups/rg/providers/Microsoft.Storage/storageAccounts/storageaccountname1",
+                    storage_account_name="storageaccountname1",
                 ),
                 DiagnosticSetting(
                     id="id2",
@@ -69,6 +70,7 @@ class Test_monitor_storage_account_with_activity_logs_is_private:
                         mock.MagicMock(category="Autoscale", enabled=False),
                     ],
                     storage_account_id="/subscriptions/1224a5-123a-123a-123a-1234567890ab/resourceGroups/rg/providers/Microsoft.Storage/storageAccounts/storageaccountname2",
+                    storage_account_name="storageaccountname2",
                 ),
             ]
         }
