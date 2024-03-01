14f09627cbe37a9bdb0489dcd894898d51050476
Pepe Fagoaga <pepe@prowler.com>
2024-03-01 09:32:41 +0100

    fix: add locations

--- prowler/providers/azure/lib/service/service.py
+++ prowler/providers/azure/lib/service/service.py
@@ -16,6 +16,7 @@ class AzureService:
         )
 
         self.subscriptions = provider.identity.subscriptions
+        # self.locations = provider.locations
         self.audit_config = provider.audit_config
 
     def __set_clients__(self, subscriptions, session, service, region_config):
