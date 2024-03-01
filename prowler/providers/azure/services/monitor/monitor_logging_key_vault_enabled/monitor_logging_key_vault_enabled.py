ca9cb087e2b4ea54f6533bcefb11e23935262332
Hugo Gálvez Ureña <hugogalvezu96@gmail.com>
2024-03-01 10:38:39 +0100

    5.1.5 check

--- prowler/providers/azure/services/monitor/monitor_logging_key_vault_enabled/monitor_logging_key_vault_enabled.py
+++ prowler/providers/azure/services/monitor/monitor_logging_key_vault_enabled/monitor_logging_key_vault_enabled.py
@@ -2,7 +2,7 @@ from prowler.lib.check.models import Check, Check_Report_Azure
 from prowler.providers.azure.services.monitor.monitor_client import monitor_client
 
 
-class monitor_diagnostic_setting_with_appropriate_categories(Check):
+class monitor_logging_key_vault_enabled(Check):
     def execute(self) -> Check_Report_Azure:
         findings = []
 
@@ -10,47 +10,22 @@ class monitor_diagnostic_setting_with_appropriate_categories(Check):
             subscription_name,
             diagnostic_settings,
         ) in monitor_client.diagnostics_settings.items():
-            report = Check_Report_Azure(self.metadata())
-            report.status = "FAIL"
-            report.subscription = subscription_name
-            report.resource_name = "Monitor"
-            report.resource_id = "Monitor"
-            report.status_extended = f"There are no diagnostic settings capturing appropiate categories in subscription {subscription_name}."
-            administrative_enabled = False
-            security_enabled = False
-            service_health_enabled = False
-            alert_enabled = False
+            if not diagnostic_settings:
+                report = Check_Report_Azure(self.metadata())
+                report.status = "PASS"
+                report.subscription = subscription_name
+                report.resource_name = "Monitor"
+                report.resource_id = "Monitor"
+                report.status_extended = f"Logging for Azure Key Vault is enabled in subscription {subscription_name} or not necessary."
+                findings.append(report)
             for diagnostic_setting in diagnostic_settings:
+                print(diagnostic_setting.type)
+                for log in diagnostic_setting.logs:
+                    print(log.category)
                 if diagnostic_setting.type == "Microsoft.KeyVault/vaults":
                     # Comprobacion de que archive to a storage account esté enabled
                     pass
-                for log in diagnostic_setting.logs:
-                    if log.category == "Administrative" and log.enabled:
-                        administrative_enabled = True
-                    if log.category == "Security" and log.enabled:
-                        security_enabled = True
-                    if log.category == "Alert" and log.enabled:
-                        service_health_enabled = True
-                    if log.category == "Policy" and log.enabled:
-                        alert_enabled = True
 
-                    if (
-                        administrative_enabled
-                        and security_enabled
-                        and service_health_enabled
-                        and alert_enabled
-                    ):
-                        report.status = "PASS"
-                        report.status_extended = f"There is at least one diagnostic setting capturing appropiate categories in subscription {subscription_name}."
-                        break
-                if (
-                    administrative_enabled
-                    and security_enabled
-                    and service_health_enabled
-                    and alert_enabled
-                ):
-                    break
-
-            findings.append(report)
+        findings.append(report)
 
         return findings
