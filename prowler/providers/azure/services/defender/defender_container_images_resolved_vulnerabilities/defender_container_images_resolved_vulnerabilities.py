18989e631b7defddcfba373f57448a7d20a28117
puchy22 <rubendltv22@gmail.com>
2024-03-04 13:40:58 +0100

    fix(defender): Change name, metadata and logic

--- prowler/providers/azure/services/defender/defender_container_images_resolved_vulnerabilities/defender_container_images_resolved_vulnerabilities.py
+++ prowler/providers/azure/services/defender/defender_container_images_resolved_vulnerabilities/defender_container_images_resolved_vulnerabilities.py
@@ -2,7 +2,7 @@ from prowler.lib.check.models import Check, Check_Report_Azure
 from prowler.providers.azure.services.defender.defender_client import defender_client
 
 
-class defender_container_images_vulnerabilities_scaned(Check):
+class defender_container_images_resolved_vulnerabilities(Check):
     def execute(self) -> Check_Report_Azure:
         findings = []
 
@@ -10,32 +10,38 @@ class defender_container_images_vulnerabilities_scaned(Check):
             subscription_name,
             assessments,
         ) in defender_client.assessments.items():
-            report = Check_Report_Azure(self.metadata())
-            report.status = "FAIL"
-            report.resource_id = "Microsoft.Security/assessments"
-            report.resource_name = "Microsoft.Security/assessments"
-            report.subscription = subscription_name
-            report.status_extended = f"Azure running container images should have vulnerabilities resolved (powered by Microsoft Defender Vulnerability Management) is not enabled in subscription '{subscription_name}'."
             if (
                 "Azure running container images should have vulnerabilities resolved (powered by Microsoft Defender Vulnerability Management)"
                 in assessments
+                and getattr(
+                    assessments[
+                        "Azure running container images should have vulnerabilities resolved (powered by Microsoft Defender Vulnerability Management)"
+                    ],
+                    "status",
+                    "NotApplicable",
+                )
+                != "NotApplicable"
             ):
+                report = Check_Report_Azure(self.metadata())
+                report.status = "PASS"
+                report.subscription = subscription_name
                 report.resource_name = assessments[
                     "Azure running container images should have vulnerabilities resolved (powered by Microsoft Defender Vulnerability Management)"
                 ].resource_name
                 report.resource_id = assessments[
                     "Azure running container images should have vulnerabilities resolved (powered by Microsoft Defender Vulnerability Management)"
                 ].resource_id
+                report.status_extended = f"Azure running container images do not have unresolved vulnerabilities in subscription '{subscription_name}'."
 
                 if (
                     assessments[
                         "Azure running container images should have vulnerabilities resolved (powered by Microsoft Defender Vulnerability Management)"
                     ].status
-                    != "Unhealthy"
+                    == "Unhealthy"
                 ):
-                    report.status = "PASS"
-                    report.status_extended = f"Azure running container images should have vulnerabilities resolved (powered by Microsoft Defender Vulnerability Management) is enabled in subscription '{subscription_name}'."
+                    report.status = "FAIL"
+                    report.status_extended = f"Azure running container images have unresolved vulnerabilities in subscription '{subscription_name}'."
 
-            findings.append(report)
+                findings.append(report)
 
         return findings
--- tests/providers/azure/services/defender/defender_container_images_resolved_vulnerabilities/defender_container_images_resolved_vulnerabilities_test.py
+++ tests/providers/azure/services/defender/defender_container_images_resolved_vulnerabilities/defender_container_images_resolved_vulnerabilities_test.py
@@ -5,20 +5,20 @@ from prowler.providers.azure.services.defender.defender_service import Assesment
 from tests.providers.azure.azure_fixtures import AZURE_SUBSCRIPTION
 
 
-class Test_defender_container_images_vulnerabilities_scaned:
+class Test_defender_container_images_resolved_vulnerabilities:
     def test_defender_no_subscriptions(self):
         defender_client = mock.MagicMock
         defender_client.assessments = {}
 
         with mock.patch(
-            "prowler.providers.azure.services.defender.defender_container_images_vulnerabilities_scaned.defender_container_images_vulnerabilities_scaned.defender_client",
+            "prowler.providers.azure.services.defender.defender_container_images_resolved_vulnerabilities.defender_container_images_resolved_vulnerabilities.defender_client",
             new=defender_client,
         ):
-            from prowler.providers.azure.services.defender.defender_container_images_vulnerabilities_scaned.defender_container_images_vulnerabilities_scaned import (
-                defender_container_images_vulnerabilities_scaned,
+            from prowler.providers.azure.services.defender.defender_container_images_resolved_vulnerabilities.defender_container_images_resolved_vulnerabilities import (
+                defender_container_images_resolved_vulnerabilities,
             )
 
-            check = defender_container_images_vulnerabilities_scaned()
+            check = defender_container_images_resolved_vulnerabilities()
             result = check.execute()
             assert len(result) == 0
 
@@ -27,24 +27,16 @@ class Test_defender_container_images_vulnerabilities_scaned:
         defender_client.assessments = {AZURE_SUBSCRIPTION: {}}
 
         with mock.patch(
-            "prowler.providers.azure.services.defender.defender_container_images_vulnerabilities_scaned.defender_container_images_vulnerabilities_scaned.defender_client",
+            "prowler.providers.azure.services.defender.defender_container_images_resolved_vulnerabilities.defender_container_images_resolved_vulnerabilities.defender_client",
             new=defender_client,
         ):
-            from prowler.providers.azure.services.defender.defender_container_images_vulnerabilities_scaned.defender_container_images_vulnerabilities_scaned import (
-                defender_container_images_vulnerabilities_scaned,
+            from prowler.providers.azure.services.defender.defender_container_images_resolved_vulnerabilities.defender_container_images_resolved_vulnerabilities import (
+                defender_container_images_resolved_vulnerabilities,
             )
 
-            check = defender_container_images_vulnerabilities_scaned()
+            check = defender_container_images_resolved_vulnerabilities()
             result = check.execute()
-            assert len(result) == 1
-            assert result[0].status == "FAIL"
-            assert result[0].resource_id == "Microsoft.Security/assessments"
-            assert result[0].resource_name == "Microsoft.Security/assessments"
-            assert result[0].subscription == AZURE_SUBSCRIPTION
-            assert (
-                result[0].status_extended
-                == f"Azure running container images should have vulnerabilities resolved (powered by Microsoft Defender Vulnerability Management) is not enabled in subscription '{AZURE_SUBSCRIPTION}'."
-            )
+            assert len(result) == 0
 
     def test_defender_subscription_no_assesment(self):
         defender_client = mock.MagicMock
@@ -59,24 +51,16 @@ class Test_defender_container_images_vulnerabilities_scaned:
         }
 
         with mock.patch(
-            "prowler.providers.azure.services.defender.defender_container_images_vulnerabilities_scaned.defender_container_images_vulnerabilities_scaned.defender_client",
+            "prowler.providers.azure.services.defender.defender_container_images_resolved_vulnerabilities.defender_container_images_resolved_vulnerabilities.defender_client",
             new=defender_client,
         ):
-            from prowler.providers.azure.services.defender.defender_container_images_vulnerabilities_scaned.defender_container_images_vulnerabilities_scaned import (
-                defender_container_images_vulnerabilities_scaned,
+            from prowler.providers.azure.services.defender.defender_container_images_resolved_vulnerabilities.defender_container_images_resolved_vulnerabilities import (
+                defender_container_images_resolved_vulnerabilities,
             )
 
-            check = defender_container_images_vulnerabilities_scaned()
+            check = defender_container_images_resolved_vulnerabilities()
             result = check.execute()
-            assert len(result) == 1
-            assert result[0].status == "FAIL"
-            assert result[0].resource_id == "Microsoft.Security/assessments"
-            assert result[0].resource_name == "Microsoft.Security/assessments"
-            assert result[0].subscription == AZURE_SUBSCRIPTION
-            assert (
-                result[0].status_extended
-                == f"Azure running container images should have vulnerabilities resolved (powered by Microsoft Defender Vulnerability Management) is not enabled in subscription '{AZURE_SUBSCRIPTION}'."
-            )
+            assert len(result) == 0
 
     def test_defender_subscription_assesment_unhealthy(self):
         defender_client = mock.MagicMock
@@ -91,14 +75,14 @@ class Test_defender_container_images_vulnerabilities_scaned:
         }
 
         with mock.patch(
-            "prowler.providers.azure.services.defender.defender_container_images_vulnerabilities_scaned.defender_container_images_vulnerabilities_scaned.defender_client",
+            "prowler.providers.azure.services.defender.defender_container_images_resolved_vulnerabilities.defender_container_images_resolved_vulnerabilities.defender_client",
             new=defender_client,
         ):
-            from prowler.providers.azure.services.defender.defender_container_images_vulnerabilities_scaned.defender_container_images_vulnerabilities_scaned import (
-                defender_container_images_vulnerabilities_scaned,
+            from prowler.providers.azure.services.defender.defender_container_images_resolved_vulnerabilities.defender_container_images_resolved_vulnerabilities import (
+                defender_container_images_resolved_vulnerabilities,
             )
 
-            check = defender_container_images_vulnerabilities_scaned()
+            check = defender_container_images_resolved_vulnerabilities()
             result = check.execute()
             assert len(result) == 1
             assert result[0].status == "FAIL"
@@ -117,7 +101,7 @@ class Test_defender_container_images_vulnerabilities_scaned:
             assert result[0].subscription == AZURE_SUBSCRIPTION
             assert (
                 result[0].status_extended
-                == f"Azure running container images should have vulnerabilities resolved (powered by Microsoft Defender Vulnerability Management) is not enabled in subscription '{AZURE_SUBSCRIPTION}'."
+                == f"Azure running container images have unresolved vulnerabilities in subscription '{AZURE_SUBSCRIPTION}'."
             )
 
     def test_defender_subscription_assesment_healthy(self):
@@ -133,14 +117,14 @@ class Test_defender_container_images_vulnerabilities_scaned:
         }
 
         with mock.patch(
-            "prowler.providers.azure.services.defender.defender_container_images_vulnerabilities_scaned.defender_container_images_vulnerabilities_scaned.defender_client",
+            "prowler.providers.azure.services.defender.defender_container_images_resolved_vulnerabilities.defender_container_images_resolved_vulnerabilities.defender_client",
             new=defender_client,
         ):
-            from prowler.providers.azure.services.defender.defender_container_images_vulnerabilities_scaned.defender_container_images_vulnerabilities_scaned import (
-                defender_container_images_vulnerabilities_scaned,
+            from prowler.providers.azure.services.defender.defender_container_images_resolved_vulnerabilities.defender_container_images_resolved_vulnerabilities import (
+                defender_container_images_resolved_vulnerabilities,
             )
 
-            check = defender_container_images_vulnerabilities_scaned()
+            check = defender_container_images_resolved_vulnerabilities()
             result = check.execute()
             assert len(result) == 1
             assert result[0].status == "PASS"
@@ -159,5 +143,5 @@ class Test_defender_container_images_vulnerabilities_scaned:
             assert result[0].subscription == AZURE_SUBSCRIPTION
             assert (
                 result[0].status_extended
-                == f"Azure running container images should have vulnerabilities resolved (powered by Microsoft Defender Vulnerability Management) is enabled in subscription '{AZURE_SUBSCRIPTION}'."
+                == f"Azure running container images do not have unresolved vulnerabilities in subscription '{AZURE_SUBSCRIPTION}'."
             )
