8430873e5b02bd58e9ba66a7e748c28522e5f9ea
Sergio Garcia <sergargar1@gmail.com>
2024-03-05 13:00:10 +0100

    fix(compliance): set the provider dynamically in Manual checks

--- prowler/lib/check/compliance.py
+++ prowler/lib/check/compliance.py
@@ -53,7 +53,7 @@ def update_checks_metadata_with_compliance(
                     check_compliance.append(compliance)
             # Create metadata for Manual Control
             manual_check_metadata = {
-                "Provider": "aws",
+                "Provider": framework.Provider,
                 "CheckID": "manual_check",
                 "CheckTitle": "Manual Check",
                 "CheckType": [],
