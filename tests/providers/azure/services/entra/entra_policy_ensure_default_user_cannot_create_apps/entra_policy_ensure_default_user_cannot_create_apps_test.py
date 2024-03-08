a28393c854b036a0cf460b4a7cf9bb2b710ee398
puchy22 <rubendltv22@gmail.com>
2024-03-08 11:27:50 +0100

    test(entra): Adapt test to new implemetnation of respective checks

--- tests/providers/azure/services/entra/entra_policy_ensure_default_user_cannot_create_apps/entra_policy_ensure_default_user_cannot_create_apps_test.py
+++ tests/providers/azure/services/entra/entra_policy_ensure_default_user_cannot_create_apps/entra_policy_ensure_default_user_cannot_create_apps_test.py
@@ -7,7 +7,7 @@ from prowler.providers.azure.services.entra.entra_service import AuthorizationPo
 class Test_entra_policy_ensure_default_user_cannot_create_apps:
     def test_entra_no_authorization_policy(self):
         entra_client = mock.MagicMock
-        entra_client.authorization_policy = None
+        entra_client.authorization_policy = {}
 
         with mock.patch(
             "prowler.providers.azure.services.entra.entra_policy_ensure_default_user_cannot_create_tenants.entra_policy_ensure_default_user_cannot_create_tenants.entra_client",
@@ -19,27 +19,21 @@ class Test_entra_policy_ensure_default_user_cannot_create_apps:
 
             check = entra_policy_ensure_default_user_cannot_create_tenants()
             result = check.execute()
-            assert len(result) == 1
-            assert result[0].status == "FAIL"
-            assert (
-                result[0].status_extended
-                == "Tenants creation is not disabled for non-admin users."
-            )
-            assert result[0].resource_name == "Default Authorization Policy"
-            assert result[0].resource_id == "Default Authorization Policy"
-            assert result[0].subscription == "All"
+            assert len(result) == 0
 
     def test_entra_default_user_role_permissions_not_allowed_to_create_apps(self):
         id = str(uuid4())
-        auth_policy = AuthorizationPolicy(
-            id=id,
-            name="Test",
-            description="Test",
-            default_user_role_permissions=mock.MagicMock(allowed_to_create_apps=False),
-        )
-
         entra_client = mock.MagicMock
-        entra_client.authorization_policy = auth_policy
+        entra_client.authorization_policy = {
+            "test.com": AuthorizationPolicy(
+                id=id,
+                name="Test",
+                description="Test",
+                default_user_role_permissions=mock.MagicMock(
+                    allowed_to_create_apps=False
+                ),
+            )
+        }
 
         with mock.patch(
             "prowler.providers.azure.services.entra.entra_policy_ensure_default_user_cannot_create_apps.entra_policy_ensure_default_user_cannot_create_apps.entra_client",
@@ -55,40 +49,42 @@ class Test_entra_policy_ensure_default_user_cannot_create_apps:
             assert result[0].status == "PASS"
             assert (
                 result[0].status_extended
-                == "Tenants creation is disabled for non-admin users."
+                == "App creation is disabled for non-admin users."
             )
             assert result[0].resource_name == "Test"
             assert result[0].resource_id == id
-            assert result[0].subscription == "All"
+            assert result[0].subscription == "All from tenant 'test.com'"
 
     def test_entra_default_user_role_permissions_allowed_to_create_apps(self):
         id = str(uuid4())
-        auth_policy = AuthorizationPolicy(
-            id=id,
-            name="Test",
-            description="Test",
-            default_user_role_permissions=mock.MagicMock(allowed_to_create_apps=True),
-        )
-
         entra_client = mock.MagicMock
-        entra_client.authorization_policy = auth_policy
+        entra_client.authorization_policy = {
+            "test.com": AuthorizationPolicy(
+                id=id,
+                name="Test",
+                description="Test",
+                default_user_role_permissions=mock.MagicMock(
+                    allowed_to_create_apps=True
+                ),
+            )
+        }
 
         with mock.patch(
-            "prowler.providers.azure.services.entra.entra_policy_ensure_default_user_cannot_create_tenants.entra_policy_ensure_default_user_cannot_create_tenants.entra_client",
+            "prowler.providers.azure.services.entra.entra_policy_ensure_default_user_cannot_create_apps.entra_policy_ensure_default_user_cannot_create_apps.entra_client",
             new=entra_client,
         ):
-            from prowler.providers.azure.services.entra.entra_policy_ensure_default_user_cannot_create_tenants.entra_policy_ensure_default_user_cannot_create_tenants import (
-                entra_policy_ensure_default_user_cannot_create_tenants,
+            from prowler.providers.azure.services.entra.entra_policy_ensure_default_user_cannot_create_apps.entra_policy_ensure_default_user_cannot_create_apps import (
+                entra_policy_ensure_default_user_cannot_create_apps,
             )
 
-            check = entra_policy_ensure_default_user_cannot_create_tenants()
+            check = entra_policy_ensure_default_user_cannot_create_apps()
             result = check.execute()
             assert len(result) == 1
             assert result[0].status == "FAIL"
             assert (
                 result[0].status_extended
-                == "Tenants creation is not disabled for non-admin users."
+                == "App creation is not disabled for non-admin users."
             )
             assert result[0].resource_name == "Test"
             assert result[0].resource_id == id
-            assert result[0].subscription == "All"
+            assert result[0].subscription == "All from tenant 'test.com'"
