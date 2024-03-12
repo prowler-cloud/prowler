f534378233fdd08a402956b5b3713d5d246ff302
puchy22 <rubendltv22@gmail.com>
2024-03-12 10:26:20 +0100

    feat(entra): Adapt azure service support multiple versions of MSGraph API

--- prowler/providers/azure/lib/service/service.py
+++ prowler/providers/azure/lib/service/service.py
@@ -5,13 +5,16 @@ from prowler.providers.azure.lib.audit_info.models import Azure_Audit_Info
 class AzureService:
     def __init__(
         self,
-        service: str,
+        services: list,
         audit_info: Azure_Audit_Info,
     ):
+        if not isinstance(services, list):
+            services = [services]
+
         self.clients = self.__set_clients__(
             audit_info.identity,
             audit_info.credentials,
-            service,
+            services,
             audit_info.azure_region_config,
         )
 
@@ -20,16 +23,36 @@ class AzureService:
 
         self.audit_config = audit_info.audit_config
 
-    def __set_clients__(self, identity, credentials, service, region_config):
+    def __set_clients__(self, identity, credentials, services, region_config):
         clients = {}
         try:
-            if "GraphServiceClient" in str(service):
-                clients.update({identity.domain: service(credentials=credentials)})
+            if "GraphServiceClient" in str(services):
+                clients.update({identity.domain: {}})
+                for client_service in services:
+                    if "msgraph_beta." in str(client_service):
+                        clients[identity.domain].update(
+                            {"beta": client_service(credentials=credentials)}
+                        )
+                    elif "msgraph." in str(client_service):
+                        clients[identity.domain].update(
+                            {"v1": client_service(credentials=credentials)}
+                        )
+                    else:
+                        clients[identity.domain].update(
+                            {
+                                str(client_service): client_service(
+                                    credentials=credentials
+                                )
+                            }
+                        )
+
             else:
                 for display_name, id in identity.subscriptions.items():
                     clients.update(
                         {
-                            display_name: service(
+                            display_name: services[
+                                0
+                            ](  # Not Entra services only support the first client
                                 credential=credentials,
                                 subscription_id=id,
                                 base_url=region_config.base_url,
@@ -37,6 +60,7 @@ class AzureService:
                             )
                         }
                     )
+
         except Exception as error:
             logger.error(
                 f"{error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
