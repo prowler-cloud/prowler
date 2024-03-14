a631cad0fe9cd94e688bf22cf6c1d946df27edfc
Sergio Garcia <sergargar1@gmail.com>
2024-03-14 11:25:32 +0100

    change test

--- tests/providers/aws/services/iam/iam_user_mfa_enabled_console_access/iam_user_mfa_enabled_console_access_test.py
+++ tests/providers/aws/services/iam/iam_user_mfa_enabled_console_access/iam_user_mfa_enabled_console_access_test.py
@@ -21,10 +21,6 @@ class Test_iam_user_mfa_enabled_console_access_test:
 
     @mock_aws
     def test_root_user_not_password_console_enabled(self):
-        iam_client = client("iam")
-        user = "test-user"
-        arn = iam_client.create_user(UserName=user)["User"]["Arn"]
-
         from prowler.providers.aws.services.iam.iam_service import IAM
 
         current_audit_info = set_mocked_aws_audit_info([AWS_REGION_US_EAST_1])
@@ -39,18 +35,37 @@ class Test_iam_user_mfa_enabled_console_access_test:
                 iam_user_mfa_enabled_console_access,
             )
 
-            service_client.credential_report[0]["password_enabled"] = "not_supported"
+            service_client.credential_report = [
+                {
+                    "user": "<root_account>",
+                    "arn": f"arn:aws:iam::{AWS_ACCOUNT_NUMBER}:root",
+                    "user_creation_time": "2022-02-17T14:59:38+00:00",
+                    "password_enabled": "not_supported",
+                    "password_last_used": "2023-05-22T09:52:24+00:00",
+                    "password_last_changed": "not_supported",
+                    "password_next_rotation": "not_supported",
+                    "mfa_active": "true",
+                    "access_key_1_active": "false",
+                    "access_key_1_last_rotated": "N/A",
+                    "access_key_1_last_used_date": "N/A",
+                    "access_key_1_last_used_region": "N/A",
+                    "access_key_1_last_used_service": "N/A",
+                    "access_key_2_active": "false",
+                    "access_key_2_last_rotated": "N/A",
+                    "access_key_2_last_used_date": "N/A",
+                    "access_key_2_last_used_region": "N/A",
+                    "access_key_2_last_used_service": "N/A",
+                    "cert_1_active": "false",
+                    "cert_1_last_rotated": "N/A",
+                    "cert_2_active": "false",
+                    "cert_2_last_rotated": "N/A",
+                }
+            ]
 
             check = iam_user_mfa_enabled_console_access()
             result = check.execute()
 
-            assert result[0].status == "PASS"
-            assert (
-                result[0].status_extended
-                == f"User {user} does not have Console Password enabled."
-            )
-            assert result[0].resource_id == user
-            assert result[0].resource_arn == arn
+            assert len(result) == 0
 
     @mock_aws
     def test_user_not_password_console_enabled(self):
