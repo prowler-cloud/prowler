0bdae0cf5d08fd36879de23813ca07a35060285c
Pepe Fagoaga <pepe@prowler.com>
2024-03-14 07:56:05 +0100

    fix(ocsf): status and conditional cloud/container

--- prowler/lib/outputs/json_ocsf/json_ocsf.py
+++ prowler/lib/outputs/json_ocsf/json_ocsf.py
@@ -31,14 +31,29 @@ def get_account_type_id_by_provider(provider: str) -> TypeID:
     return type_id
 
 
+def get_finding_status_id(status: str) -> StatusID:
+    status_id = StatusID.Other
+    if status == "FAIL":
+        status_id = StatusID.New
+    elif status == "MUTED":
+        status_id = StatusID.Suppressed
+    return status_id
+
+
 def fill_json_ocsf(finding_output: FindingOutput) -> DetectionFinding:
     try:
-        # TODO: add or delete?
-        # FindingInformation.created_time
-        return DetectionFinding(
-            activity_id=ActivityID.Create.value,
-            activity_name=ActivityID.Create.name,
+        finding_activity = ActivityID.Create
+        cloud_account_type = get_account_type_id_by_provider(finding_output.provider)
+        finding_severity = getattr(
+            SeverityID, finding_output.severity.capitalize(), SeverityID.Unknown
+        )
+        finding_status = get_finding_status_id(finding_output.status)
+
+        detection_finding = DetectionFinding(
+            activity_id=finding_activity.value,
+            activity_name=finding_activity.name,
             finding_info=FindingInformation(
+                created_time=finding_output.timestamp,
                 desc=finding_output.description,
                 title=finding_output.check_title,
                 uid=finding_output.finding_uid,
@@ -46,29 +61,6 @@ def fill_json_ocsf(finding_output: FindingOutput) -> DetectionFinding:
                 # TODO: RelatedEvent and depends_on + related_to
                 # related_events=[RelatedEvent()],
             ),
-            cloud=Cloud(
-                account=Account(
-                    name=finding_output.account_name,
-                    type_id=get_account_type_id_by_provider(
-                        finding_output.provider
-                    ).value,
-                    type=get_account_type_id_by_provider(finding_output.provider).name,
-                    uid=finding_output.account_uid,
-                ),
-                org=Organization(
-                    uid=finding_output.account_organization_uid,
-                    name=finding_output.account_organization_name,
-                ),
-                provider=finding_output.provider,
-                region=finding_output.region,
-            ),
-            # TODO: Only fill the container object if it is Kubernetes
-            container=Container(
-                name=finding_output.resource_name,
-                uid=finding_output.resource_uid,
-            ),
-            # TODO: Get the PID of the namespace (we only have the name of the namespace)
-            # namespace_pid=finding_output.region,
             event_time=finding_output.timestamp,
             remediation=Remediation(
                 desc=finding_output.remediation_recommendation_text,
@@ -85,14 +77,11 @@ def fill_json_ocsf(finding_output: FindingOutput) -> DetectionFinding:
                     )
                 ),
             ),
-            severity_id=getattr(
-                SeverityID, finding_output.severity.capitalize(), SeverityID.Unknown
-            ).value,
-            severity=getattr(
-                SeverityID, finding_output.severity.capitalize(), SeverityID.Unknown
-            ).name,
-            status_id=StatusID.Other.value,
-            status=finding_output.status,
+            severity_id=finding_severity.value,
+            severity=finding_severity.name,
+            status_id=finding_status.value,
+            status=finding_status.name,
+            status_code=finding_output.status,
             status_detail=finding_output.status_extended,
             resources=[
                 ResourceDetails(
@@ -120,6 +109,36 @@ def fill_json_ocsf(finding_output: FindingOutput) -> DetectionFinding:
             type_id=DetectionFindingTypeID.Create,
             type_name=DetectionFindingTypeID.Create.name,
         )
+
+        if finding_output.provider == "kubernetes":
+            detection_finding.container = (
+                Container(
+                    name=finding_output.resource_name,
+                    uid=finding_output.resource_uid,
+                ),
+            )
+            # TODO: Get the PID of the namespace (we only have the name of the namespace)
+            # detection_finding.namespace_pid=,
+        else:
+            detection_finding.cloud = (
+                Cloud(
+                    account=Account(
+                        name=finding_output.account_name,
+                        type_id=cloud_account_type.value,
+                        type=cloud_account_type.name,
+                        uid=finding_output.account_uid,
+                    ),
+                    org=Organization(
+                        uid=finding_output.account_organization_uid,
+                        name=finding_output.account_organization_name,
+                    ),
+                    provider=finding_output.provider,
+                    region=finding_output.region,
+                ),
+            )
+
+        return detection_finding
+
     except Exception as error:
         logger.error(
             f"{error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
