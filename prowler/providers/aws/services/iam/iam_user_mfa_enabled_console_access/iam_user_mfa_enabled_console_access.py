2bd5a11c007996148e4b4a63828d9794944db38c
Sergio Garcia <sergargar1@gmail.com>
2024-03-14 10:49:37 +0100

    fix(iam): ignore Root User in iam_user_mfa_enabled_console_access

--- prowler/providers/aws/services/iam/iam_user_mfa_enabled_console_access/iam_user_mfa_enabled_console_access.py
+++ prowler/providers/aws/services/iam/iam_user_mfa_enabled_console_access/iam_user_mfa_enabled_console_access.py
@@ -7,12 +7,12 @@ class iam_user_mfa_enabled_console_access(Check):
         findings = []
         response = iam_client.credential_report
         for user in response:
-            report = Check_Report_AWS(self.metadata())
-            report.resource_id = user["user"]
-            report.resource_arn = user["arn"]
-            report.region = iam_client.region
             # all the users but root (which by default does not support console password)
-            if user["password_enabled"] != "not_supported":
+            if user["user"] != "<root_account>":
+                report = Check_Report_AWS(self.metadata())
+                report.resource_id = user["user"]
+                report.resource_arn = user["arn"]
+                report.region = iam_client.region
                 # check if the user has password enabled
                 if user["password_enabled"] == "true":
                     if user["mfa_active"] == "false":
@@ -26,12 +26,6 @@ class iam_user_mfa_enabled_console_access(Check):
                     report.status_extended = (
                         f"User {user['user']} does not have Console Password enabled."
                     )
-            # root user
-            else:
-                report.status = "PASS"
-                report.status_extended = (
-                    f"User {user['user']} does not have Console Password enabled."
-                )
-            findings.append(report)
+                findings.append(report)
 
         return findings
