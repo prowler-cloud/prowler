8f3b1161d65a018a078920a1bcea9a10c9532c0b
Pepe Fagoaga <pepe@prowler.com>
2024-03-15 12:51:34 +0100

    fix(azure): Provider tests

--- tests/providers/azure/azure_provider_test.py
+++ tests/providers/azure/azure_provider_test.py
@@ -1,23 +1,24 @@
+from argparse import Namespace
+from datetime import datetime
+from os import rmdir
+
 import pytest
 from azure.identity import DefaultAzureCredential
-from mock import MagicMock, patch
+from freezegun import freeze_time
+from mock import patch
 
 from prowler.config.config import default_config_file_path
 from prowler.providers.azure.azure_provider import AzureProvider
-from prowler.providers.azure.models import AzureIdentityInfo, AzureRegionConfig
-
-
-def mock_set_identity_info(*_):
-    return AzureIdentityInfo()
-
-
-def mock_set_azure_credentials(*_):
-    return {}
+from prowler.providers.azure.models import (
+    AzureIdentityInfo,
+    AzureOutputOptions,
+    AzureRegionConfig,
+)
 
 
 class TestAzureProvider:
     def test_azure_provider(self):
-        arguments = MagicMock
+        arguments = Namespace()
         arguments.subscription_ids = None
         arguments.tenant_id = None
         # We need to set exactly one auth method
@@ -61,7 +62,7 @@ class TestAzureProvider:
             }
 
     def test_zure_provider_not_auth_methods(self):
-        arguments = MagicMock
+        arguments = Namespace()
         arguments.subscription_ids = None
         arguments.tenant_id = None
         # We need to set exactly one auth method
@@ -90,7 +91,7 @@ class TestAzureProvider:
             )
 
     def test_azure_provider_browser_auth_but_not_tenant_id(self):
-        arguments = MagicMock
+        arguments = Namespace()
         arguments.subscription_ids = None
         arguments.tenant_id = None
         # We need to set exactly one auth method
@@ -119,7 +120,7 @@ class TestAzureProvider:
             )
 
     def test_azure_provider_not_browser_auth_but_tenant_id(self):
-        arguments = MagicMock
+        arguments = Namespace()
         arguments.subscription_ids = None
         arguments.tenant_id = "test-tenant-id"
         # We need to set exactly one auth method
@@ -147,81 +148,99 @@ class TestAzureProvider:
                 == "Azure provider requires at least one authentication method set: [--az-cli-auth | --sp-env-auth | --browser-auth | --managed-identity-auth]"
             )
 
-    # def test_set_provider_output_options_azure_domain(self):
-    #     #  Set the cloud provider
-    #     provider = "azure"
-    #     # Set the arguments passed
-    #     arguments = Namespace()
-    #     arguments.quiet = True
-    #     arguments.output_modes = ["csv"]
-    #     arguments.output_directory = "output_test_directory"
-    #     arguments.verbose = True
-    #     arguments.only_logs = False
-    #     arguments.unix_timestamp = False
-    #     arguments.shodan = "test-api-key"
+    @patch("prowler.config.config.output_file_timestamp", new="20230101120000")
+    @freeze_time(datetime.today())
+    def test_azure_provider_output_options_with_domain(self):
+        arguments = Namespace()
+        arguments.subscription_ids = None
+        arguments.tenant_id = None
 
-    #     # Mock Azure Audit Info
-    #     audit_info = self.set_mocked_azure_audit_info()
-    #     audit_info.identity.tenant_domain = "test-domain"
+        # We need to set exactly one auth method
+        arguments.az_cli_auth = None
+        arguments.sp_env_auth = True
+        arguments.browser_auth = None
+        arguments.managed_identity_auth = None
 
-    #     mutelist_file = ""
-    #     bulk_checks_metadata = {}
-    #     output_options = set_provider_output_options(
-    #         provider, arguments, audit_info, mutelist_file, bulk_checks_metadata
-    #     )
-    #     assert isinstance(output_options, Azure_Output_Options)
-    #     assert output_options.is_quiet
-    #     assert output_options.output_modes == [
-    #         "csv",
-    #     ]
-    #     assert output_options.output_directory == arguments.output_directory
-    #     assert output_options.mutelist_file == ""
-    #     assert output_options.bulk_checks_metadata == {}
-    #     assert output_options.verbose
-    #     assert (
-    #         output_options.output_filename
-    #         == f"prowler-output-{audit_info.identity.tenant_domain}-{DATETIME}"
-    #     )
+        arguments.config_file = default_config_file_path
+        arguments.azure_region = "AzureCloud"
 
-    #     # Delete testing directory
-    #     rmdir(arguments.output_directory)
+        # Output Options
+        arguments.output_modes = ["csv"]
+        arguments.output_directory = "output_test_directory"
+        arguments.status = []
+        arguments.verbose = True
+        arguments.only_logs = False
+        arguments.unix_timestamp = False
+        arguments.shodan = "test-api-key"
+        arguments.mutelist_file = ""
 
-    # def test_set_provider_output_options_azure_tenant_ids(self):
-    #     #  Set the cloud provider
-    #     provider = "azure"
-    #     # Set the arguments passed
-    #     arguments = Namespace()
-    #     arguments.quiet = True
-    #     arguments.output_modes = ["csv"]
-    #     arguments.output_directory = "output_test_directory"
-    #     arguments.verbose = True
-    #     arguments.only_logs = False
-    #     arguments.unix_timestamp = False
-    #     arguments.shodan = "test-api-key"
+        tenant_domain = "test-domain"
+        with patch(
+            "prowler.providers.azure.azure_provider.AzureProvider.setup_identity",
+            return_value=AzureIdentityInfo(tenant_domain=tenant_domain),
+        ), patch(
+            "prowler.providers.azure.azure_provider.AzureProvider.get_locations",
+            return_value={},
+        ):
+            azure_provider = AzureProvider(arguments)
 
-    #     # Mock Azure Audit Info
-    #     audit_info = self.set_mocked_azure_audit_info()
-    #     tenants = ["tenant-1", "tenant-2"]
-    #     audit_info.identity.tenant_ids = tenants
+            azure_provider.output_options = arguments, {}
 
-    #     mutelist_file = ""
-    #     bulk_checks_metadata = {}
-    #     output_options = set_provider_output_options(
-    #         provider, arguments, audit_info, mutelist_file, bulk_checks_metadata
-    #     )
-    #     assert isinstance(output_options, Azure_Output_Options)
-    #     assert output_options.is_quiet
-    #     assert output_options.output_modes == [
-    #         "csv",
-    #     ]
-    #     assert output_options.output_directory == arguments.output_directory
-    #     assert output_options.mutelist_file == ""
-    #     assert output_options.bulk_checks_metadata == {}
-    #     assert output_options.verbose
-    #     assert (
-    #         output_options.output_filename
-    #         == f"prowler-output-{'-'.join(tenants)}-{DATETIME}"
-    #     )
+            assert isinstance(azure_provider.output_options, AzureOutputOptions)
+            assert azure_provider.output_options.status == []
+            assert azure_provider.output_options.output_modes == [
+                "csv",
+            ]
+            assert (
+                azure_provider.output_options.output_directory
+                == arguments.output_directory
+            )
+            # assert azure_provider.output_options.mutelist_file == ""
+            assert azure_provider.output_options.bulk_checks_metadata == {}
+            assert azure_provider.output_options.verbose
+            assert (
+                azure_provider.output_options.output_filename
+                == f"prowler-output-{azure_provider.identity.tenant_domain}-{datetime.today().strftime('%Y%m%d%H%M%S')}"
+            )
 
-    #     # Delete testing directory
-    #     rmdir(arguments.output_directory)
+            # Delete testing directory
+            # TODO: move this to a fixtures file
+            rmdir(f"{arguments.output_directory}/compliance")
+            rmdir(arguments.output_directory)
+
+    def test_azure_provider_output_options_tenant_ids(self):
+        # Output Options
+        arguments = Namespace()
+        arguments.output_modes = ["csv"]
+        arguments.output_directory = "output_test_directory"
+        arguments.status = []
+        arguments.verbose = True
+        arguments.only_logs = False
+        arguments.unix_timestamp = False
+        arguments.shodan = "test-api-key"
+        arguments.mutelist_file = ""
+
+        tenants = ["tenant-1", "tenant-2"]
+
+        azure_output_options = AzureOutputOptions(
+            arguments, {}, AzureIdentityInfo(tenant_ids=tenants)
+        )
+
+        assert isinstance(azure_output_options, AzureOutputOptions)
+        assert azure_output_options.status == []
+        assert azure_output_options.output_modes == [
+            "csv",
+        ]
+        assert azure_output_options.output_directory == arguments.output_directory
+        # assert azure_provider.output_options.mutelist_file == ""
+        assert azure_output_options.bulk_checks_metadata == {}
+        assert azure_output_options.verbose
+        assert (
+            azure_output_options.output_filename
+            == f"prowler-output-{'-'.join(tenants)}-{datetime.today().strftime('%Y%m%d%H%M%S')}"
+        )
+
+        # Delete testing directory
+        # TODO: move this to a fixtures file
+        rmdir(f"{arguments.output_directory}/compliance")
+        rmdir(arguments.output_directory)
