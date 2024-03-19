8583043b3b5f4a6b32a7d80401cf5f43ef90542a
Sergio Garcia <sergargar1@gmail.com>
2024-03-19 11:42:31 +0100

    fix tests

--- tests/providers/azure/services/monitor/monitor_diagnostic_setting_with_appropriate_categories/monitor_diagnostic_setting_with_appropriate_categories_test.py
+++ tests/providers/azure/services/monitor/monitor_diagnostic_setting_with_appropriate_categories/monitor_diagnostic_setting_with_appropriate_categories_test.py
@@ -1,6 +1,5 @@
 from unittest import mock
 
-from prowler.providers.azure.services.monitor.monitor_service import DiagnosticSetting
 from tests.providers.azure.azure_fixtures import (
     AZURE_SUBSCRIPTION_ID,
     set_mocked_azure_provider,
@@ -58,42 +57,6 @@ class Test_monitor_diagnostic_setting_with_appropriate_categories:
 
     def test_diagnostic_settings_configured(self):
         monitor_client = mock.MagicMock
-        monitor_client.diagnostics_settings = {
-            AZURE_SUBSCRIPTION_ID: [
-                DiagnosticSetting(
-                    id="id",
-                    logs=[
-                        mock.MagicMock(category="Administrative", enabled=True),
-                        mock.MagicMock(category="Security", enabled=True),
-                        mock.MagicMock(category="ServiceHealth", enabled=False),
-                        mock.MagicMock(category="Alert", enabled=True),
-                        mock.MagicMock(category="Recommendation", enabled=False),
-                        mock.MagicMock(category="Policy", enabled=True),
-                        mock.MagicMock(category="Autoscale", enabled=False),
-                        mock.MagicMock(category="ResourceHealth", enabled=False),
-                    ],
-                    storage_account_id="/subscriptions/1234a5-123a-123a-123a-1234567890ab/resourceGroups/rg/providers/Microsoft.Storage/storageAccounts/storageaccountname",
-                    storage_account_name="storageaccountname",
-                    name="name",
-                ),
-                DiagnosticSetting(
-                    id="id2",
-                    logs=[
-                        mock.MagicMock(category="Administrative", enabled=False),
-                        mock.MagicMock(category="Security", enabled=True),
-                        mock.MagicMock(category="ServiceHealth", enabled=False),
-                        mock.MagicMock(category="Alert", enabled=True),
-                        mock.MagicMock(category="Recommendation", enabled=False),
-                        mock.MagicMock(category="Policy", enabled=True),
-                        mock.MagicMock(category="Autoscale", enabled=False),
-                        mock.MagicMock(category="ResourceHealth", enabled=False),
-                    ],
-                    storage_account_id="/subscriptions/1224a5-123a-123a-123a-1234567890ab/resourceGroups/rg/providers/Microsoft.Storage/storageAccounts/storageaccountname2",
-                    storage_account_name="storageaccountname2",
-                    name="name2",
-                ),
-            ]
-        }
 
         with mock.patch(
             "prowler.providers.common.common.get_global_provider",
@@ -105,7 +68,46 @@ class Test_monitor_diagnostic_setting_with_appropriate_categories:
             from prowler.providers.azure.services.monitor.monitor_diagnostic_setting_with_appropriate_categories.monitor_diagnostic_setting_with_appropriate_categories import (
                 monitor_diagnostic_setting_with_appropriate_categories,
             )
+            from prowler.providers.azure.services.monitor.monitor_service import (
+                DiagnosticSetting,
+            )
 
+            monitor_client.diagnostics_settings = {
+                AZURE_SUBSCRIPTION_ID: [
+                    DiagnosticSetting(
+                        id="id",
+                        logs=[
+                            mock.MagicMock(category="Administrative", enabled=True),
+                            mock.MagicMock(category="Security", enabled=True),
+                            mock.MagicMock(category="ServiceHealth", enabled=False),
+                            mock.MagicMock(category="Alert", enabled=True),
+                            mock.MagicMock(category="Recommendation", enabled=False),
+                            mock.MagicMock(category="Policy", enabled=True),
+                            mock.MagicMock(category="Autoscale", enabled=False),
+                            mock.MagicMock(category="ResourceHealth", enabled=False),
+                        ],
+                        storage_account_id="/subscriptions/1234a5-123a-123a-123a-1234567890ab/resourceGroups/rg/providers/Microsoft.Storage/storageAccounts/storageaccountname",
+                        storage_account_name="storageaccountname",
+                        name="name",
+                    ),
+                    DiagnosticSetting(
+                        id="id2",
+                        logs=[
+                            mock.MagicMock(category="Administrative", enabled=False),
+                            mock.MagicMock(category="Security", enabled=True),
+                            mock.MagicMock(category="ServiceHealth", enabled=False),
+                            mock.MagicMock(category="Alert", enabled=True),
+                            mock.MagicMock(category="Recommendation", enabled=False),
+                            mock.MagicMock(category="Policy", enabled=True),
+                            mock.MagicMock(category="Autoscale", enabled=False),
+                            mock.MagicMock(category="ResourceHealth", enabled=False),
+                        ],
+                        storage_account_id="/subscriptions/1224a5-123a-123a-123a-1234567890ab/resourceGroups/rg/providers/Microsoft.Storage/storageAccounts/storageaccountname2",
+                        storage_account_name="storageaccountname2",
+                        name="name2",
+                    ),
+                ]
+            }
             check = monitor_diagnostic_setting_with_appropriate_categories()
             result = check.execute()
             assert len(result) == 1
