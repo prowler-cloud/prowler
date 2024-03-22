3331f7bd4e6cf89828f039cddd68aebd6f461e37
Sergio Garcia <sergargar1@gmail.com>
2024-03-22 12:51:57 +0100

    black format

--- dashboard/config.py
+++ dashboard/config.py
@@ -1,11 +1,11 @@
 import os
 
 # Emojis to be used in the compliance table
-pass_emoji = '✅'
-fail_emoji = '❌'
-info_emoji = 'ℹ️'
-manual_emoji = '✋🏽'
+pass_emoji = "✅"
+fail_emoji = "❌"
+info_emoji = "ℹ️"
+manual_emoji = "✋🏽"
 
 # Folder output path
-folder_path_overview = os.getcwd() + '/output'
-folder_path_compliance = os.getcwd() + '/output/compliance'
+folder_path_overview = os.getcwd() + "/output"
+folder_path_compliance = os.getcwd() + "/output/compliance"
