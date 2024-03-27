20abfc87e8f92d8dbfa910eee776ac5891d56094
Hugo966 <148140670+Hugo966@users.noreply.github.com>
2024-03-27 12:32:42 +0100

    feat(azure): New check related with trusted launch in vm (#3616)

---
+++ tests/providers/azure/services/vm/vm_trusted_launch_enabled/vm_trusted_launch_enabled_test.py
@@ -0,0 +1,131 @@
+from unittest import mock
+from uuid import uuid4
+
+from prowler.providers.azure.services.vm.vm_service import VirtualMachine
+from tests.providers.azure.azure_fixtures import AZURE_SUBSCRIPTION
+
+
+class Test_vm_trusted_launch_enabled:
+    def test_vm_no_subscriptions(self):
+        vm_client = mock.MagicMock
+        vm_client.virtual_machines = {}
+
+        with mock.patch(
+            "prowler.providers.azure.services.vm.vm_trusted_launch_enabled.vm_trusted_launch_enabled.vm_client",
+            new=vm_client,
+        ):
+            from prowler.providers.azure.services.vm.vm_trusted_launch_enabled.vm_trusted_launch_enabled import (
+                vm_trusted_launch_enabled,
+            )
+
+            check = vm_trusted_launch_enabled()
+            result = check.execute()
+            assert len(result) == 0
+
+    def test_vm_no_vm(self):
+        vm_client = mock.MagicMock
+        vm_client.virtual_machines = {AZURE_SUBSCRIPTION: {}}
+
+        with mock.patch(
+            "prowler.providers.azure.services.vm.vm_trusted_launch_enabled.vm_trusted_launch_enabled.vm_client",
+            new=vm_client,
+        ):
+            from prowler.providers.azure.services.vm.vm_trusted_launch_enabled.vm_trusted_launch_enabled import (
+                vm_trusted_launch_enabled,
+            )
+
+            check = vm_trusted_launch_enabled()
+            result = check.execute()
+            assert len(result) == 0
+
+    def test_vm_trusted_launch_enabled(self):
+        vm_id = str(uuid4())
+        vm_client = mock.MagicMock
+        vm_client.virtual_machines = {
+            AZURE_SUBSCRIPTION: {
+                vm_id: VirtualMachine(
+                    resource_id="/subscriptions/resource_id",
+                    resource_name="VMTest",
+                    security_profile=mock.MagicMock(
+                        security_type="TrustedLaunch",
+                        uefi_settings=mock.MagicMock(
+                            secure_boot_enabled=True,
+                            v_tpm_enabled=True,
+                        ),
+                    ),
+                    storage_profile=mock.MagicMock(
+                        os_disk=mock.MagicMock(
+                            create_option="FromImage",
+                            managed_disk=mock.MagicMock(id="managed_disk_id"),
+                        ),
+                        data_disks=[],
+                    ),
+                )
+            }
+        }
+
+        with mock.patch(
+            "prowler.providers.azure.services.vm.vm_trusted_launch_enabled.vm_trusted_launch_enabled.vm_client",
+            new=vm_client,
+        ):
+            from prowler.providers.azure.services.vm.vm_trusted_launch_enabled.vm_trusted_launch_enabled import (
+                vm_trusted_launch_enabled,
+            )
+
+            check = vm_trusted_launch_enabled()
+            result = check.execute()
+            assert len(result) == 1
+            assert result[0].status == "PASS"
+            assert result[0].subscription == AZURE_SUBSCRIPTION
+            assert result[0].resource_name == "VMTest"
+            assert result[0].resource_id == vm_id
+            assert (
+                result[0].status_extended
+                == f"VM VMTest has trusted launch enabled in subscription {AZURE_SUBSCRIPTION}"
+            )
+
+    def test_vm_trusted_launch_disabled(self):
+        vm_id = str(uuid4())
+        vm_client = mock.MagicMock
+        vm_client.virtual_machines = {
+            AZURE_SUBSCRIPTION: {
+                vm_id: VirtualMachine(
+                    resource_id="/subscriptions/resource_id",
+                    resource_name="VMTest",
+                    security_profile=mock.MagicMock(
+                        security_type="TrustedLaunch",
+                        uefi_settings=mock.MagicMock(
+                            secure_boot_enabled=False,
+                            v_tpm_enabled=False,
+                        ),
+                    ),
+                    storage_profile=mock.MagicMock(
+                        os_disk=mock.MagicMock(
+                            create_option="FromImage",
+                            managed_disk=mock.MagicMock(id="managed_disk_id"),
+                        ),
+                        data_disks=[],
+                    ),
+                )
+            }
+        }
+
+        with mock.patch(
+            "prowler.providers.azure.services.vm.vm_trusted_launch_enabled.vm_trusted_launch_enabled.vm_client",
+            new=vm_client,
+        ):
+            from prowler.providers.azure.services.vm.vm_trusted_launch_enabled.vm_trusted_launch_enabled import (
+                vm_trusted_launch_enabled,
+            )
+
+            check = vm_trusted_launch_enabled()
+            result = check.execute()
+            assert len(result) == 1
+            assert result[0].status == "FAIL"
+            assert result[0].subscription == AZURE_SUBSCRIPTION
+            assert result[0].resource_name == "VMTest"
+            assert result[0].resource_id == vm_id
+            assert (
+                result[0].status_extended
+                == f"VM VMTest has trusted launch disabled in subscription {AZURE_SUBSCRIPTION}"
+            )
