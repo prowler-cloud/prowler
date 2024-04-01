8e8c383e7e3ee623be78975217225836495cabe7
Sergio <sergio@prowler.com>
2024-04-01 18:35:47 +0200

    fix test

--- tests/providers/azure/services/entra/entra_security_defaults_enabled/entra_security_defaults_enabled_test.py
+++ tests/providers/azure/services/entra/entra_security_defaults_enabled/entra_security_defaults_enabled_test.py
@@ -1,7 +1,7 @@
 from unittest import mock
 from uuid import uuid4
 
-from tests.providers.azure.azure_fixtures import DOMAIN
+from tests.providers.azure.azure_fixtures import DOMAIN, set_mocked_azure_provider
 
 
 class Test_entra_security_defaults_enabled:
@@ -9,6 +9,9 @@ class Test_entra_security_defaults_enabled:
         entra_client = mock.MagicMock
 
         with mock.patch(
+            "prowler.providers.common.common.get_global_provider",
+            return_value=set_mocked_azure_provider(),
+        ), mock.patch(
             "prowler.providers.azure.services.entra.entra_security_defaults_enabled.entra_security_defaults_enabled.entra_client",
             new=entra_client,
         ):
@@ -26,6 +29,9 @@ class Test_entra_security_defaults_enabled:
         entra_client = mock.MagicMock
 
         with mock.patch(
+            "prowler.providers.common.common.get_global_provider",
+            return_value=set_mocked_azure_provider(),
+        ), mock.patch(
             "prowler.providers.azure.services.entra.entra_security_defaults_enabled.entra_security_defaults_enabled.entra_client",
             new=entra_client,
         ):
@@ -48,6 +54,9 @@ class Test_entra_security_defaults_enabled:
         entra_client = mock.MagicMock
 
         with mock.patch(
+            "prowler.providers.common.common.get_global_provider",
+            return_value=set_mocked_azure_provider(),
+        ), mock.patch(
             "prowler.providers.azure.services.entra.entra_security_defaults_enabled.entra_security_defaults_enabled.entra_client",
             new=entra_client,
         ):
@@ -77,6 +86,9 @@ class Test_entra_security_defaults_enabled:
         entra_client = mock.MagicMock
 
         with mock.patch(
+            "prowler.providers.common.common.get_global_provider",
+            return_value=set_mocked_azure_provider(),
+        ), mock.patch(
             "prowler.providers.azure.services.entra.entra_security_defaults_enabled.entra_security_defaults_enabled.entra_client",
             new=entra_client,
         ):
