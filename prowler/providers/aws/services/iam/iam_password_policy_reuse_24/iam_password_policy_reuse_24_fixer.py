c11003727687fb0c155776dd09c52fd59ea956ca
Sergio <sergio@prowler.com>
2024-04-15 13:57:39 +0200

    fix fixer

--- prowler/providers/aws/services/iam/iam_password_policy_reuse_24/iam_password_policy_reuse_24_fixer.py
+++ prowler/providers/aws/services/iam/iam_password_policy_reuse_24/iam_password_policy_reuse_24_fixer.py
@@ -21,33 +21,17 @@ def fixer(resource_id: str) -> bool:
     """
     try:
         iam_client.client.update_account_password_policy(
-            MinimumPasswordLength=iam_client.fixer_config.get(
-                "iam_password_policy", {}
-            ).get("MinimumPasswordLength", 14),
-            RequireSymbols=iam_client.fixer_config.get("iam_password_policy", {}).get(
-                "RequireSymbols", True
-            ),
-            RequireNumbers=iam_client.fixer_config.get("iam_password_policy", {}).get(
-                "RequireNumbers", True
-            ),
-            RequireUppercaseCharacters=iam_client.fixer_config.get(
-                "iam_password_policy", {}
-            ).get("RequireUppercaseCharacters", True),
-            RequireLowercaseCharacters=iam_client.fixer_config.get(
-                "iam_password_policy", {}
-            ).get("RequireLowercaseCharacters", True),
-            AllowUsersToChangePassword=iam_client.fixer_config.get(
-                "iam_password_policy", {}
-            ).get("AllowUsersToChangePassword", True),
-            MaxPasswordAge=iam_client.fixer_config.get("iam_password_policy", {}).get(
-                "MaxPasswordAge", 90
-            ),
+            MinimumPasswordLength=iam_client.password_policy.length,
+            RequireSymbols=iam_client.password_policy.symbols,
+            RequireNumbers=iam_client.password_policy.numbers,
+            RequireUppercaseCharacters=iam_client.password_policy.uppercase,
+            RequireLowercaseCharacters=iam_client.password_policy.lowercase,
+            AllowUsersToChangePassword=iam_client.password_policy.allow_change,
+            MaxPasswordAge=iam_client.password_policy.max_age,
             PasswordReusePrevention=iam_client.fixer_config.get(
                 "iam_password_policy", {}
             ).get("PasswordReusePrevention", 24),
-            HardExpiry=iam_client.fixer_config.get("iam_password_policy", {}).get(
-                "HardExpiry", True
-            ),
+            HardExpiry=iam_client.password_policy.hard_expiry,
         )
     except Exception as error:
         logger.error(
