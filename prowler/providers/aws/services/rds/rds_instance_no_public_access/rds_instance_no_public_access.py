80e4689190a29749bf60b3973a5b9f8e3980c914
Sergio <sergio@prowler.com>
2024-04-16 11:13:58 +0200

    chore(rds): improve rds public instance check

--- prowler/providers/aws/services/rds/rds_instance_no_public_access/rds_instance_no_public_access.py
+++ prowler/providers/aws/services/rds/rds_instance_no_public_access/rds_instance_no_public_access.py
@@ -1,5 +1,6 @@
 from prowler.lib.check.models import Check, Check_Report_AWS
 from prowler.providers.aws.services.ec2.ec2_client import ec2_client
+from prowler.providers.aws.services.ec2.lib.security_groups import check_security_group
 from prowler.providers.aws.services.rds.rds_client import rds_client
 
 
@@ -17,18 +18,25 @@ class rds_instance_no_public_access(Check):
                 f"RDS Instance {db_instance.id} is not publicly accessible."
             )
             if db_instance.public:
+                report.status_extended = f"RDS Instance {db_instance.id} is set as publicly accessible, but is not publicly exposed."
                 # Check if any DB Instance Security Group is publicly open
                 if db_instance.security_groups:
                     report.status = "PASS"
-                    report.status_extended = f"RDS Instance {db_instance.id} is public but filtered with security groups."
-                    for security_group in ec2_client.security_groups:
-                        if (
-                            security_group.id in db_instance.security_groups
-                            and security_group.public_ports
-                        ):
-                            report.status = "FAIL"
-                            report.status_extended = f"RDS Instance {db_instance.id} is set as publicly accessible."
-                            break
+                    report.status_extended = f"RDS Instance {db_instance.id} is set as publicly accessible but filtered with security groups."
+                    db_instance_port = db_instance.endpoint.get("Port")
+                    if db_instance_port:
+                        for security_group in ec2_client.security_groups:
+                            if security_group.id in db_instance.security_groups:
+                                for ingress_rule in security_group.ingress_rules:
+                                    if check_security_group(
+                                        ingress_rule,
+                                        "tcp",
+                                        [db_instance_port],
+                                        any_address=True,
+                                    ):
+                                        report.status = "FAIL"
+                                        report.status_extended = f"RDS Instance {db_instance.id} is set as publicly accessible AND security group {security_group.name} ({security_group.id}) has {db_instance.engine} port {db_instance_port} open to the Internet."
+                                        break
             findings.append(report)
 
         return findings
