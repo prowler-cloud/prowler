57e285d82f2a395bce72b75d4f0e3158a2dc9b92
Sergio <sergio@prowler.com>
2024-04-17 18:20:53 +0200

    fix test

--- tests/providers/aws/services/s3/s3_account_level_public_access_blocks/s3_account_level_public_access_blocks_fixer_test.py
+++ tests/providers/aws/services/s3/s3_account_level_public_access_blocks/s3_account_level_public_access_blocks_fixer_test.py
@@ -20,9 +20,6 @@ class Test_s3_account_level_public_access_block_fixer:
             "prowler.providers.common.common.get_global_provider",
             return_value=aws_provider,
         ), mock.patch(
-            "prowler.providers.aws.services.s3.s3_account_level_public_access_blocks.s3_account_level_public_access_blocks_fixer.s3_client",
-            new=S3(aws_provider),
-        ), mock.patch(
             "prowler.providers.aws.services.s3.s3_account_level_public_access_blocks.s3_account_level_public_access_blocks_fixer.s3control_client",
             new=S3Control(aws_provider),
         ):
