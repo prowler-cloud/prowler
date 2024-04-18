76ad104a968e114bfada4cd32b372ccf2de36d00
puchy22 <rubendltv22@gmail.com>
2024-04-18 11:02:33 +0200

    test(dns): New service test test with multiple managed zones and policies

---
+++ tests/providers/gcp/services/dns/dns_service_test.py
@@ -0,0 +1,110 @@
+from unittest.mock import MagicMock, patch
+from uuid import uuid4
+
+from prowler.providers.gcp.services.dns.dns_service import DNS
+from tests.providers.gcp.gcp_fixtures import (
+    GCP_PROJECT_ID,
+    mock_is_api_active,
+    set_mocked_gcp_provider,
+)
+
+
+def mock_api_client(_, __, ___, ____):
+    client = MagicMock()
+    # Mocking managed zones
+    managed_zone1_id = str(uuid4())
+    managed_zone2_id = str(uuid4())
+
+    client.managedZones().list().execute.return_value = {
+        "managedZones": [
+            {
+                "name": "managed_zone1",
+                "id": managed_zone1_id,
+                "dnssecConfig": {"state": "on", "defaultKeySpecs": []},
+            },
+            {
+                "name": "managed_zone2",
+                "id": managed_zone2_id,
+                "dnssecConfig": {"state": "off", "defaultKeySpecs": []},
+            },
+        ]
+    }
+    client.managedZones().list_next.return_value = None
+
+    # Mocking policies
+    policy1_id = str(uuid4())
+    policy2_id = str(uuid4())
+
+    client.policies().list().execute.return_value = {
+        "policies": [
+            {
+                "name": "policy1",
+                "id": policy1_id,
+                "enableLogging": True,
+                "networks": [
+                    {
+                        "networkUrl": "https://www.googleapis.com/compute/v1/projects/project1/global/networks/network1"
+                    }
+                ],
+            },
+            {
+                "name": "policy2",
+                "id": policy2_id,
+                "enableLogging": False,
+                "networks": [],
+            },
+        ]
+    }
+    client.policies().list_next.return_value = None
+
+    return client
+
+
+@patch(
+    "prowler.providers.gcp.lib.service.service.GCPService.__is_api_active__",
+    new=mock_is_api_active,
+)
+@patch(
+    "prowler.providers.gcp.lib.service.service.GCPService.__generate_client__",
+    new=mock_api_client,
+)
+class Test_DNS_Service:
+    def test__get_service__(self):
+        api_keys_client = DNS(set_mocked_gcp_provider())
+        assert api_keys_client.service == "dns"
+
+    def test__get_project_ids__(self):
+        api_keys_client = DNS(set_mocked_gcp_provider())
+        assert api_keys_client.project_ids.__class__.__name__ == "list"
+
+    def test__get_managed_zones__(self):
+        api_keys_client = DNS(set_mocked_gcp_provider(project_ids=[GCP_PROJECT_ID]))
+        assert len(api_keys_client.managed_zones) == 2
+
+        assert api_keys_client.managed_zones[0].name == "managed_zone1"
+        assert api_keys_client.managed_zones[0].id.__class__.__name__ == "str"
+        assert api_keys_client.managed_zones[0].dnssec
+        assert len(api_keys_client.managed_zones[0].key_specs) == 0
+        assert api_keys_client.managed_zones[0].project_id == GCP_PROJECT_ID
+
+        assert api_keys_client.managed_zones[1].name == "managed_zone2"
+        assert api_keys_client.managed_zones[1].id.__class__.__name__ == "str"
+        assert not api_keys_client.managed_zones[1].dnssec
+        assert len(api_keys_client.managed_zones[1].key_specs) == 0
+        assert api_keys_client.managed_zones[1].project_id == GCP_PROJECT_ID
+
+    def test__get_policies__(self):
+        api_keys_client = DNS(set_mocked_gcp_provider(project_ids=[GCP_PROJECT_ID]))
+        assert len(api_keys_client.policies) == 2
+
+        assert api_keys_client.policies[0].name == "policy1"
+        assert api_keys_client.policies[0].id.__class__.__name__ == "str"
+        assert api_keys_client.policies[0].logging
+        assert api_keys_client.policies[0].networks == ["network1"]
+        assert api_keys_client.policies[0].project_id == GCP_PROJECT_ID
+
+        assert api_keys_client.policies[1].name == "policy2"
+        assert api_keys_client.policies[1].id.__class__.__name__ == "str"
+        assert not api_keys_client.policies[1].logging
+        assert api_keys_client.policies[1].networks == []
+        assert api_keys_client.policies[1].project_id == GCP_PROJECT_ID
