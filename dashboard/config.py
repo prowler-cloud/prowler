551554bc582e15f3f289119f52887bfc08b1e7bd
pedrooot <pedromarting3@gmail.com>
2024-04-24 10:31:48 +0200

    feat(dashboard): improve logic on encoding format

--- dashboard/config.py
+++ dashboard/config.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 # Emojis to be used in the compliance table
 pass_emoji = "✅"
@@ -28,8 +29,8 @@ informational_color = "#3274d9"
 folder_path_overview = os.getcwd() + "/output"
 folder_path_compliance = os.getcwd() + "/output/compliance"
 
-# Encoding, if the os is windows, use cp1252
-if os.name == "nt":
+# Encoding, if the os is windows, use cp1252. Use utf-8 if it is running using python3
+if os.name == "nt" and "python" not in sys.argv[0].lower():
     encoding_format = "cp1252"
 else:
     encoding_format = "utf-8"
