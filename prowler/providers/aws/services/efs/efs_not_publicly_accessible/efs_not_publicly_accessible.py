897f7844e643a9456a30ca534a57a4613bd662b1
Sergio <sergio@prowler.com>
2024-04-30 10:03:45 +0200

    solve comments

--- prowler/providers/aws/services/efs/efs_not_publicly_accessible/efs_not_publicly_accessible.py
+++ prowler/providers/aws/services/efs/efs_not_publicly_accessible/efs_not_publicly_accessible.py
@@ -1,5 +1,6 @@
 from prowler.lib.check.models import Check, Check_Report_AWS
 from prowler.providers.aws.services.efs.efs_client import efs_client
+from prowler.providers.aws.services.efs.lib.lib import is_public_access_allowed
 
 
 class efs_not_publicly_accessible(Check):
@@ -20,9 +21,9 @@ class efs_not_publicly_accessible(Check):
                 report.status_extended = f"EFS {fs.id} doesn't have any policy which means it grants full access to any client."
             else:
                 for statement in fs.policy.get("Statement", []):
-                    if statement.get(
-                        "Effect"
-                    ) == "Allow" and self.is_public_access_allowed(statement):
+                    if statement.get("Effect") == "Allow" and is_public_access_allowed(
+                        statement
+                    ):
                         report.status = "FAIL"
                         report.status_extended = (
                             f"EFS {fs.id} has a policy which allows access to everyone."
@@ -30,37 +31,3 @@ class efs_not_publicly_accessible(Check):
                         break
             findings.append(report)
         return findings
-
-    def is_public_access_allowed(self, statement):
-        principal = statement.get("Principal")
-        if principal == "*" or (
-            isinstance(principal, dict) and "*" in principal.values()
-        ):
-            return not self.has_secure_conditions(statement)
-        return False
-
-    def has_secure_conditions(self, statement):
-        conditions = statement.get("Condition", {})
-        allowed_conditions = {
-            "aws:SourceArn",
-            "aws:SourceVpc",
-            "aws:SourceVpce",
-            "aws:SourceOwner",
-            "aws:SourceAccount",
-        }
-        if (
-            "Bool" in conditions
-            and conditions["Bool"].get("elasticfilesystem:AccessedViaMountTarget")
-            == "true"
-        ):
-            return True
-
-        # Check for conditions with nested keys
-        for _, conditions_dict in conditions.items():
-            for key, value in conditions_dict.items():
-                if isinstance(value, dict):
-                    if set(value.keys()).intersection(allowed_conditions):
-                        return True
-                elif key in allowed_conditions:
-                    return True
-        return False
--- tests/providers/aws/services/efs/efs_not_publicly_accessible/efs_not_publicly_accessible_test.py
+++ tests/providers/aws/services/efs/efs_not_publicly_accessible/efs_not_publicly_accessible_test.py
@@ -1,4 +1,3 @@
-from re import search
 from unittest import mock
 
 from prowler.providers.aws.services.efs.efs_service import FileSystem
@@ -101,12 +100,14 @@ class Test_efs_not_publicly_accessible:
             result = check.execute()
             assert len(result) == 1
             assert result[0].status == "PASS"
-            assert search(
-                "has a policy which does not allow access to everyone",
-                result[0].status_extended,
+            assert (
+                result[0].status_extended
+                == f"EFS {file_system_id} has a policy which does not allow access to everyone."
             )
             assert result[0].resource_id == file_system_id
             assert result[0].resource_arn == efs_arn
+            assert result[0].region == AWS_REGION
+            assert result[0].resource_tags == []
 
     def test_efs_valid_policy_with_mount_target_condition(self):
         efs_client = mock.MagicMock
@@ -133,12 +134,14 @@ class Test_efs_not_publicly_accessible:
             result = check.execute()
             assert len(result) == 1
             assert result[0].status == "PASS"
-            assert search(
-                "has a policy which does not allow access to everyone",
-                result[0].status_extended,
+            assert (
+                result[0].status_extended
+                == f"EFS {file_system_id} has a policy which does not allow access to everyone."
             )
             assert result[0].resource_id == file_system_id
             assert result[0].resource_arn == efs_arn
+            assert result[0].region == AWS_REGION
+            assert result[0].resource_tags == []
 
     def test_efs_valid_policy_with_source_arn_condition(self):
         efs_client = mock.MagicMock
@@ -165,12 +168,14 @@ class Test_efs_not_publicly_accessible:
             result = check.execute()
             assert len(result) == 1
             assert result[0].status == "PASS"
-            assert search(
-                "has a policy which does not allow access to everyone",
-                result[0].status_extended,
+            assert (
+                result[0].status_extended
+                == f"EFS {file_system_id} has a policy which does not allow access to everyone."
             )
             assert result[0].resource_id == file_system_id
             assert result[0].resource_arn == efs_arn
+            assert result[0].region == AWS_REGION
+            assert result[0].resource_tags == []
 
     def test_efs_invalid_policy(self):
         efs_client = mock.MagicMock
@@ -198,12 +203,14 @@ class Test_efs_not_publicly_accessible:
             result = check.execute()
             assert len(result) == 1
             assert result[0].status == "FAIL"
-            assert search(
-                "has a policy which allows access to everyone",
-                result[0].status_extended,
+            assert (
+                result[0].status_extended
+                == f"EFS {file_system_id} has a policy which allows access to everyone."
             )
             assert result[0].resource_id == file_system_id
             assert result[0].resource_arn == efs_arn
+            assert result[0].region == AWS_REGION
+            assert result[0].resource_tags == []
 
     def test_efs_no_policy(self):
         efs_client = mock.MagicMock
@@ -230,9 +237,11 @@ class Test_efs_not_publicly_accessible:
             result = check.execute()
             assert len(result) == 1
             assert result[0].status == "FAIL"
-            assert search(
-                "doesn't have any policy which means it grants full access to any client",
-                result[0].status_extended,
+            assert (
+                result[0].status_extended
+                == f"EFS {file_system_id} doesn't have any policy which means it grants full access to any client."
             )
             assert result[0].resource_id == file_system_id
             assert result[0].resource_arn == efs_arn
+            assert result[0].region == AWS_REGION
+            assert result[0].resource_tags == []
