4d2c77bc8edd48d43ec85129967e03594bbe45e2
Sergio <sergio@prowler.com>
2024-05-03 12:43:14 +0200

    fix tests part 3

--- tests/providers/gcp/services/iam/iam_organization_essential_contacts_configured/iam_organization_essential_contacts_configured_test.py
+++ tests/providers/gcp/services/iam/iam_organization_essential_contacts_configured/iam_organization_essential_contacts_configured_test.py
@@ -1,7 +1,7 @@
 from re import search
 from unittest import mock
 
-from prowler.providers.gcp.services.iam.iam_service import Organization
+from tests.providers.gcp.gcp_fixtures import set_mocked_gcp_audit_info
 
 
 class Test_iam_organization_essential_contacts_configured:
@@ -10,6 +10,9 @@ class Test_iam_organization_essential_contacts_configured:
         essentialcontacts_client.organizations = []
         essentialcontacts_client.region = "global"
         with mock.patch(
+            "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
+            new=set_mocked_gcp_audit_info(),
+        ), mock.patch(
             "prowler.providers.gcp.services.iam.iam_organization_essential_contacts_configured.iam_organization_essential_contacts_configured.essentialcontacts_client",
             new=essentialcontacts_client,
         ):
@@ -23,12 +26,12 @@ class Test_iam_organization_essential_contacts_configured:
 
     def test_iam_org_with_contacts(self):
         essentialcontacts_client = mock.MagicMock
-        essentialcontacts_client.organizations = [
-            Organization(id="test_id", name="test", contacts=True)
-        ]
         essentialcontacts_client.region = "global"
         essentialcontacts_client.organizations = []
         with mock.patch(
+            "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
+            new=set_mocked_gcp_audit_info(),
+        ), mock.patch(
             "prowler.providers.gcp.services.iam.iam_organization_essential_contacts_configured.iam_organization_essential_contacts_configured.essentialcontacts_client",
             new=essentialcontacts_client,
         ):
@@ -56,19 +59,23 @@ class Test_iam_organization_essential_contacts_configured:
 
     def test_iam_org_without_contacts(self):
         essentialcontacts_client = mock.MagicMock
-        essentialcontacts_client.organizations = [
-            Organization(id="test_id", name="test", contacts=False)
-        ]
         essentialcontacts_client.region = "global"
         essentialcontacts_client.organizations = []
         with mock.patch(
+            "prowler.providers.aws.lib.audit_info.audit_info.current_audit_info",
+            new=set_mocked_gcp_audit_info(),
+        ), mock.patch(
             "prowler.providers.gcp.services.iam.iam_organization_essential_contacts_configured.iam_organization_essential_contacts_configured.essentialcontacts_client",
             new=essentialcontacts_client,
         ):
             from prowler.providers.gcp.services.iam.iam_organization_essential_contacts_configured.iam_organization_essential_contacts_configured import (
                 iam_organization_essential_contacts_configured,
             )
+            from prowler.providers.gcp.services.iam.iam_service import Organization
 
+            essentialcontacts_client.organizations = [
+                Organization(id="test_id", name="test", contacts=False)
+            ]
             check = iam_organization_essential_contacts_configured()
             result = check.execute()
             assert len(result) == 1
