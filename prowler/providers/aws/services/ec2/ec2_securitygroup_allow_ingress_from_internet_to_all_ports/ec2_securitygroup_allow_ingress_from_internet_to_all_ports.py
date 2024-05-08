6f00bc35cc6148a2bd5efc015950c91f06c656f4
Sergio <sergio@prowler.com>
2024-05-08 18:50:09 +0200

    feat(ec2): add ec2_securitygroup_allow_ingress_from_internet_to_all_ports check

--- prowler/providers/aws/services/ec2/ec2_securitygroup_allow_ingress_from_internet_to_all_ports/ec2_securitygroup_allow_ingress_from_internet_to_all_ports.py
+++ prowler/providers/aws/services/ec2/ec2_securitygroup_allow_ingress_from_internet_to_all_ports/ec2_securitygroup_allow_ingress_from_internet_to_all_ports.py
@@ -1,10 +1,9 @@
 from prowler.lib.check.models import Check, Check_Report_AWS
 from prowler.providers.aws.services.ec2.ec2_client import ec2_client
-from prowler.providers.aws.services.ec2.lib.security_groups import check_security_group
 from prowler.providers.aws.services.vpc.vpc_client import vpc_client
 
 
-class ec2_securitygroup_allow_ingress_from_internet_to_any_port(Check):
+class ec2_securitygroup_allow_ingress_from_internet_to_all_ports(Check):
     def execute(self):
         findings = []
         for security_group in ec2_client.security_groups:
@@ -17,19 +16,14 @@ class ec2_securitygroup_allow_ingress_from_internet_to_any_port(Check):
                 report = Check_Report_AWS(self.metadata())
                 report.region = security_group.region
                 report.status = "PASS"
-                report.status_extended = f"Security group {security_group.name} ({security_group.id}) does not have any port open to the Internet."
+                report.status_extended = f"Security group {security_group.name} ({security_group.id}) does not have all ports open to the Internet."
                 report.resource_details = security_group.name
                 report.resource_id = security_group.id
                 report.resource_arn = security_group.arn
                 report.resource_tags = security_group.tags
-                if not security_group.public_ports:
-                    # Loop through every security group's ingress rule and check it
-                    for ingress_rule in security_group.ingress_rules:
-                        if check_security_group(
-                            ingress_rule, "-1", ports=None, any_address=True
-                        ):
-                            report.status = "FAIL"
-                            report.status_extended = f"Security group {security_group.name} ({security_group.id}) has at least one port open to the Internet."
+                if security_group.public_ports:
+                    report.status = "FAIL"
+                    report.status_extended = f"Security group {security_group.name} ({security_group.id}) has all ports open to the Internet."
                 findings.append(report)
 
         return findings
