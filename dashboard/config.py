e226cb06e004b1875c8c4203ab6a6b3f34a318f0
Pedro Martín <pedromarting3@gmail.com>
2024-05-08 13:14:47 +0200

    fix(encoding): set utf-8 encoding for csv (#3961)

--- dashboard/config.py
+++ dashboard/config.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 # Emojis to be used in the compliance table
 pass_emoji = "✅"
@@ -29,10 +28,6 @@ informational_color = "#3274d9"
 folder_path_overview = os.getcwd() + "/output"
 folder_path_compliance = os.getcwd() + "/output/compliance"
 
-# Encoding, if the os is windows, use cp1252. Use utf-8 if it is running using python3
-if os.name == "nt" and ".py" not in sys.argv[0].lower():
-    encoding_format = "cp1252"
-else:
-    encoding_format = "utf-8"
+encoding_format = "utf-8"
 # Error action, it is recommended to use "ignore" or "replace"
 error_action = "ignore"
