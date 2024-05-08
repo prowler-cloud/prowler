c197aa85940b9fa7a9d8f747c25ed556d52742fc
Pedro Martín <pedromarting3@gmail.com>
2024-05-08 09:02:43 +0200

    fix(dashboard): Handle encoding errors (#3847)

--- dashboard/config.py
+++ dashboard/config.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 # Emojis to be used in the compliance table
 pass_emoji = "✅"
@@ -28,5 +29,10 @@ informational_color = "#3274d9"
 folder_path_overview = os.getcwd() + "/output"
 folder_path_compliance = os.getcwd() + "/output/compliance"
 
-# Encoding
-encoding_format = "utf-8"
+# Encoding, if the os is windows, use cp1252. Use utf-8 if it is running using python3
+if os.name == "nt" and ".py" not in sys.argv[0].lower():
+    encoding_format = "cp1252"
+else:
+    encoding_format = "utf-8"
+# Error action, it is recommended to use "ignore" or "replace"
+error_action = "ignore"
