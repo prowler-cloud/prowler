49907aa486cd84862f49cb9db2c9b2b6ff77c1f1
pedrooot <pedromarting3@gmail.com>
2024-05-10 11:56:16 +0200

    fix tests

--- tests/providers/azure/lib/regions/regions_test.py
+++ tests/providers/azure/lib/regions/regions_test.py
@@ -1,11 +1,11 @@
 from azure.identity import AzureAuthorityHosts
-from msrestazure.azure_cloud import (
-    AZURE_CHINA_CLOUD,
-    AZURE_GERMAN_CLOUD,
-    AZURE_US_GOV_CLOUD,
-)
 
 from prowler.providers.azure.lib.regions.regions import get_regions_config
+
+AZURE_CHINA_CLOUD = "https://management.chinacloudapi.cn"
+AZURE_US_GOV_CLOUD = "https://management.usgovcloudapi.net"
+AZURE_GERMAN_CLOUD = "https://management.microsoftazure.de"
+AZURE_GENERIC_CLOUD = "https://management.azure.com"
 
 
 class Test_azure_regions:
@@ -19,29 +19,23 @@ class Test_azure_regions:
         expected_output = {
             "AzureCloud": {
                 "authority": None,
-                "base_url": "https://management.azure.com",
-                "credential_scopes": ["https://management.azure.com/.default"],
+                "base_url": AZURE_GENERIC_CLOUD,
+                "credential_scopes": [AZURE_GENERIC_CLOUD + "/.default"],
             },
             "AzureChinaCloud": {
                 "authority": AzureAuthorityHosts.AZURE_CHINA,
-                "base_url": AZURE_CHINA_CLOUD.endpoints.resource_manager,
-                "credential_scopes": [
-                    AZURE_CHINA_CLOUD.endpoints.resource_manager + "/.default"
-                ],
+                "base_url": AZURE_CHINA_CLOUD,
+                "credential_scopes": [AZURE_CHINA_CLOUD + "/.default"],
             },
             "AzureUSGovernment": {
                 "authority": AzureAuthorityHosts.AZURE_GOVERNMENT,
-                "base_url": AZURE_US_GOV_CLOUD.endpoints.resource_manager,
-                "credential_scopes": [
-                    AZURE_US_GOV_CLOUD.endpoints.resource_manager + "/.default"
-                ],
+                "base_url": AZURE_US_GOV_CLOUD,
+                "credential_scopes": [AZURE_US_GOV_CLOUD + "/.default"],
             },
             "AzureGermanCloud": {
                 "authority": AzureAuthorityHosts.AZURE_GERMANY,
-                "base_url": AZURE_GERMAN_CLOUD.endpoints.resource_manager,
-                "credential_scopes": [
-                    AZURE_GERMAN_CLOUD.endpoints.resource_manager + "/.default"
-                ],
+                "base_url": AZURE_GERMAN_CLOUD,
+                "credential_scopes": [AZURE_GERMAN_CLOUD + "/.default"],
             },
         }
 
