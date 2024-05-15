d3077f19e6fad20a5155182a16d1a0293f438c75
puchy22 <rubendltv22@gmail.com>
2024-05-15 16:02:51 +0200

    feat(lightsail) Add new check related with public access the addition of this new check delete the others two related with public access

--- prowler/providers/aws/services/lightsail/lightsail_instance_public/lightsail_instance_public.py
+++ prowler/providers/aws/services/lightsail/lightsail_instance_public/lightsail_instance_public.py
@@ -2,22 +2,25 @@ from prowler.lib.check.models import Check, Check_Report_AWS
 from prowler.providers.aws.services.lightsail.lightsail_client import lightsail_client
 
 
-class lightsail_instance_no_public_ip(Check):
-    def execute(self):
+class lightsail_instance_public(Check):
+    def execute(self) -> Check_Report_AWS:
         findings = []
+
         for arn_instance, instance in lightsail_client.instances.items():
             report = Check_Report_AWS(self.metadata())
             report.region = instance.region
             report.resource_id = instance.id
             report.resource_arn = arn_instance
             report.resource_tags = instance.tags
-            report.status = "PASS"
-            report.status_extended = (
-                f"Instance '{instance.name}' does not have a public IP."
-            )
-            if instance.public_ip != "":
-                report.status = "FAIL"
-                report.status_extended = f"Instance '{instance.name}' has a public IP."
+            report.status = "FAIL"
+            report.status_extended = f"Instance '{instance.name}' has public access"
+            if instance.public_ip == "" and not any(
+                port.access_type == "public" for port in instance.ports
+            ):
+                report.status = "PASS"
+                report.status_extended = (
+                    f"Instance '{instance.name}' has no public access"
+                )
 
             findings.append(report)
 
