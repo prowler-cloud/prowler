3dbb6af16c8ec4842c9e564e796bcbd6b391c9e1
puchy22 <rubendltv22@gmail.com>
2024-05-16 17:26:49 +0200

    feat(kafka): New check for encrypt in transit with TLS

---
+++ tests/providers/aws/services/kafka/kafka_cluster_in_transit_encryption_enabled/kafka_cluster_in_transit_encryption_enabled_test.py
@@ -0,0 +1,25 @@
+from unittest.mock import MagicMock, patch
+
+from tests.providers.aws.utils import AWS_REGION_US_EAST_1, set_mocked_aws_provider
+
+
+class Test_kafka_cluster_in_transit_encryption_enabled:
+    def test_kafka_no_clusters(self):
+        kafka_client = MagicMock
+        kafka_client.clusters = {}
+
+        with patch(
+            "prowler.providers.common.common.get_global_provider",
+            return_value=set_mocked_aws_provider([AWS_REGION_US_EAST_1]),
+        ), patch(
+            "prowler.providers.aws.services.kafka.kafka_service.Kafka",
+            new=kafka_client,
+        ):
+            from prowler.providers.aws.services.kafka.kafka_cluster_in_transit_encryption_enabled.kafka_cluster_in_transit_encryption_enabled import (
+                kafka_cluster_in_transit_encryption_enabled,
+            )
+
+            check = kafka_cluster_in_transit_encryption_enabled()
+            result = check.execute()
+
+            assert len(result) == 0
