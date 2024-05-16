3d19acbab2c8f3fdfbd724585620cdf76747d29f
puchy22 <rubendltv22@gmail.com>
2024-05-16 17:30:51 +0200

    feat(kafka): New check for unauthorized access

---
+++ tests/providers/aws/services/kafka/kafka_cluster_unrestricted_access_disabled/kafka_cluster_unrestricted_access_disabled_test.py
@@ -0,0 +1,25 @@
+from unittest.mock import MagicMock, patch
+
+from tests.providers.aws.utils import AWS_REGION_US_EAST_1, set_mocked_aws_provider
+
+
+class Test_kafka_cluster_unrestricted_access_disabled:
+    def test_kafka_no_clusters(self):
+        kafka_client = MagicMock
+        kafka_client.clusters = {}
+
+        with patch(
+            "prowler.providers.common.common.get_global_provider",
+            return_value=set_mocked_aws_provider([AWS_REGION_US_EAST_1]),
+        ), patch(
+            "prowler.providers.aws.services.kafka.kafka_service.Kafka",
+            new=kafka_client,
+        ):
+            from prowler.providers.aws.services.kafka.kafka_cluster_unrestricted_access_disabled.kafka_cluster_unrestricted_access_disabled import (
+                kafka_cluster_unrestricted_access_disabled,
+            )
+
+            check = kafka_cluster_unrestricted_access_disabled()
+            result = check.execute()
+
+            assert len(result) == 0
