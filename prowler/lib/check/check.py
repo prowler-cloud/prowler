98b96bffd9b9cefe289b736f00f0373954169962
Pepe Fagoaga <pepe@prowler.com>
2024-05-21 17:08:11 +0200

    feat(run_scan): WIP

--- prowler/lib/check/check.py
+++ prowler/lib/check/check.py
@@ -17,7 +17,7 @@ import prowler
 from prowler.config.config import orange_color
 from prowler.lib.check.compliance_models import load_compliance_framework
 from prowler.lib.check.custom_checks_metadata import update_check_metadata
-from prowler.lib.check.models import Check, load_check_metadata
+from prowler.lib.check.models import Check, Check_Report, load_check_metadata
 from prowler.lib.logger import logger
 from prowler.lib.mutelist.mutelist import mutelist_findings
 from prowler.lib.outputs.outputs import report
@@ -565,6 +565,7 @@ def execute_checks(
         audit_progress=0,
     )
 
+    # Refactor(CLI): This needs to be moved somewhere in the CLI
     if os.name != "nt":
         try:
             from resource import RLIMIT_NOFILE, getrlimit, setrlimit
@@ -671,6 +672,61 @@ def execute_checks(
                     )
                 bar()
             bar.title = f"-> {Fore.GREEN}Scan completed!{Style.RESET_ALL}"
+    return all_findings
+
+
+def scan(
+    checks_to_execute: list,
+    global_provider: Any,
+    custom_checks_metadata: Any,
+) -> list[Check_Report]:
+    try:
+        # List to store all the check's findings
+        all_findings = []
+        # Services and checks executed for the Audit Status
+        services_executed = set()
+        checks_executed = set()
+
+        # Initialize the Audit Metadata
+        # TODO: this should be done in the provider class
+        # Refactor(Core): Audit manager?
+        global_provider.audit_metadata = Audit_Metadata(
+            services_scanned=0,
+            expected_checks=checks_to_execute,
+            completed_checks=0,
+            audit_progress=0,
+        )
+
+        for check_name in checks_to_execute:
+            try:
+                # Recover service from check name
+                service = check_name.split("_")[0]
+
+                check_findings = execute(
+                    service,
+                    check_name,
+                    global_provider.type,
+                    global_provider.identity,
+                    services_executed,
+                    checks_executed,
+                    custom_checks_metadata,
+                )
+                all_findings.extend(check_findings)
+
+            # If check does not exists in the provider or is from another provider
+            except ModuleNotFoundError:
+                logger.error(
+                    f"Check '{check_name}' was not found for the {global_provider.type.upper()} provider"
+                )
+            except Exception as error:
+                logger.error(
+                    f"{check_name} - {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+                )
+    except Exception as error:
+        logger.error(
+            f"{check_name} - {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+        )
+
     return all_findings
 
 
