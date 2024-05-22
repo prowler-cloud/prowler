4afce673240fd7ae68e236366743e1c0b71f6ef3
puchy22 <rubendltv22@gmail.com>
2024-05-22 15:48:27 +0200

    chore(IAM): Change logic to skip after find a valid role in a subscription

--- prowler/providers/azure/services/iam/iam_custom_role_has_permissions_to_administer_resource_locks/iam_custom_role_has_permissions_to_administer_resource_locks.py
+++ prowler/providers/azure/services/iam/iam_custom_role_has_permissions_to_administer_resource_locks/iam_custom_role_has_permissions_to_administer_resource_locks.py
@@ -8,7 +8,11 @@ class iam_custom_role_has_permissions_to_administer_resource_locks(Check):
     def execute(self) -> Check_Report_Azure:
         findings = []
         for subscription, roles in iam_client.custom_roles.items():
+            exits_role_with_permission_over_locks = False
+
             for custom_role in roles:
+                if exits_role_with_permission_over_locks:
+                    break
                 report = Check_Report_Azure(self.metadata())
                 report.subscription = subscription
                 report.resource_id = custom_role.id
@@ -16,7 +20,6 @@ class iam_custom_role_has_permissions_to_administer_resource_locks(Check):
                 report.status = "FAIL"
                 report.status_extended = f"Role {custom_role.name} from subscription {subscription} has no permission to administer resource locks."
 
-                exits_role_with_permission_over_locks = False
                 for permission_item in custom_role.permissions:
                     if exits_role_with_permission_over_locks:
                         break
