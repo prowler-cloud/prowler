9eb9a23fe876282240e1083aa3f6633494656b61
puchy22 <rubendltv22@gmail.com>
2024-05-22 12:27:47 +0200

    test(kafka): Fix text import

--- tests/providers/aws/services/kafka/kafka_cluster_encryption_at_rest_uses_cmk/kafka_cluster_encryption_at_rest_uses_cmk_test.py
+++ tests/providers/aws/services/kafka/kafka_cluster_encryption_at_rest_uses_cmk/kafka_cluster_encryption_at_rest_uses_cmk_test.py
@@ -16,7 +16,7 @@ class Test_kafka_cluster_encryption_at_rest_uses_cmk:
             "prowler.providers.common.provider.Provider.get_global_provider",
             return_value=set_mocked_aws_provider([AWS_REGION_US_EAST_1]),
         ), patch(
-            "prowler.providers.aws.services.kafka.kafka_cluster_encryption_at_rest_uses_cmk.kafka_cluster_encryption_at_rest_uses_cmk.kms_client",
+            "prowler.providers.aws.services.kafka.kafka_service.Kafka",
             new=kafka_client,
         ):
             from prowler.providers.aws.services.kafka.kafka_cluster_encryption_at_rest_uses_cmk.kafka_cluster_encryption_at_rest_uses_cmk import (
