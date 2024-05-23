5fa85bdfd989c83b88c0ab1d0bc642ee18044f8b
puchy22 <rubendltv22@gmail.com>
2024-05-23 15:42:09 +0200

    chore(awslambda): Change check logic to not duplicate loops

--- prowler/providers/aws/services/awslambda/awslambda_function_not_directly_publicly_accessible_via_elbv2/awslambda_function_not_directly_publicly_accessible_via_elbv2.py
+++ prowler/providers/aws/services/awslambda/awslambda_function_not_directly_publicly_accessible_via_elbv2/awslambda_function_not_directly_publicly_accessible_via_elbv2.py
@@ -7,37 +7,6 @@ class awslambda_function_not_directly_publicly_accessible_via_elbv2(Check):
     def execute(self):
         findings = []
 
-        public_lambda_functions = {}
-        for target_group in elbv2_client.target_groups:
-            if (
-                target_group.loadbalancer.public
-                and target_group.target_type == "lambda"
-            ):
-                for function in awslambda_client.functions.values():
-                    public_access = False
-                    if function.arn == target_group.target and function.policy:
-                        for statement in function.policy["Statement"]:
-                            # Only check allow statements
-                            if statement["Effect"] == "Allow":
-                                if (
-                                    "*" in statement["Principal"]
-                                    or (
-                                        "AWS" in statement["Principal"]
-                                        and "*" in statement["Principal"]["AWS"]
-                                    )
-                                    or (
-                                        "CanonicalUser" in statement["Principal"]
-                                        and "*"
-                                        in statement["Principal"]["CanonicalUser"]
-                                    )
-                                ):
-                                    public_access = True
-                                    break
-                    if public_access:
-                        public_lambda_functions[target_group.target] = (
-                            target_group.loadbalancer
-                        )
-
         for function in awslambda_client.functions.values():
             report = Check_Report_AWS(self.metadata())
             report.region = function.region
@@ -47,9 +16,33 @@ class awslambda_function_not_directly_publicly_accessible_via_elbv2(Check):
             report.status = "PASS"
             report.status_extended = f"Lambda function {function.name} is not publicly accesible through an Internet facing Load Balancer."
 
-            if function.arn in public_lambda_functions:
-                report.status = "FAIL"
-                report.status_extended = f"Lambda function {function.name} is publicly accesible through an Internet facing Load Balancer through load balancer {public_lambda_functions[function.arn].dns}."
+            for target_group in elbv2_client.target_groups:
+                # Lambda targets group len must be 1 by definition
+                if (
+                    target_group.target_type == "lambda"
+                    and target_group.targets[0] == function.arn
+                ):
+                    for lb in elbv2_client.loadbalancersv2:
+                        if lb.arn == target_group.load_balancer_arn and lb.public:
+                            # Check that function policy is not public
+                            for statement in function.policy.get("Statement", []):
+                                if statement["Effect"] == "Allow":
+                                    if (
+                                        "*" in statement["Principal"]
+                                        or (
+                                            "*" in statement["Principal"].get("AWS", "")
+                                        )
+                                        or (
+                                            "*"
+                                            in statement["Principal"].get(
+                                                "CanonicalUser", ""
+                                            )
+                                        )
+                                    ):
+                                        report.status = "FAIL"
+                                        report.status_extended = f"Lambda function {function.name} is publicly accesible through an Internet facing Load Balancer through load balancer {lb.dns}."
+                                        break
+
             findings.append(report)
 
         return findings
