db17ca64c6061824d300552862ddc21bbecef6c2
pedrooot <pedromarting3@gmail.com>
2024-05-23 10:16:56 +0200

    chore(cli): start working on CLI

--- cli/cli.py
+++ cli/cli.py
@@ -1,42 +1,33 @@
+import time
+
 import typer
-from typing_extensions import Annotated
+from rich.progress import track
+
+from prowler.lib.banner import print_banner
+from prowler.lib.check.check import list_services, print_services
 
 app = typer.Typer()
+listers = typer.Typer()
+app.add_typer(listers, name="listers")
 
 
-@app.command()
-def create(username: str):
-    print(f"Creating user: {username}")
+@listers.command(
+    "list-services", help="List all the services that are supported by the tool."
+)
+def listServices(provider: str = "aws"):
+    print_services(list_services(provider))
 
 
-@app.command()
-def delete(
-    username: str,
-    force: Annotated[
-        bool, typer.Option(prompt="Are you sure you want to delete the user?")
-    ],
-):
-    if force:
-        print(f"Deleting user: {username}")
+@app.command("banner", help="Prints the banner of the tool.")
+def banner(show: bool = True):
+    total = 0
+    for value in track(range(100), description="Processing..."):
+        time.sleep(0.01)
+        total += 1
+    if show:
+        print_banner(show)
     else:
-        print("Operation cancelled")
-
-
-@app.command()
-def delete_all(
-    force: Annotated[
-        bool, typer.Option(prompt="Are you sure you want to delete ALL users?")
-    ],
-):
-    if force:
-        print("Deleting all users")
-    else:
-        print("Operation cancelled")
-
-
-@app.command()
-def init():
-    print("Initializing user database")
+        print("Banner is not shown.")
 
 
 if __name__ == "__main__":
