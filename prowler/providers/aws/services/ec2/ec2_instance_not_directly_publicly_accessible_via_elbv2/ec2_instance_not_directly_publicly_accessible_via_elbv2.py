7b108e6bb0b620c7469c3a8897b17ff0b8ed30f1
puchy22 <rubendltv22@gmail.com>
2024-05-24 15:59:27 +0200

    chore(awslambda): Checks all SGs before add report

--- prowler/providers/aws/services/ec2/ec2_instance_not_directly_publicly_accessible_via_elbv2/ec2_instance_not_directly_publicly_accessible_via_elbv2.py
+++ prowler/providers/aws/services/ec2/ec2_instance_not_directly_publicly_accessible_via_elbv2/ec2_instance_not_directly_publicly_accessible_via_elbv2.py
@@ -1,70 +1,12 @@
 from prowler.lib.check.models import Check, Check_Report_AWS
 from prowler.providers.aws.services.ec2.ec2_client import ec2_client
-from prowler.providers.aws.services.elbv2.elbv2_client import elbv2_client
-from prowler.providers.aws.services.vpc.vpc_client import vpc_client
 from prowler.providers.aws.services.ec2.lib.security_groups import check_security_group
-from prowler.providers.aws.services.ec2.ec2_securitygroup_allow_ingress_from_internet_to_all_ports import (
-    ec2_securitygroup_allow_ingress_from_internet_to_all_ports,
-)
+from prowler.providers.aws.services.elbv2.elbv2_client import elbv2_client
 
 
 class ec2_instance_not_directly_publicly_accessible_via_elbv2(Check):
     def execute(self):
         findings = []
-
-        public_instances = {}
-        public = False
-        for target_groups in elbv2_client.target_groups:
-            if (
-                target_groups.loadbalancer.public
-                and target_groups.target_type == "instance"
-            ):
-                for instance in ec2_client.instances:
-                    if instance.id == target_groups.target:
-                        for security_group in ec2_client.security_groups:
-                            if security_group.id in instance.security_groups:
-                                # Check if ignoring flag is set and if the VPC and the SG is in use
-                                if ec2_client.provider.scan_unused_services or (
-                                    security_group.vpc_id in vpc_client.vpcs
-                                    and vpc_client.vpcs[security_group.vpc_id].in_use
-                                    and len(security_group.network_interfaces) > 0
-                                ):
-                                    for ingress_rule in security_group.ingress_rules:
-                                        if check_security_group(
-                                            ingress_rule, "-1", any_address=True
-                                        ):
-                                            ec2_client.set_failed_check(
-                                                self.__class__.__name__,
-                                                security_group.arn,
-                                            )
-                                            public = True
-                                            break
-
-                                    if not public:
-                                        # only proceed if check "..._to_all_ports" did not run or did not FAIL to avoid to report open ports twice
-                                        if not ec2_client.is_failed_check(
-                                            ec2_securitygroup_allow_ingress_from_internet_to_all_ports.__name__,
-                                            security_group.arn,
-                                        ):
-                                            # Loop through every security group's ingress rule and check it
-                                            for (
-                                                ingress_rule
-                                            ) in security_group.ingress_rules:
-                                                if check_security_group(
-                                                    ingress_rule,
-                                                    "-1",
-                                                    ports=None,
-                                                    any_address=True,
-                                                ):
-                                                    public = True
-
-                                        else:
-                                            public = True
-                            if public:
-                                break
-            if public:
-                public_instances[target_groups.target] = target_groups.loadbalancer
-                break
 
         for instance in ec2_client.instances:
             if instance.state != "terminated":
@@ -76,9 +18,62 @@ class ec2_instance_not_directly_publicly_accessible_via_elbv2(Check):
                 report.status = "PASS"
                 report.status_extended = f"EC2 Instance {instance.id} is not publicly accesible through an Internet facing Load Balancer."
 
-                if instance.id in public_instances:
-                    report.status = "FAIL"
-                    report.status_extended = f"EC2 Instance {instance.id} is publicly accesible through an Internet facing Load Balancer through load balancer {public_instances[instance.id].dns}."
+                for target_group in elbv2_client.target_groups:
+                    if target_group.target_type == "instance" and any(
+                        target == instance.id for target in target_group.targets
+                    ):
+                        for lb in elbv2_client.loadbalancersv2:
+                            if lb.arn == target_group.load_balancer_arn and lb.public:
+                                # Find for the ec2 listener port
+                                listen_port = None
+                                for listener in lb.listeners:
+                                    for rule in listener.rules:
+                                        for action in getattr(rule, "actions", []):
+                                            if action.get("Type", "") == "forward" and (
+                                                any(
+                                                    tg.get("TargetGroupArn", "")
+                                                    == target_group.arn
+                                                    for tg in action["ForwardConfig"][
+                                                        "TargetGroups"
+                                                    ]
+                                                )
+                                                if "TargetGroups"
+                                                in action.get("ForwardConfig", {})
+                                                else action.get("TargetGroupArn", "")
+                                                == target_group.arn
+                                            ):
+                                                listen_port = listener.port
+                                                break
+
+                                # Check lb and ec2 security groups
+                                if listen_port:
+                                    safe_sgs = []
+                                    for sg in ec2_client.security_groups:
+                                        if any(
+                                            sg.id == lb_sg
+                                            for lb_sg in lb.security_groups
+                                        ) or any(
+                                            sg.id == instance_sg
+                                            for instance_sg in instance.security_groups
+                                        ):
+                                            for rule in sg.ingress_rules:
+                                                if check_security_group(
+                                                    ingress_rule=rule,
+                                                    protocol=rule.get(
+                                                        "IpProtocol", "tcp"
+                                                    ),
+                                                    ports=[listen_port],
+                                                    any_address=True,
+                                                ):
+                                                    safe_sgs.append(False)
+                                                    break
+                                                safe_sgs.append(True)
+                                    # If there is not any security group safe, the instance is publicly accessible
+                                    if not any(safe_sgs):
+                                        report.status = "FAIL"
+                                        report.status_extended = f"EC2 Instance {instance.id} is publicly accesible through an Internet facing Load Balancer '{lb.dns}'."
+                                        break
+
                 findings.append(report)
 
         return findings
