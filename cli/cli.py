093cb7e5a398ce540f2004590f51a502d31e8373
pedrooot <pedromarting3@gmail.com>
2024-05-24 10:47:59 +0200

    feat(cli): add service listing commands

--- cli/cli.py
+++ cli/cli.py
@@ -4,15 +4,42 @@ from prowler.lib.banner import print_banner
 from prowler.lib.check.check import list_services, print_services
 
 app = typer.Typer()
-listers = typer.Typer()
-app.add_typer(listers, name="listers")
+aws = typer.Typer()
+azure = typer.Typer()
+gcp = typer.Typer()
+kubernetes = typer.Typer()
+app.add_typer(aws, name="aws")
+app.add_typer(azure, name="azure")
+app.add_typer(gcp, name="gcp")
+app.add_typer(kubernetes, name="kubernetes")
 
 
-@listers.command(
-    "list-services", help="List all the services that are supported by the tool."
+@aws.command(
+    "list-services", help="List the AWS services that are supported by Prowler."
 )
-def listServices(provider: str = "aws"):
-    print_services(list_services(provider))
+def list_services_aws():
+    print_services(list_services("aws"))
+
+
+@azure.command(
+    "list-services", help="List the Azure services that are supported by Prowler."
+)
+def list_services_azure():
+    print_services(list_services("azure"))
+
+
+@gcp.command(
+    "list-services", help="List the GCP services that are supported by Prowler."
+)
+def list_services_gcp():
+    print_services(list_services("gcp"))
+
+
+@kubernetes.command(
+    "list-services", help="List the Kubernetes services that are supported by Prowler."
+)
+def list_services_kubernetes():
+    print_services(list_services("kubernetes"))
 
 
 @app.command("banner", help="Prints the banner of the tool.")
