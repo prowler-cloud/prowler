41bb430b98f86c5297fa12f594dc430a4448f203
Pepe Fagoaga <pepe@prowler.com>
2024-05-31 09:08:21 +0200

    refactor(aws_provider): call mutelist from diff opts

--- prowler/providers/aws/aws_provider.py
+++ prowler/providers/aws/aws_provider.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+import re
 import sys
 from argparse import Namespace
 from datetime import datetime
@@ -21,7 +22,10 @@ from prowler.config.config import (
 )
 from prowler.lib.check.check import list_modules, recover_checks_from_service
 from prowler.lib.logger import logger
-from prowler.lib.mutelist.mutelist import parse_mutelist_file
+from prowler.lib.mutelist.mutelist import (
+    get_mutelist_file_from_local_file,
+    validate_mutelist,
+)
 from prowler.lib.utils.utils import open_file, parse_json_file, print_boxes
 from prowler.providers.aws.config import (
     AWS_STS_GLOBAL_ENDPOINT_REGION,
@@ -30,6 +34,11 @@ from prowler.providers.aws.config import (
 )
 from prowler.providers.aws.lib.arn.arn import parse_iam_credentials_arn
 from prowler.providers.aws.lib.arn.models import ARN
+from prowler.providers.aws.lib.mutelist.mutelist import (
+    get_mutelist_file_from_dynamodb,
+    get_mutelist_file_from_lambda,
+    get_mutelist_file_from_s3,
+)
 from prowler.providers.aws.lib.organizations.organizations import (
     get_organizations_metadata,
     parse_organizations_metadata,
@@ -303,9 +312,28 @@ class AwsProvider(Provider):
         if not mutelist_path:
             mutelist_path = get_default_mute_file_path(self.type)
         if mutelist_path:
-            mutelist = parse_mutelist_file(
-                mutelist_path, self._session.current_session, self._identity.account
-            )
+            # Mutelist from S3 URI
+            if re.search("^s3://([^/]+)/(.*?([^/]+))$", mutelist_path):
+                mutelist = get_mutelist_file_from_s3(
+                    mutelist_path, self._session.current_session, self._identity.account
+                )
+            # Mutelist from Lambda Function ARN
+            elif re.search(r"^arn:(\w+):lambda:", mutelist_path):
+                mutelist = get_mutelist_file_from_lambda(
+                    mutelist_path, self._session.current_session, self._identity.account
+                )
+            # Mutelist from DynamoDB ARN
+            elif re.search(
+                r"^arn:aws(-cn|-us-gov)?:dynamodb:[a-z]{2}-[a-z-]+-[1-9]{1}:[0-9]{12}:table\/[a-zA-Z0-9._-]+$",
+                mutelist_path,
+            ):
+                mutelist = get_mutelist_file_from_dynamodb(
+                    mutelist_path, self._session.current_session, self._identity.account
+                )
+            else:
+                mutelist = get_mutelist_file_from_local_file(mutelist_path)
+
+            mutelist = validate_mutelist(mutelist)
         else:
             mutelist = {}
 
