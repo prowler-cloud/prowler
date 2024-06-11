f7d21bc81262d98d88cfabc5d36cf6275c0e293f
puchy22 <rubendltv22@gmail.com>
2024-06-11 10:30:18 +0200

    chore(network): Change logic to make only one finding per subcription

--- prowler/providers/azure/services/network/network_watcher_enabled/network_watcher_enabled.py
+++ prowler/providers/azure/services/network/network_watcher_enabled/network_watcher_enabled.py
@@ -5,24 +5,25 @@ from prowler.providers.azure.services.network.network_client import network_clie
 class network_watcher_enabled(Check):
     def execute(self) -> Check_Report_Azure:
         findings = []
-        nw_locations = []
         for subscription, network_watchers in network_client.network_watchers.items():
-            for network_watcher in network_watchers:
-                nw_locations.append(network_watcher.location)
-        for subscription, locations in network_client.locations.items():
-            for location in locations:
-                report = Check_Report_Azure(self.metadata())
-                report.subscription = subscription
-                report.resource_name = "Network Watcher"
-                report.location = location
-                report.resource_id = f"/subscriptions/{subscription}/providers/Microsoft.Network/networkWatchers/{location}"
-                if location not in nw_locations:
-                    report.status = "FAIL"
-                    report.status_extended = f"Network Watcher is not enabled for the location {location} in subscription {subscription}."
-                    findings.append(report)
-                else:
-                    report.status = "PASS"
-                    report.status_extended = f"Network Watcher is enabled for the location {location} in subscription {subscription}."
-                    findings.append(report)
+            report = Check_Report_Azure(self.metadata())
+            report.subscription = subscription
+            report.resource_name = "Network Watcher"
+            report.location = "Global"
+            report.resource_id = f"/subscriptions/{network_client.subscriptions[subscription]}/resourceGroups/NetworkWatcherRG/providers/Microsoft.Network/networkWatchers/NetworkWatcher_*"
+            report.status = "FAIL"
+            report.status_extended = f"Network Watcher is not enabled for all locations in subscription '{subscription}'."
+
+            if len(network_watchers) >= len(
+                network_client.locations[subscription]
+            ) and all(
+                location
+                in [network_watcher.location for network_watcher in network_watchers]
+                for location in network_client.locations[subscription]
+            ):
+                report.status = "PASS"
+                report.status_extended = f"Network Watcher is enabled for all locations in subscription '{subscription}'."
+
+            findings.append(report)
 
         return findings
