b2bf49538b00b450170d4832fe04bdb6986ce0a7
puchy22 <rubendltv22@gmail.com>
2024-06-13 11:48:14 +0200

    chore(network): Improve bussiness logic using sets to improve efficiency and readibility

--- prowler/providers/azure/services/network/network_watcher_enabled/network_watcher_enabled.py
+++ prowler/providers/azure/services/network/network_watcher_enabled/network_watcher_enabled.py
@@ -3,7 +3,7 @@ from prowler.providers.azure.services.network.network_client import network_clie
 
 
 class network_watcher_enabled(Check):
-    def execute(self) -> Check_Report_Azure:
+    def execute(self) -> list[Check_Report_Azure]:
         findings = []
         for subscription, network_watchers in network_client.network_watchers.items():
             report = Check_Report_Azure(self.metadata())
@@ -11,18 +11,17 @@ class network_watcher_enabled(Check):
             report.resource_name = "Network Watcher"
             report.location = "Global"
             report.resource_id = f"/subscriptions/{network_client.subscriptions[subscription]}/resourceGroups/NetworkWatcherRG/providers/Microsoft.Network/networkWatchers/NetworkWatcher_*"
-            report.status = "FAIL"
-            report.status_extended = f"Network Watcher is not enabled for all locations in subscription '{subscription}'."
 
-            if len(network_watchers) >= len(
-                network_client.locations[subscription]
-            ) and all(
-                location
-                in [network_watcher.location for network_watcher in network_watchers]
-                for location in network_client.locations[subscription]
-            ):
+            missing_locations = set(network_client.locations[subscription]) - set(
+                network_watcher.location for network_watcher in network_watchers
+            )
+
+            if missing_locations:
+                report.status = "FAIL"
+                report.status_extended = f"Network Watcher is not enabled for the following locations in subscription '{subscription}': {', '.join(missing_locations)}"
+            else:
                 report.status = "PASS"
-                report.status_extended = f"Network Watcher is enabled for all locations in subscription '{subscription}'."
+                report.status_extended = f"Network Watcher is enabled for all locations in subscription '{subscription}'"
 
             findings.append(report)
 
