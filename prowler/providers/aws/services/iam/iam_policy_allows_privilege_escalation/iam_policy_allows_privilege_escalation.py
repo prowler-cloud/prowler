85b8ef4050251bac5e82ff3be1e30e7a75504a57
puchy22 <rubendltv22@gmail.com>
2024-06-17 11:34:44 +0200

    chore(iam): Delete moved comments

--- prowler/providers/aws/services/iam/iam_policy_allows_privilege_escalation/iam_policy_allows_privilege_escalation.py
+++ prowler/providers/aws/services/iam/iam_policy_allows_privilege_escalation/iam_policy_allows_privilege_escalation.py
@@ -4,21 +4,6 @@ from prowler.providers.aws.services.iam.lib.privilege_escalation import (
     check_privilege_escalation,
 )
 
-# Does the tool analyze both users and roles, or just one or the other? --> Everything using AttachementCount.
-# Does the tool take a principal-centric or policy-centric approach? --> Policy-centric approach.
-# Does the tool handle resource constraints? --> We don't check if the policy affects all resources or not, we check everything.
-# Does the tool consider the permissions of service roles? --> Just checks policies.
-# Does the tool handle transitive privesc paths (i.e., attack chains)? --> Not yet.
-# Does the tool handle the DENY effect as expected? --> Yes, it checks DENY's statements with Action and NotAction.
-# Does the tool handle NotAction as expected? --> Yes
-# Does the tool handle Condition constraints? --> Not yet.
-# Does the tool handle service control policy (SCP) restrictions? --> No, SCP are within Organizations AWS API.
-
-# Based on:
-# - https://bishopfox.com/blog/privilege-escalation-in-aws
-# - https://github.com/RhinoSecurityLabs/Security-Research/blob/master/tools/aws-pentest-tools/aws_escalate.py
-# - https://rhinosecuritylabs.com/aws/aws-privilege-escalation-methods-mitigation/
-
 
 class iam_policy_allows_privilege_escalation(Check):
     def execute(self) -> Check_Report_AWS:
