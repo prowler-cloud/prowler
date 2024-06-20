da44c0eb8ebd1a7f4bb4d55b55eb77281485013d
puchy22 <rubendltv22@gmail.com>
2024-06-20 12:33:59 +0200

    test(s3): Add new case for private buckets for policy conditions

--- tests/providers/aws/services/s3/s3_bucket_public_access/s3_bucket_public_access_test.py
+++ tests/providers/aws/services/s3/s3_bucket_public_access/s3_bucket_public_access_test.py
@@ -350,6 +350,140 @@ class Test_s3_bucket_public_access:
                     assert result[0].region == AWS_REGION_US_EAST_1
 
     @mock_aws
+    def test_bucket_not_public_due_to_policy_conditions_from_vpc(self):
+        s3_client = client("s3", region_name=AWS_REGION_US_EAST_1)
+        bucket_name_us = "bucket_test_us"
+        s3_client.create_bucket(Bucket=bucket_name_us)
+        # Generate S3Control Client
+        s3control_client = client("s3control", region_name=AWS_REGION_US_EAST_1)
+        s3control_client.put_public_access_block(
+            AccountId=AWS_ACCOUNT_NUMBER,
+            PublicAccessBlockConfiguration={
+                "BlockPublicAcls": False,
+                "IgnorePublicAcls": False,
+                "BlockPublicPolicy": False,
+                "RestrictPublicBuckets": False,
+            },
+        )
+        s3_client.put_public_access_block(
+            Bucket=bucket_name_us,
+            PublicAccessBlockConfiguration={
+                "BlockPublicAcls": False,
+                "IgnorePublicAcls": False,
+                "BlockPublicPolicy": False,
+                "RestrictPublicBuckets": False,
+            },
+        )
+        public_write_policy = '{"Version": "2012-10-17","Id": "PutObjPolicy","Statement": [{"Sid": "PublicWritePolicy","Effect": "Allow","Principal": "*","Action": "s3:PutObject","Resource": "arn:aws:s3:::bucket_test_us/*","Condition": {"StringEquals": {"aws:SourceVpc": "vpc-123456"}}}]}'
+        s3_client.put_bucket_policy(
+            Bucket=bucket_name_us,
+            Policy=public_write_policy,
+        )
+        from prowler.providers.aws.services.s3.s3_service import S3, S3Control
+
+        aws_provider = set_mocked_aws_provider([AWS_REGION_US_EAST_1])
+
+        with mock.patch(
+            "prowler.providers.common.provider.Provider.get_global_provider",
+            return_value=aws_provider,
+        ):
+            with mock.patch(
+                "prowler.providers.aws.services.s3.s3_bucket_public_access.s3_bucket_public_access.s3_client",
+                new=S3(aws_provider),
+            ):
+                with mock.patch(
+                    "prowler.providers.aws.services.s3.s3_bucket_public_access.s3_bucket_public_access.s3control_client",
+                    new=S3Control(aws_provider),
+                ):
+                    # Test Check
+                    from prowler.providers.aws.services.s3.s3_bucket_public_access.s3_bucket_public_access import (
+                        s3_bucket_public_access,
+                    )
+
+                    check = s3_bucket_public_access()
+                    result = check.execute()
+
+                    assert len(result) == 1
+                    assert result[0].status == "PASS"
+                    assert (
+                        result[0].status_extended
+                        == f"S3 Bucket {bucket_name_us} is not public."
+                    )
+                    assert result[0].resource_id == bucket_name_us
+                    assert (
+                        result[0].resource_arn
+                        == f"arn:{aws_provider.identity.partition}:s3:::{bucket_name_us}"
+                    )
+                    assert result[0].region == AWS_REGION_US_EAST_1
+
+    @mock_aws
+    def test_bucket_not_public_due_to_policy_conditions_from_private_ip(self):
+        s3_client = client("s3", region_name=AWS_REGION_US_EAST_1)
+        bucket_name_us = "bucket_test_us"
+        s3_client.create_bucket(Bucket=bucket_name_us)
+        # Generate S3Control Client
+        s3control_client = client("s3control", region_name=AWS_REGION_US_EAST_1)
+        s3control_client.put_public_access_block(
+            AccountId=AWS_ACCOUNT_NUMBER,
+            PublicAccessBlockConfiguration={
+                "BlockPublicAcls": False,
+                "IgnorePublicAcls": False,
+                "BlockPublicPolicy": False,
+                "RestrictPublicBuckets": False,
+            },
+        )
+        s3_client.put_public_access_block(
+            Bucket=bucket_name_us,
+            PublicAccessBlockConfiguration={
+                "BlockPublicAcls": False,
+                "IgnorePublicAcls": False,
+                "BlockPublicPolicy": False,
+                "RestrictPublicBuckets": False,
+            },
+        )
+        public_write_policy = '{"Version": "2012-10-17","Id": "PutObjPolicy","Statement": [{"Sid": "PublicWritePolicy","Effect": "Allow","Principal": "*","Action": "s3:PutObject","Resource": "arn:aws:s3:::bucket_test_us/*","Condition": {"IpAddress": {"aws:SourceIp": "10.0.0.25"}}}]}'
+        s3_client.put_bucket_policy(
+            Bucket=bucket_name_us,
+            Policy=public_write_policy,
+        )
+        from prowler.providers.aws.services.s3.s3_service import S3, S3Control
+
+        aws_provider = set_mocked_aws_provider([AWS_REGION_US_EAST_1])
+
+        with mock.patch(
+            "prowler.providers.common.provider.Provider.get_global_provider",
+            return_value=aws_provider,
+        ):
+            with mock.patch(
+                "prowler.providers.aws.services.s3.s3_bucket_public_access.s3_bucket_public_access.s3_client",
+                new=S3(aws_provider),
+            ):
+                with mock.patch(
+                    "prowler.providers.aws.services.s3.s3_bucket_public_access.s3_bucket_public_access.s3control_client",
+                    new=S3Control(aws_provider),
+                ):
+                    # Test Check
+                    from prowler.providers.aws.services.s3.s3_bucket_public_access.s3_bucket_public_access import (
+                        s3_bucket_public_access,
+                    )
+
+                    check = s3_bucket_public_access()
+                    result = check.execute()
+
+                    assert len(result) == 1
+                    assert result[0].status == "PASS"
+                    assert (
+                        result[0].status_extended
+                        == f"S3 Bucket {bucket_name_us} is not public."
+                    )
+                    assert result[0].resource_id == bucket_name_us
+                    assert (
+                        result[0].resource_arn
+                        == f"arn:{aws_provider.identity.partition}:s3:::{bucket_name_us}"
+                    )
+                    assert result[0].region == AWS_REGION_US_EAST_1
+
+    @mock_aws
     def test_bucket_not_public(self):
         s3_client = client("s3", region_name=AWS_REGION_US_EAST_1)
         bucket_name_us = "bucket_test_us"
