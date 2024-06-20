9fb775be61b6cd48ef85dfdca2261c1fcb298242
puchy22 <rubendltv22@gmail.com>
2024-06-20 12:31:10 +0200

    test(iam): Cases for private and public conditions

--- tests/providers/aws/services/iam/lib/policy_test.py
+++ tests/providers/aws/services/iam/lib/policy_test.py
@@ -1,4 +1,6 @@
 from prowler.providers.aws.services.iam.lib.policy import (
+    check_full_service_access,
+    has_private_conditions,
     is_policy_cross_account,
     is_policy_public,
 )
@@ -89,3 +91,76 @@ class Test_Policy:
         assert is_policy_public(policy2)
         assert not is_policy_public(policy3)
         assert not is_policy_public(policy4)
+
+    def test_check_full_service_access(self):
+        policy1 = {
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Action": "s3:*",
+                    "Resource": "*",
+                }
+            ]
+        }
+        policy2 = {
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Action": "s3:Get*",
+                    "Resource": "*",
+                }
+            ]
+        }
+        policy3 = {
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Action": "s3:*",
+                    "Resource": "arn:aws:s3:::example_bucket/*",
+                }
+            ]
+        }
+        policy4 = {
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Action": "s3:*",
+                    "Resource": "arn:aws:s3:::example_bucket",
+                }
+            ]
+        }
+
+        assert check_full_service_access("s3", policy1)
+        assert not check_full_service_access("s3", policy2)
+        assert not check_full_service_access("s3", policy3)
+        assert not check_full_service_access("s3", policy4)
+
+    def test_statemetns_with_private_conditions(self):
+        statement_no_conditions = {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": "*",
+        }
+        statement_condition_from_vpc = {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": "*",
+            "Condition": {"StringEquals": {"aws:SourceVpc": "vpc-123456"}},
+        }
+        statement_condition_public_IP = {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": "*",
+            "Condition": {"IpAddress": {"aws:SourceIp": "1.2.3.4"}},
+        }
+        statement_condition_private_IP = {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": "*",
+            "Condition": {"IpAddress": {"aws:SourceIp": "192.168.4.234"}},
+        }
+
+        assert not has_private_conditions(statement_no_conditions)
+        assert has_private_conditions(statement_condition_from_vpc)
+        assert not has_private_conditions(statement_condition_public_IP)
+        assert has_private_conditions(statement_condition_private_IP)
