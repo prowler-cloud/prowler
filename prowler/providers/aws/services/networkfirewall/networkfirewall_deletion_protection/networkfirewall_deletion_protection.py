630d980861e977ab00c2516a766fd3a1b77d0d96
sansns-aws <107269923+sansns@users.noreply.github.com>
2024-06-27 10:08:31 -0400

    feat(NetworkFirewall): Add Deletion Protection Check (#4318)
    Co-authored-by: Sergio <sergio@prowler.com>

---
+++ prowler/providers/aws/services/networkfirewall/networkfirewall_deletion_protection/networkfirewall_deletion_protection.py
@@ -0,0 +1,26 @@
+from prowler.lib.check.models import Check, Check_Report_AWS
+from prowler.providers.aws.services.networkfirewall.networkfirewall_client import (
+    networkfirewall_client,
+)
+
+
+class networkfirewall_deletion_protection(Check):
+    def execute(self):
+        findings = []
+        for firewall in networkfirewall_client.network_firewalls:
+            report = Check_Report_AWS(self.metadata())
+            report.region = firewall.region
+            report.resource_id = firewall.name
+            report.resource_arn = firewall.arn
+            report.resource_tags = firewall.tags
+            report.status = "FAIL"
+            report.status_extended = f"Network Firewall {firewall.name} does not have deletion protection enabled."
+            if firewall.deletion_protection:
+                report.status = "PASS"
+                report.status_extended = (
+                    f"Network Firewall {firewall.name} has deletion protection enabled."
+                )
+
+            findings.append(report)
+
+        return findings
