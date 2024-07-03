e6ae53932385e7803c56ad12b4bee757f92f61fa
Rubén De la Torre Vico <rubendltv22@gmail.com>
2024-07-03 21:51:19 +0200

    feat(IAM): Add inline policies checks and improve custom policy checks (#4255)

--- prowler/providers/aws/services/iam/iam_inline_policy_no_full_access_to_cloudtrail/iam_inline_policy_no_full_access_to_cloudtrail.py
+++ prowler/providers/aws/services/iam/iam_inline_policy_no_full_access_to_cloudtrail/iam_inline_policy_no_full_access_to_cloudtrail.py
@@ -5,25 +5,27 @@ from prowler.providers.aws.services.iam.lib.policy import check_full_service_acc
 critical_service = "cloudtrail"
 
 
-class iam_policy_no_full_access_to_cloudtrail(Check):
+class iam_inline_policy_no_full_access_to_cloudtrail(Check):
     def execute(self) -> Check_Report_AWS:
         findings = []
+
         for policy in iam_client.policies:
-            # Check only custom policies
-            if policy.type == "Custom":
+            # Check only inline policies
+            if policy.type == "Inline":
                 report = Check_Report_AWS(self.metadata())
                 report.region = iam_client.region
                 report.resource_arn = policy.arn
                 report.resource_id = policy.name
                 report.resource_tags = policy.tags
                 report.status = "PASS"
-                report.status_extended = f"Custom Policy {policy.name} does not allow '{critical_service}:*' privileges."
+                report.status_extended = f"Inline Policy {policy.name} does not allow '{critical_service}:*' privileges."
 
                 if policy.document and check_full_service_access(
                     critical_service, policy.document
                 ):
                     report.status = "FAIL"
-                    report.status_extended = f"Custom Policy {policy.name} allows '{critical_service}:*' privileges."
+                    report.status_extended = f"Inline Policy {policy.name} allows '{critical_service}:*' privileges to all resources."
 
                 findings.append(report)
+
         return findings
