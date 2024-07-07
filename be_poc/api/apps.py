01a0d07151bf12cad45905f1ba717de6441363e4
Sophia Dao <sophia@prowler.com>
2024-07-07 09:50:48 -0500

    chore: clean-up repo

--- be_poc/api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
--- be_poc/be_poc/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'be_poc.settings')
-
-application = get_wsgi_application()
