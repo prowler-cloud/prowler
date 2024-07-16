bbb56407af75b27a20704cd9be50a1660f0c21b9
Rubén De la Torre Vico <rubendltv22@gmail.com>
2024-07-16 13:53:26 +0200

    chore(compute): Rename tests to match with checks

--- prowler/providers/gcp/services/compute/compute_network_default_in_use/compute_network_default_in_use.py
+++ prowler/providers/gcp/services/compute/compute_network_default_in_use/compute_network_default_in_use.py
@@ -5,32 +5,32 @@ from prowler.providers.gcp.services.compute.compute_client import compute_client
 class compute_network_default_in_use(Check):
     def execute(self) -> Check_Report_GCP:
         findings = []
-        # Check if default network is in use for each project
         projects_with_default_network = set()
+
+        # Identify projects with the default network
         for network in compute_client.networks:
             if network.name == "default":
                 projects_with_default_network.add(network.project_id)
-                report = Check_Report_GCP(self.metadata())
-                report.project_id = network.project_id
-                report.resource_id = "default"
-                report.resource_name = "default"
-                report.location = "global"
+
+        # Generate reports for all projects
+        for project in compute_client.project_ids:
+            report = Check_Report_GCP(self.metadata())
+            report.project_id = project
+            report.resource_id = "default"
+            report.resource_name = "default"
+            report.location = "global"
+
+            if project in projects_with_default_network:
                 report.status = "FAIL"
                 report.status_extended = (
-                    f"Default network is in use in project {network.project_id}."
+                    f"Default network is in use in project {project}."
                 )
-                findings.append(report)
-
-        for project in compute_client.project_ids:
-            if project not in projects_with_default_network:
-                report = Check_Report_GCP(self.metadata())
-                report.project_id = project
-                report.resource_id = "default"
-                report.resource_name = "default"
-                report.location = "global"
+            else:
                 report.status = "PASS"
                 report.status_extended = (
                     f"Default network does not exist in project {project}."
                 )
+
+            findings.append(report)
 
         return findings
