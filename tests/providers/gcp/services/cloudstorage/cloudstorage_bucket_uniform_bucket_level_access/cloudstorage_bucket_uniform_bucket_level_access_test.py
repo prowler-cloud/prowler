4732feaf0fe85c4a04ac5d982c33b168435328cd
Daniel Barranquero <danielbo2001@gmail.com>
2024-07-17 09:34:24 +0200

    test(cloudstorage): add test for bucket with uniform level access

--- tests/providers/gcp/services/cloudstorage/cloudstorage_bucket_uniform_bucket_level_access/cloudstorage_bucket_uniform_bucket_level_access_test.py
+++ tests/providers/gcp/services/cloudstorage/cloudstorage_bucket_uniform_bucket_level_access/cloudstorage_bucket_uniform_bucket_level_access_test.py
@@ -1,10 +1,9 @@
 from unittest import mock
-import re
 
 from tests.providers.gcp.gcp_fixtures import GCP_PROJECT_ID, set_mocked_gcp_provider
 
-class TestCloudStorageBucketPublicAccess:
-    def test_bucket_public_access(self):
+class TestCloudStorageBucketUniformBucketLevelAccess:
+    def test_bucket_with_uniform_bucket_level_access_enabled(self):
         cloudstorage_client = mock.MagicMock()
         
         cloudstorage_client.project_ids = [GCP_PROJECT_ID]
@@ -17,7 +16,7 @@ class TestCloudStorageBucketPublicAccess:
             id="bucket1",
             region="US",
             uniform_bucket_level_access=True,
-            public=True,
+            public=False,
             project_id=GCP_PROJECT_ID,
         )]
 
@@ -25,20 +24,20 @@ class TestCloudStorageBucketPublicAccess:
             "prowler.providers.common.provider.Provider.get_global_provider",
             return_value=set_mocked_gcp_provider(),
         ), mock.patch(
-            "prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_public_access.cloudstorage_bucket_public_access.cloudstorage_client",
+            "prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_uniform_bucket_level_access.cloudstorage_bucket_uniform_bucket_level_access.cloudstorage_client",
             new=cloudstorage_client,
         ):
-            from prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_public_access.cloudstorage_bucket_public_access import cloudstorage_bucket_public_access
+            from prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_uniform_bucket_level_access.cloudstorage_bucket_uniform_bucket_level_access import cloudstorage_bucket_uniform_bucket_level_access
 
-            check = cloudstorage_bucket_public_access()
+            check = cloudstorage_bucket_uniform_bucket_level_access()
             result = check.execute()
 
             assert len(result) == 1
-            assert result[0].status == "FAIL"
-            assert re.search("Bucket .* is publicly accessible", result[0].status_extended)
+            assert result[0].status == "PASS"
+            assert "Bucket bucket1 has uniform Bucket Level Access enabled." in result[0].status_extended
             assert result[0].resource_id == "bucket1"
 
-    def test_bucket_no_public_access(self):
+    def test_bucket_with_uniform_bucket_level_access_disabled(self):
         cloudstorage_client = mock.MagicMock()
         
         cloudstorage_client.project_ids = [GCP_PROJECT_ID]
@@ -50,7 +49,7 @@ class TestCloudStorageBucketPublicAccess:
             name="bucket2",
             id="bucket2",
             region="US",
-            uniform_bucket_level_access=True,
+            uniform_bucket_level_access=False,
             public=False,
             project_id=GCP_PROJECT_ID,
         )]
@@ -59,41 +58,36 @@ class TestCloudStorageBucketPublicAccess:
             "prowler.providers.common.provider.Provider.get_global_provider",
             return_value=set_mocked_gcp_provider(),
         ), mock.patch(
-            "prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_public_access.cloudstorage_bucket_public_access.cloudstorage_client",
+            "prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_uniform_bucket_level_access.cloudstorage_bucket_uniform_bucket_level_access.cloudstorage_client",
             new=cloudstorage_client,
         ):
-            from prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_public_access.cloudstorage_bucket_public_access import cloudstorage_bucket_public_access
+            from prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_uniform_bucket_level_access.cloudstorage_bucket_uniform_bucket_level_access import cloudstorage_bucket_uniform_bucket_level_access
 
-            check = cloudstorage_bucket_public_access()
+            check = cloudstorage_bucket_uniform_bucket_level_access()
             result = check.execute()
 
             assert len(result) == 1
-            assert result[0].status == "PASS"
-            assert re.search("Bucket .* is not publicly accessible", result[0].status_extended)
+            assert result[0].status == "FAIL"
+            assert "Bucket bucket2 has uniform Bucket Level Access disabled." in result[0].status_extended
             assert result[0].resource_id == "bucket2"
 
     def test_no_buckets(self):
         cloudstorage_client = mock.MagicMock()
-        logging_client = mock.MagicMock()
         
         cloudstorage_client.project_ids = [GCP_PROJECT_ID]
         cloudstorage_client.region = "global"
-        logging_client.sinks = [mock.MagicMock(destination="storage.googleapis.com/bucket1")]
-
         cloudstorage_client.buckets = []
 
         with mock.patch(
             "prowler.providers.common.provider.Provider.get_global_provider",
             return_value=set_mocked_gcp_provider(),
         ), mock.patch(
-            "prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_public_access.cloudstorage_bucket_public_access.cloudstorage_client",
+            "prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_uniform_bucket_level_access.cloudstorage_bucket_uniform_bucket_level_access.cloudstorage_client",
             new=cloudstorage_client,
         ):
-            from prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_public_access.cloudstorage_bucket_public_access import (
-                cloudstorage_bucket_public_access,
-            )
+            from prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_uniform_bucket_level_access.cloudstorage_bucket_uniform_bucket_level_access import cloudstorage_bucket_uniform_bucket_level_access
 
-            check = cloudstorage_bucket_public_access()
+            check = cloudstorage_bucket_uniform_bucket_level_access()
             result = check.execute()
 
-            assert len(result) == 0        
+            assert len(result) == 0
