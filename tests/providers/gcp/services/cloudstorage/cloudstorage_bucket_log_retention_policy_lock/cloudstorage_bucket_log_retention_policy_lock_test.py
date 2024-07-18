39a7960e77172b575a8efdb30717a65e5735e047
Daniel Barranquero <danielbo2001@gmail.com>
2024-07-18 13:03:12 +0200

    test(cloudstorage): Suggested changes on bucket retention policy test done

--- tests/providers/gcp/services/cloudstorage/cloudstorage_bucket_log_retention_policy_lock/cloudstorage_bucket_log_retention_policy_lock_test.py
+++ tests/providers/gcp/services/cloudstorage/cloudstorage_bucket_log_retention_policy_lock/cloudstorage_bucket_log_retention_policy_lock_test.py
@@ -1,7 +1,11 @@
 import re
 from unittest import mock
 
-from tests.providers.gcp.gcp_fixtures import GCP_PROJECT_ID, set_mocked_gcp_provider
+from tests.providers.gcp.gcp_fixtures import (
+    GCP_PROJECT_ID,
+    GCP_US_CENTER1_LOCATION,
+    set_mocked_gcp_provider,
+)
 
 
 class TestCloudStorageBucketLogRetentionPolicyLock:
@@ -22,30 +26,28 @@ class TestCloudStorageBucketLogRetentionPolicyLock:
             from prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_log_retention_policy_lock.cloudstorage_bucket_log_retention_policy_lock import (
                 cloudstorage_bucket_log_retention_policy_lock,
             )
+            from prowler.providers.gcp.services.cloudstorage.cloudstorage_service import (
+                Bucket,
+            )
+            from prowler.providers.gcp.services.logging.logging_service import Sink
 
             cloudstorage_client.project_ids = [GCP_PROJECT_ID]
-            cloudstorage_client.region = "global"
-
-            from prowler.providers.gcp.services.logging.logging_service import Sink
+            cloudstorage_client.region = GCP_US_CENTER1_LOCATION
 
             logging_client.sinks = [
                 Sink(
                     name="sink1",
-                    destination="storage.googleapis.com/bucket1",
+                    destination="storage.googleapis.com/example-bucket",
                     filter="all",
                     project_id=GCP_PROJECT_ID,
                 )
             ]
 
-            from prowler.providers.gcp.services.cloudstorage.cloudstorage_service import (
-                Bucket,
-            )
-
             cloudstorage_client.buckets = [
                 Bucket(
-                    name="bucket1",
-                    id="bucket1",
-                    region="US",
+                    name="example-bucket",
+                    id="example-bucket",
+                    region=GCP_US_CENTER1_LOCATION,
                     uniform_bucket_level_access=True,
                     public=True,
                     retention_policy={"isLocked": True},
@@ -59,11 +61,12 @@ class TestCloudStorageBucketLogRetentionPolicyLock:
             assert len(result) == 1
             assert result[0].status == "PASS"
             assert re.search(
-                "Retention Policy with Bucket Lock", result[0].status_extended
+                f"Log Sink Bucket {cloudstorage_client.buckets[0].name} has a Retention Policy with Bucket Lock",
+                result[0].status_extended,
             )
-            assert result[0].resource_id == "bucket1"
-            assert result[0].resource_name == "bucket1"
-            assert result[0].location == "US"
+            assert result[0].resource_id == "example-bucket"
+            assert result[0].resource_name == "example-bucket"
+            assert result[0].location == GCP_US_CENTER1_LOCATION
             assert result[0].project_id == GCP_PROJECT_ID
 
     def test_bucket_with_retention_policy_without_lock(self):
@@ -83,30 +86,28 @@ class TestCloudStorageBucketLogRetentionPolicyLock:
             from prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_log_retention_policy_lock.cloudstorage_bucket_log_retention_policy_lock import (
                 cloudstorage_bucket_log_retention_policy_lock,
             )
+            from prowler.providers.gcp.services.cloudstorage.cloudstorage_service import (
+                Bucket,
+            )
+            from prowler.providers.gcp.services.logging.logging_service import Sink
 
             cloudstorage_client.project_ids = [GCP_PROJECT_ID]
-            cloudstorage_client.region = "global"
-
-            from prowler.providers.gcp.services.logging.logging_service import Sink
+            cloudstorage_client.region = GCP_US_CENTER1_LOCATION
 
             logging_client.sinks = [
                 Sink(
                     name="sink1",
-                    destination="storage.googleapis.com/bucket1",
+                    destination="storage.googleapis.com/example-bucket",
                     filter="all",
                     project_id=GCP_PROJECT_ID,
                 )
             ]
 
-            from prowler.providers.gcp.services.cloudstorage.cloudstorage_service import (
-                Bucket,
-            )
-
             cloudstorage_client.buckets = [
                 Bucket(
-                    name="bucket1",
-                    id="bucket1",
-                    region="US",
+                    name="example-bucket",
+                    id="example-bucket",
+                    region=GCP_US_CENTER1_LOCATION,
                     uniform_bucket_level_access=True,
                     public=True,
                     retention_policy={"isLocked": False},
@@ -120,11 +121,12 @@ class TestCloudStorageBucketLogRetentionPolicyLock:
             assert len(result) == 1
             assert result[0].status == "FAIL"
             assert re.search(
-                "no Retention Policy but without Bucket Lock", result[0].status_extended
+                f"Log Sink Bucket {cloudstorage_client.buckets[0].name} has no Retention Policy but without Bucket Lock",
+                result[0].status_extended,
             )
-            assert result[0].resource_id == "bucket1"
-            assert result[0].resource_name == "bucket1"
-            assert result[0].location == "US"
+            assert result[0].resource_id == "example-bucket"
+            assert result[0].resource_name == "example-bucket"
+            assert result[0].location == GCP_US_CENTER1_LOCATION
             assert result[0].project_id == GCP_PROJECT_ID
 
     def test_bucket_without_retention_policy(self):
@@ -144,30 +146,28 @@ class TestCloudStorageBucketLogRetentionPolicyLock:
             from prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_log_retention_policy_lock.cloudstorage_bucket_log_retention_policy_lock import (
                 cloudstorage_bucket_log_retention_policy_lock,
             )
+            from prowler.providers.gcp.services.cloudstorage.cloudstorage_service import (
+                Bucket,
+            )
+            from prowler.providers.gcp.services.logging.logging_service import Sink
 
             cloudstorage_client.project_ids = [GCP_PROJECT_ID]
-            cloudstorage_client.region = "global"
-
-            from prowler.providers.gcp.services.logging.logging_service import Sink
+            cloudstorage_client.region = GCP_US_CENTER1_LOCATION
 
             logging_client.sinks = [
                 Sink(
                     name="sink1",
-                    destination="storage.googleapis.com/bucket1",
+                    destination="storage.googleapis.com/example-bucket",
                     filter="all",
                     project_id=GCP_PROJECT_ID,
                 )
             ]
 
-            from prowler.providers.gcp.services.cloudstorage.cloudstorage_service import (
-                Bucket,
-            )
-
             cloudstorage_client.buckets = [
                 Bucket(
-                    name="bucket1",
-                    id="bucket1",
-                    region="US",
+                    name="example-bucket",
+                    id="example-bucket",
+                    region=GCP_US_CENTER1_LOCATION,
                     uniform_bucket_level_access=True,
                     public=True,
                     retention_policy=None,
@@ -180,10 +180,13 @@ class TestCloudStorageBucketLogRetentionPolicyLock:
 
             assert len(result) == 1
             assert result[0].status == "FAIL"
-            assert re.search("no Retention Policy", result[0].status_extended)
-            assert result[0].resource_id == "bucket1"
-            assert result[0].resource_name == "bucket1"
-            assert result[0].location == "US"
+            assert re.search(
+                f"Log Sink Bucket {cloudstorage_client.buckets[0].name} has no Retention Policy",
+                result[0].status_extended,
+            )
+            assert result[0].resource_id == "example-bucket"
+            assert result[0].resource_name == "example-bucket"
+            assert result[0].location == GCP_US_CENTER1_LOCATION
             assert result[0].project_id == GCP_PROJECT_ID
 
     def test_no_buckets(self):
@@ -203,16 +206,15 @@ class TestCloudStorageBucketLogRetentionPolicyLock:
             from prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_log_retention_policy_lock.cloudstorage_bucket_log_retention_policy_lock import (
                 cloudstorage_bucket_log_retention_policy_lock,
             )
+            from prowler.providers.gcp.services.logging.logging_service import Sink
 
             cloudstorage_client.project_ids = [GCP_PROJECT_ID]
-            cloudstorage_client.region = "global"
-
-            from prowler.providers.gcp.services.logging.logging_service import Sink
+            cloudstorage_client.region = GCP_US_CENTER1_LOCATION
 
             logging_client.sinks = [
                 Sink(
                     name="sink1",
-                    destination="storage.googleapis.com/bucket1",
+                    destination="storage.googleapis.com/example-bucket",
                     filter="all",
                     project_id=GCP_PROJECT_ID,
                 )
@@ -244,7 +246,7 @@ class TestCloudStorageBucketLogRetentionPolicyLock:
             )
 
             cloudstorage_client.project_ids = [GCP_PROJECT_ID]
-            cloudstorage_client.region = "global"
+            cloudstorage_client.region = GCP_US_CENTER1_LOCATION
 
             logging_client.sinks = []
 
