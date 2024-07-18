7598a91d09f62f311b2a3f4f9b91e2fc4ce0484d
Daniel Barranquero <danielbo2001@gmail.com>
2024-07-18 13:10:02 +0200

    test(cloudstorage): Suggested changes on bucket public access test done

--- tests/providers/gcp/services/cloudstorage/cloudstorage_bucket_public_access/cloudstorage_bucket_public_access_test.py
+++ tests/providers/gcp/services/cloudstorage/cloudstorage_bucket_public_access/cloudstorage_bucket_public_access_test.py
@@ -1,7 +1,11 @@
 import re
 from unittest import mock
 
-from tests.providers.gcp.gcp_fixtures import GCP_PROJECT_ID, set_mocked_gcp_provider
+from tests.providers.gcp.gcp_fixtures import (
+    GCP_PROJECT_ID,
+    GCP_US_CENTER1_LOCATION,
+    set_mocked_gcp_provider,
+)
 
 
 class TestCloudStorageBucketPublicAccess:
@@ -18,19 +22,18 @@ class TestCloudStorageBucketPublicAccess:
             from prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_public_access.cloudstorage_bucket_public_access import (
                 cloudstorage_bucket_public_access,
             )
-
-            cloudstorage_client.project_ids = [GCP_PROJECT_ID]
-            cloudstorage_client.region = "global"
-
             from prowler.providers.gcp.services.cloudstorage.cloudstorage_service import (
                 Bucket,
             )
 
+            cloudstorage_client.project_ids = [GCP_PROJECT_ID]
+            cloudstorage_client.region = GCP_US_CENTER1_LOCATION
+
             cloudstorage_client.buckets = [
                 Bucket(
-                    name="bucket1",
-                    id="bucket1",
-                    region="US",
+                    name="example-bucket",
+                    id="example-bucket",
+                    region=GCP_US_CENTER1_LOCATION,
                     uniform_bucket_level_access=True,
                     public=True,
                     project_id=GCP_PROJECT_ID,
@@ -43,11 +46,12 @@ class TestCloudStorageBucketPublicAccess:
             assert len(result) == 1
             assert result[0].status == "FAIL"
             assert re.search(
-                "Bucket .* is publicly accessible", result[0].status_extended
+                f"Bucket {cloudstorage_client.buckets[0].name} is publicly accessible",
+                result[0].status_extended,
             )
-            assert result[0].resource_id == "bucket1"
-            assert result[0].resource_name == "bucket1"
-            assert result[0].location == "US"
+            assert result[0].resource_id == "example-bucket"
+            assert result[0].resource_name == "example-bucket"
+            assert result[0].location == GCP_US_CENTER1_LOCATION
             assert result[0].project_id == GCP_PROJECT_ID
 
     def test_bucket_no_public_access(self):
@@ -63,19 +67,18 @@ class TestCloudStorageBucketPublicAccess:
             from prowler.providers.gcp.services.cloudstorage.cloudstorage_bucket_public_access.cloudstorage_bucket_public_access import (
                 cloudstorage_bucket_public_access,
             )
-
-            cloudstorage_client.project_ids = [GCP_PROJECT_ID]
-            cloudstorage_client.region = "global"
-
             from prowler.providers.gcp.services.cloudstorage.cloudstorage_service import (
                 Bucket,
             )
 
+            cloudstorage_client.project_ids = [GCP_PROJECT_ID]
+            cloudstorage_client.region = GCP_US_CENTER1_LOCATION
+
             cloudstorage_client.buckets = [
                 Bucket(
-                    name="bucket2",
-                    id="bucket2",
-                    region="US",
+                    name="example-bucket",
+                    id="example-bucket",
+                    region=GCP_US_CENTER1_LOCATION,
                     uniform_bucket_level_access=True,
                     public=False,
                     project_id=GCP_PROJECT_ID,
@@ -88,11 +91,12 @@ class TestCloudStorageBucketPublicAccess:
             assert len(result) == 1
             assert result[0].status == "PASS"
             assert re.search(
-                "Bucket .* is not publicly accessible", result[0].status_extended
+                f"Bucket {cloudstorage_client.buckets[0].name} is not publicly accessible",
+                result[0].status_extended,
             )
-            assert result[0].resource_id == "bucket2"
-            assert result[0].resource_name == "bucket2"
-            assert result[0].location == "US"
+            assert result[0].resource_id == "example-bucket"
+            assert result[0].resource_name == "example-bucket"
+            assert result[0].location == GCP_US_CENTER1_LOCATION
             assert result[0].project_id == GCP_PROJECT_ID
 
     def test_no_buckets(self):
@@ -110,7 +114,7 @@ class TestCloudStorageBucketPublicAccess:
             )
 
             cloudstorage_client.project_ids = [GCP_PROJECT_ID]
-            cloudstorage_client.region = "global"
+            cloudstorage_client.region = GCP_US_CENTER1_LOCATION
             cloudstorage_client.buckets = []
 
             check = cloudstorage_bucket_public_access()
