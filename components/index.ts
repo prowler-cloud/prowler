db30c0253d78795d538471d08f8ccf3740463eb1
Pablo Lara <larabjj@gmail.com>
2024-07-23 15:37:10 +0200

    feat: ScanStatus component is ready to use it

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,5 @@
 export * from "./providers/AccountInfo";
+export * from "./providers/ScanStatus";
 export * from "./ui/header/Header";
 export * from "./ui/sidebar";
 export * from "./ui/table/CustomTable";
