dd2c92d80561f09ff17cedc0c32ccb99b48c68b3
Pablo Lara <larabjj@gmail.com>
2024-07-24 16:08:15 +0200

    feat: account component is ready to use it (#16)

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
+export * from "./providers/AccountInfo";
 export * from "./ui/header/Header";
 export * from "./ui/sidebar";
 export * from "./ui/table/CustomTable";
---
+++ types/components.ts
@@ -0,0 +1,10 @@
+import { SVGProps } from "react";
+
+export type IconSvgProps = SVGProps<SVGSVGElement> & {
+  size?: number;
+};
+
+export type IconProps = {
+  icon: React.FC<IconSvgProps>;
+  style?: React.CSSProperties;
+};
