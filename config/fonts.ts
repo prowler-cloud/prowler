b7d324f1b0789d4f52c2fff364525bd8f91530fe
Pablo Lara <larabjj@gmail.com>
2024-08-05 13:09:44 +0200

    feat: add modal and functionality for adding providers

--- config/fonts.ts
+++ config/fonts.ts
@@ -6,9 +6,11 @@ import {
 export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
+  preload: false,
 });
 
 export const fontMono = FontMono({
   subsets: ["latin"],
   variable: "--font-mono",
+  preload: false,
 });
