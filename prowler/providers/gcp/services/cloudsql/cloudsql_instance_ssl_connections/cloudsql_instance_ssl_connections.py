4f0faf71fa8dda24cf31281b15631cf96a0f6753
pedrooot <pedromarting3@gmail.com>
2024-08-05 15:31:20 +0200

    fix(cloudsql): check sslMode

--- prowler/providers/gcp/services/cloudsql/cloudsql_instance_ssl_connections/cloudsql_instance_ssl_connections.py
+++ prowler/providers/gcp/services/cloudsql/cloudsql_instance_ssl_connections/cloudsql_instance_ssl_connections.py
@@ -12,12 +12,16 @@ class cloudsql_instance_ssl_connections(Check):
             report.resource_name = instance.name
             report.location = instance.region
             report.status = "PASS"
-            report.status_extended = (
-                f"Database Instance {instance.name} requires SSL connections."
-            )
-            if not instance.ssl:
+            report.status_extended = f"Database Instance {instance.name} requires SSL connections and allows only encrypted connections."
+            if not instance.require_ssl and instance.ssl_mode != "ENCRYPTED_ONLY":
                 report.status = "FAIL"
-                report.status_extended = f"Database Instance {instance.name} does not require SSL connections."
+                report.status_extended = f"Database Instance {instance.name} does not require SSL connections and allows unencrypted connections."
+            elif not instance.require_ssl:
+                report.status = "FAIL"
+                report.status_extended = f"Database Instance {instance.name} does not require SSL connections but allows only encrypted connections."
+            elif instance.ssl_mode != "ENCRYPTED_ONLY":
+                report.status = "FAIL"
+                report.status_extended = f"Database Instance {instance.name} requires SSL connections but allows unencrypted connections."
             findings.append(report)
 
         return findings
