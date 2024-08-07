d1fdbe87e937fbfa560cbd864276c9f34f8a5825
Rubén De la Torre Vico <rubendltv22@gmail.com>
2024-08-07 12:46:27 +0200

    chore(awslabmda): Change to have in count that is really public Internet exposed

--- prowler/providers/aws/services/awslambda/awslambda_function_not_publicly_accessible/awslambda_function_not_publicly_accessible.py
+++ prowler/providers/aws/services/awslambda/awslambda_function_not_publicly_accessible/awslambda_function_not_publicly_accessible.py
@@ -1,5 +1,6 @@
 from prowler.lib.check.models import Check, Check_Report_AWS
 from prowler.providers.aws.services.awslambda.awslambda_client import awslambda_client
+from prowler.providers.aws.services.elbv2.elbv2_client import elbv2_client
 
 
 class awslambda_function_not_publicly_accessible(Check):
@@ -13,30 +14,68 @@ class awslambda_function_not_publicly_accessible(Check):
             report.resource_tags = function.tags
 
             report.status = "PASS"
-            report.status_extended = f"Lambda function {function.name} has a policy resource-based policy not public."
+            report.status_extended = (
+                f"Lambda function {function.name} is not publicly accessible."
+            )
 
-            public_access = False
-            if function.policy:
-                for statement in function.policy["Statement"]:
-                    # Only check allow statements
-                    if statement["Effect"] == "Allow":
-                        if (
-                            "*" in statement["Principal"]
-                            or (
-                                "AWS" in statement["Principal"]
-                                and "*" in statement["Principal"]["AWS"]
-                            )
-                            or (
-                                "CanonicalUser" in statement["Principal"]
-                                and "*" in statement["Principal"]["CanonicalUser"]
-                            )
-                        ):
-                            public_access = True
+            # 1. Check if the function is associated with a public load balancer
+            target_group_redirects_alb_to_function = ""
+
+            for tg_arn, target_group in elbv2_client.target_groups.items():
+                if (
+                    target_group.target_type == "lambda"
+                ) and function.arn in target_group.targets:
+                    for lb_arn in target_group.load_balancer_arns:
+                        lb = elbv2_client.loadbalancersv2.get(
+                            lb_arn,
+                            None,  # Bug: If prowler is filtering by ARN, this will fail because loadbalancersv2 are filtered
+                        )
+                        if lb and lb.scheme == "internet-facing":
+                            target_group_redirects_alb_to_function = tg_arn
                             break
 
-            if public_access:
+            public_policy = False
+
+            # 2. Check if the function policy allows public access
+            if target_group_redirects_alb_to_function and function.policy:
+                for statement in function.policy["Statement"]:
+                    # Only check allow statements
+                    if (
+                        statement["Effect"] == "Allow"
+                        and (
+                            "*" in statement["Principal"]
+                            or "*" in statement["Principal"].get("AWS", "")
+                            or "*" in statement["Principal"].get("CanonicalUser", "")
+                            or "elasticloadbalancing.amazonaws.com"
+                            in statement["Principal"].get("Service", "")
+                        )
+                        and (
+                            "*" in statement["Action"]
+                            or "lambda:InvokeFunction" in statement["Action"]
+                        )
+                        and (
+                            "*" in statement["Resource"]
+                            or function.arn in statement["Resource"]
+                        )
+                    ):
+                        if statement.get("Condition", {}):
+                            if (
+                                statement["Condition"]
+                                .get("ArnLike", {})
+                                .get("AWS:SourceArn", "")
+                                == target_group_redirects_alb_to_function
+                            ):
+                                public_policy = True
+                                break
+                        else:
+                            public_policy = True
+                            break
+
+            if public_policy:
                 report.status = "FAIL"
-                report.status_extended = f"Lambda function {function.name} has a policy resource-based policy with public access."
+                report.status_extended = (
+                    f"Lambda function {function.name} is publicly accessible."
+                )
 
             findings.append(report)
 
