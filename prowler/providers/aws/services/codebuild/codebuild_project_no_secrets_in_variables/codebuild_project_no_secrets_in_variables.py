cd2f742511b567c0dd9fb405553cb1d5b106818f
Daniel Barranquero <danielbo2001@gmail.com>
2024-08-07 11:56:21 +0200

    feat(aws): Adapt detect secrets to this check

--- prowler/providers/aws/services/codebuild/codebuild_project_no_secrets_in_variables/codebuild_project_no_secrets_in_variables.py
+++ prowler/providers/aws/services/codebuild/codebuild_project_no_secrets_in_variables/codebuild_project_no_secrets_in_variables.py
@@ -1,10 +1,13 @@
-from prowler.lib.check.models import Check, Check_Report_AWS
-from prowler.providers.aws.services.codebuild.codebuild_client import codebuild_client
-from detect_secrets import SecretsCollection
-from detect_secrets.settings import default_settings
-import tempfile
 import json
 import os
+import tempfile
+
+from detect_secrets import SecretsCollection
+from detect_secrets.settings import default_settings
+
+from prowler.lib.check.models import Check, Check_Report_AWS
+from prowler.providers.aws.services.codebuild.codebuild_client import codebuild_client
+
 
 class codebuild_project_no_secrets_in_variables(Check):
     def execute(self):
@@ -22,7 +25,12 @@ class codebuild_project_no_secrets_in_variables(Check):
                 for env_var in project.environment_variables:
                     if env_var.type == "PLAINTEXT":
                         temp_file = tempfile.NamedTemporaryFile(delete=False)
-                        temp_file.write(bytes(json.dumps({env_var.name: env_var.value}), encoding='utf-8'))
+                        temp_file.write(
+                            bytes(
+                                json.dumps({env_var.name: env_var.value}),
+                                encoding="utf-8",
+                            )
+                        )
                         temp_file.close()
 
                         secrets = SecretsCollection()
@@ -31,7 +39,7 @@ class codebuild_project_no_secrets_in_variables(Check):
 
                         if secrets.json():
                             secrets_found.append(env_var.name)
-                        
+
                         os.remove(temp_file.name)
 
             if secrets_found:
