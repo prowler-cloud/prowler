3511cd977ad47861d25fcdae5a1d43241b7a95dc
Pablo Lara <larabjj@gmail.com>
2024-08-07 09:06:38 +0200

    chore: remove the old modal component

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,7 +1,6 @@
 export * from "./alert/Alert";
 export * from "./dialog/Dialog";
 export * from "./header/Header";
-export * from "./modal";
 export * from "./sidebar";
 export * from "./table/StatusBadge";
 export * from "./table/Table";
