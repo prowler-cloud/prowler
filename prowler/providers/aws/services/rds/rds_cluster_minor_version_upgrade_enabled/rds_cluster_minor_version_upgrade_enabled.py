d6ee86128711ec50d01982a91d25cb6f43528622
Daniel Barranquero <danielbo2001@gmail.com>
2024-08-12 16:23:31 +0200

    feat(aws): Requested changes done

--- prowler/providers/aws/services/rds/rds_cluster_minor_version_upgrade_enabled/rds_cluster_minor_version_upgrade_enabled.py
+++ prowler/providers/aws/services/rds/rds_cluster_minor_version_upgrade_enabled/rds_cluster_minor_version_upgrade_enabled.py
@@ -6,6 +6,7 @@ class rds_cluster_minor_version_upgrade_enabled(Check):
     def execute(self):
         findings = []
         for db_cluster in rds_client.db_clusters:
+            # Auto minor version upgrade is only available for non-Aurora Multi-AZ DB clusters
             if rds_client.db_clusters[db_cluster].multi_az:
                 report = Check_Report_AWS(self.metadata())
                 report.region = rds_client.db_clusters[db_cluster].region
