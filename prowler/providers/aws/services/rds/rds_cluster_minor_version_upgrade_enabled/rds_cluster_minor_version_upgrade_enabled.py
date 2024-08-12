26603a30547b5d8518571cd8e00b7af857bfaf3d
Daniel Barranquero <danielbo2001@gmail.com>
2024-08-12 13:07:28 +0200

    feat(aws): Add test and modify check

--- prowler/providers/aws/services/rds/rds_cluster_minor_version_upgrade_enabled/rds_cluster_minor_version_upgrade_enabled.py
+++ prowler/providers/aws/services/rds/rds_cluster_minor_version_upgrade_enabled/rds_cluster_minor_version_upgrade_enabled.py
@@ -6,18 +6,18 @@ class rds_cluster_minor_version_upgrade_enabled(Check):
     def execute(self):
         findings = []
         for db_cluster in rds_client.db_clusters:
-            if db_cluster.multi_az:
+            if rds_client.db_clusters[db_cluster].multi_az:
                 report = Check_Report_AWS(self.metadata())
-                report.region = db_cluster.region
-                report.resource_id = db_cluster.id
-                report.resource_arn = db_cluster.arn
-                report.resource_tags = db_cluster.tags
-                if db_cluster.auto_minor_version_upgrade:
+                report.region = rds_client.db_clusters[db_cluster].region
+                report.resource_id = rds_client.db_clusters[db_cluster].id
+                report.resource_arn = rds_client.db_clusters[db_cluster].arn
+                report.resource_tags = rds_client.db_clusters[db_cluster].tags
+                if rds_client.db_clusters[db_cluster].auto_minor_version_upgrade:
                     report.status = "PASS"
-                    report.status_extended = f"RDS Cluster {db_cluster.id} has minor version upgrade enabled."
+                    report.status_extended = f"RDS Cluster {rds_client.db_clusters[db_cluster].id} has minor version upgrade enabled."
                 else:
                     report.status = "FAIL"
-                    report.status_extended = f"RDS Cluster {db_cluster.id} does not have minor version upgrade enabled."
+                    report.status_extended = f"RDS Cluster {rds_client.db_clusters[db_cluster].id} does not have minor version upgrade enabled."
 
                 findings.append(report)
 
---
+++ tests/providers/aws/services/rds/rds_cluster_minor_version_upgrade_enabled/rds_cluster_minor_version_upgrade_enabled_test.py
@@ -0,0 +1,201 @@
+from unittest import mock
+
+import botocore
+from moto import mock_aws
+
+from prowler.providers.aws.services.rds.rds_service import DBCluster
+from tests.providers.aws.utils import (
+    AWS_ACCOUNT_NUMBER,
+    AWS_REGION_US_EAST_1,
+    set_mocked_aws_provider,
+)
+
+make_api_call = botocore.client.BaseClient._make_api_call
+
+
+def mock_make_api_call(self, operation_name, kwarg):
+    if operation_name == "DescribeDBEngineVersions":
+        return {
+            "DBEngineVersions": [
+                {
+                    "Engine": "aurora-mysql",
+                    "EngineVersion": "8.0.32",
+                    "DBEngineDescription": "description",
+                    "DBEngineVersionDescription": "description",
+                },
+            ]
+        }
+    return make_api_call(self, operation_name, kwarg)
+
+
+@mock.patch("botocore.client.BaseClient._make_api_call", new=mock_make_api_call)
+class Test_rds_cluster_minor_version_upgrade_enabled:
+    @mock_aws
+    def test_rds_no_clusters(self):
+        from prowler.providers.aws.services.rds.rds_service import RDS
+
+        aws_provider = set_mocked_aws_provider([AWS_REGION_US_EAST_1])
+
+        with mock.patch(
+            "prowler.providers.common.provider.Provider.get_global_provider",
+            return_value=aws_provider,
+        ):
+            with mock.patch(
+                "prowler.providers.aws.services.rds.rds_cluster_minor_version_upgrade_enabled.rds_cluster_minor_version_upgrade_enabled.rds_client",
+                new=RDS(aws_provider),
+            ):
+                from prowler.providers.aws.services.rds.rds_cluster_minor_version_upgrade_enabled.rds_cluster_minor_version_upgrade_enabled import (
+                    rds_cluster_minor_version_upgrade_enabled,
+                )
+
+                check = rds_cluster_minor_version_upgrade_enabled()
+                result = check.execute()
+
+                assert len(result) == 0
+
+    def test_rds_cluster_no_multi(self):
+        rds_client = mock.MagicMock
+        rds_client.db_clusters = {
+            "db-cluster-1": DBCluster(
+                id="db-cluster-1",
+                arn=f"arn:aws:rds:{AWS_REGION_US_EAST_1}:{AWS_ACCOUNT_NUMBER}:cluster:db-cluster-1",
+                endpoint="",
+                engine="aurora-postgresql",
+                status="available",
+                public=False,
+                encrypted=True,
+                auto_minor_version_upgrade=False,
+                backup_retention_period=7,
+                backtrack=0,
+                cloudwatch_logs=[],
+                deletion_protection=False,
+                parameter_group="default.aurora-postgresql10",
+                multi_az=False,
+                username="admin",
+                iam_auth=False,
+                region=AWS_REGION_US_EAST_1,
+                tags=[],
+            )
+        }
+
+        with mock.patch(
+            "prowler.providers.aws.services.rds.rds_service.RDS",
+            new=rds_client,
+        ), mock.patch(
+            "prowler.providers.aws.services.rds.rds_cluster_minor_version_upgrade_enabled.rds_cluster_minor_version_upgrade_enabled.rds_client",
+            new=rds_client,
+        ):
+            from prowler.providers.aws.services.rds.rds_cluster_minor_version_upgrade_enabled.rds_cluster_minor_version_upgrade_enabled import (
+                rds_cluster_minor_version_upgrade_enabled,
+            )
+
+            check = rds_cluster_minor_version_upgrade_enabled()
+            result = check.execute()
+
+            assert len(result) == 0
+
+    def test_rds_cluster_no_auto_upgrade(self):
+        rds_client = mock.MagicMock
+        rds_client.db_clusters = {
+            "db-cluster-1": DBCluster(
+                id="db-cluster-1",
+                arn=f"arn:aws:rds:{AWS_REGION_US_EAST_1}:{AWS_ACCOUNT_NUMBER}:cluster:db-cluster-1",
+                endpoint="",
+                engine="aurora-postgresql",
+                status="available",
+                public=False,
+                encrypted=True,
+                auto_minor_version_upgrade=False,
+                backup_retention_period=7,
+                backtrack=0,
+                cloudwatch_logs=[],
+                deletion_protection=False,
+                parameter_group="default.aurora-postgresql10",
+                multi_az=True,
+                username="admin",
+                iam_auth=False,
+                region=AWS_REGION_US_EAST_1,
+                tags=[],
+            )
+        }
+
+        with mock.patch(
+            "prowler.providers.aws.services.rds.rds_service.RDS",
+            new=rds_client,
+        ), mock.patch(
+            "prowler.providers.aws.services.rds.rds_cluster_minor_version_upgrade_enabled.rds_cluster_minor_version_upgrade_enabled.rds_client",
+            new=rds_client,
+        ):
+            from prowler.providers.aws.services.rds.rds_cluster_minor_version_upgrade_enabled.rds_cluster_minor_version_upgrade_enabled import (
+                rds_cluster_minor_version_upgrade_enabled,
+            )
+
+            check = rds_cluster_minor_version_upgrade_enabled()
+            result = check.execute()
+
+            assert len(result) == 1
+            assert result[0].status == "FAIL"
+            assert (
+                result[0].status_extended
+                == "RDS Cluster db-cluster-1 does not have minor version upgrade enabled."
+            )
+            assert result[0].resource_id == "db-cluster-1"
+            assert result[0].region == AWS_REGION_US_EAST_1
+            assert (
+                result[0].resource_arn
+                == f"arn:aws:rds:{AWS_REGION_US_EAST_1}:{AWS_ACCOUNT_NUMBER}:cluster:db-cluster-1"
+            )
+            assert result[0].resource_tags == []
+
+    def test_rds_cluster_with_auto_upgrade(self):
+        rds_client = mock.MagicMock
+        rds_client.db_clusters = {
+            "db-cluster-1": DBCluster(
+                id="db-cluster-1",
+                arn=f"arn:aws:rds:{AWS_REGION_US_EAST_1}:{AWS_ACCOUNT_NUMBER}:cluster:db-cluster-1",
+                endpoint="",
+                engine="aurora-postgresql",
+                status="available",
+                public=False,
+                encrypted=True,
+                auto_minor_version_upgrade=True,
+                backup_retention_period=7,
+                backtrack=0,
+                cloudwatch_logs=[],
+                deletion_protection=False,
+                parameter_group="default.aurora-postgresql10",
+                multi_az=True,
+                username="admin",
+                iam_auth=False,
+                region=AWS_REGION_US_EAST_1,
+                tags=[],
+            )
+        }
+
+        with mock.patch(
+            "prowler.providers.aws.services.rds.rds_service.RDS",
+            new=rds_client,
+        ), mock.patch(
+            "prowler.providers.aws.services.rds.rds_cluster_minor_version_upgrade_enabled.rds_cluster_minor_version_upgrade_enabled.rds_client",
+            new=rds_client,
+        ):
+            from prowler.providers.aws.services.rds.rds_cluster_minor_version_upgrade_enabled.rds_cluster_minor_version_upgrade_enabled import (
+                rds_cluster_minor_version_upgrade_enabled,
+            )
+
+            check = rds_cluster_minor_version_upgrade_enabled()
+            result = check.execute()
+
+            assert len(result) == 1
+            assert result[0].status == "PASS"
+            assert (
+                result[0].status_extended
+                == "RDS Cluster db-cluster-1 has minor version upgrade enabled."
+            )
+            assert result[0].resource_id == "db-cluster-1"
+            assert result[0].region == AWS_REGION_US_EAST_1
+            assert (
+                result[0].resource_arn
+                == f"arn:aws:rds:{AWS_REGION_US_EAST_1}:{AWS_ACCOUNT_NUMBER}:cluster:db-cluster-1"
+            )
+            assert result[0].resource_tags == []
