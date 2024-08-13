092ad10c5671c460e82d3eceb10b996b614ae88f
Pablo Lara <larabjj@gmail.com>
2024-08-13 10:12:48 +0200

    chore: add all icons for services

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -3,3 +3,4 @@ export * from "./providers/AwsProvider";
 export * from "./providers/AzureProvider";
 export * from "./providers/GoogleCloudProvider";
 export * from "./prowler/ProwlerIcons";
+export * from "./services/IconServices";
