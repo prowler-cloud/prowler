8a57434d7c5c31547042be412f84e2821670be97
Daniel Barranquero <danielbo2001@gmail.com>
2024-08-13 10:05:28 +0200

    feat(aws): Change instances checks for default admin

--- prowler/providers/aws/services/rds/rds_instance_default_admin/rds_instance_default_admin.py
+++ prowler/providers/aws/services/rds/rds_instance_default_admin/rds_instance_default_admin.py
@@ -27,21 +27,4 @@ class rds_instance_default_admin(Check):
 
                 findings.append(report)
 
-        for db_cluster in rds_client.db_clusters:
-            report = Check_Report_AWS(self.metadata())
-            report.region = rds_client.db_clusters[db_cluster].region
-            report.resource_id = rds_client.db_clusters[db_cluster].id
-            report.resource_arn = db_cluster
-            report.resource_tags = rds_client.db_clusters[db_cluster].tags
-            report.status = "FAIL"
-            report.status_extended = f"RDS Cluster {rds_client.db_clusters[db_cluster].id} is using the default master username."
-            if (
-                rds_client.db_clusters[db_cluster].username != "admin"
-                and rds_client.db_clusters[db_cluster].username != "postgres"
-            ):
-                report.status = "PASS"
-                report.status_extended = f"RDS Cluster {rds_client.db_clusters[db_cluster].id} is not using the default master username."
-
-            findings.append(report)
-
         return findings
