4a96631af31d2bc459ba7af1a05090e2035a2935
Daniel Barranquero <danielbo2001@gmail.com>
2024-08-13 09:50:18 +0200

    feat(aws): Add not supported engines for IAM authentication as pass

--- prowler/providers/aws/services/rds/rds_instance_iam_authentication_enabled/rds_instance_iam_authentication_enabled.py
+++ prowler/providers/aws/services/rds/rds_instance_iam_authentication_enabled/rds_instance_iam_authentication_enabled.py
@@ -19,18 +19,22 @@ class rds_instance_iam_authentication_enabled(Check):
             report.resource_id = db_instance.id
             report.resource_arn = db_instance.arn
             report.resource_tags = db_instance.tags
-            report.status = "FAIL"
-            report.status_extended = f"RDS Instance {db_instance.id} does not have IAM authentication enabled."
+            report.status = "PASS"
+            report.status_extended = (
+                f"RDS Instance {db_instance.id} does not support IAM authentication."
+            )
 
             # Check DB Instance to make sure its not part of a cluster.
             if not db_instance.cluster_id and any(
                 engine in db_instance.engine for engine in supported_engines
             ):
                 if db_instance.iam_auth:
-                    report.status = "PASS"
                     report.status_extended = (
                         f"RDS Instance {db_instance.id} has IAM authentication enabled."
                     )
+                else:
+                    report.status = "FAIL"
+                    report.status_extended = f"RDS Instance {db_instance.id} does not have IAM authentication enabled."
 
                 findings.append(report)
         return findings
