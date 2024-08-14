4c0a14b96cbb697b06764c8023cd6e5d0405222b
Pablo Lara <larabjj@gmail.com>
2024-08-14 09:52:03 +0200

    chore: update components with the new paths

--- app/(prowler)/services/page.tsx
+++ app/(prowler)/services/page.tsx
@@ -1,5 +1,34 @@
-import React from "react";
-
+import { FilterControls } from "@/components/filters";
+import {
+  AmazonEC2Icon,
+  AmazonEMRIcon,
+  AmazonGuardDutyIcon,
+  AmazonInspectorIcon,
+  AmazonMacieIcon,
+  AmazonRDSIcon,
+  AmazonRoute53Icon,
+  AmazonS3Icon,
+  AmazonSNSIcon,
+  AmazonVPCIcon,
+  AWSAccountIcon,
+  AWSAthenaIcon,
+  AWSCertificateManagerIcon,
+  AWSCloudFormationIcon,
+  AWSCloudTrailIcon,
+  AWSCloudWatchIcon,
+  AWSConfigIcon,
+  AWSDatabaseMigrationServiceIcon,
+  AWSGlueIcon,
+  AWSIAMIcon,
+  AWSLambdaIcon,
+  AWSNetworkFirewallIcon,
+  AWSOrganizationsIcon,
+  AWSResourceExplorerIcon,
+  AWSSecurityHubIcon,
+  AWSSystemsManagerIncidentManagerIcon,
+  AWSTrustedAdvisorIcon,
+  IAMAccessAnalyzerIcon,
+} from "@/components/icons";
 import { Header } from "@/components/ui";
 
 export default function Services() {
@@ -9,8 +38,36 @@ export default function Services() {
         title="Services"
         icon="material-symbols:linked-services-outline"
       />
+      <FilterControls />
 
-      <p>Hi hi from Services page</p>
+      <IAMAccessAnalyzerIcon />
+      <AWSAccountIcon />
+      <AWSCertificateManagerIcon />
+      <AWSAthenaIcon />
+      <AWSLambdaIcon />
+      <AWSCloudFormationIcon />
+      <AWSCloudTrailIcon />
+      <AWSCloudWatchIcon />
+      <AWSConfigIcon />
+      <AWSDatabaseMigrationServiceIcon />
+      <AmazonEC2Icon />
+      <AmazonEMRIcon />
+      <AWSGlueIcon />
+      <AmazonGuardDutyIcon />
+      <AmazonInspectorIcon />
+      <AWSIAMIcon />
+      <AmazonMacieIcon />
+      <AWSNetworkFirewallIcon />
+      <AWSOrganizationsIcon />
+      <AmazonRDSIcon />
+      <AWSResourceExplorerIcon />
+      <AmazonRoute53Icon />
+      <AmazonS3Icon />
+      <AWSSecurityHubIcon />
+      <AmazonSNSIcon />
+      <AWSSystemsManagerIncidentManagerIcon />
+      <AWSTrustedAdvisorIcon />
+      <AmazonVPCIcon />
     </>
   );
 }
