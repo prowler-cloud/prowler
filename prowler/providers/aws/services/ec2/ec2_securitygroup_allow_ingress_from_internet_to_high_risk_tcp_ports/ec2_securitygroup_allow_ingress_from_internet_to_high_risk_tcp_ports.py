5e29b4c8fd23409eaaf8f2e44ffbd5e1b9d4f8d9
MarioRgzLpz <mariorgzlpz1809@gmail.com>
2024-08-14 16:14:53 +0200

    fix(ec2): Adjust the code to the change of the securitygroup list to dict

--- prowler/providers/aws/services/ec2/ec2_securitygroup_allow_ingress_from_internet_to_high_risk_tcp_ports/ec2_securitygroup_allow_ingress_from_internet_to_high_risk_tcp_ports.py
+++ prowler/providers/aws/services/ec2/ec2_securitygroup_allow_ingress_from_internet_to_high_risk_tcp_ports/ec2_securitygroup_allow_ingress_from_internet_to_high_risk_tcp_ports.py
@@ -10,7 +10,7 @@ from prowler.providers.aws.services.vpc.vpc_client import vpc_client
 class ec2_securitygroup_allow_ingress_from_internet_to_high_risk_tcp_ports(Check):
     def execute(self):
         findings = []
-        for security_group in ec2_client.security_groups:
+        for security_group_arn, security_group in ec2_client.security_groups.items():
             # Check if ignoring flag is set and if the VPC and the SG is in use
             if ec2_client.provider.scan_unused_services or (
                 security_group.vpc_id in vpc_client.vpcs
@@ -26,14 +26,14 @@ class ec2_securitygroup_allow_ingress_from_internet_to_high_risk_tcp_ports(Check
                     report.region = security_group.region
                     report.resource_details = security_group.name
                     report.resource_id = security_group.id
-                    report.resource_arn = security_group.arn
+                    report.resource_arn = security_group_arn
                     report.resource_tags = security_group.tags
                     report.status = "PASS"
                     report.status_extended = f"Security group {security_group.name} ({security_group.id}) does not have port {port} open to the Internet."
                     # only proceed if check "..._to_all_ports" did not run or did not FAIL to avoid to report open ports twice
                     if not ec2_client.is_failed_check(
                         ec2_securitygroup_allow_ingress_from_internet_to_all_ports.__name__,
-                        security_group.arn,
+                        security_group_arn,
                     ):
                         # Loop through every security group's ingress rule and check it
                         for ingress_rule in security_group.ingress_rules:
