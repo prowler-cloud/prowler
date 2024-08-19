1a8bc14587cdbeee67ff4aae6dfc7c19ea2f227c
Rubén De la Torre Vico <rubendltv22@gmail.com>
2024-08-19 20:22:21 +0200

    feat(awslambda): New check to ensure that a function is inside VPC (#4783)
    Co-authored-by: Sergio Garcia <38561120+sergargar@users.noreply.github.com>

--- prowler/providers/aws/services/awslambda/awslambda_service.py
+++ prowler/providers/aws/services/awslambda/awslambda_service.py
@@ -37,13 +37,13 @@ class Lambda(AWSService):
                     ):
                         lambda_name = function["FunctionName"]
                         lambda_arn = function["FunctionArn"]
+                        vpc_config = function.get("VpcConfig", {})
                         # We must use the Lambda ARN as the dict key since we could have Lambdas in different regions with the same name
                         self.functions[lambda_arn] = Function(
                             name=lambda_name,
                             arn=lambda_arn,
-                            security_groups=function.get("VpcConfig", {}).get(
-                                "SecurityGroupIds", []
-                            ),
+                            security_groups=vpc_config.get("SecurityGroupIds", []),
+                            vpc_id=vpc_config.get("VpcId"),
                             region=regional_client.region,
                         )
                         if "Runtime" in function:
@@ -201,4 +201,5 @@ class Function(BaseModel):
     policy: dict = None
     code: LambdaCode = None
     url_config: URLConfig = None
+    vpc_id: Optional[str]
     tags: Optional[list] = []
