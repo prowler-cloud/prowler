d6cf716a94a228b1242602c8c12f8a6c68ebba77
Daniel Barranquero <danielbo2001@gmail.com>
2024-08-19 13:16:00 +0200

    feat(rds): Add instance copy tags to snapshots check

---
+++ prowler/providers/aws/services/rds/rds_instance_copy_tags/rds_instance_copy_tags.py
@@ -0,0 +1,24 @@
+from prowler.lib.check.models import Check, Check_Report_AWS
+from prowler.providers.aws.services.rds.rds_client import rds_client
+
+
+class rds_instance_copy_tags(Check):
+    def execute(self):
+        findings = []
+        for db_instance in rds_client.db_instances:
+            if not db_instance.engine == "aurora":
+                report = Check_Report_AWS(self.metadata())
+                report.region = db_instance.region
+                report.resource_id = db_instance.id
+                report.resource_arn = db_instance.arn
+                report.resource_tags = db_instance.tags
+                if db_instance.copy_tags_to_snapshot:
+                    report.status = "PASS"
+                    report.status_extended = f"RDS Instance {db_instance.id} which is not clustered has IAM authentication enabled."
+                else:
+                    report.status = "FAIL"
+                    report.status_extended = f"RDS Instance {db_instance.id} which is not clustered does not have IAM authentication enabled."
+
+                findings.append(report)
+
+        return findings
