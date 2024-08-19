a756cab0f4ecf75ace269387a3d5fc7f33438c0d
Daniel Barranquero <danielbo2001@gmail.com>
2024-08-19 12:49:03 +0200

    chore(rds): Change instance check

--- prowler/providers/aws/services/rds/rds_instance_multi_az/rds_instance_multi_az.py
+++ prowler/providers/aws/services/rds/rds_instance_multi_az/rds_instance_multi_az.py
@@ -6,19 +6,20 @@ class rds_instance_multi_az(Check):
     def execute(self):
         findings = []
         for db_instance in rds_client.db_instances:
-            report = Check_Report_AWS(self.metadata())
-            report.region = db_instance.region
-            report.resource_id = db_instance.id
-            report.resource_arn = db_instance.arn
-            report.resource_tags = db_instance.tags
             # Check if is member of a cluster
-            if not db_instance.cluster_id and db_instance.multi_az:
-                report.status = "PASS"
-                report.status_extended = f"RDS Instance {db_instance.id} which is not clustered has multi-AZ enabled."
-            else:
-                report.status = "FAIL"
-                report.status_extended = f"RDS Instance {db_instance.id} which is not clustered does not have multi-AZ enabled."
+            if not db_instance.cluster_id:
+                report = Check_Report_AWS(self.metadata())
+                report.region = db_instance.region
+                report.resource_id = db_instance.id
+                report.resource_arn = db_instance.arn
+                report.resource_tags = db_instance.tags
+                if db_instance.multi_az:
+                    report.status = "PASS"
+                    report.status_extended = f"RDS Instance {db_instance.id} which is not clustered has multi-AZ enabled."
+                else:
+                    report.status = "FAIL"
+                    report.status_extended = f"RDS Instance {db_instance.id} which is not clustered does not have multi-AZ enabled."
 
-            findings.append(report)
+                findings.append(report)
 
         return findings
