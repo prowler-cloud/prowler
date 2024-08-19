f61ed5ddf5e250c4eedeb61c81464164ad27eba3
Pablo Lara <larabjj@gmail.com>
2024-08-19 12:14:17 +0200

    chore: simplify AttackSurface component

--- components/overview/AttackSurface.tsx
+++ components/overview/AttackSurface.tsx
@@ -5,22 +5,18 @@ import ActionCard from "../ui/action-card/ActionCard";
 const cardData = [
   {
     findings: 3,
-    icon: "solar:danger-triangle-bold",
     title: "Internet Exposed Resources",
   },
   {
     findings: 15,
-    icon: "solar:danger-triangle-bold",
     title: "Exposed Secrets",
   },
   {
     findings: 0,
-    icon: "heroicons:shield-check-solid",
     title: "IAM Policies Leading to Privilege Escalation",
   },
   {
     findings: 0,
-    icon: "heroicons:shield-check-solid",
     title: "EC2 with Metadata Service V1 (IMDSv1)",
   },
 ];
@@ -32,7 +28,11 @@ export const AttackSurface = () => {
         <ActionCard
           key={index}
           color={card.findings > 0 ? "fail" : "success"}
-          icon={card.findings > 0 ? "solar:danger-triangle-bold" : card.icon}
+          icon={
+            card.findings > 0
+              ? "solar:danger-triangle-bold"
+              : "heroicons:shield-check-solid"
+          }
           title={card.title}
           description={
             card.findings > 0 ? "Review Required" : "No Issues Found"
