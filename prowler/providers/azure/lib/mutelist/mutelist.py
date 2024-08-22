b87c1cb56b7c818b61ef48c811514a776745e02f
Sergio <sergio@prowler.com>
2024-08-22 10:50:53 -0400

    fix mutelist

--- prowler/providers/azure/lib/mutelist/mutelist.py
+++ prowler/providers/azure/lib/mutelist/mutelist.py
@@ -1,17 +1,17 @@
-from prowler.lib.check.models import Check_Report_Azure
 from prowler.lib.mutelist.mutelist import Mutelist
+from prowler.lib.outputs.finding import Finding
 from prowler.lib.outputs.utils import unroll_dict, unroll_tags
 
 
 class AzureMutelist(Mutelist):
     def is_finding_muted(
         self,
-        finding: Check_Report_Azure,
+        finding: Finding,
     ) -> bool:
         return self.is_muted(
-            finding.subscription,
-            finding.check_metadata.CheckID,
-            finding.location,
+            finding.account_name,
+            finding.check_id,
+            finding.region,
             finding.resource_name,
             unroll_dict(unroll_tags(finding.resource_tags)),
         )
