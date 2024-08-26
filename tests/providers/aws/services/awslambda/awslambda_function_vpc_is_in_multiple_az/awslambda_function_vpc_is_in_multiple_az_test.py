5b6edbe1154a5e6a544c480607f5c4a4da06f612
Rubén De la Torre Vico <rubendltv22@gmail.com>
2024-08-26 12:15:46 +0200

    test(awslambda): test case for failed check logic

--- tests/providers/aws/services/awslambda/awslambda_function_vpc_is_in_multiple_az/awslambda_function_vpc_is_in_multiple_az_test.py
+++ tests/providers/aws/services/awslambda/awslambda_function_vpc_is_in_multiple_az/awslambda_function_vpc_is_in_multiple_az_test.py
@@ -373,3 +373,73 @@ class Test_awslambda_function_vpc_is_in_multiple_az:
             assert result[0].resource_id == function_name
             assert result[0].resource_arn == function["FunctionArn"]
             assert result[0].resource_tags == [{}]
+
+    @mock_aws
+    def test_function_no_vpc_pass_to_avoid_fail_twice(self):
+        # Create IAM Role for Lambda Function
+        iam_client = client("iam", region_name=AWS_REGION_EU_WEST_1)
+        role_name = "test-role"
+        assume_role_policy_document = {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {"Service": "lambda.amazonaws.com"},
+                    "Action": "sts:AssumeRole",
+                }
+            ],
+        }
+        role_arn = iam_client.create_role(
+            RoleName=role_name,
+            AssumeRolePolicyDocument=dumps(assume_role_policy_document),
+        )["Role"]["Arn"]
+
+        # Create Lambda Function outside VPC
+        lambda_client = client("lambda", region_name=AWS_REGION_EU_WEST_1)
+        function_name = "test_function"
+        lambda_client.create_function(
+            FunctionName=function_name,
+            Runtime="python3.8",
+            Role=role_arn,
+            Handler="lambda_function.lambda_handler",
+            Code={"ZipFile": b"file not used"},
+        )["FunctionArn"]
+
+        from prowler.providers.aws.services.awslambda.awslambda_service import Lambda
+        from prowler.providers.aws.services.vpc.vpc_service import VPC
+
+        aws_provider = set_mocked_aws_provider([AWS_REGION_EU_WEST_1])
+
+        with mock.patch(
+            "prowler.providers.common.provider.Provider.get_global_provider",
+            return_value=aws_provider,
+        ), mock.patch(
+            "prowler.providers.aws.services.awslambda.awslambda_function_vpc_is_in_multiple_az.awslambda_function_vpc_is_in_multiple_az.awslambda_client",
+            new=Lambda(aws_provider),
+        ), mock.patch(
+            "prowler.providers.aws.services.awslambda.awslambda_function_vpc_is_in_multiple_az.awslambda_function_vpc_is_in_multiple_az.vpc_client",
+            new=VPC(aws_provider),
+        ):
+            # Test check inside_vpc first
+
+            from prowler.providers.aws.services.awslambda.awslambda_function_inside_vpc.awslambda_function_inside_vpc import (
+                awslambda_function_inside_vpc,
+            )
+
+            check = awslambda_function_inside_vpc()
+
+            result = check.execute()
+
+            assert len(result) == 1
+            assert result[0].status == "FAIL"
+
+            # Now test if function is in multiple AZs to ensure it does not fail twice
+
+            from prowler.providers.aws.services.awslambda.awslambda_function_vpc_is_in_multiple_az.awslambda_function_vpc_is_in_multiple_az import (
+                awslambda_function_vpc_is_in_multiple_az,
+            )
+
+            check = awslambda_function_vpc_is_in_multiple_az()
+            result = check.execute()
+
+            assert len(result) == 0
