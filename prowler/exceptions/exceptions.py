d108a2cb78f06a54881fa54b97aaecf70a5085b9
pedrooot <pedromarting3@gmail.com>
2024-08-27 14:09:38 +0200

    feat(aws): resolve comments

--- prowler/exceptions/exceptions.py
+++ prowler/exceptions/exceptions.py
@@ -2,67 +2,47 @@ class ProwlerException(Exception):
     """Base exception for all Prowler SDK errors."""
 
     ERROR_CODES = {
-        (1901, "ProviderConnectionError"): {
-            "message": "Provider connection error",
-            "remediation": "Check your network connection and ensure the service is reachable.",
+        (1901, "UnexpectedError"): {
+            "message": "Unexpected error occurred.",
+            "remediation": "Please review the error message and try again.",
             "file": "{file}",
             "provider": "{provider}",
-        },
-        (1902, "ProviderAuthenticationError"): {
-            "message": "Provider authentication failed",
-            "remediation": "Verify your credentials and ensure they have the necessary permissions.",
-            "file": "{file}",
-            "provider": "{provider}",
-        },
-        (1903, "ProviderTimeoutError"): {
-            "message": "Request to provider timed out",
-            "remediation": "Consider increasing the timeout setting or check the service status.",
-            "file": "{file}",
-            "provider": "{provider}",
-        },
-        (1905, "FileExistsError"): {
-            "message": "File could not be updated, it already exists",
-            "remediation": "Provide a different file or set overwrite=True to overwrite the existing file.",
-            "file": "{file}",
-            "provider": "{provider}",
-        },
+        }
     }
 
     def __init__(
         self, code, provider=None, file=None, original_exception=None, error_info=None
     ):
+        """
+        Initialize the ProwlerException class.
+
+        Args:
+            code (int): The error code.
+            provider (str): The provider name.
+            file (str): The file name.
+            original_exception (Exception): The original exception.
+            error_info (dict): The error information.
+
+        Example:
+            A ProwlerException is raised with the following parameters and format:
+            >>> original_exception = Exception("Unexpected error occurred.")
+            ProwlerException(1901, "AWS", "file.txt", original_exception)
+            >>> [1901] Unexpected error occurred. - Please review the error message and try again. - file.txt - Unexpected error occurred. - AWS
+        """
         self.code = code
         self.provider = provider
         self.file = file
-        # Use class name as the second key in the tuple
         if error_info is None:
             error_info = self.ERROR_CODES.get((code, self.__class__.__name__))
-        self.message = error_info["message"]
-        self.remediation = error_info["remediation"]
+        self.message = error_info.get("message")
+        self.remediation = error_info.get("remediation")
         self.original_exception = original_exception
+        # Format -> [code] message - remediation - file - original_exception - provider
         super().__init__(
             f"[{self.code}] {self.message} - {self.remediation} - {self.file} - {self.original_exception} - {self.provider}"
         )
 
 
-# Specific exception classes remain the same
-class ProviderConnectionError(ProwlerException):
+class UnexpectedError(ProwlerException):
     def __init__(self, provider, file, original_exception=None):
         super().__init__(1901, provider, file, original_exception)
-
-
-class ProviderAuthenticationError(ProwlerException):
-    def __init__(self, provider, file, original_exception=None):
-        super().__init__(1902, provider, file, original_exception)
-
-
-class ProviderTimeoutError(ProwlerException):
-    def __init__(self, provider, file, original_exception=None):
-        super().__init__(1903, provider, file, original_exception)
-
-
-class FileExistsError(ProwlerException):
-    def __init__(self, file, original_exception=None):
-        super().__init__(
-            1905, provider=None, file=file, original_exception=original_exception
-        )
--- prowler/providers/aws/exceptions/exceptions.py
+++ prowler/providers/aws/exceptions/exceptions.py
@@ -5,79 +5,79 @@ class AWSBaseException(ProwlerException):
     """Base class for AWS errors."""
 
     AWS_ERROR_CODES = {
-        (1906, "AWSClientError"): {
+        (1902, "AWSClientError"): {
             "message": "AWS ClientError occurred",
             "remediation": "Check your AWS client configuration and permissions.",
             "file": "{file}",
             "provider": "AWS",
         },
-        (1907, "AWSProfileNotFoundError"): {
+        (1903, "AWSProfileNotFoundError"): {
             "message": "AWS Profile not found",
             "remediation": "Ensure the AWS profile is correctly configured.",
             "file": "{file}",
             "provider": "AWS",
         },
-        (1908, "AWSNoCredentialsError"): {
+        (1904, "AWSNoCredentialsError"): {
             "message": "No AWS credentials found",
             "remediation": "Verify that AWS credentials are properly set up.",
             "file": "{file}",
             "provider": "AWS",
         },
-        (1909, "AWSArgumentTypeValidationError"): {
+        (1905, "AWSArgumentTypeValidationError"): {
             "message": "AWS argument type validation error",
             "remediation": "Check the provided argument types specific to AWS and ensure they meet the required format.",
             "file": "{file}",
             "provider": "AWS",
         },
-        (1910, "AWSSetUpSessionError"): {
+        (1906, "AWSSetUpSessionError"): {
             "message": "AWS session setup error",
             "remediation": "Check the AWS session setup and ensure it is properly configured.",
             "file": "{file}",
             "provider": "AWS",
         },
-        (1911, "AWSIAMRoleARNRegionNotEmtpy"): {
+        (1907, "AWSIAMRoleARNRegionNotEmtpy"): {
             "message": "AWS IAM Role ARN region is not empty",
             "remediation": "Check the AWS IAM Role ARN region and ensure it is empty.",
             "file": "{file}",
             "provider": "AWS",
         },
-        (1912, "AWSIAMRoleARNPartitionEmpty"): {
+        (1908, "AWSIAMRoleARNPartitionEmpty"): {
             "message": "AWS IAM Role ARN partition is empty",
             "remediation": "Check the AWS IAM Role ARN partition and ensure it is not empty.",
             "file": "{file}",
             "provider": "AWS",
         },
-        (1913, "AWSIAMRoleARNMissingFields"): {
+        (1909, "AWSIAMRoleARNMissingFields"): {
             "message": "AWS IAM Role ARN missing fields",
             "remediation": "Check the AWS IAM Role ARN and ensure all required fields are present.",
             "file": "{file}",
             "provider": "AWS",
         },
-        (1914, "AWSIAMRoleARNServiceNotIAMnorSTS"): {
+        (1910, "AWSIAMRoleARNServiceNotIAMnorSTS"): {
             "message": "AWS IAM Role ARN service is not IAM nor STS",
             "remediation": "Check the AWS IAM Role ARN service and ensure it is either IAM or STS.",
             "file": "{file}",
             "provider": "AWS",
         },
-        (1915, "AWSIAMRoleARNInvalidAccountID"): {
+        (1911, "AWSIAMRoleARNInvalidAccountID"): {
             "message": "AWS IAM Role ARN account ID is invalid",
             "remediation": "Check the AWS IAM Role ARN account ID and ensure it is a valid 12-digit number.",
             "file": "{file}",
             "provider": "AWS",
         },
-        (1916, "AWSIAMRoleARNInvalidResourceType"): {
+        (1912, "AWSIAMRoleARNInvalidResourceType"): {
             "message": "AWS IAM Role ARN resource type is invalid",
             "remediation": "Check the AWS IAM Role ARN resource type and ensure it is valid.",
             "file": "{file}",
             "provider": "AWS",
         },
-        (1917, "AWSIAMRoleARNEmptyResource"): {
+        (1913, "AWSIAMRoleARNEmptyResource"): {
             "message": "AWS IAM Role ARN resource is empty",
             "remediation": "Check the AWS IAM Role ARN resource and ensure it is not empty.",
             "file": "{file}",
             "provider": "AWS",
         },
-        (1918, "AWSAssumeRoleError"): {
+        (1914, "AWSAssumeRoleError"): {
             "message": "AWS assume role error",
             "remediation": "",
             "file": "{file}",
@@ -100,35 +100,35 @@ class AWSCredentialsError(AWSBaseException):
 class AWSClientError(AWSCredentialsError):
     def __init__(self, file=None, original_exception=None):
         super().__init__(
-            1906, provider="AWS", file=file, original_exception=original_exception
+            1902, provider="AWS", file=file, original_exception=original_exception
         )
 
 
 class AWSProfileNotFoundError(AWSCredentialsError):
     def __init__(self, file=None, original_exception=None):
         super().__init__(
-            1907, provider="AWS", file=file, original_exception=original_exception
+            1903, provider="AWS", file=file, original_exception=original_exception
         )
 
 
 class AWSNoCredentialsError(AWSCredentialsError):
     def __init__(self, file=None, original_exception=None):
         super().__init__(
-            1908, provider="AWS", file=file, original_exception=original_exception
+            1904, provider="AWS", file=file, original_exception=original_exception
         )
 
 
 class AWSArgumentTypeValidationError(AWSBaseException):
     def __init__(self, file=None, original_exception=None):
         super().__init__(
-            1909, provider="AWS", file=file, original_exception=original_exception
+            1905, provider="AWS", file=file, original_exception=original_exception
         )
 
 
 class AWSSetUpSessionError(AWSBaseException):
     def __init__(self, file=None, original_exception=None):
         super().__init__(
-            1910, provider="AWS", file=file, original_exception=original_exception
+            1906, provider="AWS", file=file, original_exception=original_exception
         )
 
 
@@ -142,54 +142,54 @@ class AWSRoleArnError(AWSBaseException):
 class AWSIAMRoleARNRegionNotEmtpy(AWSRoleArnError):
     def __init__(self, file=None, original_exception=None):
         super().__init__(
-            1911, provider="AWS", file=file, original_exception=original_exception
+            1907, provider="AWS", file=file, original_exception=original_exception
         )
 
 
 class AWSIAMRoleARNPartitionEmpty(AWSRoleArnError):
     def __init__(self, file=None, original_exception=None):
         super().__init__(
-            1912, provider="AWS", file=file, original_exception=original_exception
+            1908, provider="AWS", file=file, original_exception=original_exception
         )
 
 
 class AWSIAMRoleARNMissingFields(AWSRoleArnError):
     def __init__(self, file=None, original_exception=None):
         super().__init__(
-            1913, provider="AWS", file=file, original_exception=original_exception
+            1909, provider="AWS", file=file, original_exception=original_exception
         )
 
 
 class AWSIAMRoleARNServiceNotIAMnorSTS(AWSRoleArnError):
     def __init__(self, file=None, original_exception=None):
         super().__init__(
-            1914, provider="AWS", file=file, original_exception=original_exception
+            1910, provider="AWS", file=file, original_exception=original_exception
         )
 
 
 class AWSIAMRoleARNInvalidAccountID(AWSRoleArnError):
     def __init__(self, file=None, original_exception=None):
         super().__init__(
-            1915, provider="AWS", file=file, original_exception=original_exception
+            1911, provider="AWS", file=file, original_exception=original_exception
         )
 
 
 class AWSIAMRoleARNInvalidResourceType(AWSRoleArnError):
     def __init__(self, file=None, original_exception=None):
         super().__init__(
-            1916, provider="AWS", file=file, original_exception=original_exception
+            1912, provider="AWS", file=file, original_exception=original_exception
         )
 
 
 class AWSIAMRoleARNEmptyResource(AWSRoleArnError):
     def __init__(self, file=None, original_exception=None):
         super().__init__(
-            1917, provider="AWS", file=file, original_exception=original_exception
+            1913, provider="AWS", file=file, original_exception=original_exception
         )
 
 
 class AWSAssumeRoleError(AWSBaseException):
     def __init__(self, file=None, original_exception=None):
         super().__init__(
-            1918, provider="AWS", file=file, original_exception=original_exception
+            1914, provider="AWS", file=file, original_exception=original_exception
         )
