e7097ba74969caca5e04c023886a37973577167a
cr0hn <cr0hn@cr0hn.com>
2024-08-29 16:11:20 +0200

    Add force_gc context manager to manage garbage collection
    Introduce force_gc to temporarily disable and enable garbage collection within a code block. This utility can be used to better control memory management by ensuring garbage collection is only performed at specific points.
    
    This is useful when you want to ensure that a certain piece of code does not cause memory leaks or excessive garbage collection.

---
+++ prowler/lib/utils/gc.py
@@ -0,0 +1,35 @@
+import gc
+
+from contextlib import contextmanager
+
+
+@contextmanager
+def force_gc(disable_gc=False):
+    """
+    Context manager that temporarily disables garbage collection if `disable_gc` argument is True.
+
+    Args:
+        disable_gc (bool): If True, garbage collection will be disabled temporarily.
+
+    Yields:
+        None
+
+    Notes:
+        - If `disable_gc` is True, garbage collection will be disabled before entering the block.
+        - The block of code should be indented after the `yield` statement.
+        - Garbage collection will be invoked with `gc.collect()` before exiting the block, regardless of the value of `disable_gc`.
+        - If `disable_gc` is True, garbage collection will be enabled after exiting the block.
+    """
+    if disable_gc:
+        gc.disable()
+
+    try:
+        yield
+    finally:
+        gc.collect()
+
+    if disable_gc:
+        gc.enable()
+
+
+__all__ = ('force_gc',)
