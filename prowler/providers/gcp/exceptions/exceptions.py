dbabfe60ed194a7101a55fa37e6d652bbf1b1c4c
pedrooot <pedromarting3@gmail.com>
2024-08-30 14:05:40 +0200

    feat(gcp): add custom exceptions class for gcp

--- prowler/providers/gcp/exceptions/exceptions.py
+++ prowler/providers/gcp/exceptions/exceptions.py
@@ -2,8 +2,89 @@ from prowler.exceptions.exceptions import ProwlerException
 
 
 class GCPBaseException(ProwlerException):
-    def __init__(self, message, remediation, file):
-        self.message = message
-        self.remediation = remediation
-        self.file = file
-        self.provider = "GCP"
+    """Base class for GCP Errors."""
+
+    GCP_ERROR_CODES = {
+        (1925, "GCPCloudResourceManagerAPINotUsedError"): {
+            "message": "Cloud Resource Manager API not used",
+            "remediation": "Enable the Cloud Resource Manager API for the project.",
+            "file": "{file}",
+            "provider": "GCP",
+        },
+        (1926, "GCPHTTPError"): {
+            "message": "HTTP error",
+            "remediation": "Check the HTTP error and ensure the request is properly formatted.",
+            "file": "{file}",
+            "provider": "GCP",
+        },
+        (1927, "GCPNoAccesibleProjectsError"): {
+            "message": "No accessible projects",
+            "remediation": "Ensure the project is accessible and properly set up.",
+            "file": "{file}",
+            "provider": "GCP",
+        },
+        (1928, "GCPSetUpSessionError"): {
+            "message": "Error setting up session",
+            "remediation": "Check the session setup and ensure it is properly set up.",
+            "file": "{file}",
+            "provider": "GCP",
+        },
+        (1929, "GCPGetProjectError"): {
+            "message": "Error getting project",
+            "remediation": "Check the project and ensure it is properly set up.",
+            "file": "{file}",
+            "provider": "GCP",
+        },
+        (1930, "GCPTestConnectionError"): {
+            "message": "Error testing connection to GCP",
+            "remediation": "Check the connection and ensure it is properly set up.",
+            "file": "{file}",
+            "provider": "GCP",
+        },
+    }
+
+    def __init__(self, code, provider="GCP", file=None, original_exception=None):
+        error_info = self.GCP_ERROR_CODES.get((code, self.__class__.__name__))
+        super().__init__(code, provider, file, original_exception, error_info)
+
+
+class GCPCloudResourceManagerAPINotUsedError(GCPBaseException):
+    def __init__(self, file=None, original_exception=None):
+        super().__init__(
+            1925, provider="GCP", file=file, original_exception=original_exception
+        )
+
+
+class GCPHTTPError(GCPBaseException):
+    def __init__(self, file=None, original_exception=None):
+        super().__init__(
+            1926, provider="GCP", file=file, original_exception=original_exception
+        )
+
+
+class GCPNoAccesibleProjectsError(GCPBaseException):
+    def __init__(self, file=None, original_exception=None):
+        super().__init__(
+            1927, provider="GCP", file=file, original_exception=original_exception
+        )
+
+
+class GCPSetUpSessionError(GCPBaseException):
+    def __init__(self, file=None, original_exception=None):
+        super().__init__(
+            1928, provider="GCP", file=file, original_exception=original_exception
+        )
+
+
+class GCPGetProjectError(GCPBaseException):
+    def __init__(self, file=None, original_exception=None):
+        super().__init__(
+            1929, provider="GCP", file=file, original_exception=original_exception
+        )
+
+
+class GCPTestConnectionError(GCPBaseException):
+    def __init__(self, file=None, original_exception=None):
+        super().__init__(
+            1930, provider="GCP", file=file, original_exception=original_exception
+        )
