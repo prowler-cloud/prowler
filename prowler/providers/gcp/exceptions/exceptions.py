5f4fa0c65e6ca7c22433c3c5dfc3dc33f6bef14b
pedrooot <pedromarting3@gmail.com>
2024-09-03 08:01:54 +0200

    feat(gcp): resolve comments

--- prowler/providers/gcp/exceptions/exceptions.py
+++ prowler/providers/gcp/exceptions/exceptions.py
@@ -9,37 +9,37 @@ class GCPBaseException(ProwlerException):
             "message": "Cloud Resource Manager API not used",
             "remediation": "Enable the Cloud Resource Manager API for the project.",
             "file": "{file}",
-            "provider": "GCP",
+            "provider": "{provider}",
         },
         (1926, "GCPHTTPError"): {
             "message": "HTTP error",
             "remediation": "Check the HTTP error and ensure the request is properly formatted.",
             "file": "{file}",
-            "provider": "GCP",
+            "provider": "{provider}",
         },
         (1927, "GCPNoAccesibleProjectsError"): {
-            "message": "No accessible projects",
+            "message": "No Project IDs can be accessed via Google Credentials",
             "remediation": "Ensure the project is accessible and properly set up.",
             "file": "{file}",
-            "provider": "GCP",
+            "provider": "{provider}",
         },
         (1928, "GCPSetUpSessionError"): {
             "message": "Error setting up session",
             "remediation": "Check the session setup and ensure it is properly set up.",
             "file": "{file}",
-            "provider": "GCP",
+            "provider": "{provider}",
         },
         (1929, "GCPGetProjectError"): {
             "message": "Error getting project",
             "remediation": "Check the project and ensure it is properly set up.",
             "file": "{file}",
-            "provider": "GCP",
+            "provider": "{provider}",
         },
         (1930, "GCPTestConnectionError"): {
             "message": "Error testing connection to GCP",
             "remediation": "Check the connection and ensure it is properly set up.",
             "file": "{file}",
-            "provider": "GCP",
+            "provider": "{provider}",
         },
     }
 
@@ -50,41 +50,29 @@ class GCPBaseException(ProwlerException):
 
 class GCPCloudResourceManagerAPINotUsedError(GCPBaseException):
     def __init__(self, file=None, original_exception=None):
-        super().__init__(
-            1925, provider="GCP", file=file, original_exception=original_exception
-        )
+        super().__init__(1925, file=file, original_exception=original_exception)
 
 
 class GCPHTTPError(GCPBaseException):
     def __init__(self, file=None, original_exception=None):
-        super().__init__(
-            1926, provider="GCP", file=file, original_exception=original_exception
-        )
+        super().__init__(1926, file=file, original_exception=original_exception)
 
 
 class GCPNoAccesibleProjectsError(GCPBaseException):
     def __init__(self, file=None, original_exception=None):
-        super().__init__(
-            1927, provider="GCP", file=file, original_exception=original_exception
-        )
+        super().__init__(1927, file=file, original_exception=original_exception)
 
 
 class GCPSetUpSessionError(GCPBaseException):
     def __init__(self, file=None, original_exception=None):
-        super().__init__(
-            1928, provider="GCP", file=file, original_exception=original_exception
-        )
+        super().__init__(1928, file=file, original_exception=original_exception)
 
 
 class GCPGetProjectError(GCPBaseException):
     def __init__(self, file=None, original_exception=None):
-        super().__init__(
-            1929, provider="GCP", file=file, original_exception=original_exception
-        )
+        super().__init__(1929, file=file, original_exception=original_exception)
 
 
 class GCPTestConnectionError(GCPBaseException):
     def __init__(self, file=None, original_exception=None):
-        super().__init__(
-            1930, provider="GCP", file=file, original_exception=original_exception
-        )
+        super().__init__(1930, file=file, original_exception=original_exception)
