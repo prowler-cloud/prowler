79a0fb4669b00ea605dfd173140d10ea520f3cac
Lefteris Gilmaz <lefterisgilmaz@Lefteriss-MacBook-Pro.local>
2024-09-03 12:56:59 +0100

    adds verify-full in the test case

--- tests/providers/aws/services/dms/dms_endpoint_ssl_enabled_test/dms_endpoint_ssl_enabled_test.py
+++ tests/providers/aws/services/dms/dms_endpoint_ssl_enabled_test/dms_endpoint_ssl_enabled_test.py
@@ -135,3 +135,31 @@ class Test_dms_endpoint_ssl_enabled:
             self.assertEqual(finding.resource_arn, "arn:aws:dms:us-west-2:123456789012:endpoint:ABCDEFGHIJKLMNOP")
             self.assertEqual(finding.region, "us-west-2")
             self.assertIn("is using SSL with mode: verify-ca", finding.status_extended)  
+
+    
+    def test_dms_ssl_enabled_ssl_verify_full(self):
+        dms_client = mock.MagicMock
+        dms_client.endpoints = {
+            "arn:aws:dms:us-west-2:123456789012:endpoint:ABCDEFGHIJKLMNOP": mock.MagicMock(
+                endpoint_arn="arn:aws:dms:us-west-2:123456789012:endpoint:ABCDEFGHIJKLMNOP",
+                ssl_mode="verify-full",
+                region="us-west-2"
+            )
+        }
+
+        with mock.patch(
+            "prowler.providers.aws.services.dms.dms_client.dms_client",
+            new=dms_client,
+        ):
+            from prowler.providers.aws.services.dms.dms_endpoint_ssl_enabled.dms_endpoint_ssl_enabled import (
+            dms_endpoint_ssl_enabled,
+            )
+            check = dms_endpoint_ssl_enabled()
+            result = check.execute() 
+            finding = result[0]
+            self.assertEqual(len(result), 1)
+            self.assertEqual(finding.status, "PASS")  
+            self.assertEqual(finding.resource_id, "arn:aws:dms:us-west-2:123456789012:endpoint:ABCDEFGHIJKLMNOP")
+            self.assertEqual(finding.resource_arn, "arn:aws:dms:us-west-2:123456789012:endpoint:ABCDEFGHIJKLMNOP")
+            self.assertEqual(finding.region, "us-west-2")
+            self.assertIn("is using SSL with mode: verify-full", finding.status_extended)  
