d9a8ca628ab463026d588303d5c8c21cb8d20526
pedrooot <pedromarting3@gmail.com>
2024-09-03 14:53:53 +0200

    feat(azure): add validations for CLI inside the provider

--- prowler/providers/azure/lib/arguments/arguments.py
+++ prowler/providers/azure/lib/arguments/arguments.py
@@ -12,19 +12,16 @@ def init_parser(self):
     azure_auth_modes_group.add_argument(
         "--az-cli-auth",
         action="store_true",
-        type=validate_arguments,
         help="Use Azure CLI credentials to log in against Azure",
     )
     azure_auth_modes_group.add_argument(
         "--sp-env-auth",
         action="store_true",
-        type=validate_arguments,
         help="Use Service Principal environment variables authentication to log in against Azure",
     )
     azure_auth_modes_group.add_argument(
         "--browser-auth",
         action="store_true",
-        type=validate_arguments,
         help="Use browser authentication to log in against Azure, --tenant-id is required for this option",
     )
     azure_auth_modes_group.add_argument(
@@ -45,7 +42,6 @@ def init_parser(self):
         "--tenant-id",
         nargs="?",
         default=None,
-        type=validate_arguments,
         help="Azure Tenant ID to be used with --browser-auth option",
     )
     # Regions
@@ -57,45 +53,6 @@ def init_parser(self):
         type=validate_azure_region,
         help="Azure region from `az cloud list --output table`, by default AzureCloud",
     )
-
-
-def validate_arguments(
-    az_cli_auth: bool,
-    sp_env_auth: bool,
-    browser_auth: bool,
-    managed_identity_auth: bool,
-    tenant_id: str,
-):
-    """
-    Validates the authentication arguments for the Azure provider.
-
-    Args:
-        az_cli_auth (bool): Flag indicating whether AZ CLI authentication is enabled.
-        sp_env_auth (bool): Flag indicating whether Service Principal environment authentication is enabled.
-        browser_auth (bool): Flag indicating whether browser authentication is enabled.
-        managed_identity_auth (bool): Flag indicating whether managed identity authentication is enabled.
-        tenant_id (str): The Azure Tenant ID.
-
-    Raises:
-        AzureBrowserAuthNoTenantIDError: If browser authentication is enabled but the tenant ID is not found.
-    """
-    if not browser_auth and tenant_id:
-        raise ArgumentTypeError(
-            "Azure Tenant ID (--tenant-id) is required for browser authentication mode"
-        )
-    elif (
-        not az_cli_auth
-        and not sp_env_auth
-        and not browser_auth
-        and not managed_identity_auth
-    ):
-        raise ArgumentTypeError(
-            "Azure provider requires at least one authentication method set: [--az-cli-auth | --sp-env-auth | --browser-auth | --managed-identity-auth]"
-        )
-    elif browser_auth and not tenant_id:
-        raise ArgumentTypeError(
-            "Azure Tenant ID (--tenant-id) is required for browser authentication mode"
-        )
 
 
 def validate_azure_region(region):
