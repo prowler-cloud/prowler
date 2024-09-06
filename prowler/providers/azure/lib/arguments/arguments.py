9377d17621b70c8e791a38fcbc647d96933f58b1
pedrooot <pedromarting3@gmail.com>
2024-09-06 13:52:13 +0200

    fix: tests

--- prowler/providers/azure/lib/arguments/arguments.py
+++ prowler/providers/azure/lib/arguments/arguments.py
@@ -1,4 +1,4 @@
-from argparse import ArgumentTypeError
+from argparse import ArgumentTypeError, Namespace
 
 
 def init_parser(self):
@@ -70,41 +70,35 @@ def validate_azure_region(region):
     return region
 
 
-def validate_arguments(
-    az_cli_auth: bool,
-    sp_env_auth: bool,
-    browser_auth: bool,
-    managed_identity_auth: bool,
-    tenant_id: str,
-):
+def validate_arguments(args: Namespace) -> tuple[bool, str]:
     """
     Validates the authentication arguments for the Azure provider.
 
     Args:
-        az_cli_auth (bool): Flag indicating whether AZ CLI authentication is enabled.
-        sp_env_auth (bool): Flag indicating whether Service Principal environment authentication is enabled.
-        browser_auth (bool): Flag indicating whether browser authentication is enabled.
-        managed_identity_auth (bool): Flag indicating whether managed identity authentication is enabled.
-        tenant_id (str): The Azure Tenant ID.
+        args (Namespace): The parsed arguments from the Azure provider.
 
-    Raises:
-        ArgumentTypeError: If the Azure provider requires at least one authentication method set.
-        ArgumentTypeError: If the Azure Tenant ID is required for browser authentication mode
+    Returns:
+        tuple[bool, str]: A tuple containing a boolean indicating if the arguments are valid and a string with an error message if the arguments are invalid
     """
-    if not browser_auth and tenant_id:
-        raise ArgumentTypeError(
-            "Azure Tenant ID (--tenant-id) is required for browser authentication mode"
+    if not args.browser_auth and args.tenant_id:
+        return (
+            False,
+            "Azure Tenant ID (--tenant-id) is required for browser authentication mode",
         )
     elif (
-        not az_cli_auth
-        and not sp_env_auth
-        and not browser_auth
-        and not managed_identity_auth
+        not args.az_cli_auth
+        and not args.sp_env_auth
+        and not args.browser_auth
+        and not args.managed_identity_auth
     ):
-        raise ArgumentTypeError(
-            "Azure provider requires at least one authentication method set: [--az-cli-auth | --sp-env-auth | --browser-auth | --managed-identity-auth]"
+        return (
+            False,
+            "Azure provider requires at least one authentication method set: [--az-cli-auth | --sp-env-auth | --browser-auth | --managed-identity-auth]",
         )
-    elif browser_auth and not tenant_id:
-        raise ArgumentTypeError(
-            "Azure Tenant ID (--tenant-id) is required for browser authentication mode"
+    elif args.browser_auth and not args.tenant_id:
+        return (
+            False,
+            "Azure Tenant ID (--tenant-id) is required for browser authentication mode",
         )
+
+    return (True, "")
