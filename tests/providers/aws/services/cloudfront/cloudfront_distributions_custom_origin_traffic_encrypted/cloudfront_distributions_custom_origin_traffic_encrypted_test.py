42667f38240c77d3d0e5288fb86e556d53f1b1cc
HugoPBrito <hugopbrit@gmail.com>
2024-09-09 10:22:53 +0200

    feat(cloudfront): added testing

---
+++ tests/providers/aws/services/cloudfront/cloudfront_distributions_custom_origin_traffic_encrypted/cloudfront_distributions_custom_origin_traffic_encrypted_test.py
@@ -0,0 +1,199 @@
+from unittest import mock
+
+from prowler.providers.aws.services.cloudfront.cloudfront_service import (
+    DefaultCacheConfigBehaviour,
+    Distribution,
+    ViewerProtocolPolicy,
+)
+from tests.providers.aws.utils import AWS_ACCOUNT_NUMBER
+
+DISTRIBUTION_ID = "E27LVI50CSW06W"
+DISTRIBUTION_ARN = (
+    f"arn:aws:cloudfront::{AWS_ACCOUNT_NUMBER}:distribution/{DISTRIBUTION_ID}"
+)
+REGION = "eu-west-1"
+
+
+class Test_cloudfront_distributions_custom_origin_traffic_encrypted:
+    def test_no_distributions(self):
+        cloudfront_client = mock.MagicMock
+        cloudfront_client.distributions = {}
+        with mock.patch(
+            "prowler.providers.aws.services.cloudfront.cloudfront_service.CloudFront",
+            new=cloudfront_client,
+        ):
+            # Test Check
+            from prowler.providers.aws.services.cloudfront.cloudfront_distributions_custom_origin_traffic_encrypted.cloudfront_distributions_custom_origin_traffic_encrypted import (
+                cloudfront_distributions_custom_origin_traffic_encrypted,
+            )
+
+            check = cloudfront_distributions_custom_origin_traffic_encrypted()
+            result = check.execute()
+
+            assert len(result) == 0
+
+    def test_distribution_no_traffic_encryption(self):
+        cloudfront_client = mock.MagicMock
+        cloudfront_client.distributions = {
+            "DISTRIBUTION_ID": Distribution(
+                arn=DISTRIBUTION_ARN,
+                id=DISTRIBUTION_ID,
+                region=REGION,
+                origins=[],
+                default_cache_config=DefaultCacheConfigBehaviour(
+                    realtime_log_config_arn="",
+                    viewer_protocol_policy=ViewerProtocolPolicy.allow_all,
+                    field_level_encryption_id="",
+                ),
+                default_root_object="",
+                origin_protocol_policy="",
+                viewer_protocol_policy="",
+            )
+        }
+
+        with mock.patch(
+            "prowler.providers.aws.services.cloudfront.cloudfront_service.CloudFront",
+            new=cloudfront_client,
+        ):
+            # Test Check
+            from prowler.providers.aws.services.cloudfront.cloudfront_distributions_custom_origin_traffic_encrypted.cloudfront_distributions_custom_origin_traffic_encrypted import (
+                cloudfront_distributions_custom_origin_traffic_encrypted,
+            )
+
+            check = cloudfront_distributions_custom_origin_traffic_encrypted()
+            result = check.execute()
+
+            assert len(result) == 1
+            assert result[0].region == REGION
+            assert result[0].resource_arn == DISTRIBUTION_ARN
+            assert result[0].resource_id == DISTRIBUTION_ID
+            assert result[0].status == "FAIL"
+            assert (
+                result[0].status_extended
+                == f"CloudFront Distribution {DISTRIBUTION_ID} does not encrypt traﬃc to custom origins."
+            )
+            assert result[0].resource_tags == []
+
+    def test_distribution_http_only(self):
+        cloudfront_client = mock.MagicMock
+        cloudfront_client.distributions = {
+            "DISTRIBUTION_ID": Distribution(
+                arn=DISTRIBUTION_ARN,
+                id=DISTRIBUTION_ID,
+                region=REGION,
+                origins=[],
+                default_cache_config=DefaultCacheConfigBehaviour(
+                    realtime_log_config_arn="",
+                    viewer_protocol_policy=ViewerProtocolPolicy.allow_all,
+                    field_level_encryption_id="",
+                ),
+                default_root_object="index.html",
+                origin_protocol_policy="http-only",
+            )
+        }
+
+        with mock.patch(
+            "prowler.providers.aws.services.cloudfront.cloudfront_service.CloudFront",
+            new=cloudfront_client,
+        ):
+            # Test Check
+            from prowler.providers.aws.services.cloudfront.cloudfront_distributions_custom_origin_traffic_encrypted.cloudfront_distributions_custom_origin_traffic_encrypted import (
+                cloudfront_distributions_custom_origin_traffic_encrypted,
+            )
+
+            check = cloudfront_distributions_custom_origin_traffic_encrypted()
+            result = check.execute()
+
+            assert len(result) == 1
+            assert result[0].region == REGION
+            assert result[0].resource_arn == DISTRIBUTION_ARN
+            assert result[0].resource_id == DISTRIBUTION_ID
+            assert result[0].status == "FAIL"
+            assert (
+                result[0].status_extended
+                == f"CloudFront Distribution {DISTRIBUTION_ID} does not encrypt traﬃc to custom origins."
+            )
+            assert result[0].resource_tags == []
+
+    def test_distribution_match_viewer_allow_all(self):
+        cloudfront_client = mock.MagicMock
+        cloudfront_client.distributions = {
+            "DISTRIBUTION_ID": Distribution(
+                arn=DISTRIBUTION_ARN,
+                id=DISTRIBUTION_ID,
+                region=REGION,
+                origins=[],
+                default_cache_config=DefaultCacheConfigBehaviour(
+                    realtime_log_config_arn="",
+                    viewer_protocol_policy=ViewerProtocolPolicy.allow_all,
+                    field_level_encryption_id="",
+                ),
+                default_root_object="index.html",
+                origin_protocol_policy="match-viewer",
+                viewer_protocol_policy="allow-all",
+            )
+        }
+
+        with mock.patch(
+            "prowler.providers.aws.services.cloudfront.cloudfront_service.CloudFront",
+            new=cloudfront_client,
+        ):
+            # Test Check
+            from prowler.providers.aws.services.cloudfront.cloudfront_distributions_custom_origin_traffic_encrypted.cloudfront_distributions_custom_origin_traffic_encrypted import (
+                cloudfront_distributions_custom_origin_traffic_encrypted,
+            )
+
+            check = cloudfront_distributions_custom_origin_traffic_encrypted()
+            result = check.execute()
+
+            assert len(result) == 1
+            assert result[0].region == REGION
+            assert result[0].resource_arn == DISTRIBUTION_ARN
+            assert result[0].resource_id == DISTRIBUTION_ID
+            assert result[0].status == "FAIL"
+            assert (
+                result[0].status_extended
+                == f"CloudFront Distribution {DISTRIBUTION_ID} does not encrypt traﬃc to custom origins."
+            )
+            assert result[0].resource_tags == []
+
+    def test_distribution_traffic_encrypted(self):
+        cloudfront_client = mock.MagicMock
+        cloudfront_client.distributions = {
+            "DISTRIBUTION_ID": Distribution(
+                arn=DISTRIBUTION_ARN,
+                id=DISTRIBUTION_ID,
+                region=REGION,
+                origins=[],
+                default_cache_config=DefaultCacheConfigBehaviour(
+                    realtime_log_config_arn="",
+                    viewer_protocol_policy=ViewerProtocolPolicy.allow_all,
+                    field_level_encryption_id="",
+                ),
+                default_root_object="index.html",
+                origin_protocol_policy="https-only",
+            )
+        }
+
+        with mock.patch(
+            "prowler.providers.aws.services.cloudfront.cloudfront_service.CloudFront",
+            new=cloudfront_client,
+        ):
+            # Test Check
+            from prowler.providers.aws.services.cloudfront.cloudfront_distributions_custom_origin_traffic_encrypted.cloudfront_distributions_custom_origin_traffic_encrypted import (
+                cloudfront_distributions_custom_origin_traffic_encrypted,
+            )
+
+            check = cloudfront_distributions_custom_origin_traffic_encrypted()
+            result = check.execute()
+
+            assert len(result) == 1
+            assert result[0].region == REGION
+            assert result[0].resource_arn == DISTRIBUTION_ARN
+            assert result[0].resource_id == DISTRIBUTION_ID
+            assert result[0].status == "PASS"
+            assert (
+                result[0].status_extended
+                == f"CloudFront Distribution {DISTRIBUTION_ID} does encrypt traﬃc to custom origins."
+            )
+            assert result[0].resource_tags == []
