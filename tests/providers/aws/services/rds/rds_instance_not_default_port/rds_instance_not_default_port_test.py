cbaf769b7b59f3f86ff247c4a3926adcd348a614
Daniel Barranquero <danielbo2001@gmail.com>
2024-09-11 11:52:21 +0200

    chore(rds): Add asserts left on some tests

--- tests/providers/aws/services/rds/rds_instance_not_default_port/rds_instance_not_default_port_test.py
+++ tests/providers/aws/services/rds/rds_instance_not_default_port/rds_instance_not_default_port_test.py
@@ -3,7 +3,11 @@ from unittest import mock
 from boto3 import client
 from moto import mock_aws
 
-from tests.providers.aws.utils import AWS_REGION_US_EAST_1, set_mocked_aws_provider
+from tests.providers.aws.utils import (
+    AWS_ACCOUNT_NUMBER,
+    AWS_REGION_US_EAST_1,
+    set_mocked_aws_provider,
+)
 
 
 class Test_rds_instance_not_default_port:
@@ -45,49 +49,7 @@ class Test_rds_instance_not_default_port:
             PubliclyAccessible=True,
             AutoMinorVersionUpgrade=True,
             BackupRetentionPeriod=10,
-            Port=5432,  # Default Postgres port
-            Tags=[{"Key": "test", "Value": "test"}],
-        )
-
-        from prowler.providers.aws.services.rds.rds_service import RDS
-
-        aws_provider = set_mocked_aws_provider([AWS_REGION_US_EAST_1])
-
-        with mock.patch(
-            "prowler.providers.common.provider.Provider.get_global_provider",
-            return_value=aws_provider,
-        ):
-            with mock.patch(
-                "prowler.providers.aws.services.rds.rds_instance_not_default_port.rds_instance_not_default_port.rds_client",
-                new=RDS(aws_provider),
-            ):
-                # Test Check
-                from prowler.providers.aws.services.rds.rds_instance_not_default_port.rds_instance_not_default_port import (
-                    rds_instance_not_default_port,
-                )
-
-                check = rds_instance_not_default_port()
-                result = check.execute()
-
-                assert len(result) == 1
-                assert result[0].status == "FAIL"
-                assert "using the default port" in result[0].status_extended
-
-    @mock_aws
-    def test_rds_instance_not_default_port_sg_not_allowed(self):
-        conn = client("rds", region_name=AWS_REGION_US_EAST_1)
-        conn.create_db_instance(
-            DBInstanceIdentifier="db-master-1",
-            AllocatedStorage=10,
-            Engine="postgres",
-            DBName="staging-postgres",
-            DBInstanceClass="db.m1.small",
-            StorageEncrypted=True,
-            DeletionProtection=True,
-            PubliclyAccessible=True,
-            AutoMinorVersionUpgrade=True,
-            BackupRetentionPeriod=10,
-            Port=5433,  # Non-default Postgres port
+            Port=5432,
             Tags=[{"Key": "test", "Value": "test"}],
         )
 
@@ -114,9 +76,68 @@ class Test_rds_instance_not_default_port:
                 assert len(result) == 1
                 assert result[0].status == "FAIL"
                 assert (
-                    "none of the associated security groups allow access to this port"
-                    in result[0].status_extended
+                    result[0].status_extended
+                    == "RDS Instance db-master-1 is using the default port 5432 for postgres."
                 )
+                assert result[0].resource_id == "db-master-1"
+                assert result[0].region == AWS_REGION_US_EAST_1
+                assert (
+                    result[0].resource_arn
+                    == f"arn:aws:rds:{AWS_REGION_US_EAST_1}:{AWS_ACCOUNT_NUMBER}:db:db-master-1"
+                )
+                assert result[0].resource_tags == [{"Key": "test", "Value": "test"}]
+
+    @mock_aws
+    def test_rds_instance_not_default_port_sg_not_allowed(self):
+        conn = client("rds", region_name=AWS_REGION_US_EAST_1)
+        conn.create_db_instance(
+            DBInstanceIdentifier="db-master-1",
+            AllocatedStorage=10,
+            Engine="postgres",
+            DBName="staging-postgres",
+            DBInstanceClass="db.m1.small",
+            StorageEncrypted=True,
+            DeletionProtection=True,
+            PubliclyAccessible=True,
+            AutoMinorVersionUpgrade=True,
+            BackupRetentionPeriod=10,
+            Port=5433,
+            Tags=[{"Key": "test", "Value": "test"}],
+        )
+
+        from prowler.providers.aws.services.rds.rds_service import RDS
+
+        aws_provider = set_mocked_aws_provider([AWS_REGION_US_EAST_1])
+
+        with mock.patch(
+            "prowler.providers.common.provider.Provider.get_global_provider",
+            return_value=aws_provider,
+        ):
+            with mock.patch(
+                "prowler.providers.aws.services.rds.rds_instance_not_default_port.rds_instance_not_default_port.rds_client",
+                new=RDS(aws_provider),
+            ):
+                # Test Check
+                from prowler.providers.aws.services.rds.rds_instance_not_default_port.rds_instance_not_default_port import (
+                    rds_instance_not_default_port,
+                )
+
+                check = rds_instance_not_default_port()
+                result = check.execute()
+
+                assert len(result) == 1
+                assert result[0].status == "FAIL"
+                assert (
+                    result[0].status_extended
+                    == "RDS Instance db-master-1 is using a non-default port (5433) for postgres, but none of the associated security groups allow access to this port."
+                )
+                assert result[0].resource_id == "db-master-1"
+                assert result[0].region == AWS_REGION_US_EAST_1
+                assert (
+                    result[0].resource_arn
+                    == f"arn:aws:rds:{AWS_REGION_US_EAST_1}:{AWS_ACCOUNT_NUMBER}:db:db-master-1"
+                )
+                assert result[0].resource_tags == [{"Key": "test", "Value": "test"}]
 
     @mock_aws
     def test_rds_instance_not_default_port_sg_allowed(self):
@@ -182,3 +203,14 @@ class Test_rds_instance_not_default_port:
 
                 assert len(result) == 1
                 assert result[0].status == "PASS"
+                assert (
+                    result[0].status_extended
+                    == "RDS Instance db-master-1 is using a non-default port (5433) for postgres and the security group vpc-12345678 allows access on this port."
+                )
+                assert result[0].resource_id == "db-master-1"
+                assert result[0].region == AWS_REGION_US_EAST_1
+                assert (
+                    result[0].resource_arn
+                    == f"arn:aws:rds:{AWS_REGION_US_EAST_1}:{AWS_ACCOUNT_NUMBER}:db:db-master-1"
+                )
+                assert result[0].resource_tags == [{"Key": "test", "Value": "test"}]
