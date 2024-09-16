71902698f0f10d0e5f29013a7d307708c92e0d0f
MarioRgzLpz <mariorgzlpz1809@gmail.com>
2024-09-16 11:16:29 +0200

    feat(sagemaker): Add new models EndpointConfig and ProductionVariant to the service with respective unit tests

--- prowler/providers/aws/services/sagemaker/sagemaker_service.py
+++ prowler/providers/aws/services/sagemaker/sagemaker_service.py
@@ -16,12 +16,15 @@ class SageMaker(AWSService):
         self.sagemaker_notebook_instances = []
         self.sagemaker_models = []
         self.sagemaker_training_jobs = []
+        self.endpoint_configs = {}
         self.__threading_call__(self._list_notebook_instances)
         self.__threading_call__(self._list_models)
         self.__threading_call__(self._list_training_jobs)
-        self._describe_model(self.regional_clients)
-        self._describe_notebook_instance(self.regional_clients)
-        self._describe_training_job(self.regional_clients)
+        self.__threading_call__(self._list_endpoint_configs)
+        self.__threading_call__(self._describe_model)
+        self.__threading_call__(self._describe_notebook_instance)
+        self.__threading_call__(self._describe_training_job)
+        self.__threading_call__(self._describe_endpoint_config)
         self._list_tags_for_resource()
 
     def _list_notebook_instances(self, regional_client):
@@ -100,7 +103,7 @@ class SageMaker(AWSService):
         logger.info("SageMaker - describing notebook instances...")
         try:
             for notebook_instance in self.sagemaker_notebook_instances:
-                regional_client = regional_clients[notebook_instance.region]
+                regional_client = self.regional_clients[notebook_instance.region]
                 try:
                     describe_notebook_instance = (
                         regional_client.describe_notebook_instance(
@@ -139,7 +142,7 @@ class SageMaker(AWSService):
         logger.info("SageMaker - describing models...")
         try:
             for model in self.sagemaker_models:
-                regional_client = regional_clients[model.region]
+                regional_client = self.regional_clients[model.region]
                 describe_model = regional_client.describe_model(ModelName=model.name)
                 if "EnableNetworkIsolation" in describe_model:
                     model.network_isolation = describe_model["EnableNetworkIsolation"]
@@ -157,7 +160,7 @@ class SageMaker(AWSService):
         logger.info("SageMaker - describing training jobs...")
         try:
             for training_job in self.sagemaker_training_jobs:
-                regional_client = regional_clients[training_job.region]
+                regional_client = self.regional_clients[training_job.region]
                 describe_training_job = regional_client.describe_training_job(
                     TrainingJobName=training_job.name
                 )
@@ -217,6 +220,66 @@ class SageMaker(AWSService):
             logger.error(
                 f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
             )
+        try:
+            for endpoint in self.endpoint_configs.values():
+                regional_client = self.regional_clients[endpoint.region]
+                response = regional_client.list_tags(ResourceArn=endpoint.arn)["Tags"]
+                endpoint.tags = response
+        except Exception as error:
+            logger.error(
+                f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+            )
+
+    def _list_endpoint_configs(self, regional_client):
+        logger.info("SageMaker - listing endpoint configs...")
+        try:
+            list_endpoint_config_paginator = regional_client.get_paginator(
+                "list_endpoint_configs"
+            )
+            for page in list_endpoint_config_paginator.paginate():
+                for endpoint_config in page["EndpointConfigs"]:
+                    if not self.audit_resources or (
+                        is_resource_filtered(
+                            endpoint_config["EndpointConfigArn"], self.audit_resources
+                        )
+                    ):
+                        self.endpoint_configs[endpoint_config["EndpointConfigArn"]] = (
+                            EndpointConfig(
+                                name=endpoint_config["EndpointConfigName"],
+                                region=regional_client.region,
+                                arn=endpoint_config["EndpointConfigArn"],
+                            )
+                        )
+        except Exception as error:
+            logger.error(
+                f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+            )
+
+    def _describe_endpoint_config(self, regional_clients):
+        logger.info("SageMaker - describing endpoint configs...")
+        try:
+            for endpoint_config in self.endpoint_configs.values():
+                regional_client = self.regional_clients[endpoint_config.region]
+                describe_endpoint_config = regional_client.describe_endpoint_config(
+                    EndpointConfigName=endpoint_config.name
+                )
+                production_variants = []
+                for production_variant in describe_endpoint_config[
+                    "ProductionVariants"
+                ]:
+                    production_variants.append(
+                        ProductionVariant(
+                            name=production_variant["VariantName"],
+                            initial_instance_count=production_variant[
+                                "InitialInstanceCount"
+                            ],
+                        )
+                    )
+                endpoint_config.production_variants = production_variants
+        except Exception as error:
+            logger.error(
+                f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+            )
 
 
 class NotebookInstance(BaseModel):
@@ -247,4 +310,17 @@ class TrainingJob(BaseModel):
     volume_kms_key_id: str = None
     network_isolation: bool = None
     vpc_config_subnets: list[str] = []
+    tags: Optional[list] = []
+
+
+class ProductionVariant(BaseModel):
+    name: str
+    initial_instance_count: int
+
+
+class EndpointConfig(BaseModel):
+    name: str
+    region: str
+    arn: str
+    production_variants: list[ProductionVariant] = []
     tags: Optional[list] = []
