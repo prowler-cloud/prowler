ae794c7c328fc4ba8335d554e647c2651d30a64f
Hugo Pereira Brito <101209179+HugoPBrito@users.noreply.github.com>
2024-09-18 19:26:35 +0200

    feat(cloudfront): Ensure Cloudfront distributions have origin failover configured (#4868)
    Co-authored-by: Sergio Garcia <38561120+sergargar@users.noreply.github.com>

--- tests/providers/aws/services/cloudfront/cloudfront_distributions_multiple_origin_failover_configured/cloudfront_distributions_multiple_origin_failover_configured_test.py
+++ tests/providers/aws/services/cloudfront/cloudfront_distributions_multiple_origin_failover_configured/cloudfront_distributions_multiple_origin_failover_configured_test.py
@@ -1,6 +1,10 @@
 from unittest import mock
 
-from prowler.providers.aws.services.cloudfront.cloudfront_service import Distribution
+from prowler.providers.aws.services.cloudfront.cloudfront_service import (
+    DefaultCacheConfigBehaviour,
+    Distribution,
+    ViewerProtocolPolicy,
+)
 from tests.providers.aws.utils import AWS_ACCOUNT_NUMBER
 
 DISTRIBUTION_ID = "E27LVI50CSW06W"
@@ -10,7 +14,7 @@ DISTRIBUTION_ARN = (
 REGION = "eu-west-1"
 
 
-class Test_cloudfront_distributions_using_waf:
+class Test_cloudfront_distributions_multiple_origin_failover_configured:
     def test_no_distributions(self):
         cloudfront_client = mock.MagicMock
         cloudfront_client.distributions = {}
@@ -19,25 +23,28 @@ class Test_cloudfront_distributions_using_waf:
             new=cloudfront_client,
         ):
             # Test Check
-            from prowler.providers.aws.services.cloudfront.cloudfront_distributions_using_waf.cloudfront_distributions_using_waf import (
-                cloudfront_distributions_using_waf,
+            from prowler.providers.aws.services.cloudfront.cloudfront_distributions_multiple_origin_failover_configured.cloudfront_distributions_multiple_origin_failover_configured import (
+                cloudfront_distributions_multiple_origin_failover_configured,
             )
 
-            check = cloudfront_distributions_using_waf()
+            check = cloudfront_distributions_multiple_origin_failover_configured()
             result = check.execute()
 
             assert len(result) == 0
 
-    def test_one_distribution_waf(self):
-        wef_acl_id = "TEST-WAF-ACL"
+    def test_no_origin_failover(self):
         cloudfront_client = mock.MagicMock
         cloudfront_client.distributions = {
             "DISTRIBUTION_ID": Distribution(
                 arn=DISTRIBUTION_ARN,
                 id=DISTRIBUTION_ID,
                 region=REGION,
-                web_acl_id=wef_acl_id,
                 origins=[],
+                default_cache_config=DefaultCacheConfigBehaviour(
+                    realtime_log_config_arn="",
+                    viewer_protocol_policy=ViewerProtocolPolicy.allow_all,
+                    field_level_encryption_id="",
+                ),
                 origin_failover=False,
             )
         }
@@ -47,46 +54,11 @@ class Test_cloudfront_distributions_using_waf:
             new=cloudfront_client,
         ):
             # Test Check
-            from prowler.providers.aws.services.cloudfront.cloudfront_distributions_using_waf.cloudfront_distributions_using_waf import (
-                cloudfront_distributions_using_waf,
+            from prowler.providers.aws.services.cloudfront.cloudfront_distributions_multiple_origin_failover_configured.cloudfront_distributions_multiple_origin_failover_configured import (
+                cloudfront_distributions_multiple_origin_failover_configured,
             )
 
-            check = cloudfront_distributions_using_waf()
-            result = check.execute()
-
-            assert len(result) == 1
-            assert result[0].region == REGION
-            assert result[0].resource_arn == DISTRIBUTION_ARN
-            assert result[0].resource_id == DISTRIBUTION_ID
-            assert result[0].status == "PASS"
-            assert (
-                result[0].status_extended
-                == f"CloudFront Distribution {DISTRIBUTION_ID} is using AWS WAF web ACL {wef_acl_id}."
-            )
-            assert result[0].resource_tags == []
-
-    def test_one_distribution_no_waf(self):
-        cloudfront_client = mock.MagicMock
-        cloudfront_client.distributions = {
-            "DISTRIBUTION_ID": Distribution(
-                arn=DISTRIBUTION_ARN,
-                id=DISTRIBUTION_ID,
-                region=REGION,
-                origins=[],
-                origin_failover=False,
-            )
-        }
-
-        with mock.patch(
-            "prowler.providers.aws.services.cloudfront.cloudfront_service.CloudFront",
-            new=cloudfront_client,
-        ):
-            # Test Check
-            from prowler.providers.aws.services.cloudfront.cloudfront_distributions_using_waf.cloudfront_distributions_using_waf import (
-                cloudfront_distributions_using_waf,
-            )
-
-            check = cloudfront_distributions_using_waf()
+            check = cloudfront_distributions_multiple_origin_failover_configured()
             result = check.execute()
 
             assert len(result) == 1
@@ -96,6 +68,46 @@ class Test_cloudfront_distributions_using_waf:
             assert result[0].status == "FAIL"
             assert (
                 result[0].status_extended
-                == f"CloudFront Distribution {DISTRIBUTION_ID} is not using AWS WAF web ACL."
+                == f"CloudFront Distribution {DISTRIBUTION_ID} does not have an origin group configured with at least 2 origins."
+            )
+            assert result[0].resource_tags == []
+
+    def test_origin_failover(self):
+        cloudfront_client = mock.MagicMock
+        cloudfront_client.distributions = {
+            "DISTRIBUTION_ID": Distribution(
+                arn=DISTRIBUTION_ARN,
+                id=DISTRIBUTION_ID,
+                region=REGION,
+                origins=[],
+                default_cache_config=DefaultCacheConfigBehaviour(
+                    realtime_log_config_arn="",
+                    viewer_protocol_policy=ViewerProtocolPolicy.allow_all,
+                    field_level_encryption_id="",
+                ),
+                origin_failover=True,
+            )
+        }
+
+        with mock.patch(
+            "prowler.providers.aws.services.cloudfront.cloudfront_service.CloudFront",
+            new=cloudfront_client,
+        ):
+            # Test Check
+            from prowler.providers.aws.services.cloudfront.cloudfront_distributions_multiple_origin_failover_configured.cloudfront_distributions_multiple_origin_failover_configured import (
+                cloudfront_distributions_multiple_origin_failover_configured,
+            )
+
+            check = cloudfront_distributions_multiple_origin_failover_configured()
+            result = check.execute()
+
+            assert len(result) == 1
+            assert result[0].region == REGION
+            assert result[0].resource_arn == DISTRIBUTION_ARN
+            assert result[0].resource_id == DISTRIBUTION_ID
+            assert result[0].status == "PASS"
+            assert (
+                result[0].status_extended
+                == f"CloudFront Distribution {DISTRIBUTION_ID} has an origin group with at least 2 origins configured."
             )
             assert result[0].resource_tags == []
