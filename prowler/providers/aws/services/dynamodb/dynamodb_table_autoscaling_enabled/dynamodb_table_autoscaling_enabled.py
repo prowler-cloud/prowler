c371e8b1e613c78f21d75b89dad1e82382abeab4
Daniel Barranquero <danielbo2001@gmail.com>
2024-09-20 17:17:37 +0200

    feat(dynamodb): Add check code

---
+++ prowler/providers/aws/services/dynamodb/dynamodb_table_autoscaling_enabled/dynamodb_table_autoscaling_enabled.py
@@ -0,0 +1,35 @@
+from prowler.lib.check.models import Check, Check_Report_AWS
+from prowler.providers.aws.services.dynamodb.dynamodb_client import dynamodb_client
+
+
+class dynamodb_table_autoscaling_enabled(Check):
+    def execute(self):
+        findings = []
+        for table in dynamodb_client.tables:
+            report = Check_Report_AWS(self.metadata())
+            report.region = table.region
+            report.resource_id = table.name
+            report.resource_arn = table.arn
+            report.resource_tags = table.tags
+            report.status = "FAIL"
+            report.status_extended = f"DynamoDB table {table.name} does not automatically scale capacity with demand."
+            if table.billing_mode == "PAY_PER_REQUEST":
+                report.status = "PASS"
+                report.status_extended = f"DynamoDB table {table.name} uses PAY_PER_REQUEST billing mode and automatically scales capacity with demand."
+            elif table.billing_mode == "PROVISIONED":
+                if table.read_autoscaling and table.write_autoscaling:
+                    report.status = "PASS"
+                    report.status_extended = f"DynamoDB table {table.name} is in PROVISIONED mode with auto scaling enabled for both read and write capacity units."
+                else:
+                    s = f"DynamoDB table {table.name} is in PROVISIONED mode without auto scaling enabled for "
+                    if not table.read_autoscaling:
+                        s.append("read")
+                        if not table.write_autoscaling:
+                            s.append(" and write.")
+                        else:
+                            s.append(".")
+                    else:
+                        s.append("write.")
+                    report.status_extended = s
+            findings.append(report)
+        return findings
