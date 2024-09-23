aa118c05c561c79afed2bed77ff2cabb7c58c9b7
Daniel Barranquero <74871504+danibarranqueroo@users.noreply.github.com>
2024-09-23 21:05:56 +0200

    feat(rds): add new check `rds_cluster_non_default_port` (#5113)

--- tests/providers/aws/services/rds/rds_cluster_non_default_port/rds_cluster_non_default_port_test.py
+++ tests/providers/aws/services/rds/rds_cluster_non_default_port/rds_cluster_non_default_port_test.py
@@ -10,9 +10,9 @@ from tests.providers.aws.utils import (
 )
 
 
-class Test_rds_instance_non_default_port:
+class Test_rds_cluster_non_default_port:
     @mock_aws
-    def test_rds_no_instances(self):
+    def test_rds_no_clusters(self):
         from prowler.providers.aws.services.rds.rds_service import RDS
 
         aws_provider = set_mocked_aws_provider([AWS_REGION_US_EAST_1])
@@ -22,32 +22,28 @@ class Test_rds_instance_non_default_port:
             return_value=aws_provider,
         ):
             with mock.patch(
-                "prowler.providers.aws.services.rds.rds_instance_non_default_port.rds_instance_non_default_port.rds_client",
+                "prowler.providers.aws.services.rds.rds_cluster_non_default_port.rds_cluster_non_default_port.rds_client",
                 new=RDS(aws_provider),
             ):
-                from prowler.providers.aws.services.rds.rds_instance_non_default_port.rds_instance_non_default_port import (
-                    rds_instance_non_default_port,
+                from prowler.providers.aws.services.rds.rds_cluster_non_default_port.rds_cluster_non_default_port import (
+                    rds_cluster_non_default_port,
                 )
 
-                check = rds_instance_non_default_port()
+                check = rds_cluster_non_default_port()
                 result = check.execute()
 
                 assert len(result) == 0
 
     @mock_aws
-    def test_rds_instance_using_default_port(self):
+    def test_rds_cluster_using_default_port(self):
         conn = client("rds", region_name=AWS_REGION_US_EAST_1)
-        conn.create_db_instance(
-            DBInstanceIdentifier="db-master-1",
-            AllocatedStorage=10,
-            Engine="postgres",
-            DBName="staging-postgres",
-            DBInstanceClass="db.m1.small",
+        conn.create_db_cluster(
+            DBClusterIdentifier="db-cluster-1",
+            Engine="aurora-postgresql",
             StorageEncrypted=True,
             DeletionProtection=True,
-            PubliclyAccessible=True,
-            AutoMinorVersionUpgrade=True,
-            BackupRetentionPeriod=10,
+            MasterUsername="cluster",
+            MasterUserPassword="password",
             Port=5432,
             Tags=[{"Key": "test", "Value": "test"}],
         )
@@ -61,44 +57,40 @@ class Test_rds_instance_non_default_port:
             return_value=aws_provider,
         ):
             with mock.patch(
-                "prowler.providers.aws.services.rds.rds_instance_non_default_port.rds_instance_non_default_port.rds_client",
+                "prowler.providers.aws.services.rds.rds_cluster_non_default_port.rds_cluster_non_default_port.rds_client",
                 new=RDS(aws_provider),
             ):
-                from prowler.providers.aws.services.rds.rds_instance_non_default_port.rds_instance_non_default_port import (
-                    rds_instance_non_default_port,
+                from prowler.providers.aws.services.rds.rds_cluster_non_default_port.rds_cluster_non_default_port import (
+                    rds_cluster_non_default_port,
                 )
 
-                check = rds_instance_non_default_port()
+                check = rds_cluster_non_default_port()
                 result = check.execute()
 
                 assert len(result) == 1
                 assert result[0].status == "FAIL"
                 assert (
                     result[0].status_extended
-                    == "RDS Instance db-master-1 is using the default port 5432 for postgres."
+                    == "RDS Cluster db-cluster-1 is using the default port 5432 for aurora-postgresql."
                 )
-                assert result[0].resource_id == "db-master-1"
+                assert result[0].resource_id == "db-cluster-1"
                 assert result[0].region == AWS_REGION_US_EAST_1
                 assert (
                     result[0].resource_arn
-                    == f"arn:aws:rds:{AWS_REGION_US_EAST_1}:{AWS_ACCOUNT_NUMBER}:db:db-master-1"
+                    == f"arn:aws:rds:{AWS_REGION_US_EAST_1}:{AWS_ACCOUNT_NUMBER}:cluster:db-cluster-1"
                 )
                 assert result[0].resource_tags == [{"Key": "test", "Value": "test"}]
 
     @mock_aws
-    def test_rds_instance_using_non_default_port(self):
+    def test_rds_cluster_using_non_default_port(self):
         conn = client("rds", region_name=AWS_REGION_US_EAST_1)
-        conn.create_db_instance(
-            DBInstanceIdentifier="db-master-2",
-            AllocatedStorage=10,
-            Engine="postgres",
-            DBName="production-postgres",
-            DBInstanceClass="db.m1.small",
+        conn.create_db_cluster(
+            DBClusterIdentifier="db-cluster-1",
+            Engine="aurora-postgresql",
             StorageEncrypted=True,
             DeletionProtection=True,
-            PubliclyAccessible=True,
-            AutoMinorVersionUpgrade=True,
-            BackupRetentionPeriod=10,
+            MasterUsername="cluster",
+            MasterUserPassword="password",
             Port=5433,
             Tags=[{"Key": "env", "Value": "production"}],
         )
@@ -112,46 +104,42 @@ class Test_rds_instance_non_default_port:
             return_value=aws_provider,
         ):
             with mock.patch(
-                "prowler.providers.aws.services.rds.rds_instance_non_default_port.rds_instance_non_default_port.rds_client",
+                "prowler.providers.aws.services.rds.rds_cluster_non_default_port.rds_cluster_non_default_port.rds_client",
                 new=RDS(aws_provider),
             ):
-                from prowler.providers.aws.services.rds.rds_instance_non_default_port.rds_instance_non_default_port import (
-                    rds_instance_non_default_port,
+                from prowler.providers.aws.services.rds.rds_cluster_non_default_port.rds_cluster_non_default_port import (
+                    rds_cluster_non_default_port,
                 )
 
-                check = rds_instance_non_default_port()
+                check = rds_cluster_non_default_port()
                 result = check.execute()
 
                 assert len(result) == 1
                 assert result[0].status == "PASS"
                 assert (
                     result[0].status_extended
-                    == "RDS Instance db-master-2 is not using the default port 5433 for postgres."
+                    == "RDS Cluster db-cluster-1 is not using the default port 5433 for aurora-postgresql."
                 )
-                assert result[0].resource_id == "db-master-2"
+                assert result[0].resource_id == "db-cluster-1"
                 assert result[0].region == AWS_REGION_US_EAST_1
                 assert (
                     result[0].resource_arn
-                    == f"arn:aws:rds:{AWS_REGION_US_EAST_1}:{AWS_ACCOUNT_NUMBER}:db:db-master-2"
+                    == f"arn:aws:rds:{AWS_REGION_US_EAST_1}:{AWS_ACCOUNT_NUMBER}:cluster:db-cluster-1"
                 )
                 assert result[0].resource_tags == [
                     {"Key": "env", "Value": "production"}
                 ]
 
     @mock_aws
-    def test_rds_instance_mariadb_default_port(self):
+    def test_rds_cluster_mysql_default_port(self):
         conn = client("rds", region_name=AWS_REGION_US_EAST_1)
-        conn.create_db_instance(
-            DBInstanceIdentifier="db-master-3",
-            AllocatedStorage=10,
-            Engine="mariadb",
-            DBName="staging-mariadb",
-            DBInstanceClass="db.m1.small",
+        conn.create_db_cluster(
+            DBClusterIdentifier="db-cluster-1",
+            Engine="mysql",
             StorageEncrypted=True,
             DeletionProtection=True,
-            PubliclyAccessible=True,
-            AutoMinorVersionUpgrade=True,
-            BackupRetentionPeriod=10,
+            MasterUsername="cluster",
+            MasterUserPassword="password",
             Port=3306,
             Tags=[{"Key": "env", "Value": "staging"}],
         )
@@ -165,44 +153,40 @@ class Test_rds_instance_non_default_port:
             return_value=aws_provider,
         ):
             with mock.patch(
-                "prowler.providers.aws.services.rds.rds_instance_non_default_port.rds_instance_non_default_port.rds_client",
+                "prowler.providers.aws.services.rds.rds_cluster_non_default_port.rds_cluster_non_default_port.rds_client",
                 new=RDS(aws_provider),
             ):
-                from prowler.providers.aws.services.rds.rds_instance_non_default_port.rds_instance_non_default_port import (
-                    rds_instance_non_default_port,
+                from prowler.providers.aws.services.rds.rds_cluster_non_default_port.rds_cluster_non_default_port import (
+                    rds_cluster_non_default_port,
                 )
 
-                check = rds_instance_non_default_port()
+                check = rds_cluster_non_default_port()
                 result = check.execute()
 
                 assert len(result) == 1
                 assert result[0].status == "FAIL"
                 assert (
                     result[0].status_extended
-                    == "RDS Instance db-master-3 is using the default port 3306 for mariadb."
+                    == "RDS Cluster db-cluster-1 is using the default port 3306 for mysql."
                 )
-                assert result[0].resource_id == "db-master-3"
+                assert result[0].resource_id == "db-cluster-1"
                 assert result[0].region == AWS_REGION_US_EAST_1
                 assert (
                     result[0].resource_arn
-                    == f"arn:aws:rds:{AWS_REGION_US_EAST_1}:{AWS_ACCOUNT_NUMBER}:db:db-master-3"
+                    == f"arn:aws:rds:{AWS_REGION_US_EAST_1}:{AWS_ACCOUNT_NUMBER}:cluster:db-cluster-1"
                 )
                 assert result[0].resource_tags == [{"Key": "env", "Value": "staging"}]
 
     @mock_aws
-    def test_rds_instance_mariadb_non_default_port(self):
+    def test_rds_cluster_mysql_non_default_port(self):
         conn = client("rds", region_name=AWS_REGION_US_EAST_1)
-        conn.create_db_instance(
-            DBInstanceIdentifier="db-master-4",
-            AllocatedStorage=10,
-            Engine="mariadb",
-            DBName="production-mariadb",
-            DBInstanceClass="db.m1.small",
+        conn.create_db_cluster(
+            DBClusterIdentifier="db-cluster-1",
+            Engine="mysql",
             StorageEncrypted=True,
             DeletionProtection=True,
-            PubliclyAccessible=True,
-            AutoMinorVersionUpgrade=True,
-            BackupRetentionPeriod=10,
+            MasterUsername="cluster",
+            MasterUserPassword="password",
             Port=3307,
             Tags=[{"Key": "env", "Value": "production"}],
         )
@@ -216,27 +200,27 @@ class Test_rds_instance_non_default_port:
             return_value=aws_provider,
         ):
             with mock.patch(
-                "prowler.providers.aws.services.rds.rds_instance_non_default_port.rds_instance_non_default_port.rds_client",
+                "prowler.providers.aws.services.rds.rds_cluster_non_default_port.rds_cluster_non_default_port.rds_client",
                 new=RDS(aws_provider),
             ):
-                from prowler.providers.aws.services.rds.rds_instance_non_default_port.rds_instance_non_default_port import (
-                    rds_instance_non_default_port,
+                from prowler.providers.aws.services.rds.rds_cluster_non_default_port.rds_cluster_non_default_port import (
+                    rds_cluster_non_default_port,
                 )
 
-                check = rds_instance_non_default_port()
+                check = rds_cluster_non_default_port()
                 result = check.execute()
 
                 assert len(result) == 1
                 assert result[0].status == "PASS"
                 assert (
                     result[0].status_extended
-                    == "RDS Instance db-master-4 is not using the default port 3307 for mariadb."
+                    == "RDS Cluster db-cluster-1 is not using the default port 3307 for mysql."
                 )
-                assert result[0].resource_id == "db-master-4"
+                assert result[0].resource_id == "db-cluster-1"
                 assert result[0].region == AWS_REGION_US_EAST_1
                 assert (
                     result[0].resource_arn
-                    == f"arn:aws:rds:{AWS_REGION_US_EAST_1}:{AWS_ACCOUNT_NUMBER}:db:db-master-4"
+                    == f"arn:aws:rds:{AWS_REGION_US_EAST_1}:{AWS_ACCOUNT_NUMBER}:cluster:db-cluster-1"
                 )
                 assert result[0].resource_tags == [
                     {"Key": "env", "Value": "production"}
