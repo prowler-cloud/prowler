a928e1d2cad77a362599a5c1c52bf9f417c007c3
Sergio <sergio@prowler.com>
2024-09-24 11:46:58 -0400

    fix: solve conflicts

--- prowler/providers/aws/services/elasticache/elasticache_service.py
+++ prowler/providers/aws/services/elasticache/elasticache_service.py
@@ -68,63 +68,7 @@ class ElastiCache(AWSService):
                 f"{error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
             )
 
-<<<<<<< HEAD
     def __list_tags_for_resource__(self):
-=======
-    def _describe_replication_groups(self, regional_client):
-        # Redis Clusters
-        logger.info("Elasticache - Describing Replication Groups...")
-        try:
-            for repl_group in regional_client.describe_replication_groups()[
-                "ReplicationGroups"
-            ]:
-                try:
-                    replication_arn = repl_group["ARN"]
-                    if not self.audit_resources or (
-                        is_resource_filtered(replication_arn, self.audit_resources)
-                    ):
-                        # Get first cluster version as they all have the same unless an upgrade is being made
-                        member_clusters = repl_group.get("MemberClusters", [])
-                        engine_version = "0.0"
-                        if member_clusters:
-                            cluster_arn = f"arn:{self.audited_partition}:elasticache:{regional_client.region}:{self.audited_account}:cluster:{member_clusters[0]}"
-                            engine_version = self.clusters[cluster_arn].engine_version
-
-                        self.replication_groups[replication_arn] = ReplicationGroup(
-                            id=repl_group["ReplicationGroupId"],
-                            arn=replication_arn,
-                            region=regional_client.region,
-                            status=repl_group["Status"],
-                            snapshot_retention=repl_group.get(
-                                "SnapshotRetentionLimit", 0
-                            ),
-                            encrypted=repl_group.get("AtRestEncryptionEnabled", False),
-                            transit_encryption=repl_group.get(
-                                "TransitEncryptionEnabled", False
-                            ),
-                            multi_az=repl_group.get("MultiAZ", "disabled"),
-                            auto_minor_version_upgrade=repl_group.get(
-                                "AutoMinorVersionUpgrade", False
-                            ),
-                            automatic_failover=repl_group.get(
-                                "AutomaticFailover", "disabled"
-                            ),
-                            auth_token_enabled=repl_group.get(
-                                "AuthTokenEnabled", False
-                            ),
-                            engine_version=engine_version,
-                        )
-                except Exception as error:
-                    logger.error(
-                        f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
-                    )
-        except Exception as error:
-            logger.error(
-                f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
-            )
-
-    def _list_tags_for_resource(self):
->>>>>>> 348cea67c (fix(aws): always use audited partition (#5174))
         logger.info("Elasticache - Listing Tags...")
         try:
             for cluster in self.clusters.values():
