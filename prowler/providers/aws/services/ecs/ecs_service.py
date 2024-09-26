0963d44d58ba9100f9cc2e3383b63331ef9aa12e
Sergio <sergio@prowler.com>
2024-09-26 16:46:36 -0400

    chore: revision

--- prowler/providers/aws/services/ecs/ecs_service.py
+++ prowler/providers/aws/services/ecs/ecs_service.py
@@ -77,9 +77,11 @@ class ECS(AWSService):
                         environment=environment,
                     )
                 )
-                task_definition.pid_mode = response["taskDefinition"].get("pidMode", "")
+            task_definition.pid_mode = response["taskDefinition"].get("pidMode", "")
             task_definition.tags = response.get("tags")
-            task_definition.network_mode = response["taskDefinition"].get("networkMode")
+            task_definition.network_mode = response["taskDefinition"].get(
+                "networkMode", "bridge"
+            )
         except Exception as error:
             logger.error(
                 f"{error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
