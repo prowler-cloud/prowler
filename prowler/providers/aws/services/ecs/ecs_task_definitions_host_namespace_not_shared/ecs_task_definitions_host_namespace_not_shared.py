77f075f8d2da3c7e2ca06ef3d7360557a8f0056e
Sergio <sergio@prowler.com>
2024-09-26 16:25:33 -0400

    chore: revision

--- prowler/providers/aws/services/ecs/ecs_task_definitions_host_namespace_not_shared/ecs_task_definitions_host_namespace_not_shared.py
+++ prowler/providers/aws/services/ecs/ecs_task_definitions_host_namespace_not_shared/ecs_task_definitions_host_namespace_not_shared.py
@@ -12,9 +12,9 @@ class ecs_task_definitions_host_namespace_not_shared(Check):
             report.resource_arn = task_definition.arn
             report.resource_tags = task_definition.tags
             report.status = "PASS"
-            report.status_extended = f"ECS task definition '{task_definition.name}' with revision {task_definition.revision} does not share a host's process namespace with its containers."
+            report.status_extended = f"ECS task definition {task_definition.name} with revision {task_definition.revision} does not share a host's process namespace with its containers."
             if task_definition.pid_mode == "host":
                 report.status = "FAIL"
-                report.status_extended = f"ECS task definition '{task_definition.name}' with revision {task_definition.revision} is configured to share a host's process namespace with its containers."
+                report.status_extended = f"ECS task definition {task_definition.name} with revision {task_definition.revision} is configured to share a host's process namespace with its containers."
             findings.append(report)
         return findings
