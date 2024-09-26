c544a069a24a9a6c4c371bbe5a550f82ff7cd064
Pablo Lara <larabjj@gmail.com>
2024-09-26 16:01:42 +0200

    feat: create and integrate custom button component

---
+++ components/ui/custom/CustomButton.tsx
@@ -0,0 +1,84 @@
+import { Button } from "@nextui-org/react";
+import clsx from "clsx";
+
+export const buttonClasses = {
+  base: "w-full md:w-fit px-4 inline-flex items-center justify-center relative z-0 text-center whitespace-nowrap",
+  primary: "bg-default-100 hover:bg-default-200 text-default-800",
+  secondary: "bg-prowler-grey-light dark:bg-prowler-grey-medium text-white",
+  action: "text-white bg-prowler-blue-smoky dark:bg-prowler-grey-medium",
+  dashed:
+    "border border-default border-dashed bg-transparent hover:bg-accent justify-center whitespace-nowrap font-medium shadow-sm hover:bg-accent hover:text-accent-foreground",
+  transparent: "border-0 border-transparent bg-transparent",
+  disabled: "pointer-events-none opacity-40",
+  hover: "hover:shadow-md",
+};
+
+interface ButtonProps {
+  variant?:
+    | "solid"
+    | "faded"
+    | "bordered"
+    | "light"
+    | "flat"
+    | "ghost"
+    | "dashed"
+    | "shadow";
+  color?:
+    | "primary"
+    | "secondary"
+    | "action"
+    | "success"
+    | "warning"
+    | "danger"
+    | "transparent";
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  children: React.ReactNode;
+  startContent?: React.ReactNode;
+  endContent?: React.ReactNode;
+  size?: "sm" | "md" | "lg";
+  radius?: "none" | "sm" | "md" | "lg" | "full";
+  dashed?: boolean;
+  disabled?: boolean;
+  isLoading?: boolean;
+  isIconOnly?: boolean;
+}
+
+export const CustomButton = ({
+  variant = "solid",
+  color = "primary",
+  onClick,
+  children,
+  startContent,
+  endContent,
+  size = "md",
+  radius = "sm",
+  disabled = false,
+  isLoading = false,
+  isIconOnly,
+  ...props
+}: ButtonProps) => (
+  <Button
+    type="button"
+    onClick={onClick}
+    variant={variant}
+    color={color}
+    className={clsx(buttonClasses.base, {
+      [buttonClasses.primary]: color === "primary",
+      [buttonClasses.secondary]: color === "secondary",
+      [buttonClasses.action]: color === "action",
+      [buttonClasses.dashed]: variant === "dashed",
+      [buttonClasses.transparent]: color === "transparent",
+      [buttonClasses.disabled]: disabled,
+      [buttonClasses.hover]: color !== "transparent" && !disabled,
+    })}
+    startContent={startContent}
+    endContent={endContent}
+    size={size}
+    radius={radius}
+    isLoading={isLoading}
+    isIconOnly={isIconOnly}
+    {...props}
+  >
+    {children}
+  </Button>
+);
