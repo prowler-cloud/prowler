ba1f8c9a3a7367a4d5311e7c91ffe9ca847e176e
Pablo Lara <larabjj@gmail.com>
2024-09-28 06:13:28 +0200

    chore: handle lib folder dependencies with nextui

---
+++ lib/custom.ts
@@ -0,0 +1,21 @@
+import { MetaDataProps } from "@/types";
+
+export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
+
+export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
+
+export const getPaginationInfo = (metadata: MetaDataProps) => {
+  const currentPage = metadata?.pagination.page ?? "1";
+  const totalPages = metadata?.pagination.pages;
+  const totalEntries = metadata?.pagination.count;
+
+  return { currentPage, totalPages, totalEntries };
+};
+
+export function encryptKey(passkey: string) {
+  return btoa(passkey);
+}
+
+export function decryptKey(passkey: string) {
+  return atob(passkey);
+}
