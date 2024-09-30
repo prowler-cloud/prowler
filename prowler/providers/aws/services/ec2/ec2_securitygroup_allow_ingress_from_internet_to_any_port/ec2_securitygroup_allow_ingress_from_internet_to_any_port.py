060e1382b8508b191d0602635d93d9272d9e8e45
Sergio <sergio@prowler.com>
2024-09-30 12:39:28 -0400

    chore: improve logic

--- prowler/providers/aws/services/ec2/ec2_securitygroup_allow_ingress_from_internet_to_any_port/ec2_securitygroup_allow_ingress_from_internet_to_any_port.py
+++ prowler/providers/aws/services/ec2/ec2_securitygroup_allow_ingress_from_internet_to_any_port/ec2_securitygroup_allow_ingress_from_internet_to_any_port.py
@@ -12,38 +12,38 @@ class ec2_securitygroup_allow_ingress_from_internet_to_any_port(Check):
     def execute(self):
         findings = []
         for security_group_arn, security_group in ec2_client.security_groups.items():
-            # Check if ignoring flag is set and if the VPC and the SG is in use
-            if ec2_client.provider.scan_unused_services or (
-                security_group.vpc_id in vpc_client.vpcs
-                and vpc_client.vpcs[security_group.vpc_id].in_use
-                and len(security_group.network_interfaces) > 0
+            # Only execute the check if the check ec2_securitygroup_allow_ingress_from_internet_to_all_ports has not failed
+            if not ec2_client.is_failed_check(
+                ec2_securitygroup_allow_ingress_from_internet_to_all_ports.__name__,
+                security_group_arn,
             ):
-                report = Check_Report_AWS(self.metadata())
-                report.region = security_group.region
-                report.status = "PASS"
-                report.status_extended = f"Security group {security_group.name} ({security_group.id}) does not have any port open to the Internet."
-                report.resource_details = security_group.name
-                report.resource_id = security_group.id
-                report.resource_arn = security_group_arn
-                report.resource_tags = security_group.tags
-                for ingress_rule in security_group.ingress_rules:
-                    if check_security_group(
-                        ingress_rule, "-1", ports=None, any_address=True
-                    ):
-                        self.check_enis(
-                            report=report,
-                            security_group_name=security_group.name,
-                            security_group_id=security_group.id,
-                            enis=security_group.network_interfaces,
-                        )
-
-                    if report.status == "FAIL":
-                        break
-                # Only add the report if the check ec2_securitygroup_allow_ingress_from_internet_to_all_ports has not failed
-                if not ec2_client.is_failed_check(
-                    ec2_securitygroup_allow_ingress_from_internet_to_all_ports.__name__,
-                    security_group_arn,
+                # Check if ignoring flag is set and if the VPC and the SG is in use
+                if ec2_client.provider.scan_unused_services or (
+                    security_group.vpc_id in vpc_client.vpcs
+                    and vpc_client.vpcs[security_group.vpc_id].in_use
+                    and len(security_group.network_interfaces) > 0
                 ):
+                    report = Check_Report_AWS(self.metadata())
+                    report.region = security_group.region
+                    report.status = "PASS"
+                    report.status_extended = f"Security group {security_group.name} ({security_group.id}) does not have any port open to the Internet."
+                    report.resource_details = security_group.name
+                    report.resource_id = security_group.id
+                    report.resource_arn = security_group_arn
+                    report.resource_tags = security_group.tags
+                    for ingress_rule in security_group.ingress_rules:
+                        if check_security_group(
+                            ingress_rule, "-1", ports=None, any_address=True
+                        ):
+                            self.check_enis(
+                                report=report,
+                                security_group_name=security_group.name,
+                                security_group_id=security_group.id,
+                                enis=security_group.network_interfaces,
+                            )
+
+                        if report.status == "FAIL":
+                            break
                     findings.append(report)
 
         return findings
