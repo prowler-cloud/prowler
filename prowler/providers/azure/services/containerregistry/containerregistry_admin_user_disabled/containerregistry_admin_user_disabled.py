d009b06a6ba86ab4119658ae4d08b3bbedd5e195
Rubén De la Torre Vico <rubendltv22@gmail.com>
2024-09-30 13:54:44 +0200

    chore(containerregistry): change variables names to adapt to out naming convention

--- prowler/providers/azure/services/containerregistry/containerregistry_admin_user_disabled/containerregistry_admin_user_disabled.py
+++ prowler/providers/azure/services/containerregistry/containerregistry_admin_user_disabled/containerregistry_admin_user_disabled.py
@@ -9,18 +9,18 @@ class containerregistry_admin_user_disabled(Check):
         findings = []
 
         for subscription, registries in containerregistry_client.registries.items():
-            for registry_id, ContainerRegistryInfo in registries.items():
+            for registry_id, conatiner_registry_info in registries.items():
                 report = Check_Report_Azure(self.metadata())
                 report.subscription = subscription
-                report.resource_name = ContainerRegistryInfo.name
+                report.resource_name = conatiner_registry_info.name
                 report.resource_id = registry_id
-                report.location = ContainerRegistryInfo.location
+                report.location = conatiner_registry_info.location
                 report.status = "FAIL"
-                report.status_extended = f"Container Registry {ContainerRegistryInfo.name} from subscription {subscription} has its admin user enabled."
+                report.status_extended = f"Container Registry {conatiner_registry_info.name} from subscription {subscription} has its admin user enabled."
 
-                if not ContainerRegistryInfo.admin_user_enabled:
+                if not conatiner_registry_info.admin_user_enabled:
                     report.status = "PASS"
-                    report.status_extended = f"Container Registry {ContainerRegistryInfo.name} from subscription {subscription} has its admin user disabled."
+                    report.status_extended = f"Container Registry {conatiner_registry_info.name} from subscription {subscription} has its admin user disabled."
 
                 findings.append(report)
 
