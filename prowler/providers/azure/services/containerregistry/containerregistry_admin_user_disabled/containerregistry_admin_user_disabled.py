17e392aa3c2e31d15afc4169c9913adf573d0761
Sergio <sergio@prowler.com>
2024-09-30 11:03:10 -0400

    fix: typo

--- prowler/providers/azure/services/containerregistry/containerregistry_admin_user_disabled/containerregistry_admin_user_disabled.py
+++ prowler/providers/azure/services/containerregistry/containerregistry_admin_user_disabled/containerregistry_admin_user_disabled.py
@@ -9,18 +9,18 @@ class containerregistry_admin_user_disabled(Check):
         findings = []
 
         for subscription, registries in containerregistry_client.registries.items():
-            for registry_id, conatiner_registry_info in registries.items():
+            for registry_id, container_registry_info in registries.items():
                 report = Check_Report_Azure(self.metadata())
                 report.subscription = subscription
-                report.resource_name = conatiner_registry_info.name
+                report.resource_name = container_registry_info.name
                 report.resource_id = registry_id
-                report.location = conatiner_registry_info.location
+                report.location = container_registry_info.location
                 report.status = "FAIL"
-                report.status_extended = f"Container Registry {conatiner_registry_info.name} from subscription {subscription} has its admin user enabled."
+                report.status_extended = f"Container Registry {container_registry_info.name} from subscription {subscription} has its admin user enabled."
 
-                if not conatiner_registry_info.admin_user_enabled:
+                if not container_registry_info.admin_user_enabled:
                     report.status = "PASS"
-                    report.status_extended = f"Container Registry {conatiner_registry_info.name} from subscription {subscription} has its admin user disabled."
+                    report.status_extended = f"Container Registry {container_registry_info.name} from subscription {subscription} has its admin user disabled."
 
                 findings.append(report)
 
