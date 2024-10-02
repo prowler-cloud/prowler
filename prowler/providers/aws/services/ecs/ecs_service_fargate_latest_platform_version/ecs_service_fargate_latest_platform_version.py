fd3d7ca40597d9160e0638d95fedcbc37ece3e98
Sergio <sergio@prowler.com>
2024-10-02 16:01:00 -0400

    chore: revision

--- prowler/providers/aws/services/ecs/ecs_service_fargate_latest_platform_version/ecs_service_fargate_latest_platform_version.py
+++ prowler/providers/aws/services/ecs/ecs_service_fargate_latest_platform_version/ecs_service_fargate_latest_platform_version.py
@@ -18,26 +18,21 @@ class ecs_service_fargate_latest_platform_version(Check):
                 fargate_latest_windows_version = ecs_client.audit_config.get(
                     "fargate_windows_latest_version", "1.0.0"
                 )
-                if service.platform_family == "Linux":
-                    if (
-                        service.platform_version == fargate_latest_linux_version
-                        or service.platform_version == "LATEST"
-                    ):
-                        report.status = "PASS"
-                        report.status_extended = f"ECS Service {service.name} is using latest FARGATE Linux version {fargate_latest_linux_version}."
-                    else:
-                        report.status = "FAIL"
-                        report.status_extended = f"ECS Service {service.name} is not using latest FARGATE Linux version {fargate_latest_linux_version}, currently using {service.platform_version}."
-                elif service.platform_family == "Windows":
-                    if (
-                        service.platform_version == fargate_latest_windows_version
-                        or service.platform_version == "LATEST"
-                    ):
-                        report.status = "PASS"
-                        report.status_extended = f"ECS Service {service.name} is using latest FARGATE Windows version {fargate_latest_windows_version}."
-                    else:
-                        report.status = "FAIL"
-                        report.status_extended = f"ECS Service {service.name} is not using latest FARGATE Windows version {fargate_latest_windows_version}, currently using {service.platform_version}."
+                report.status = "PASS"
+                report.status_extended = f"ECS Service {service.name} is using latest FARGATE {service.platform_family} version {fargate_latest_linux_version if service.platform_family == "Linux" else fargate_latest_windows_version}."
+                if (
+                    service.platform_version != "LATEST"
+                    and (
+                        service.platform_family == "Linux"
+                        and service.platform_version != fargate_latest_linux_version
+                    )
+                    or (
+                        service.platform_family == "Windows"
+                        and service.platform_version != fargate_latest_windows_version
+                    )
+                ):
+                    report.status = "FAIL"
+                    report.status_extended = f"ECS Service {service.name} is not using latest FARGATE {service.platform_family} version {fargate_latest_linux_version if service.platform_family == "Linux" else fargate_latest_windows_version}, currently using {service.platform_version}."
 
                 findings.append(report)
         return findings
