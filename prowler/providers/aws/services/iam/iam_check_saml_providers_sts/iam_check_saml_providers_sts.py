992671ac364158471ea93fab29d17a37538ce0f0
Rubén De la Torre Vico <rubendltv22@gmail.com>
2024-10-03 09:59:54 +0200

    chore(iam): add tags to report

--- prowler/providers/aws/services/iam/iam_check_saml_providers_sts/iam_check_saml_providers_sts.py
+++ prowler/providers/aws/services/iam/iam_check_saml_providers_sts/iam_check_saml_providers_sts.py
@@ -5,17 +5,15 @@ from prowler.providers.aws.services.iam.iam_client import iam_client
 class iam_check_saml_providers_sts(Check):
     def execute(self) -> Check_Report_AWS:
         findings = []
-        if iam_client.saml_providers:
-            for provider in iam_client.saml_providers:
-                report = Check_Report_AWS(self.metadata())
-                provider_name = provider["Arn"].split("/")[1]
-                report.resource_id = provider_name
-                report.resource_arn = provider["Arn"]
-                report.region = iam_client.region
-                report.status = "PASS"
-                report.status_extended = (
-                    f"SAML Provider {provider_name} has been found."
-                )
-                findings.append(report)
+
+        for provider_arn, provider in iam_client.saml_providers.items():
+            report = Check_Report_AWS(self.metadata())
+            report.resource_id = provider.name
+            report.resource_arn = provider_arn
+            report.resource_tags = provider.tags
+            report.region = iam_client.region
+            report.status = "PASS"
+            report.status_extended = f"SAML Provider {provider.name} has been found."
+            findings.append(report)
 
         return findings
