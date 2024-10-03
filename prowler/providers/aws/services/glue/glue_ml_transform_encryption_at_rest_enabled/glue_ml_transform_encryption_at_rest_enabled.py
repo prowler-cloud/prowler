b2fa0e1b0988f9a6cfe489d93407863e40b5c459
Rubén De la Torre Vico <rubendltv22@gmail.com>
2024-10-03 12:54:01 +0200

    chore(glue): make a PASS by default and add tags

--- prowler/providers/aws/services/glue/glue_ml_transform_encryption_at_rest_enabled/glue_ml_transform_encryption_at_rest_enabled.py
+++ prowler/providers/aws/services/glue/glue_ml_transform_encryption_at_rest_enabled/glue_ml_transform_encryption_at_rest_enabled.py
@@ -1,38 +1,26 @@
-from typing import List
 from prowler.lib.check.models import Check, Check_Report_AWS
 from prowler.providers.aws.services.glue.glue_client import glue_client
+
 
 class glue_ml_transform_encryption_at_rest_enabled(Check):
     def execute(self):
         findings = []
 
-        for crafted_arn, transform in glue_client.ml_transforms.items():
+        for ml_transform_arn, ml_transform in glue_client.ml_transforms.items():
             report = Check_Report_AWS(self.metadata())
-            report.resource_id = transform.id
-            report.resource_arn = crafted_arn
-            report.resource_tags = transform.tags
-            report.region = transform.region
-            if transform.transform_encryption == "DISABLED":
+            report.resource_id = ml_transform.id
+            report.resource_arn = ml_transform_arn
+            report.region = ml_transform.region
+            report.resource_tags = ml_transform.tags
+            report.status = "PASS"
+            report.status_extended = f"Glue ML Transform {ml_transform.name} is encrypted at rest (SSE-KMS mode)."
+
+            if ml_transform.user_data_encryption == "DISABLED":
                 report.status = "FAIL"
-                report.status_extended = f"Glue ML Transform {transform.name} has encryption DISABLED at rest."
-            else:
-                report.status = "PASS"
-                report.status_extended = f"Glue ML Transform {transform.name} has encryption enabled (SSE-KMS mode) at rest."
+                report.status_extended = (
+                    f"Glue ML Transform {ml_transform.name} is not encrypted at rest."
+                )
 
             findings.append(report)
 
         return findings
-        
-
-            
-                    
-
-
-
-         
-               
-            
-            
-                
-
-                
