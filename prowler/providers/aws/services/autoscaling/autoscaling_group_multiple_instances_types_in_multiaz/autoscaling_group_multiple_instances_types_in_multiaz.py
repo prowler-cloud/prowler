f7934827538ea94a107042e295a5868f85e32d32
Daniel Barranquero <danielbo2001@gmail.com>
2024-10-08 11:27:36 +0200

    feat(autoscaling): add check code and metadata

---
+++ prowler/providers/aws/services/autoscaling/autoscaling_group_multiple_instances_types_in_multiaz/autoscaling_group_multiple_instances_types_in_multiaz.py
@@ -0,0 +1,29 @@
+from prowler.lib.check.models import Check, Check_Report_AWS
+from prowler.providers.aws.services.autoscaling.autoscaling_client import (
+    autoscaling_client,
+)
+
+
+class autoscaling_group_multiple_instances_types_in_multiaz(Check):
+    def execute(self):
+        findings = []
+        for group in autoscaling_client.groups:
+            report = Check_Report_AWS(self.metadata())
+            report.region = group.region
+            report.resource_id = group.name
+            report.resource_arn = group.arn
+            report.resource_tags = group.tags
+            report.status = "FAIL"
+            report.status_extended = (
+                f"Autoscaling group {group.name} has one or less instance types."
+            )
+            if len(group.instance_types) > 1:
+                if len(group.availability_zones) > 1:
+                    report.status = "PASS"
+                    report.status_extended = f"Autoscaling group {group.name} has multiple instance types in multiple availability zones."
+                else:
+                    report.status_extended = f"Autoscaling group {group.name} has multiple instance type in single availability zone."
+
+            findings.append(report)
+
+        return findings
