ed7dcf5a569df7ede0b01938f5f98a3f211ed69e
Rubén De la Torre Vico <rubendltv22@gmail.com>
2024-10-08 18:11:33 +0200

    chore(ecs): mock all tests using moto (#5326)
    (cherry picked from commit b27898de1db578d32a9688202ac0cebbf326815a)
    
    # Conflicts:
    #	tests/providers/aws/services/ecs/ecs_service_fargate_latest_platform_version/ecs_service_fargate_latest_platform_version_test.py
    #	tests/providers/aws/services/ecs/ecs_task_definitions_logging_enabled/ecs_task_definitions_logging_enabled_test.py

--- tests/providers/aws/services/ecs/ecs_task_definitions_logging_enabled/ecs_task_definitions_logging_enabled_test.py
+++ tests/providers/aws/services/ecs/ecs_task_definitions_logging_enabled/ecs_task_definitions_logging_enabled_test.py
@@ -1,5 +1,6 @@
-from unittest import mock
+from unittest.mock import patch
 
+<<<<<<< HEAD
 import botocore
 
 from prowler.providers.aws.services.ecs.ecs_service import (
@@ -11,11 +12,16 @@ from tests.providers.aws.utils import (
     AWS_REGION_US_EAST_1,
     set_mocked_aws_provider,
 )
+=======
+from boto3 import client
+from moto import mock_aws
+
+from tests.providers.aws.utils import AWS_REGION_US_EAST_1, set_mocked_aws_provider
+>>>>>>> b27898de1 (chore(ecs): mock all tests using moto (#5326))
 
 TASK_NAME = "test-task"
 TASK_REVISION = "1"
 CONTAINER_NAME = "test-container"
-TASK_ARN = f"arn:aws:ecs:{AWS_REGION_US_EAST_1}:{AWS_ACCOUNT_NUMBER}:task-definition/{TASK_NAME}:{TASK_REVISION}"
 
 
 make_api_call = botocore.client.BaseClient._make_api_call
@@ -49,12 +55,22 @@ def mock_make_api_call(self, operation_name, kwarg):
 
 class Test_ecs_task_definitions_logging_enabled:
     def test_no_task_definitions(self):
-        ecs_client = mock.MagicMock
-        ecs_client.task_definitions = {}
+        from prowler.providers.aws.services.ecs.ecs_service import ECS
 
+<<<<<<< HEAD
         with mock.patch(
             "prowler.providers.aws.services.ecs.ecs_service.ECS",
             ecs_client,
+=======
+        mocked_aws_provider = set_mocked_aws_provider([AWS_REGION_US_EAST_1])
+
+        with patch(
+            "prowler.providers.common.provider.Provider.get_global_provider",
+            return_value=mocked_aws_provider,
+        ), patch(
+            "prowler.providers.aws.services.ecs.ecs_task_definitions_logging_enabled.ecs_task_definitions_logging_enabled.ecs_client",
+            new=ECS(mocked_aws_provider),
+>>>>>>> b27898de1 (chore(ecs): mock all tests using moto (#5326))
         ):
             from prowler.providers.aws.services.ecs.ecs_task_definitions_logging_enabled.ecs_task_definitions_logging_enabled import (
                 ecs_task_definitions_logging_enabled,
@@ -64,6 +80,7 @@ class Test_ecs_task_definitions_logging_enabled:
             result = check.execute()
             assert len(result) == 0
 
+<<<<<<< HEAD
     @mock.patch("botocore.client.BaseClient._make_api_call", new=mock_make_api_call)
     def test_task_definition_no_logconfiguration(self):
 
@@ -114,6 +131,37 @@ class Test_ecs_task_definitions_logging_enabled:
         with mock.patch(
             "prowler.providers.aws.services.ecs.ecs_service.ECS",
             ecs_client,
+=======
+    @mock_aws
+    def test_task_definition_no_logconfiguration(self):
+        ecs_client = client("ecs", region_name=AWS_REGION_US_EAST_1)
+
+        task_arn = ecs_client.register_task_definition(
+            family=TASK_NAME,
+            containerDefinitions=[
+                {
+                    "name": CONTAINER_NAME,
+                    "image": "ubuntu",
+                    "memory": 128,
+                    "readonlyRootFilesystem": True,
+                    "privileged": False,
+                    "user": "appuser",
+                    "environment": [],
+                }
+            ],
+        )["taskDefinition"]["taskDefinitionArn"]
+
+        from prowler.providers.aws.services.ecs.ecs_service import ECS
+
+        mocked_aws_provider = set_mocked_aws_provider([AWS_REGION_US_EAST_1])
+
+        with patch(
+            "prowler.providers.common.provider.Provider.get_global_provider",
+            return_value=mocked_aws_provider,
+        ), patch(
+            "prowler.providers.aws.services.ecs.ecs_task_definitions_logging_enabled.ecs_task_definitions_logging_enabled.ecs_client",
+            new=ECS(mocked_aws_provider),
+>>>>>>> b27898de1 (chore(ecs): mock all tests using moto (#5326))
         ):
             from prowler.providers.aws.services.ecs.ecs_task_definitions_logging_enabled.ecs_task_definitions_logging_enabled import (
                 ecs_task_definitions_logging_enabled,
@@ -127,30 +175,47 @@ class Test_ecs_task_definitions_logging_enabled:
                 result[0].status_extended
                 == f"ECS task definition {TASK_NAME} with revision {TASK_REVISION} has containers running with no logging configuration: {CONTAINER_NAME}"
             )
+            assert result[0].resource_id == f"{TASK_NAME}:{TASK_REVISION}"
+            assert result[0].resource_arn == task_arn
+            assert result[0].region == AWS_REGION_US_EAST_1
+            assert result[0].resource_tags == []
 
+    @mock_aws
     def test_task_definition_privileged_container(self):
-        ecs_client = mock.MagicMock
-        ecs_client.task_definitions = {}
-        ecs_client.task_definitions[TASK_ARN] = TaskDefinition(
-            name=TASK_NAME,
-            arn=TASK_ARN,
-            revision=TASK_REVISION,
-            region=AWS_REGION_US_EAST_1,
-            network_mode="host",
-            container_definitions=[
-                ContainerDefinition(
-                    name=CONTAINER_NAME,
-                    privileged=True,
-                    user="root",
-                    environment=[],
-                    log_driver="awslogs",
-                )
-            ],
-        )
+        ecs_client = client("ecs", region_name=AWS_REGION_US_EAST_1)
 
+<<<<<<< HEAD
         with mock.patch(
             "prowler.providers.aws.services.ecs.ecs_service.ECS",
             ecs_client,
+=======
+        task_arn = ecs_client.register_task_definition(
+            family=TASK_NAME,
+            containerDefinitions=[
+                {
+                    "name": CONTAINER_NAME,
+                    "image": "ubuntu",
+                    "memory": 128,
+                    "readonlyRootFilesystem": True,
+                    "privileged": True,
+                    "user": "root",
+                    "environment": [],
+                    "logConfiguration": {"logDriver": "awslogs"},
+                }
+            ],
+        )["taskDefinition"]["taskDefinitionArn"]
+
+        from prowler.providers.aws.services.ecs.ecs_service import ECS
+
+        mocked_aws_provider = set_mocked_aws_provider([AWS_REGION_US_EAST_1])
+
+        with patch(
+            "prowler.providers.common.provider.Provider.get_global_provider",
+            return_value=mocked_aws_provider,
+        ), patch(
+            "prowler.providers.aws.services.ecs.ecs_task_definitions_logging_enabled.ecs_task_definitions_logging_enabled.ecs_client",
+            new=ECS(mocked_aws_provider),
+>>>>>>> b27898de1 (chore(ecs): mock all tests using moto (#5326))
         ):
             from prowler.providers.aws.services.ecs.ecs_task_definitions_logging_enabled.ecs_task_definitions_logging_enabled import (
                 ecs_task_definitions_logging_enabled,
@@ -164,3 +229,7 @@ class Test_ecs_task_definitions_logging_enabled:
                 result[0].status_extended
                 == f"ECS task definition {TASK_NAME} with revision {TASK_REVISION} containers have logging configured."
             )
+            assert result[0].resource_id == f"{TASK_NAME}:{TASK_REVISION}"
+            assert result[0].resource_arn == task_arn
+            assert result[0].region == AWS_REGION_US_EAST_1
+            assert result[0].resource_tags == []
