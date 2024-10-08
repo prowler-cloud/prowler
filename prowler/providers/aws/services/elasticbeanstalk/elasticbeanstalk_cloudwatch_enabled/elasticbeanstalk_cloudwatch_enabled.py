5bf3ce1fb53c5fc9972f0065659714c492e5283d
MarioRgzLpz <mariorgzlpz1809@gmail.com>
2024-10-08 19:03:15 +0200

    feat(elasticbeanstalk): Add check logic with respective unit test. Add metadata too

---
+++ prowler/providers/aws/services/elasticbeanstalk/elasticbeanstalk_cloudwatch_enabled/elasticbeanstalk_cloudwatch_enabled.py
@@ -0,0 +1,28 @@
+from prowler.lib.check.models import Check, Check_Report_AWS
+from prowler.providers.aws.services.elasticbeanstalk.elasticbeanstalk_client import (
+    elasticbeanstalk_client,
+)
+
+
+class elasticbeanstalk_cloudwatch_enabled(Check):
+    def execute(self):
+        findings = []
+        for environment in elasticbeanstalk_client.environments.values():
+            report = Check_Report_AWS(self.metadata())
+            report.region = environment.region
+            report.resource_id = environment.name
+            report.resource_arn = environment.arn
+            report.resource_tags = environment.tags
+            report.status = "PASS"
+            report.status_extended = f"Elastic Beanstalk environment {environment.name} is sending logs to CloudWatch Logs."
+
+            if (
+                environment.cloudwatch_option is None
+                or environment.cloudwatch_option != "true"
+            ):
+                report.status = "FAIL"
+                report.status_extended = f"Elastic Beanstalk environment {environment.name} is not sending logs to CloudWatch Logs."
+
+            findings.append(report)
+
+        return findings
