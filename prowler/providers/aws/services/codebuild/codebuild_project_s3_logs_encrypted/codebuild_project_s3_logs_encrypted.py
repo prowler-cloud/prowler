0d43d2a1e140a612e6b6798bcf9d3b4017e3a8c0
Rubén De la Torre Vico <rubendltv22@gmail.com>
2024-10-10 16:09:37 +0200

    feat(codebuild): add new check to ensure S3 Logs are encrypted at rest

---
+++ prowler/providers/aws/services/codebuild/codebuild_project_s3_logs_encrypted/codebuild_project_s3_logs_encrypted.py
@@ -0,0 +1,27 @@
+from prowler.lib.check.models import Check, Check_Report_AWS
+from prowler.providers.aws.services.codebuild.codebuild_client import codebuild_client
+
+
+class codebuild_project_s3_logs_encrypted(Check):
+    def execute(self):
+        findings = []
+        for project in codebuild_client.projects.values():
+            if (
+                getattr(project, "s3_logs", None)
+                and project.s3_logs.bucket_location
+                and project.s3_logs.status == "ENABLED"
+            ):
+                report = Check_Report_AWS(self.metadata())
+                report.region = project.region
+                report.resource_id = project.name
+                report.resource_arn = project.arn
+                report.status = "PASS"
+                report.status_extended = f"CodeBuild project {project.name} has S3 logs stored in {project.s3_logs.bucket_location} are encrypted."
+
+                if not project.s3_logs.encrypted:
+                    report.status = "FAIL"
+                    report.status_extended = f"CodeBuild project {project.name} has S3 logs stored in {project.s3_logs.bucket_location} are not encrypted."
+
+                findings.append(report)
+
+        return findings
