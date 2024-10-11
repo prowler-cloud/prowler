16579b4ed967d53dccba57706737d1f39df7e065
Sergio <sergio@prowler.com>
2024-10-11 16:40:46 -0400

    add tests

--- prowler/providers/aws/services/bedrock/bedrock_guardrail_sensitive_information_filter_enabled/bedrock_guardrail_sensitive_information_filter_enabled.py
+++ prowler/providers/aws/services/bedrock/bedrock_guardrail_sensitive_information_filter_enabled/bedrock_guardrail_sensitive_information_filter_enabled.py
@@ -12,7 +12,7 @@ class bedrock_guardrail_sensitive_information_filter_enabled(Check):
             report.resource_arn = guardrail.arn
             report.resource_tags = guardrail.tags
             report.status = "PASS"
-            report.status_extended = f"Bedrock Guardrail {guardrail.name} block or mask sensitive information."
+            report.status_extended = f"Bedrock Guardrail {guardrail.name} is blocking or masking sensitive information."
             if not guardrail.sensitive_information_filter:
                 report.status = "FAIL"
                 report.status_extended = f"Bedrock Guardrail {guardrail.name} is not configured to block or mask sensitive information."
