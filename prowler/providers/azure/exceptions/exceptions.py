f2a726efc4c4f7b628d5d0738e9b9a9707636463
pedrooot <pedromarting3@gmail.com>
2024-10-14 11:05:19 +0200

    feat(azure): add provider id validation inside test_connection

--- prowler/providers/azure/exceptions/exceptions.py
+++ prowler/providers/azure/exceptions/exceptions.py
@@ -97,6 +97,10 @@ class AzureBaseException(ProwlerException):
             "message": "The provided tenant ID and client ID do not belong to the provided client secret",
             "remediation": "Check the tenant ID and client ID and ensure they belong to the provided client secret.",
         },
+        (1937, "AzureInvalidAccountCredentialsError"): {
+            "message": "The provided provider_id does not math with the valid subscriptions",
+            "remediation": "Check the provider_id and ensure it is a valid subscription for the given credentials.",
+        },
     }
 
     def __init__(self, code, file=None, original_exception=None, message=None):
@@ -278,4 +282,11 @@ class AzureTenantIdAndClientIdNotBelongingToClientSecretError(AzureCredentialsEr
     def __init__(self, file=None, original_exception=None, message=None):
         super().__init__(
             1936, file=file, original_exception=original_exception, message=message
+        )
+
+
+class AzureInvalidAccountCredentialsError(AzureBaseException):
+    def __init__(self, file=None, original_exception=None, message=None):
+        super().__init__(
+            1937, file=file, original_exception=original_exception, message=message
         )
