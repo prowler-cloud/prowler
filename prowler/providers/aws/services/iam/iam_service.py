6a380dcac438e97c85bf3976f8a23884398d2a0d
Sergio Garcia <38561120+sergargar@users.noreply.github.com>
2024-10-14 13:58:42 -0400

    fix(iam): update AWS Support policy (#5399)
    (cherry picked from commit 54280ee2dc3c9c3b082bc127bffd6f1f2edc127b)

--- prowler/providers/aws/services/iam/iam_service.py
+++ prowler/providers/aws/services/iam/iam_service.py
@@ -69,7 +69,9 @@ class IAM(AWSService):
         self._list_attached_role_policies()
         self._list_mfa_devices()
         self.password_policy = self._get_password_policy()
-        support_policy_arn = f"arn:{self.audited_partition}:iam::aws:policy/aws-service-role/AWSSupportServiceRolePolicy"
+        support_policy_arn = (
+            f"arn:{self.audited_partition}:iam::aws:policy/AWSSupportAccess"
+        )
         self.entities_role_attached_to_support_policy = (
             self._list_entities_role_for_policy(support_policy_arn)
         )
