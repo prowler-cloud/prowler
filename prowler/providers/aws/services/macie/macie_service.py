afec51a94ae788503bf3b82b8549b2c3af2cd8a0
MarioRgzLpz <mariorgzlpz1809@gmail.com>
2024-10-14 10:31:04 +0200

    feat(macie): Add new attribute to session model in macie service for the new check

--- prowler/providers/aws/services/macie/macie_service.py
+++ prowler/providers/aws/services/macie/macie_service.py
@@ -11,6 +11,9 @@ class Macie(AWSService):
         super().__init__("macie2", provider)
         self.sessions = []
         self.__threading_call__(self._get_macie_session)
+        self.__threading_call__(
+            self._get_automated_discovery_configuration, self.sessions
+        )
 
     def _get_session_arn_template(self, region):
         return f"arn:{self.audited_partition}:macie:{region}:{self.audited_account}:session"
@@ -38,7 +41,20 @@ class Macie(AWSService):
                     f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
                 )
 
+    def _get_automated_discovery_configuration(self, session):
+        logger.info("Macie - Get Automated Discovery Configuration...")
+        try:
+            regional_client = self.regional_clients[session.region]
+            response = regional_client.get_automated_discovery_configuration()
+            session.automated_discovery_status = response["status"]
+
+        except Exception as error:
+            logger.error(
+                f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+            )
+
 
 class Session(BaseModel):
     status: str
+    automated_discovery_status: str = None
     region: str
