2a6aa9126a38355a50a6cc05d3dea3210829d75f
MarioRgzLpz <mariorgzlpz1809@gmail.com>
2024-10-16 08:32:04 +0200

    chore(fsx): Make check not to create finding when copy_tags_to_volumes is None

--- prowler/providers/aws/services/fsx/fsx_file_system_copy_tags_to_volumes_enabled/fsx_file_system_copy_tags_to_volumes_enabled.py
+++ prowler/providers/aws/services/fsx/fsx_file_system_copy_tags_to_volumes_enabled/fsx_file_system_copy_tags_to_volumes_enabled.py
@@ -6,7 +6,7 @@ class fsx_file_system_copy_tags_to_volumes_enabled(Check):
     def execute(self):
         findings = []
         for file_system in fsx_client.file_systems.values():
-            if file_system.type == "OPENZFS":
+            if file_system.copy_tags_to_volumes is not None:
                 report = Check_Report_AWS(self.metadata())
                 report.region = file_system.region
                 report.resource_id = file_system.id
