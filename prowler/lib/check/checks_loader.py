8411fcb5fc8b3ec8a4e30abb4f526ec289a0516f
Pepe Fagoaga <pepe@prowler.com>
2024-10-18 11:39:48 +0200

    chore(severities): Use enum (#5460)

--- prowler/lib/check/checks_loader.py
+++ prowler/lib/check/checks_loader.py
@@ -1,10 +1,10 @@
 from colorama import Fore, Style
 
-from prowler.config.config import valid_severities
 from prowler.lib.check.check import (
     parse_checks_from_compliance_framework,
     parse_checks_from_file,
 )
+from prowler.lib.check.models import Severity
 from prowler.lib.check.utils import (
     recover_checks_from_provider,
     recover_checks_from_service,
@@ -29,7 +29,7 @@ def load_checks_to_execute(
         # Local subsets
         checks_to_execute = set()
         check_aliases = {}
-        check_severities = {key: [] for key in valid_severities}
+        check_severities = {severity.value: [] for severity in Severity}
         check_categories = {}
 
         # First, loop over the bulk_checks_metadata to extract the needed subsets
