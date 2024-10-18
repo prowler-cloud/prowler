56210f2c8b64db6df28f8033098b187148751fa3
pedrooot <pedromarting3@gmail.com>
2024-10-18 09:33:40 +0200

    feat(s3): add same format for exceptions

--- prowler/providers/aws/lib/s3/exceptions/exceptions.py
+++ prowler/providers/aws/lib/s3/exceptions/exceptions.py
@@ -1,11 +1,12 @@
 from prowler.exceptions.exceptions import ProwlerException
 
 
+# Exceptions codes from 6000 to 6999 are reserved for S3 exceptions
 class S3BaseException(ProwlerException):
     """Base class for S3 exceptions."""
 
     S3_ERROR_CODES = {
-        (2500, "S3TestConnectionError"): {
+        (6000, "S3TestConnectionError"): {
             "message": "Failed to test connection to S3 bucket.",
             "remediation": "Check the S3 bucket name and permissions.",
         }
@@ -28,5 +29,5 @@ class S3BaseException(ProwlerException):
 class S3TestConnectionError(S3BaseException):
     def __init__(self, file=None, original_exception=None, message=None):
         super().__init__(
-            2500, file=file, original_exception=original_exception, message=message
+            6000, file=file, original_exception=original_exception, message=message
         )
