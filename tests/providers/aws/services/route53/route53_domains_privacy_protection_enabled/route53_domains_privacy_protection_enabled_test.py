7bf31862b72d1260ca28817ddb466165f25bd086
Sergio <sergio@prowler.com>
2024-10-22 12:27:00 -0700

    fix(aws): review checks

--- tests/providers/aws/services/route53/route53_domains_privacy_protection_enabled/route53_domains_privacy_protection_enabled_test.py
+++ tests/providers/aws/services/route53/route53_domains_privacy_protection_enabled/route53_domains_privacy_protection_enabled_test.py
@@ -1,7 +1,7 @@
 from unittest import mock
 
 from prowler.providers.aws.services.route53.route53_service import Domain
-from tests.providers.aws.utils import AWS_REGION_US_EAST_1
+from tests.providers.aws.utils import AWS_ACCOUNT_ARN, AWS_REGION_US_EAST_1
 
 
 class Test_route53_domains_privacy_protection_enabled:
@@ -25,6 +25,7 @@ class Test_route53_domains_privacy_protection_enabled:
 
     def test_domain_privacy_protection_disabled(self):
         route53domains = mock.MagicMock
+        route53domains.audited_account_arn = AWS_ACCOUNT_ARN
         domain_name = "test-domain.com"
         route53domains.domains = {
             domain_name: Domain(
@@ -46,6 +47,7 @@ class Test_route53_domains_privacy_protection_enabled:
 
             assert len(result) == 1
             assert result[0].resource_id == domain_name
+            assert result[0].resource_arn == AWS_ACCOUNT_ARN
             assert result[0].region == AWS_REGION_US_EAST_1
             assert result[0].status == "FAIL"
             assert (
@@ -55,6 +57,7 @@ class Test_route53_domains_privacy_protection_enabled:
 
     def test_domain_privacy_protection_enabled(self):
         route53domains = mock.MagicMock
+        route53domains.audited_account_arn = AWS_ACCOUNT_ARN
         domain_name = "test-domain.com"
         route53domains.domains = {
             domain_name: Domain(
@@ -76,6 +79,7 @@ class Test_route53_domains_privacy_protection_enabled:
 
             assert len(result) == 1
             assert result[0].resource_id == domain_name
+            assert result[0].resource_arn == AWS_ACCOUNT_ARN
             assert result[0].region == AWS_REGION_US_EAST_1
             assert result[0].status == "PASS"
             assert (
