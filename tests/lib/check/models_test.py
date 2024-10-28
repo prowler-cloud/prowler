9af920c722ae7aeddadd68832b0bf716d228db1d
pedrooot <pedromarting3@gmail.com>
2024-10-28 11:25:12 +0100

    feat(check): delete change

--- tests/lib/check/models_test.py
+++ tests/lib/check/models_test.py
@@ -240,7 +240,6 @@ class TestCheckMetada:
         result = CheckMetadata.list(
             bulk_compliance_frameworks=bulk_compliance_frameworks,
             compliance_framework="framework1_azure",
-            provider=None,
         )
 
         # Assertions
