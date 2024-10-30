13c49f616601abe193e5cea3b10a356a4c8c072e
MarioRgzLpz <mariorgzlpz1809@gmail.com>
2024-10-30 10:58:48 +0100

    feat(guardduty): Add check logic with respective unit test. Add metadata too

---
+++ prowler/providers/aws/services/guardduty/guardduty_eks_runtime_monitoring_enabled/guardduty_eks_runtime_monitoring_enabled.py
@@ -0,0 +1,25 @@
+from prowler.lib.check.models import Check, Check_Report_AWS
+from prowler.providers.aws.services.guardduty.guardduty_client import guardduty_client
+
+
+class guardduty_eks_runtime_monitoring_enabled(Check):
+    def execute(self):
+        findings = []
+        for detector in guardduty_client.detectors:
+            if detector.status:
+                report = Check_Report_AWS(self.metadata())
+                report.region = detector.region
+                report.resource_id = detector.id
+                report.resource_arn = detector.arn
+                report.resource_tags = detector.tags
+                report.status = "FAIL"
+                report.status_extended = (
+                    "GuardDuty detector does not have Runtime Monitoring enabled."
+                )
+                if detector.eks_runtime_monitoring:
+                    report.status = "PASS"
+                    report.status_extended = (
+                        "GuardDuty detector has Runtime Monitoring enabled."
+                    )
+                findings.append(report)
+        return findings
