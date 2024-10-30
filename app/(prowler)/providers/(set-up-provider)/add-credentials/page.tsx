d0b599214637e3ddc67fc7b69895c683f9026376
Pablo Lara <larabjj@gmail.com>
2024-10-30 17:30:29 +0100

    feat: redirect on add credentials page if there is no provider associated

--- app/(prowler)/providers/(set-up-provider)/add-credentials/page.tsx
+++ app/(prowler)/providers/(set-up-provider)/add-credentials/page.tsx
@@ -1,11 +1,16 @@
+import { redirect } from "next/navigation";
 import React from "react";
 
 import { AddCredentialsForm } from "@/components/providers/workflow/forms";
 
-export default function AddCredentialsPage({
-  searchParams,
-}: {
+interface Props {
   searchParams: { provider: string; id: string };
-}) {
+}
+
+export default function AddCredentialsPage({ searchParams }: Props) {
+  if (!searchParams.provider || !searchParams.id) {
+    redirect("/providers/connect-account");
+  }
+
   return <AddCredentialsForm searchParams={searchParams} />;
 }
