e817736d1ebd5d0c892c66fabb68c3615192a353
Daniel Barranquero <danielbo2001@gmail.com>
2024-10-30 09:44:06 +0100

    chore(dms): add mogodb auth type to the service

--- prowler/providers/aws/services/dms/dms_service.py
+++ prowler/providers/aws/services/dms/dms_service.py
@@ -71,6 +71,9 @@ class DMS(AWSService):
                             id=endpoint["EndpointIdentifier"],
                             region=regional_client.region,
                             ssl_mode=endpoint.get("SslMode", False),
+                            mongodb_auth_type=endpoint.get("MongoDbSettings", {}).get(
+                                "AuthType", "no"
+                            ),
                         )
         except Exception as error:
             logger.error(
@@ -94,6 +97,7 @@ class Endpoint(BaseModel):
     region: str
     ssl_mode: str
     tags: Optional[list]
+    mongodb_auth_type: Optional[str]
 
 
 class RepInstance(BaseModel):
