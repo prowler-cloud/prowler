191b62eaccb4c2a71a2a29ec968f5bb6fe088c76
MarioRgzLpz <mariorgzlpz1809@gmail.com>
2024-10-31 15:29:24 +0100

    feat(appsync): Add check logic with respective unit tests. Add metadata too

---
+++ prowler/providers/aws/services/appsync/appsync_field_level_logging_enabled/appsync_field_level_logging_enabled.py
@@ -0,0 +1,25 @@
+from prowler.lib.check.models import Check, Check_Report_AWS
+from prowler.providers.aws.services.appsync.appsync_client import appsync_client
+
+
+class appsync_field_level_logging_enabled(Check):
+    def execute(self):
+        findings = []
+        for api in appsync_client.graphqlapis.values():
+            report = Check_Report_AWS(self.metadata())
+            report.region = api.region
+            report.resource_id = api.id
+            report.resource_arn = api.arn
+            report.resource_tags = api.tags
+            report.status = "PASS"
+            report.status_extended = (
+                f"AppSync API {api.name} has field log level enabled."
+            )
+            if api.field_log_level != "ALL" and api.field_log_level != "ERROR":
+                report.status = "FAIL"
+                report.status_extended = (
+                    f"AppSync API {api.name} does not have field log level enabled."
+                )
+            findings.append(report)
+
+        return findings
