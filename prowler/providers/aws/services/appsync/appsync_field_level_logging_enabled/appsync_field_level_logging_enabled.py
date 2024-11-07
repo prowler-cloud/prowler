7297e8fe55514954068c033e642f426128488508
MarioRgzLpz <mariorgzlpz1809@gmail.com>
2024-11-07 20:43:08 +0100

    feat(servicecatalog): Clarify that we only check for GraphQL APIs because boto3 does not have a method to check for other types of AppSync APIs and fix typo in graphql_apis

--- prowler/providers/aws/services/appsync/appsync_field_level_logging_enabled/appsync_field_level_logging_enabled.py
+++ prowler/providers/aws/services/appsync/appsync_field_level_logging_enabled/appsync_field_level_logging_enabled.py
@@ -5,7 +5,8 @@ from prowler.providers.aws.services.appsync.appsync_client import appsync_client
 class appsync_field_level_logging_enabled(Check):
     def execute(self):
         findings = []
-        for api in appsync_client.graphqlapis.values():
+        # Check only GraphQL APIs because boto3 does not have a method to get other types of AppSync APIs (list_apis is not working)
+        for api in appsync_client.graphql_apis.values():
             report = Check_Report_AWS(self.metadata())
             report.region = api.region
             report.resource_id = api.id
