327b4f4bbae04f2c676bc04ce09ec22a0fa26f82
Pablo Lara <larabjj@gmail.com>
2024-11-08 10:42:31 +0100

    chore: style tweaks for sheet component

--- components/ui/sheet/trigger-sheet.tsx
+++ components/ui/sheet/trigger-sheet.tsx
@@ -27,7 +27,7 @@ export function TriggerSheet({
       <SheetTrigger className="flex items-center gap-2">
         {triggerComponent}
       </SheetTrigger>
-      <SheetContent className="max-w-[95vw] pt-10 md:max-w-[45vw]">
+      <SheetContent className="my-4 max-h-[calc(100vh-2rem)] max-w-[95vw] rounded-l-xl pt-10 md:my-8 md:max-h-[calc(100vh-4rem)] md:max-w-[55vw]">
         <SheetHeader>
           <SheetTitle className="sr-only">{title}</SheetTitle>
           <SheetDescription className="sr-only">{description}</SheetDescription>
