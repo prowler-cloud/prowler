48f663f1a65642733a876abed199a819c769b933
pedrooot <pedromarting3@gmail.com>
2024-11-13 18:23:56 +0100

    feat(gcp): return an empty set

--- prowler/providers/gcp/gcp_provider.py
+++ prowler/providers/gcp/gcp_provider.py
@@ -680,4 +680,4 @@ class GcpProvider(Provider):
             logger.error(
                 f"{error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
             )
-            return []
+            return set()
