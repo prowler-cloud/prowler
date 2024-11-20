c11e6449f9905e6dc6c303d1594fe56044722cf0
HugoPBrito <hugopbrit@gmail.com>
2024-11-20 11:42:31 +0100

    fix: reverted name

--- tests/providers/github/github_fixtures.py
+++ tests/providers/github/github_fixtures.py
@@ -1,3 +1,22 @@
+from mock import MagicMock
+from prowler.providers.github.github_provider import GithubProvider
+from prowler.providers.github.models import GithubIdentityInfo, GithubSession
+
 # GitHub Identity
-ACCOUNT_NAME = "Account Name"
-ACCOUNT_ID = ""
+ACCOUNT_NAME = "account-name"
+ACCOUNT_ID = "account-id"
+ACCOUNT_URL = "/user"
+
+# Mocked GitHub Provider
+def set_mocked_github_provider(
+    credentials: 
+) -> GithubProvider:
+    provider = MagicMock()
+    provider.type = "github"
+    provider.session = GithubSession()
+    provider.identity = GithubIdentityInfo(
+        account_name=ACCOUNT_NAME,
+        account_id=ACCOUNT_ID,
+        account_url=ACCOUNT_URL,
+    )
+    return provider
