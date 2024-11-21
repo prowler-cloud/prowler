76d0471e545fe1fff729f3d57c248a2eed09b79f
Pedro Miguel Díaz Peña <pm.diaz.pena@gmail.com>
2024-11-21 13:00:04 +0100

    refactor(azure): search service raname to aisearch service feat: using a dict to store all the services

--- prowler/providers/azure/services/search/search_client.py
+++
@@ -1,4 +0,0 @@
-from prowler.providers.azure.services.search.search_service import Search
-from prowler.providers.common.provider import Provider
-
-search_client = AISearch(Provider.get_global_provider())
