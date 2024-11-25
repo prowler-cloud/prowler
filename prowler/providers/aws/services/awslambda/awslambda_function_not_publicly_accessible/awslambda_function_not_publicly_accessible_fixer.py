9e32fe774de0f14a5416e7079e34dce2e7911644
Daniel Barranquero <danielbo2001@gmail.com>
2024-11-25 13:09:53 +0100

    chore(awslambda): remove all policies instead of only the principal

--- prowler/providers/aws/services/awslambda/awslambda_function_not_publicly_accessible/awslambda_function_not_publicly_accessible_fixer.py
+++ prowler/providers/aws/services/awslambda/awslambda_function_not_publicly_accessible/awslambda_function_not_publicly_accessible_fixer.py
@@ -6,8 +6,8 @@ from prowler.providers.aws.services.awslambda.awslambda_client import awslambda_
 
 def fixer(resource_id: str, region: str) -> bool:
     """
-    Modify the Lambda function's resource-based policy to remove public access.
-    Specifically, this fixer removes the public permission statement from the function's policy.
+    Remove the Lambda function's resource-based policy to prevent public access.
+    Specifically, this fixer deletes all permission statements associated with the Lambda function's policy.
     Requires the lambda:RemovePermission permission.
     Permissions:
     {
@@ -24,36 +24,22 @@ def fixer(resource_id: str, region: str) -> bool:
         resource_id (str): The Lambda function name or ARN.
         region (str): AWS region where the Lambda function exists.
     Returns:
-        bool: True if the operation is successful (public access is removed), False otherwise.
+        bool: True if the operation is successful (policy removed), False otherwise.
     """
     try:
         regional_client = awslambda_client.regional_clients[region]
-
         policy_response = regional_client.get_policy(FunctionName=resource_id)
-
         policy = policy_response.get("Policy")
         if policy:
             if isinstance(policy, str):
                 policy = json.loads(policy)
 
-            statement_id = None
             for statement in policy.get("Statement", []):
-                if "Principal" in statement and "*" in statement["Principal"]:
-                    statement_id = statement.get("Sid")
-                    break
-
-            if statement_id:
-                regional_client.remove_permission(
-                    FunctionName=resource_id, StatementId=statement_id
-                )
-            else:
-                logger.error(
-                    f"{region} -- No public permission found for function {resource_id}"
-                )
-                return True
-        else:
-            logger.error(f"{region} -- No policy found for function {resource_id}")
-            return True
+                statement_id = statement.get("Sid")
+                if statement_id:
+                    regional_client.remove_permission(
+                        FunctionName=resource_id, StatementId=statement_id
+                    )
 
     except Exception as error:
         logger.error(
