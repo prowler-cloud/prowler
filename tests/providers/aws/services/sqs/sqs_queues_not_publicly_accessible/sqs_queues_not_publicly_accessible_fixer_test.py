4ad333de13b5b39ef0aaa31a26ea251f17f48aee
Daniel Barranquero <danielbo2001@gmail.com>
2024-11-26 14:56:47 +0100

    chore(sqs): remove innecessary test

--- tests/providers/aws/services/sqs/sqs_queues_not_publicly_accessible/sqs_queues_not_publicly_accessible_fixer_test.py
+++ tests/providers/aws/services/sqs/sqs_queues_not_publicly_accessible/sqs_queues_not_publicly_accessible_fixer_test.py
@@ -73,30 +73,6 @@ class Test_sqs_queues_not_publicly_accessible_fixer:
             assert fixer(queue_url, AWS_REGION_EU_WEST_1)
 
     @mock_aws
-    def test_queue_not_public(self):
-        sqs_client = client("sqs", region_name=AWS_REGION_EU_WEST_1)
-
-        queue_url = sqs_client.create_queue(QueueName="test-queue")["QueueUrl"]
-
-        aws_provider = set_mocked_aws_provider([AWS_REGION_EU_WEST_1])
-
-        from prowler.providers.aws.services.sqs.sqs_service import SQS
-
-        with mock.patch(
-            "prowler.providers.common.provider.Provider.get_global_provider",
-            return_value=aws_provider,
-        ), mock.patch(
-            "prowler.providers.aws.services.sqs.sqs_queues_not_publicly_accessible.sqs_queues_not_publicly_accessible_fixer.sqs_client",
-            new=SQS(aws_provider),
-        ):
-            # Test Fixer
-            from prowler.providers.aws.services.sqs.sqs_queues_not_publicly_accessible.sqs_queues_not_publicly_accessible_fixer import (
-                fixer,
-            )
-
-            assert fixer(queue_url, AWS_REGION_EU_WEST_1)
-
-    @mock_aws
     def test_queue_public_error(self):
         sqs_client = client("sqs", region_name=AWS_REGION_EU_WEST_1)
 
