13cfb3b55a0e5479d9abd3e6b50b45265c765d2e
Pablo Lara <larabjj@gmail.com>
2024-11-27 12:42:24 +0100

    feat: add findings by status chart to the overview page

--- ui/components/charts/index.ts
+++ ui/components/charts/index.ts
@@ -1,2 +1,1 @@
 export * from "./SeverityChart";
-export * from "./StatusChart";
--- ui/components/overview/index.ts
+++ ui/components/overview/index.ts
@@ -1,4 +1,6 @@
 export * from "./AttackSurface";
+export * from "./findings-by-status-chart/findings-by-status-chart";
+export * from "./findings-by-status-chart/skeleton-findings-status-chart";
 export * from "./new-findings-table/link-to-findings/link-to-findings";
 export * from "./provider-overview/provider-overview";
 export * from "./provider-overview/skeleton-provider-overview";
