3547153c0a74dae44daffad749f1a04f27016634
Pablo Lara <larabjj@gmail.com>
2024-11-30 18:51:26 +0100

    feat(v5): tweaks UI (#5971)

--- ui/components/findings/table/column-findings.tsx
+++ ui/components/findings/table/column-findings.tsx
@@ -5,6 +5,7 @@ import { useSearchParams } from "next/navigation";
 
 import { DataTableRowDetails } from "@/components/findings/table";
 import { InfoIcon } from "@/components/icons";
+import { DateWithTime } from "@/components/ui/entities";
 import { TriggerSheet } from "@/components/ui/sheet";
 import {
   DataTableColumnHeader,
@@ -12,8 +13,6 @@ import {
   StatusFindingBadge,
 } from "@/components/ui/table";
 import { FindingProps } from "@/types";
-
-import { DataTableRowActions } from "./data-table-row-actions";
 
 const getFindingsData = (row: { original: FindingProps }) => {
   return row.original;
@@ -43,25 +42,57 @@ const getProviderData = (
   );
 };
 
-const getScanData = (
-  row: { original: FindingProps },
-  field: keyof FindingProps["relationships"]["scan"]["attributes"],
-) => {
-  return (
-    row.original.relationships?.scan?.attributes?.[field] ||
-    `No ${field} found in scan`
-  );
-};
+// const getScanData = (
+//   row: { original: FindingProps },
+//   field: keyof FindingProps["relationships"]["scan"]["attributes"],
+// ) => {
+//   return (
+//     row.original.relationships?.scan?.attributes?.[field] ||
+//     `No ${field} found in scan`
+//   );
+// };
 
 export const ColumnFindings: ColumnDef<FindingProps>[] = [
   {
+    id: "moreInfo",
+    header: "Details",
+    cell: ({ row }) => {
+      const searchParams = useSearchParams();
+      const findingId = searchParams.get("id");
+      const isOpen = findingId === row.original.id;
+      return (
+        <div className="flex justify-center">
+          <TriggerSheet
+            triggerComponent={<InfoIcon className="text-primary" size={16} />}
+            title="Finding Details"
+            description="View the finding details"
+            defaultOpen={isOpen}
+          >
+            <DataTableRowDetails
+              entityId={row.original.id}
+              findingDetails={row.original}
+            />
+          </TriggerSheet>
+        </div>
+      );
+    },
+  },
+  {
     accessorKey: "check",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title={"Check"} param="check_id" />
+      <DataTableColumnHeader
+        column={column}
+        title={"Finding"}
+        param="check_id"
+      />
     ),
     cell: ({ row }) => {
       const { checktitle } = getFindingsMetadata(row);
-      return <p className="max-w-96 truncate text-small">{checktitle}</p>;
+      return (
+        <p className="max-w-[450px] whitespace-normal break-words text-small">
+          {checktitle}
+        </p>
+      );
     },
   },
   {
@@ -94,20 +125,40 @@ export const ColumnFindings: ColumnDef<FindingProps>[] = [
     },
   },
   {
-    accessorKey: "scanName",
-    header: "Scan Name",
+    accessorKey: "updated_at",
+    header: ({ column }) => (
+      <DataTableColumnHeader
+        column={column}
+        title={"Last seen"}
+        param="updated_at"
+      />
+    ),
     cell: ({ row }) => {
-      const name = getScanData(row, "name");
-
+      const {
+        attributes: { updated_at },
+      } = getFindingsData(row);
       return (
-        <p className="text-small">
-          {typeof name === "string" || typeof name === "number"
-            ? name
-            : "Invalid data"}
-        </p>
+        <div className="w-[100px]">
+          <DateWithTime dateTime={updated_at} />
+        </div>
       );
     },
   },
+  // {
+  //   accessorKey: "scanName",
+  //   header: "Scan Name",
+  //   cell: ({ row }) => {
+  //     const name = getScanData(row, "name");
+
+  //     return (
+  //       <p className="text-small">
+  //         {typeof name === "string" || typeof name === "number"
+  //           ? name
+  //           : "Invalid data"}
+  //       </p>
+  //     );
+  //   },
+  // },
   {
     accessorKey: "region",
     header: "Region",
@@ -115,9 +166,9 @@ export const ColumnFindings: ColumnDef<FindingProps>[] = [
       const region = getResourceData(row, "region");
 
       return (
-        <>
-          <div>{typeof region === "string" ? region : "Invalid region"}</div>
-        </>
+        <div className="w-[80px]">
+          {typeof region === "string" ? region : "Invalid region"}
+        </div>
       );
     },
   },
@@ -142,36 +193,6 @@ export const ColumnFindings: ColumnDef<FindingProps>[] = [
           </p>
         </>
       );
-    },
-  },
-  {
-    id: "moreInfo",
-    header: "Details",
-    cell: ({ row }) => {
-      const searchParams = useSearchParams();
-      const findingId = searchParams.get("id");
-      const isOpen = findingId === row.original.id;
-      return (
-        <div className="flex justify-center">
-          <TriggerSheet
-            triggerComponent={<InfoIcon className="text-primary" size={16} />}
-            title="Finding Details"
-            description="View the finding details"
-            defaultOpen={isOpen}
-          >
-            <DataTableRowDetails
-              entityId={row.original.id}
-              findingDetails={row.original}
-            />
-          </TriggerSheet>
-        </div>
-      );
-    },
-  },
-  {
-    id: "actions",
-    cell: ({ row }) => {
-      return <DataTableRowActions row={row} />;
     },
   },
 ];
--- ui/components/overview/new-findings-table/table/column-new-findings-to-date.tsx
+++ ui/components/overview/new-findings-table/table/column-new-findings-to-date.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
+import { useSearchParams } from "next/navigation";
 
-import { DataTableColumnHeader, SeverityBadge } from "@/components/ui/table";
+import { DataTableRowDetails } from "@/components/findings/table";
+import { InfoIcon } from "@/components/icons";
+import { DateWithTime } from "@/components/ui/entities";
+import { TriggerSheet } from "@/components/ui/sheet";
+import { SeverityBadge, StatusFindingBadge } from "@/components/ui/table";
 import { FindingProps } from "@/types";
 
 const getFindingsData = (row: { original: FindingProps }) => {
@@ -13,31 +18,132 @@ const getFindingsMetadata = (row: { original: FindingProps }) => {
   return row.original.attributes.check_metadata;
 };
 
+const getResourceData = (
+  row: { original: FindingProps },
+  field: keyof FindingProps["relationships"]["resource"]["attributes"],
+) => {
+  return (
+    row.original.relationships?.resource?.attributes?.[field] ||
+    `No ${field} found in resource`
+  );
+};
+
+const getProviderData = (
+  row: { original: FindingProps },
+  field: keyof FindingProps["relationships"]["provider"]["attributes"],
+) => {
+  return (
+    row.original.relationships?.provider?.attributes?.[field] ||
+    `No ${field} found in provider`
+  );
+};
+
 export const ColumnNewFindingsToDate: ColumnDef<FindingProps>[] = [
   {
+    id: "moreInfo",
+    header: "Details",
+    cell: ({ row }) => {
+      const searchParams = useSearchParams();
+      const findingId = searchParams.get("id");
+      const isOpen = findingId === row.original.id;
+      return (
+        <div className="flex justify-center">
+          <TriggerSheet
+            triggerComponent={<InfoIcon className="text-primary" size={16} />}
+            title="Finding Details"
+            description="View the finding details"
+            defaultOpen={isOpen}
+          >
+            <DataTableRowDetails
+              entityId={row.original.id}
+              findingDetails={row.original}
+            />
+          </TriggerSheet>
+        </div>
+      );
+    },
+  },
+  {
     accessorKey: "check",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title={"Check"} param="check_id" />
-    ),
+    header: "Finding",
     cell: ({ row }) => {
       const { checktitle } = getFindingsMetadata(row);
-      return <p className="max-w-80 truncate text-small">{checktitle}</p>;
+      return (
+        <p className="max-w-[450px] whitespace-normal break-words text-small">
+          {checktitle}
+        </p>
+      );
     },
   },
   {
     accessorKey: "severity",
-    header: ({ column }) => (
-      <DataTableColumnHeader
-        column={column}
-        title={"Severity"}
-        param="severity"
-      />
-    ),
+    header: "Severity",
     cell: ({ row }) => {
       const {
         attributes: { severity },
       } = getFindingsData(row);
       return <SeverityBadge severity={severity} />;
+    },
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      const {
+        attributes: { status },
+      } = getFindingsData(row);
+
+      return <StatusFindingBadge size="sm" status={status} />;
+    },
+  },
+  {
+    accessorKey: "updated_at",
+    header: "Last seen",
+    cell: ({ row }) => {
+      const {
+        attributes: { updated_at },
+      } = getFindingsData(row);
+      return (
+        <div className="w-[100px]">
+          <DateWithTime dateTime={updated_at} />
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "region",
+    header: "Region",
+    cell: ({ row }) => {
+      const region = getResourceData(row, "region");
+
+      return (
+        <div className="w-[80px]">
+          {typeof region === "string" ? region : "Invalid region"}
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "service",
+    header: "Service",
+    cell: ({ row }) => {
+      const { servicename } = getFindingsMetadata(row);
+      return <p className="max-w-96 truncate text-small">{servicename}</p>;
+    },
+  },
+  {
+    accessorKey: "account",
+    header: "Account",
+    cell: ({ row }) => {
+      const account = getProviderData(row, "uid");
+
+      return (
+        <>
+          <p className="max-w-96 truncate text-small">
+            {typeof account === "string" ? account : "Invalid account"}
+          </p>
+        </>
+      );
     },
   },
 ];
