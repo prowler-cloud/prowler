6ce13f2386d05e8b982d2ea72288903cebf7b028
Rubén De la Torre Vico <ruben@prowler.com>
2024-12-03 17:09:05 +0100

    feat(app): add TLS version to status_extended PASS

--- prowler/providers/azure/services/app/app_minimum_tls_version_12/app_minimum_tls_version_12.py
+++ prowler/providers/azure/services/app/app_minimum_tls_version_12/app_minimum_tls_version_12.py
@@ -23,7 +23,7 @@ class app_minimum_tls_version_12(Check):
                     app.configurations, "min_tls_version", ""
                 ) in ["1.2", "1.3"]:
                     report.status = "PASS"
-                    report.status_extended = f"Minimum TLS version is set to 1.2 for app '{app_name}' in subscription '{subscription_name}'."
+                    report.status_extended = f"Minimum TLS version is set to {app.configurations.min_tls_version} for app '{app_name}' in subscription '{subscription_name}'."
 
                 findings.append(report)
 
