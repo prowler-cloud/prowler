b1574dbbd47389e43167669975e4b807542be1d5
Daniel Barranquero <danielbo2001@gmail.com>
2024-12-11 13:05:35 +0100

    chore(opensearch): modify fixer to only remove policy

--- prowler/providers/aws/services/opensearch/opensearch_service_domains_not_publicly_accessible/opensearch_service_domains_not_publicly_accessible_fixer.py
+++ prowler/providers/aws/services/opensearch/opensearch_service_domains_not_publicly_accessible/opensearch_service_domains_not_publicly_accessible_fixer.py
@@ -1,5 +1,3 @@
-import json
-
 from prowler.lib.logger import logger
 from prowler.providers.aws.services.opensearch.opensearch_client import (
     opensearch_client,
@@ -8,9 +6,9 @@ from prowler.providers.aws.services.opensearch.opensearch_client import (
 
 def fixer(resource_id: str, region: str) -> bool:
     """
-    Modify the OpenSearch domain's resource-based policy to remove public access and replace it with trusted account access.
-    Specifically, this fixer checks if any statement has a public Principal (e.g., "*" or "AWS": "*")
-    and replaces it with the ARN of the trusted AWS account. Requires the es:UpdateDomainConfig permission.
+    Modify the OpenSearch domain's resource-based policy to remove public access.
+    Specifically, this fixer update the domain config and add an empty policy to remove the old one.
+    Requires the es:UpdateDomainConfig permission.
     Permissions:
     {
         "Version": "2012-10-17",
@@ -29,28 +27,11 @@ def fixer(resource_id: str, region: str) -> bool:
         bool: True if the operation is successful (policy updated), False otherwise.
     """
     try:
-        account_id = opensearch_client.audited_account
-        audited_partition = opensearch_client.audited_partition
-
         regional_client = opensearch_client.regional_clients[region]
-
-        trusted_policy = {
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Effect": "Allow",
-                    "Principal": {
-                        "AWS": f"arn:{audited_partition}:iam::{account_id}:root"
-                    },
-                    "Action": "es:*",
-                    "Resource": f"arn:{audited_partition}:es:{region}:{account_id}:domain/{resource_id}/*",
-                }
-            ],
-        }
 
         regional_client.update_domain_config(
             DomainName=resource_id,
-            AccessPolicies=json.dumps(trusted_policy),
+            AccessPolicies="",
         )
 
     except Exception as error:
