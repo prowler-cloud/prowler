25a776bf14525543eb57b813efd8404788018774
Daniel Barranquero <danielbo2001@gmail.com>
2024-12-12 10:24:25 +0100

    fix(autoscaling): remove old code

--- tests/providers/aws/services/autoscaling/autoscaling_group_launch_configuration_no_public_ip/autoscaling_group_launch_configuration_no_public_ip_test.py
+++ tests/providers/aws/services/autoscaling/autoscaling_group_launch_configuration_no_public_ip/autoscaling_group_launch_configuration_no_public_ip_test.py
@@ -171,10 +171,6 @@ class Test_autoscaling_group_launch_configuration_no_public_ip:
             AvailabilityZones=["us-east-1a", "us-east-1b"],
         )
 
-        autoscaling_client.describe_auto_scaling_groups(
-            AutoScalingGroupNames=[autoscaling_group_name]
-        )["AutoScalingGroups"][0]["AutoScalingGroupARN"]
-
         from prowler.providers.aws.services.autoscaling.autoscaling_service import (
             AutoScaling,
         )
