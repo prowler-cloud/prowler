dccd38a39a541f363fe20e1916166bd3bec79a64
Daniel Barranquero <danielbo2001@gmail.com>
2024-12-13 08:16:37 +0100

    chore(s3): use delete policy instead of set policy

--- prowler/providers/aws/services/s3/s3_bucket_policy_public_write_access/s3_bucket_policy_public_write_access_fixer.py
+++ prowler/providers/aws/services/s3/s3_bucket_policy_public_write_access/s3_bucket_policy_public_write_access_fixer.py
@@ -1,5 +1,3 @@
-import json
-
 from prowler.lib.logger import logger
 from prowler.providers.aws.services.s3.s3_client import s3_client
 
@@ -7,15 +5,15 @@ from prowler.providers.aws.services.s3.s3_client import s3_client
 def fixer(resource_id: str, region: str) -> bool:
     """
     Modify the S3 bucket's policy to remove public access.
-    Specifically, this fixer replaces any policy allowing public access
-    with a trusted access policy for the AWS account. Requires the s3:SetBucketPolicy permission.
+    Specifically, this fixer delete the policy of the public bucket.
+    Requires the s3:DeleteBucketPolicy permission.
     Permissions:
     {
         "Version": "2012-10-17",
         "Statement": [
             {
                 "Effect": "Allow",
-                "Action": "s3:SetBucketPolicy",
+                "Action": "s3:DeleteBucketPolicy",
                 "Resource": "*"
             }
         ]
@@ -28,28 +26,13 @@ def fixer(resource_id: str, region: str) -> bool:
     """
     try:
         regional_client = s3_client.regional_clients[region]
-        account_id = s3_client.audited_account
-        audited_partition = s3_client.audited_partition
-        trusted_policy = {
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Sid": "ProwlerFixerStatement",
-                    "Effect": "Allow",
-                    "Principal": {
-                        "AWS": f"arn:{audited_partition}:iam::{account_id}:root"
-                    },
-                    "Action": "s3:*",
-                }
-            ],
-        }
 
-        regional_client.put_bucket_policy(
-            Bucket=resource_id, Policy=json.dumps(trusted_policy)
-        )
+        regional_client.delete_bucket_policy(Bucket=resource_id)
 
     except Exception as error:
         logger.error(
             f"{region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
         )
         return False
+    else:
+        return True
