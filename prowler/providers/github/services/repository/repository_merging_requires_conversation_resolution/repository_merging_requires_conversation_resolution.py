bcb8c7d3f8812edfb31274ab862cb5dbfbbcacab
HugoPBrito <hugopbrit@gmail.com>
2024-12-16 13:17:56 +0000

    feat: added check logic and metadata

---
+++ prowler/providers/github/services/repository/repository_merging_requires_conversation_resolution/repository_merging_requires_conversation_resolution.py
@@ -0,0 +1,44 @@
+from typing import List
+
+from prowler.lib.check.models import Check, Check_Report_Github
+from prowler.providers.github.services.repository.repository_client import (
+    repository_client,
+)
+
+
+class repository_merging_requires_conversation_resolution(Check):
+    """Check if a repository requires
+
+    This class verifies whether each repository
+    """
+
+    def execute(self) -> List[Check_Report_Github]:
+        """Execute the Github Repository Merging Requires
+
+        Iterates over all repositories and checks if they
+
+        Returns:
+            List[Check_Report_Github]: A list of reports for each repository
+        """
+        findings = []
+        for repo in repository_client.repositories.values():
+            report = Check_Report_Github(self.metadata())
+            report.resource_id = repo.id
+            report.resource_name = repo.name
+            report.status = "FAIL"
+            report.status_extended = (
+                f"Repository {repo.name} does not require conversation resolution."
+            )
+
+            if (
+                repo.default_branch_protection
+                and repo.default_branch_protection.conversation_resolution
+            ):
+                report.status = "PASS"
+                report.status_extended = (
+                    f"Repository {repo.name} does require conversation resolution."
+                )
+
+            findings.append(report)
+
+        return findings
