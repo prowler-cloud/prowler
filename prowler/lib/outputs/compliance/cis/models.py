c98a7f6429bda631d871c77c2bc17595d9c2d0eb
HugoPBrito <hugopbrit@gmail.com>
2024-12-17 13:35:20 +0000

    feat: add GithubCISModel

--- prowler/lib/outputs/compliance/cis/models.py
+++ prowler/lib/outputs/compliance/cis/models.py
@@ -129,6 +129,29 @@ class GithubCISModel(BaseModel):
     """
 
     Provider: str
+    Description: str
+    Account_Name: str
+    Account_Id: str
+    AssessmentDate: str
+    Requirements_Id: str
+    Requirements_Description: str
+    Requirements_Attributes_Section: str
+    Requirements_Attributes_Profile: str
+    Requirements_Attributes_AssessmentStatus: str
+    Requirements_Attributes_Description: str
+    Requirements_Attributes_RationaleStatement: str
+    Requirements_Attributes_ImpactStatement: str
+    Requirements_Attributes_RemediationProcedure: str
+    Requirements_Attributes_AuditProcedure: str
+    Requirements_Attributes_AdditionalInformation: str
+    Requirements_Attributes_References: str
+    Requirements_Attributes_DefaultValue: str
+    Status: str
+    StatusExtended: str
+    ResourceId: str
+    ResourceName: str
+    CheckId: str
+    Muted: bool
 
 
 # TODO: Create a parent class for the common fields of CIS and have the specific classes from each provider to inherit from it.
