88f944fd32cd174df88955a32cd019702fb10415
Sergio Garcia <hello@mistercloudsec.com>
2024-12-19 11:32:33 -0500

    fix(aws): solve `None` type errors (#6268)
    (cherry picked from commit bb5b63f62f8d49b490c47fdedf2a7b9c2a60a121)

--- prowler/providers/aws/services/cloudwatch/lib/metric_filters.py
+++ prowler/providers/aws/services/cloudwatch/lib/metric_filters.py
@@ -18,24 +18,24 @@ def check_cloudwatch_log_metric_filter(
         for trail in trails.values():
             if trail.log_group_arn:
                 log_groups.append(trail.log_group_arn.split(":")[6])
-    # 2. Describe metric filters for previous log groups
-    for metric_filter in metric_filters:
-        if metric_filter.log_group.name in log_groups and re.search(
-            metric_filter_pattern, metric_filter.pattern, flags=re.DOTALL
-        ):
-            report.resource_id = metric_filter.log_group.name
-            report.resource_arn = metric_filter.log_group.arn
-            report.region = metric_filter.log_group.region
-            report.resource_tags = getattr(metric_filter.log_group, "tags", [])
-            report.status = "FAIL"
-            report.status_extended = f"CloudWatch log group {metric_filter.log_group.name} found with metric filter {metric_filter.name} but no alarms associated."
-            # 3. Check if there is an alarm for the metric
-            for alarm in metric_alarms:
-                if alarm.metric == metric_filter.metric:
-                    report.status = "PASS"
-                    report.status_extended = f"CloudWatch log group {metric_filter.log_group.name} found with metric filter {metric_filter.name} and alarms set."
+        # 2. Describe metric filters for previous log groups
+        for metric_filter in metric_filters:
+            if metric_filter.log_group.name in log_groups and re.search(
+                metric_filter_pattern, metric_filter.pattern, flags=re.DOTALL
+            ):
+                report.resource_id = metric_filter.log_group.name
+                report.resource_arn = metric_filter.log_group.arn
+                report.region = metric_filter.log_group.region
+                report.resource_tags = getattr(metric_filter.log_group, "tags", [])
+                report.status = "FAIL"
+                report.status_extended = f"CloudWatch log group {metric_filter.log_group.name} found with metric filter {metric_filter.name} but no alarms associated."
+                # 3. Check if there is an alarm for the metric
+                for alarm in metric_alarms:
+                    if alarm.metric == metric_filter.metric:
+                        report.status = "PASS"
+                        report.status_extended = f"CloudWatch log group {metric_filter.log_group.name} found with metric filter {metric_filter.name} and alarms set."
+                        break
+                if report.status == "PASS":
                     break
-            if report.status == "PASS":
-                break
 
     return report
