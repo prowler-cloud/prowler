afb1dcf1366bc1a85eb2ed6a3a60020bd5e14791
HugoPBrito <hugopbrit@gmail.com>
2025-01-14 19:14:32 +0100

    feat: included resource metadata in all d* services
    Also added  arn to Table models in DynamoDB

--- prowler/lib/check/models.py
+++ prowler/lib/check/models.py
@@ -5,7 +5,7 @@ import sys
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from enum import Enum
-from typing import Set
+from typing import Any, Dict, Set
 
 from pydantic import BaseModel, ValidationError, validator
 
@@ -410,7 +410,28 @@ class Check_Report:
     resource_tags: list
     muted: bool
 
-    def __init__(self, metadata, resource=None):
+    def __init__(self, metadata: Dict, resource: Any = None) -> None:
+        """Initialize the Check's finding information.
+        Args:
+            metadata: The metadata of the check.
+            resource: Basic information about the resource. Defaults to None.
+                      Only accepted BaseModels (dict attribute), custom models (to_dict attribute) or objects with __dict__.
+        """
+        self.status = ""
+        self.check_metadata = CheckMetadata.parse_raw(metadata)
+
+        if hasattr(resource, "dict"):
+            self.resource_metadata = resource.dict()
+        elif hasattr(resource, "to_dict"):
+            self.resource_metadata = resource.to_dict()
+        elif hasattr(resource, "__dict__"):
+            self.resource_metadata = resource.__dict__
+        else:
+            logger.error(
+                f"Resource metadata {type(resource)} could not be converted to dict"
+            )
+            self.resource_metadata = {}
+
         self.status = ""
         self.check_metadata = CheckMetadata.parse_raw(metadata)
         self.resource_metadata = resource.dict() if resource else {}
