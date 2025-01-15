77950f606981bb00efd70b451ffa24e82669acde
Daniel Barranquero <74871504+danibarranqueroo@users.noreply.github.com>
2025-01-15 23:22:08 +0100

    chore(aws): add resource metadata to services from `t` to `w` (#6546)

--- prowler/providers/aws/services/waf/waf_global_webacl_with_rules/waf_global_webacl_with_rules.py
+++ prowler/providers/aws/services/waf/waf_global_webacl_with_rules/waf_global_webacl_with_rules.py
@@ -6,11 +6,7 @@ class waf_global_webacl_with_rules(Check):
     def execute(self):
         findings = []
         for acl in waf_client.web_acls.values():
-            report = Check_Report_AWS(self.metadata())
-            report.region = acl.region
-            report.resource_id = acl.id
-            report.resource_arn = acl.arn
-            report.resource_tags = acl.tags
+            report = Check_Report_AWS(metadata=self.metadata(), resource_metadata=acl)
             report.status = "FAIL"
             report.status_extended = f"AWS WAF Global Web ACL {acl.name} does not have any rules or rule groups."
 
