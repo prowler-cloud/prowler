b0aeaa55bc46ce4a1df6938f90e249370c466762
MrCloudSec <hello@mistercloudsec.com>
2025-01-15 10:39:11 -0500

    chore: add network metadata

--- prowler/providers/gcp/services/compute/compute_network_default_in_use/compute_network_default_in_use.py
+++ prowler/providers/gcp/services/compute/compute_network_default_in_use/compute_network_default_in_use.py
@@ -5,12 +5,12 @@ from prowler.providers.gcp.services.compute.compute_client import compute_client
 class compute_network_default_in_use(Check):
     def execute(self) -> Check_Report_GCP:
         findings = []
-        projects_with_default_network = set()
+        projects_with_default_network = {}
 
         # Identify projects with the default network
         for network in compute_client.networks:
             if network.name == "default":
-                projects_with_default_network.add(network.project_id)
+                projects_with_default_network[network.project_id] = network
 
         # Generate reports for all projects
         for project in compute_client.project_ids:
@@ -27,6 +27,7 @@ class compute_network_default_in_use(Check):
                 report.status_extended = (
                     f"Default network is in use in project {project}."
                 )
+                report.resource_metadata = projects_with_default_network[project]
             else:
                 report.status = "PASS"
                 report.status_extended = (
