16354a257e073887385eeab853b5c8f6f10a3081
Daniel Barranquero <danielbo2001@gmail.com>
2025-01-15 19:12:08 +0100

    chore(kafka): add resource metadata and arn

--- tests/providers/aws/services/kafka/kafka_cluster_encryption_at_rest_uses_cmk/kafka_cluster_encryption_at_rest_uses_cmk_test.py
+++ tests/providers/aws/services/kafka/kafka_cluster_encryption_at_rest_uses_cmk/kafka_cluster_encryption_at_rest_uses_cmk_test.py
@@ -34,6 +34,7 @@ class Test_kafka_cluster_encryption_at_rest_uses_cmk:
             "arn:aws:kafka:us-east-1:123456789012:cluster/demo-cluster-1/6357e0b2-0e6a-4b86-a0b4-70df934c2e31-5": Cluster(
                 id="6357e0b2-0e6a-4b86-a0b4-70df934c2e31-5",
                 name="demo-cluster-1",
+                arn="arn:aws:kafka:us-east-1:123456789012:cluster/demo-cluster-1/6357e0b2-0e6a-4b86-a0b4-70df934c2e31-5",
                 region=AWS_REGION_US_EAST_1,
                 tags=[],
                 state="ACTIVE",
@@ -95,6 +96,7 @@ class Test_kafka_cluster_encryption_at_rest_uses_cmk:
             "arn:aws:kafka:us-east-1:123456789012:cluster/demo-cluster-1/6357e0b2-0e6a-4b86-a0b4-70df934c2e31-5": Cluster(
                 id="6357e0b2-0e6a-4b86-a0b4-70df934c2e31-5",
                 name="demo-cluster-1",
+                arn="arn:aws:kafka:us-east-1:123456789012:cluster/demo-cluster-1/6357e0b2-0e6a-4b86-a0b4-70df934c2e31-5",
                 region=AWS_REGION_US_EAST_1,
                 tags=[],
                 state="ACTIVE",
