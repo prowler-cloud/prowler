04b2168746f507858498425f63f32dc976d98a7a
Daniel Barranquero <danielbo2001@gmail.com>
2025-01-15 20:35:59 +0100

    chore(wellarchitected): add resource metadata

--- prowler/providers/aws/services/wellarchitected/wellarchitected_workload_no_high_or_medium_risks/wellarchitected_workload_no_high_or_medium_risks.py
+++ prowler/providers/aws/services/wellarchitected/wellarchitected_workload_no_high_or_medium_risks/wellarchitected_workload_no_high_or_medium_risks.py
@@ -8,11 +8,9 @@ class wellarchitected_workload_no_high_or_medium_risks(Check):
     def execute(self):
         findings = []
         for workload in wellarchitected_client.workloads:
-            report = Check_Report_AWS(self.metadata())
-            report.region = workload.region
-            report.resource_id = workload.id
-            report.resource_arn = workload.arn
-            report.resource_tags = workload.tags
+            report = Check_Report_AWS(
+                metadata=self.metadata(), resource_metadata=workload
+            )
             report.status = "PASS"
             report.status_extended = f"Well Architected workload {workload.name} does not contain high or medium risks."
             if "HIGH" in workload.risks or "MEDIUM" in workload.risks:
