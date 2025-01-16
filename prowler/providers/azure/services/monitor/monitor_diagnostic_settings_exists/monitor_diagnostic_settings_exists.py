bfc624acb3a4a4d7a4af7a186e051003b70bcd7d
Sergio Garcia <hello@mistercloudsec.com>
2025-01-16 09:21:15 -0500

    chore: revision

--- prowler/providers/azure/services/monitor/monitor_diagnostic_settings_exists/monitor_diagnostic_settings_exists.py
+++ prowler/providers/azure/services/monitor/monitor_diagnostic_settings_exists/monitor_diagnostic_settings_exists.py
@@ -10,7 +10,7 @@ class monitor_diagnostic_settings_exists(Check):
             subscription_name,
             diagnostic_settings,
         ) in monitor_client.diagnostics_settings.items():
-            report = Check_Report_Azure(self.metadata())
+            report = Check_Report_Azure(self.metadata(), diagnostic_settings)
             report.subscription = subscription_name
             report.resource_name = "Diagnostic Settings"
             report.resource_id = "diagnostic_settings"
