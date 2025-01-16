de76a168c0c1ca77cd6263d36bc57cbfec38c558
Rubén De la Torre Vico <rubendltv22@gmail.com>
2025-01-16 17:44:43 +0100

    feat(storage): extract Storage resource metadata automated (#6563)
    Co-authored-by: Sergio Garcia <hello@mistercloudsec.com>

--- prowler/providers/azure/services/storage/storage_ensure_azure_services_are_trusted_to_access_is_enabled/storage_ensure_azure_services_are_trusted_to_access_is_enabled.py
+++ prowler/providers/azure/services/storage/storage_ensure_azure_services_are_trusted_to_access_is_enabled/storage_ensure_azure_services_are_trusted_to_access_is_enabled.py
@@ -7,13 +7,13 @@ class storage_ensure_azure_services_are_trusted_to_access_is_enabled(Check):
         findings = []
         for subscription, storage_accounts in storage_client.storage_accounts.items():
             for storage_account in storage_accounts:
-                report = Check_Report_Azure(self.metadata())
+                report = Check_Report_Azure(
+                    metadata=self.metadata(), resource_metadata=storage_account
+                )
                 report.subscription = subscription
                 report.status = "PASS"
-                report.location = storage_account.location
                 report.status_extended = f"Storage account {storage_account.name} from subscription {subscription} allows trusted Microsoft services to access this storage account."
-                report.resource_name = storage_account.name
-                report.resource_id = storage_account.id
+
                 if "AzureServices" not in storage_account.network_rule_set.bypass:
                     report.status = "FAIL"
                     report.status_extended = f"Storage account {storage_account.name} from subscription {subscription} does not allow trusted Microsoft services to access this storage account."
