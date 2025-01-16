4a04a668790731a6592d52bf767cd011cb46887e
HugoPBrito <hugopbrit@gmail.com>
2025-01-16 12:44:54 +0100

    fix: resource metadata

--- prowler/providers/github/services/repository/repository_code_changes_multi_approval_requirement/repository_code_changes_multi_approval_requirement.py
+++ prowler/providers/github/services/repository/repository_code_changes_multi_approval_requirement/repository_code_changes_multi_approval_requirement.py
@@ -25,8 +25,6 @@ class repository_code_changes_multi_approval_requirement(Check):
             report = Check_Report_Github(
                 metadata=self.metadata(), resource_metadata=repo
             )
-            report.resource_id = repo.id
-            report.resource_name = repo.name
             report.status = "FAIL"
             report.status_extended = f"Repository {repo.name} does not enforce at least 2 approvals for code changes."
 
