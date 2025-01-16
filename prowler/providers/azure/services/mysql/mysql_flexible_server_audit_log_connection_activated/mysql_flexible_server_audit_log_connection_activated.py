71ae857d24986371b86099e7b5b64c9332347234
Rubén De la Torre Vico <ruben@prowler.com>
2025-01-16 13:17:50 +0100

    feat(mysql): use new automatic constructor for check report

--- prowler/providers/azure/services/mysql/mysql_flexible_server_audit_log_connection_activated/mysql_flexible_server_audit_log_connection_activated.py
+++ prowler/providers/azure/services/mysql/mysql_flexible_server_audit_log_connection_activated/mysql_flexible_server_audit_log_connection_activated.py
@@ -10,17 +10,13 @@ class mysql_flexible_server_audit_log_connection_activated(Check):
             subscription_name,
             servers,
         ) in mysql_client.flexible_servers.items():
-            for (
-                server_name,
-                server,
-            ) in servers.items():
-                report = Check_Report_Azure(self.metadata())
-                report.status = "FAIL"
+            for server in servers.values():
+                report = Check_Report_Azure(
+                    metadata=self.metadata(), resource_metadata=server
+                )
                 report.subscription = subscription_name
-                report.resource_name = server_name
-                report.resource_id = server_name
-                report.location = server.location
-                report.status_extended = f"Audit log is disabled for server {server_name} in subscription {subscription_name}."
+                report.status = "FAIL"
+                report.status_extended = f"Audit log is disabled for server {server.name} in subscription {subscription_name}."
 
                 if "audit_log_events" in server.configurations:
                     report.resource_id = server.configurations[
@@ -31,7 +27,7 @@ class mysql_flexible_server_audit_log_connection_activated(Check):
                         "audit_log_events"
                     ].value.split(","):
                         report.status = "PASS"
-                        report.status_extended = f"Audit log is enabled for server {server_name} in subscription {subscription_name}."
+                        report.status_extended = f"Audit log is enabled for server {server.name} in subscription {subscription_name}."
 
                 findings.append(report)
 
