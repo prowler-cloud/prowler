fd8361ae2c2a1900e772ce61a181970393399c44
Víctor Fernández Poyatos <victor@prowler.com>
2025-01-16 15:29:02 +0100

    feat(db): Update Django DB manager to use psycopg3 and connection pooling (#6541)
    # Conflicts:
    #	api/poetry.lock
    #	api/pyproject.toml

--- api/src/backend/config/django/production.py
+++ api/src/backend/config/django/production.py
@@ -1,7 +1,6 @@
 from config.django.base import *  # noqa
 from config.env import env
 
-
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
@@ -15,6 +14,14 @@ DATABASES = {
         "PASSWORD": env("POSTGRES_PASSWORD"),
         "HOST": env("POSTGRES_HOST"),
         "PORT": env("POSTGRES_PORT"),
+        "OPTIONS": {
+            "pool": {
+                "min_size": DB_CP_MIN_SIZE,  # noqa: F405
+                "max_size": DB_CP_MAX_SIZE,  # noqa: F405
+                "max_idle": DB_CP_MAX_IDLE,  # noqa: F405
+                "max_lifetime": DB_CP_MAX_LIFETIME,  # noqa: F405
+            }
+        },
     },
     "admin": {
         "ENGINE": "psqlextra.backend",
@@ -23,6 +30,14 @@ DATABASES = {
         "PASSWORD": env("POSTGRES_ADMIN_PASSWORD"),
         "HOST": env("POSTGRES_HOST"),
         "PORT": env("POSTGRES_PORT"),
+        "OPTIONS": {
+            "pool": {
+                "min_size": DB_CP_MIN_SIZE,  # noqa: F405
+                "max_size": DB_CP_MAX_SIZE,  # noqa: F405
+                "max_idle": DB_CP_MAX_IDLE,  # noqa: F405
+                "max_lifetime": DB_CP_MAX_LIFETIME,  # noqa: F405
+            }
+        },
     },
 }
 DATABASES["default"] = DATABASES["prowler_user"]
