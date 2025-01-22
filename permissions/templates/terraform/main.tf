c1ac5ad83d2f230ed32ad8d9ab3f6f90e36fa235
Pepe Fagoaga <pepe@prowler.com>
2025-01-22 12:08:19 +0100

    chore: add terraform validation

--- permissions/templates/terraform/main.tf
+++ permissions/templates/terraform/main.tf
@@ -3,12 +3,22 @@
 variable "external_id" {
   type        = string
   description = "This is the External ID that Prowler will use to assume the role ProwlerScan IAM Role."
+
+  validation {
+    condition     = length(var.external_id) > 0
+    error_message = "ExternalId must not be empty."
+  }
 }
 
 variable "account_id" {
   type        = string
   description = "AWS Account ID that will assume the role created, if you are deploying this template to be used in Prowler Cloud please do not edit this."
   default     = "232136659152"
+
+  validation {
+    condition     = length(var.account_id) == 12
+    error_message = "AccountId must be a valid AWS Account ID."
+  }
 }
 
 variable "iam_principal" {
