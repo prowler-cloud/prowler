ccdb54d7c361c3eb88651d75e15c1c2fce07e735
Mario Rodriguez Lopez <101330800+MarioRgzLpz@users.noreply.github.com>
2025-01-24 19:14:17 +0100

    feat(m365): add Microsoft 365 provider (#5902)
    Co-authored-by: Daniel Barranquero <danielbo2001@gmail.com>
    Co-authored-by: HugoPBrito <hugopbrit@gmail.com>
    Co-authored-by: MrCloudSec <hello@mistercloudsec.com>

---
+++ prowler/providers/microsoft365/services/admincenter/admincenter_groups_not_public_visibility/admincenter_groups_not_public_visibility.py
@@ -0,0 +1,25 @@
+from prowler.lib.check.models import Check, Check_Report_Microsoft365
+from prowler.providers.microsoft365.services.admincenter.admincenter_client import (
+    admincenter_client,
+)
+
+
+class admincenter_groups_not_public_visibility(Check):
+    def execute(self) -> Check_Report_Microsoft365:
+        findings = []
+        for group in admincenter_client.groups.values():
+            report = Check_Report_Microsoft365(metadata=self.metadata(), resource=group)
+            report.resource_id = group.id
+            report.resource_name = group.name
+            report.status = "FAIL"
+            report.status_extended = f"Group {group.name} has {group.visibility} visibility and should be Private."
+
+            if group.visibility != "Public":
+                report.status = "PASS"
+                report.status_extended = (
+                    f"Group {group.name} has {group.visibility} visibility."
+                )
+
+            findings.append(report)
+
+        return findings
---
+++ tests/providers/microsoft365/lib/regions/microsoft365_regions_test.py
@@ -0,0 +1,38 @@
+from azure.identity import AzureAuthorityHosts
+
+from prowler.providers.microsoft365.lib.regions.microsoft365_regions import (
+    MICROSOFT365_CHINA_CLOUD,
+    MICROSOFT365_GENERIC_CLOUD,
+    MICROSOFT365_US_GOV_CLOUD,
+    get_regions_config,
+)
+
+
+class Test_microsoft365_regions:
+    def test_get_regions_config(self):
+        allowed_regions = [
+            "Microsoft365Global",
+            "Microsoft365China",
+            "Microsoft365USGovernment",
+        ]
+        expected_output = {
+            "Microsoft365Global": {
+                "authority": None,
+                "base_url": MICROSOFT365_GENERIC_CLOUD,
+                "credential_scopes": [MICROSOFT365_GENERIC_CLOUD + "/.default"],
+            },
+            "Microsoft365China": {
+                "authority": AzureAuthorityHosts.AZURE_CHINA,
+                "base_url": MICROSOFT365_CHINA_CLOUD,
+                "credential_scopes": [MICROSOFT365_CHINA_CLOUD + "/.default"],
+            },
+            "Microsoft365USGovernment": {
+                "authority": AzureAuthorityHosts.AZURE_GOVERNMENT,
+                "base_url": MICROSOFT365_US_GOV_CLOUD,
+                "credential_scopes": [MICROSOFT365_US_GOV_CLOUD + "/.default"],
+            },
+        }
+
+        for region in allowed_regions:
+            region_config = get_regions_config(region)
+            assert region_config == expected_output[region]
