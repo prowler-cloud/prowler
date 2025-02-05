f330b67d21337d15c8e9ffc582b8137ac5e8aa32
MrCloudSec <hello@mistercloudsec.com>
2025-02-05 10:41:05 -0500

    chore: revision

--- prowler/providers/microsoft365/services/entra/entra_thirdparty_integrated_apps_not_allowed/entra_thirdparty_integrated_apps_not_allowed.py
+++ prowler/providers/microsoft365/services/entra/entra_thirdparty_integrated_apps_not_allowed/entra_thirdparty_integrated_apps_not_allowed.py
@@ -7,12 +7,8 @@ class entra_thirdparty_integrated_apps_not_allowed(Check):
         findings = []
 
         auth_policy = entra_client.authorization_policy
-        report = Check_Report_Microsoft365(self.metadata())
+        report = Check_Report_Microsoft365(self.metadata(), auth_policy)
         report.status = "FAIL"
-        report.tenant_id = entra_client.audited_tenant
-        report.tenant_domain = entra_client.audited_domain
-        report.resource_name = getattr(auth_policy, "name", "Authorization Policy")
-        report.resource_id = getattr(auth_policy, "id", "authorizationPolicy")
         report.status_extended = "App creation is not disabled for non-admin users."
 
         if getattr(auth_policy, "default_user_role_permissions", None) and not getattr(
