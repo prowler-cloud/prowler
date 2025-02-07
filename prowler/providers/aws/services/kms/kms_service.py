8cde08248c57c602f4476bb66672810ad301608a
MrCloudSec <hello@mistercloudsec.com>
2025-02-07 11:40:15 -0500

    chore: revision

--- prowler/providers/aws/services/kms/kms_service.py
+++ prowler/providers/aws/services/kms/kms_service.py
@@ -26,15 +26,20 @@ class KMS(AWSService):
             list_keys_paginator = regional_client.get_paginator("list_keys")
             for page in list_keys_paginator.paginate():
                 for key in page["Keys"]:
-                    if not self.audit_resources or (
-                        is_resource_filtered(key["KeyArn"], self.audit_resources)
-                    ):
-                        self.keys.append(
-                            Key(
-                                id=key["KeyId"],
-                                arn=key["KeyArn"],
-                                region=regional_client.region,
+                    try:
+                        if not self.audit_resources or (
+                            is_resource_filtered(key["KeyArn"], self.audit_resources)
+                        ):
+                            self.keys.append(
+                                Key(
+                                    id=key["KeyId"],
+                                    arn=key["KeyArn"],
+                                    region=regional_client.region,
+                                )
                             )
+                    except Exception as error:
+                        logger.error(
+                            f"{regional_client.region} -- {error.__class__.__name__}:{error.__traceback__.tb_lineno} -- {error}"
                         )
         except Exception as error:
             logger.error(
@@ -43,73 +48,93 @@ class KMS(AWSService):
 
     def _describe_key(self):
         logger.info("KMS - Describing Key...")
-        for key in self.keys:
-            regional_client = self.regional_clients[key.region]
-            try:
-                response = regional_client.describe_key(KeyId=key.id)
-                key.state = response["KeyMetadata"]["KeyState"]
-                key.origin = response["KeyMetadata"]["Origin"]
-                key.manager = response["KeyMetadata"]["KeyManager"]
-                key.spec = response["KeyMetadata"]["CustomerMasterKeySpec"]
-                key.multi_region = response["KeyMetadata"]["MultiRegion"]
-            except Exception as error:
-                logger.error(
-                    f"{regional_client.region} -- {error.__class__.__name__}:{error.__traceback__.tb_lineno} -- {error}"
-                )
+        try:
+            for key in self.keys:
+                regional_client = self.regional_clients[key.region]
+                try:
+                    response = regional_client.describe_key(KeyId=key.id)
+                    key.state = response["KeyMetadata"]["KeyState"]
+                    key.origin = response["KeyMetadata"]["Origin"]
+                    key.manager = response["KeyMetadata"]["KeyManager"]
+                    key.spec = response["KeyMetadata"]["CustomerMasterKeySpec"]
+                    key.multi_region = response["KeyMetadata"]["MultiRegion"]
+                except Exception as error:
+                    logger.error(
+                        f"{regional_client.region} -- {error.__class__.__name__}:{error.__traceback__.tb_lineno} -- {error}"
+                    )
+        except Exception as error:
+            logger.error(
+                f"{regional_client.region} -- {error.__class__.__name__}:{error.__traceback__.tb_lineno} -- {error}"
+            )
 
     def _get_key_rotation_status(self):
         logger.info("KMS - Get Key Rotation Status...")
-        for key in self.keys:
-            if (
-                key.origin
-                and key.manager
-                and "EXTERNAL" not in key.origin
-                and "AWS" not in key.manager
-            ):
-                regional_client = self.regional_clients[key.region]
-                try:
-                    key.rotation_enabled = regional_client.get_key_rotation_status(
-                        KeyId=key.id
-                    )["KeyRotationEnabled"]
-                except Exception as error:
-                    logger.error(
-                        f"{regional_client.region} -- {error.__class__.__name__}:{error.__traceback__.tb_lineno} -- {error}"
-                    )
+        try:
+            for key in self.keys:
+                if (
+                    key.origin
+                    and key.manager
+                    and "EXTERNAL" not in key.origin
+                    and "AWS" not in key.manager
+                ):
+                    regional_client = self.regional_clients[key.region]
+                    try:
+                        key.rotation_enabled = regional_client.get_key_rotation_status(
+                            KeyId=key.id
+                        )["KeyRotationEnabled"]
+                    except Exception as error:
+                        logger.error(
+                            f"{regional_client.region} -- {error.__class__.__name__}:{error.__traceback__.tb_lineno} -- {error}"
+                        )
+        except Exception as error:
+            logger.error(
+                f"{regional_client.region} -- {error.__class__.__name__}:{error.__traceback__.tb_lineno} -- {error}"
+            )
 
     def _get_key_policy(self):
         logger.info("KMS - Get Key Policy...")
-        for key in self.keys:
-            if (
-                key.manager and key.manager == "CUSTOMER"
-            ):  # only customer KMS have policies
-                regional_client = self.regional_clients[key.region]
-                try:
-                    key.policy = json.loads(
-                        regional_client.get_key_policy(
-                            KeyId=key.id, PolicyName="default"
-                        )["Policy"]
-                    )
-                except Exception as error:
-                    logger.error(
-                        f"{regional_client.region} -- {error.__class__.__name__}:{error.__traceback__.tb_lineno} -- {error}"
-                    )
+        try:
+            for key in self.keys:
+                if (
+                    key.manager and key.manager == "CUSTOMER"
+                ):  # only customer KMS have policies
+                    regional_client = self.regional_clients[key.region]
+                    try:
+                        key.policy = json.loads(
+                            regional_client.get_key_policy(
+                                KeyId=key.id, PolicyName="default"
+                            )["Policy"]
+                        )
+                    except Exception as error:
+                        logger.error(
+                            f"{regional_client.region} -- {error.__class__.__name__}:{error.__traceback__.tb_lineno} -- {error}"
+                        )
+        except Exception as error:
+            logger.error(
+                f"{regional_client.region} -- {error.__class__.__name__}:{error.__traceback__.tb_lineno} -- {error}"
+            )
 
     def _list_resource_tags(self):
         logger.info("KMS - List Tags...")
-        for key in self.keys:
-            if (
-                key.manager and key.manager == "CUSTOMER"
-            ):  # only check customer KMS keys
-                try:
-                    regional_client = self.regional_clients[key.region]
-                    response = regional_client.list_resource_tags(
-                        KeyId=key.id,
-                    )["Tags"]
-                    key.tags = response
-                except Exception as error:
-                    logger.error(
-                        f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
-                    )
+        try:
+            for key in self.keys:
+                if (
+                    key.manager and key.manager == "CUSTOMER"
+                ):  # only check customer KMS keys
+                    try:
+                        regional_client = self.regional_clients[key.region]
+                        response = regional_client.list_resource_tags(
+                            KeyId=key.id,
+                        )["Tags"]
+                        key.tags = response
+                    except Exception as error:
+                        logger.error(
+                            f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+                        )
+        except Exception as error:
+            logger.error(
+                f"{regional_client.region} -- {error.__class__.__name__}:{error.__traceback__.tb_lineno} -- {error}"
+            )
 
 
 class Key(BaseModel):
