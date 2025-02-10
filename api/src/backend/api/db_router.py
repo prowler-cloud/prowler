3ec2684616a0d4ff2e933e495b2c63ae6f34d476
Víctor Fernández Poyatos <victor@prowler.com>
2025-02-10 15:55:57 +0100

    ref(migrations): remove social login migrations in favour of db router rules

--- api/src/backend/api/db_router.py
+++ api/src/backend/api/db_router.py
@@ -1,20 +1,21 @@
+ALLOWED_APPS = ("django", "socialaccount", "account", "authtoken", "silk")
+
+
 class MainRouter:
     default_db = "default"
     admin_db = "admin"
 
     def db_for_read(self, model, **hints):  # noqa: F841
         model_table_name = model._meta.db_table
-        if model_table_name.startswith("django_") or model_table_name.startswith(
-            "silk_"
+        if model_table_name.startswith("django_") or any(
+            model_table_name.startswith(f"{app}_") for app in ALLOWED_APPS
         ):
             return self.admin_db
         return None
 
     def db_for_write(self, model, **hints):  # noqa: F841
         model_table_name = model._meta.db_table
-        if model_table_name.startswith("django_") or model_table_name.startswith(
-            "silk_"
-        ):
+        if any(model_table_name.startswith(f"{app}_") for app in ALLOWED_APPS):
             return self.admin_db
         return None
 
