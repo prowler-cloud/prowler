34ecf1c3752ee41f2856aafb5783869fffe3237f
Pepe Fagoaga <pepe@prowler.com>
2025-02-26 14:43:51 +0100

    fix(findings): change uid from varchar to text

--- api/src/backend/api/models.py
+++ api/src/backend/api/models.py
@@ -635,7 +635,7 @@ class Finding(PostgresPartitionedModel, RowLevelSecurityProtectedModel):
     updated_at = models.DateTimeField(auto_now=True, editable=False)
     first_seen_at = models.DateTimeField(editable=False, null=True)
 
-    uid = models.CharField(max_length=300)
+    uid = models.TextField(blank=True, null=True)
     delta = FindingDeltaEnumField(
         choices=DeltaChoices.choices,
         blank=True,
