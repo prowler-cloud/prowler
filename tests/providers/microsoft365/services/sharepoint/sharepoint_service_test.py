18895d5583e9366e02692b5bb77e515104fe5c02
Daniel Barranquero <danielbo2001@gmail.com>
2025-02-27 09:56:26 +0100

    chore(sharepoint): refactor service test

--- tests/providers/microsoft365/services/sharepoint/sharepoint_service_test.py
+++ tests/providers/microsoft365/services/sharepoint/sharepoint_service_test.py
@@ -2,7 +2,6 @@ from unittest.mock import patch
 
 from prowler.providers.microsoft365.models import Microsoft365IdentityInfo
 from prowler.providers.microsoft365.services.sharepoint.sharepoint_service import (
-    OneDriveSharedContent,
     SharePoint,
     SharePointSettings,
 )
@@ -13,34 +12,19 @@ from tests.providers.microsoft365.microsoft365_fixtures import (
 
 
 async def mock_sharepoint_get_settings(_):
-    return {
-        DOMAIN: SharePointSettings(
-            id=DOMAIN,
-            sharingCapability="ExternalUserAndGuestSharing",
-            sharingAllowedDomainList=["allowed-domain.com"],
-            sharingBlockedDomainList=["blocked-domain.com"],
-            sharingDomainRestrictionMode="allowList",
-            resharingEnabled=False,
-            modernAuthentication=True,
-        )
-    }
-
-
-async def mock_sharepoint_get_one_drive_shared_content(_):
-    return {
-        DOMAIN: OneDriveSharedContent(
-            totalSharedContent=42,
-        )
-    }
+    return SharePointSettings(
+        sharingCapability="ExternalUserAndGuestSharing",
+        sharingAllowedDomainList=["allowed-domain.com"],
+        sharingBlockedDomainList=["blocked-domain.com"],
+        sharingDomainRestrictionMode="allowList",
+        resharingEnabled=False,
+        modernAuthentication=True,
+    )
 
 
 @patch(
     "prowler.providers.microsoft365.services.sharepoint.sharepoint_service.SharePoint._get_settings",
     new=mock_sharepoint_get_settings,
-)
-@patch(
-    "prowler.providers.microsoft365.services.sharepoint.sharepoint_service.SharePoint._get_one_drive_shared_content",
-    new=mock_sharepoint_get_one_drive_shared_content,
 )
 class Test_SharePoint_Service:
     def test_get_client(self):
@@ -53,20 +37,10 @@ class Test_SharePoint_Service:
 
     def test_get_settings(self):
         sharepoint_client = SharePoint(set_mocked_microsoft365_provider())
-        assert DOMAIN in sharepoint_client.settings
-        settings = sharepoint_client.settings[DOMAIN]
-
-        assert settings.id == DOMAIN
+        settings = sharepoint_client.settings
         assert settings.sharingCapability == "ExternalUserAndGuestSharing"
         assert settings.sharingAllowedDomainList == ["allowed-domain.com"]
         assert settings.sharingBlockedDomainList == ["blocked-domain.com"]
         assert settings.sharingDomainRestrictionMode == "allowList"
         assert settings.resharingEnabled is False
         assert settings.modernAuthentication is True
-
-    def test_get_one_drive_shared_content(self):
-        sharepoint_client = SharePoint(set_mocked_microsoft365_provider())
-        assert DOMAIN in sharepoint_client.one_drive_shared_content
-        shared_content = sharepoint_client.one_drive_shared_content[DOMAIN]
-
-        assert shared_content.totalSharedContent == 42
