8146b0416df4252290978aa213c66ab6df5b5a4a
HugoPBrito <hugopbrit@gmail.com>
2025-03-03 13:29:33 +0100

    feat: add tests

--- tests/providers/microsoft365/services/entra/entra_admin_users_sign_in_frequency_enabled/entra_admin_users_sign_in_frequency_enabled_test.py
+++ tests/providers/microsoft365/services/entra/entra_admin_users_sign_in_frequency_enabled/entra_admin_users_sign_in_frequency_enabled_test.py
@@ -5,6 +5,7 @@ from prowler.providers.microsoft365.services.entra.entra_service import (
     ApplicationsConditions,
     ConditionalAccessPolicyState,
     Conditions,
+    GrantControls,
     PersistentBrowser,
     SessionControls,
     SignInFrequency,
@@ -90,6 +91,7 @@ class Test_entra_admin_users_sign_in_frequency_enabled:
                             excluded_roles=[],
                         ),
                     ),
+                    grant_controls=GrantControls(built_in_controls=[]),
                     session_controls=SessionControls(
                         persistent_browser=PersistentBrowser(
                             is_enabled=False, mode="always"
@@ -127,6 +129,7 @@ class Test_entra_admin_users_sign_in_frequency_enabled:
                             excluded_roles=[],
                         ),
                     ),
+                    grant_controls=GrantControls(built_in_controls=[]),
                     session_controls=SessionControls(
                         persistent_browser=PersistentBrowser(
                             is_enabled=False, mode="always"
@@ -199,6 +202,7 @@ class Test_entra_admin_users_sign_in_frequency_enabled:
                             excluded_roles=[],
                         ),
                     ),
+                    grant_controls=GrantControls(built_in_controls=[]),
                     session_controls=SessionControls(
                         persistent_browser=PersistentBrowser(
                             is_enabled=True, mode="never"
@@ -250,6 +254,7 @@ class Test_entra_admin_users_sign_in_frequency_enabled:
                             excluded_roles=[],
                         ),
                     ),
+                    grant_controls=GrantControls(built_in_controls=[]),
                     session_controls=SessionControls(
                         persistent_browser=PersistentBrowser(
                             is_enabled=True, mode="never"
