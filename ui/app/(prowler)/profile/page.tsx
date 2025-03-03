bdc759d34ce70446081643fdb7064daa61dfb19b
Pablo Lara <larabjj@gmail.com>
2025-03-03 12:30:28 +0100

    feat(sidebar): sidebar with new functionalities (#7018)

--- ui/app/(prowler)/profile/page.tsx
+++ ui/app/(prowler)/profile/page.tsx
@@ -1,17 +1,14 @@
-import { Spacer } from "@nextui-org/react";
 import React, { Suspense } from "react";
 
 import { getProfileInfo } from "@/actions/users/users";
-import { Header } from "@/components/ui";
+import { ContentLayout } from "@/components/ui";
 import { SkeletonUserInfo } from "@/components/users/profile";
 import { UserInfo } from "@/components/users/profile/user-info";
 import { UserProfileProps } from "@/types";
 
 export default async function Profile() {
   return (
-    <>
-      <Header title="User Profile" icon="ci:users" />
-      <Spacer y={4} />
+    <ContentLayout title="User Profile" icon="ci:users">
       <div className="min-h-screen">
         <div className="container mx-auto space-y-8 px-0 py-6">
           <div className="grid grid-cols-12 gap-6">
@@ -23,7 +20,7 @@ export default async function Profile() {
           </div>
         </div>
       </div>
-    </>
+    </ContentLayout>
   );
 }
 
