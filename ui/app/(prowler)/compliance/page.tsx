2314c7af3c57e2d8b0be7f67fd07a8aa39d1975f
Pablo Lara <larabjj@gmail.com>
2025-03-05 14:03:05 +0100

    chore: add search to the compliance page

--- ui/app/(prowler)/compliance/page.tsx
+++ ui/app/(prowler)/compliance/page.tsx
@@ -4,6 +4,7 @@ import { Spacer } from "@nextui-org/react";
 import { Suspense } from "react";
 
 import { getCompliancesOverview } from "@/actions/compliances";
+import { getMetadataInfo } from "@/actions/findings";
 import { getProvider } from "@/actions/providers";
 import { getScans } from "@/actions/scans";
 import {
@@ -12,7 +13,9 @@ import {
   NoScansAvailable,
 } from "@/components/compliance";
 import { DataCompliance } from "@/components/compliance/data-compliance";
+import { FilterControls } from "@/components/filters";
 import { ContentLayout } from "@/components/ui";
+import { DataTableFilterCustom } from "@/components/ui/table/data-table-filter-custom";
 import { ComplianceOverviewData, ScanProps, SearchParamsProps } from "@/types";
 
 export default async function Compliance({
@@ -22,19 +25,19 @@ export default async function Compliance({
 }) {
   const searchParamsKey = JSON.stringify(searchParams || {});
 
-  const scansData = await getScans({});
+  const scansData = await getScans({
+    filters: {
+      "filter[state]": "completed",
+    },
+  });
 
   if (!scansData?.data) {
     return <NoScansAvailable />;
   }
 
-  const completedScans = scansData.data.filter(
-    (scan: ScanProps) => scan.attributes.state === "completed",
-  );
-
   // Expand scans with provider information
   const expandedScansData = await Promise.all(
-    completedScans.map(async (scan: ScanProps) => {
+    scansData.data.map(async (scan: ScanProps) => {
       const providerId = scan.relationships?.provider?.data?.id;
 
       if (!providerId) {
@@ -62,11 +65,32 @@ export default async function Compliance({
   const selectedScanId =
     searchParams.scanId || expandedScansData[0]?.id || null;
 
+  const metadataInfoData = await getMetadataInfo({
+    filters: {
+      "filter[scan]": selectedScanId,
+    },
+  });
+
+  const uniqueRegions = metadataInfoData?.data?.attributes?.regions || [];
+
   return (
     <ContentLayout title="Compliance" icon="fluent-mdl2:compliance-audit">
       {selectedScanId ? (
         <>
+          <FilterControls search />
+          <Spacer y={8} />
           <DataCompliance scans={expandedScansData} />
+          <Spacer y={8} />
+          <DataTableFilterCustom
+            filters={[
+              {
+                key: "region__in",
+                labelCheckboxGroup: "Regions",
+                values: uniqueRegions,
+              },
+            ]}
+            defaultOpen={true}
+          />
           <Spacer y={12} />
           <Suspense key={searchParamsKey} fallback={<ComplianceSkeletonGrid />}>
             <SSRComplianceGrid searchParams={searchParams} />
@@ -87,9 +111,18 @@ const SSRComplianceGrid = async ({
   const scanId = searchParams.scanId?.toString() || "";
   const regionFilter = searchParams["filter[region__in]"]?.toString() || "";
 
+  // Extract all filter parameters
+  const filters = Object.fromEntries(
+    Object.entries(searchParams).filter(([key]) => key.startsWith("filter[")),
+  );
+
+  // Extract query from filters
+  const query = (filters["filter[search]"] as string) || "";
+
   const compliancesData = await getCompliancesOverview({
     scanId,
     region: regionFilter,
+    query,
   });
 
   // Check if the response contains no data
--- ui/components/compliance/data-compliance/data-compliance.tsx
+++ ui/components/compliance/data-compliance/data-compliance.tsx
@@ -1,21 +1,17 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import { SelectScanComplianceData } from "@/components/compliance/data-compliance";
-import { CrossIcon } from "@/components/icons";
-import { CustomButton, CustomDropdownFilter } from "@/components/ui/custom";
 import { SelectScanComplianceDataProps } from "@/types";
 interface DataComplianceProps {
   scans: SelectScanComplianceDataProps["scans"];
-  regions?: string[];
 }
 
-export const DataCompliance = ({ scans, regions }: DataComplianceProps) => {
+export const DataCompliance = ({ scans }: DataComplianceProps) => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const [showClearButton, setShowClearButton] = useState(false);
 
   const scanIdParam = searchParams.get("scanId");
 
@@ -29,44 +25,11 @@ export const DataCompliance = ({ scans, regions }: DataComplianceProps) => {
     }
   }, [scans, scanIdParam, searchParams, router]);
 
-  useEffect(() => {
-    const hasFilters = Array.from(searchParams.keys()).some(
-      (key) => key.startsWith("filter[") || key === "sort",
-    );
-    setShowClearButton(hasFilters);
-  }, [searchParams]);
-
   const handleScanChange = (selectedKey: string) => {
     const params = new URLSearchParams(searchParams);
     params.set("scanId", selectedKey);
     router.push(`?${params.toString()}`);
   };
-
-  const pushDropdownFilter = useCallback(
-    (key: string, values: string[]) => {
-      const params = new URLSearchParams(searchParams);
-      const filterKey = `filter[${key}]`;
-
-      if (values.length === 0) {
-        params.delete(filterKey);
-      } else {
-        params.set(filterKey, values.join(","));
-      }
-
-      router.push(`?${params.toString()}`);
-    },
-    [router, searchParams],
-  );
-
-  const clearAllFilters = useCallback(() => {
-    const params = new URLSearchParams(searchParams.toString());
-    Array.from(params.keys()).forEach((key) => {
-      if (key.startsWith("filter[") || key === "sort") {
-        params.delete(key);
-      }
-    });
-    router.push(`?${params.toString()}`, { scroll: false });
-  }, [router, searchParams]);
 
   return (
     <div className="flex flex-col gap-4">
@@ -76,27 +39,6 @@ export const DataCompliance = ({ scans, regions }: DataComplianceProps) => {
           selectedScanId={selectedScanId}
           onSelectionChange={handleScanChange}
         />
-        <CustomDropdownFilter
-          filter={{
-            key: "region__in",
-            values: regions || [],
-            labelCheckboxGroup: "Regions",
-          }}
-          onFilterChange={pushDropdownFilter}
-        />
-        {showClearButton && (
-          <CustomButton
-            ariaLabel="Reset"
-            className="w-full md:w-fit"
-            onPress={clearAllFilters}
-            variant="dashed"
-            size="sm"
-            endContent={<CrossIcon size={24} />}
-            radius="sm"
-          >
-            Reset
-          </CustomButton>
-        )}
       </div>
     </div>
   );
