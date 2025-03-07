2a95763b8fa923af9dc6ed4c812ef90da10f1d1c
Alejandro Ferrándiz Polo <afp91@alu.ua.es>
2025-03-07 13:53:28 +0100

    algunos cambios hechos

--- prowler/providers/ionos/lib/arguments/arguments.py
+++ prowler/providers/ionos/lib/arguments/arguments.py
@@ -12,26 +12,26 @@ def init_parser(self):
 
     ionos_parser.add_argument(
         "--ionos-username",
-        required=True,
+        required=False,
         help="The username for IONOS Cloud authentication."
     )
     
     ionos_parser.add_argument(
         "--ionos-password",
-        required=True,
+        required=False,
         help="The password for IONOS Cloud authentication."
     )
     
-    ionos_parser.add_argument(
-        "--config-file",
-        required=False,
-        default=None,
-        help="Path to the configuration file for IONOS Cloud auditing."
-    )
+    #ionos_parser.add_argument(
+    #    "--config-file",
+    #    required=False,
+    #    default=None,
+    #    help="Path to the configuration file for IONOS Cloud auditing."
+    #)
     
-    ionos_parser.add_argument(
-        "--mutelist-file",
-        required=False,
-        default=None,
-        help="Path to the mutelist file to ignore specific checks."
-    )
+    #ionos_parser.add_argument(
+    #    "--mutelist-file",
+    #    required=False,
+    #    default=None,
+    #    help="Path to the mutelist file to ignore specific checks."
+    #)
