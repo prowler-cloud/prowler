eb5d4a525736871a88e030e5917848a6b7d773a4
HugoPBrito <hugopbrit@gmail.com>
2025-03-10 11:26:47 +0100

    fix: validation error

--- tests/providers/microsoft365/services/entra/entra_admin_portals_role_limited_access/entra_admin_portals_role_limited_access_test.py
+++ tests/providers/microsoft365/services/entra/entra_admin_portals_role_limited_access/entra_admin_portals_role_limited_access_test.py
@@ -10,6 +10,7 @@ from prowler.providers.microsoft365.services.entra.entra_service import (
     PersistentBrowser,
     SessionControls,
     SignInFrequency,
+    SignInFrequencyInterval,
     UsersConditions,
 )
 from tests.providers.microsoft365.microsoft365_fixtures import (
@@ -98,7 +99,10 @@ class Test_entra_admin_portals_role_limited_access:
                             is_enabled=False, mode="always"
                         ),
                         sign_in_frequency=SignInFrequency(
-                            is_enabled=False, frequency=None
+                            is_enabled=False,
+                            frequency=None,
+                            type=None,
+                            interval=SignInFrequencyInterval.EVERY_TIME,
                         ),
                     ),
                     state=ConditionalAccessPolicyState.DISABLED,
@@ -168,7 +172,10 @@ class Test_entra_admin_portals_role_limited_access:
                             is_enabled=False, mode="always"
                         ),
                         sign_in_frequency=SignInFrequency(
-                            is_enabled=False, frequency=None
+                            is_enabled=False,
+                            frequency=None,
+                            type=None,
+                            interval=SignInFrequencyInterval.EVERY_TIME,
                         ),
                     ),
                     state=ConditionalAccessPolicyState.ENABLED_FOR_REPORTING,
@@ -209,7 +216,10 @@ class Test_entra_admin_portals_role_limited_access:
                             is_enabled=False, mode="always"
                         ),
                         sign_in_frequency=SignInFrequency(
-                            is_enabled=False, frequency=None
+                            is_enabled=False,
+                            frequency=None,
+                            type=None,
+                            interval=SignInFrequencyInterval.EVERY_TIME,
                         ),
                     ),
                     state=ConditionalAccessPolicyState.ENABLED_FOR_REPORTING,
