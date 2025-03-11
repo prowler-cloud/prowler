0b87b63cdd3481e273dfb00a2e89a94f739c959a
Hugo Pereira Brito <101209179+HugoPBrito@users.noreply.github.com>
2025-03-11 11:30:37 +0100

    refactor(microsoft365): resource metadata assertions (#7169)

--- tests/providers/microsoft365/services/sharepoint/sharepoint_guest_sharing_restricted/sharepoint_guest_sharing_restricted_test.py
+++ tests/providers/microsoft365/services/sharepoint/sharepoint_guest_sharing_restricted/sharepoint_guest_sharing_restricted_test.py
@@ -52,14 +52,7 @@ class Test_sharepoint_guest_sharing_restricted:
             assert result[0].resource_id == DOMAIN
             assert result[0].location == "global"
             assert result[0].resource_name == "SharePoint Settings"
-            assert result[0].resource == {
-                "sharingCapability": "ExternalUserSharingOnly",
-                "sharingAllowedDomainList": ["allowed-domain.com"],
-                "sharingBlockedDomainList": ["blocked-domain.com"],
-                "sharingDomainRestrictionMode": "allowList",
-                "resharingEnabled": False,
-                "legacyAuth": True,
-            }
+            assert result[0].resource == sharepoint_client.settings.dict()
 
     def test_guest_sharing_not_restricted(self):
         """
@@ -103,14 +96,7 @@ class Test_sharepoint_guest_sharing_restricted:
             assert result[0].resource_id == DOMAIN
             assert result[0].location == "global"
             assert result[0].resource_name == "SharePoint Settings"
-            assert result[0].resource == {
-                "sharingCapability": "ExternalUserSharingOnly",
-                "sharingAllowedDomainList": ["allowed-domain.com"],
-                "sharingBlockedDomainList": ["blocked-domain.com"],
-                "sharingDomainRestrictionMode": "allowList",
-                "resharingEnabled": True,
-                "legacyAuth": True,
-            }
+            assert result[0].resource == sharepoint_client.settings.dict()
 
     def test_empty_settings(self):
         """
