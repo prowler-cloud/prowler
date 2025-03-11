a77d6ebaa2efe4c593989cf4c7172f0db963989a
Hugo Pereira Brito <101209179+HugoPBrito@users.noreply.github.com>
2025-03-11 19:18:33 +0100

    feat(microsoft365): add new check `entra_admin_users_sign_in_frequency_enabled` (#7020)
    Co-authored-by: MrCloudSec <hello@mistercloudsec.com>

--- tests/providers/microsoft365/services/entra/microsoft365_entra_service_test.py
+++ tests/providers/microsoft365/services/entra/microsoft365_entra_service_test.py
@@ -3,9 +3,21 @@ from unittest.mock import patch
 from prowler.providers.microsoft365.models import Microsoft365IdentityInfo
 from prowler.providers.microsoft365.services.entra.entra_service import (
     AdminConsentPolicy,
+    ApplicationsConditions,
     AuthorizationPolicy,
+    ConditionalAccessGrantControl,
+    ConditionalAccessPolicy,
+    ConditionalAccessPolicyState,
+    Conditions,
     DefaultUserRolePermissions,
     Entra,
+    GrantControls,
+    PersistentBrowser,
+    SessionControls,
+    SignInFrequency,
+    SignInFrequencyInterval,
+    SignInFrequencyType,
+    UsersConditions,
 )
 from tests.providers.microsoft365.microsoft365_fixtures import (
     DOMAIN,
@@ -26,6 +38,45 @@ async def mock_entra_get_authorization_policy(_):
             allowed_to_read_other_users=True,
         ),
     )
+
+
+async def mock_entra_get_conditional_access_policies(_):
+    return {
+        "id-1": ConditionalAccessPolicy(
+            id="id-1",
+            display_name="Name 1",
+            conditions=Conditions(
+                application_conditions=ApplicationsConditions(
+                    included_applications=["app-1", "app-2"],
+                    excluded_applications=["app-3", "app-4"],
+                ),
+                user_conditions=UsersConditions(
+                    included_groups=["group-1", "group-2"],
+                    excluded_groups=["group-3", "group-4"],
+                    included_users=["user-1", "user-2"],
+                    excluded_users=["user-3", "user-4"],
+                    included_roles=["role-1", "role-2"],
+                    excluded_roles=["role-3", "role-4"],
+                ),
+            ),
+            grant_controls=GrantControls(
+                built_in_controls=[ConditionalAccessGrantControl.BLOCK]
+            ),
+            session_controls=SessionControls(
+                persistent_browser=PersistentBrowser(
+                    is_enabled=True,
+                    mode="always",
+                ),
+                sign_in_frequency=SignInFrequency(
+                    is_enabled=True,
+                    frequency=24,
+                    type=SignInFrequencyType.HOURS,
+                    interval=SignInFrequencyInterval.TIME_BASED,
+                ),
+            ),
+            state=ConditionalAccessPolicyState.ENABLED_FOR_REPORTING,
+        )
+    }
 
 
 async def mock_entra_get_groups(_):
@@ -81,6 +132,49 @@ class Test_Entra_Service:
                 allowed_to_read_other_users=True,
             )
         )
+
+    @patch(
+        "prowler.providers.microsoft365.services.entra.entra_service.Entra._get_conditional_access_policies",
+        new=mock_entra_get_conditional_access_policies,
+    )
+    def test_get_conditional_access_policies(self):
+        entra_client = Entra(set_mocked_microsoft365_provider())
+        assert entra_client.conditional_access_policies == {
+            "id-1": ConditionalAccessPolicy(
+                id="id-1",
+                display_name="Name 1",
+                conditions=Conditions(
+                    application_conditions=ApplicationsConditions(
+                        included_applications=["app-1", "app-2"],
+                        excluded_applications=["app-3", "app-4"],
+                    ),
+                    user_conditions=UsersConditions(
+                        included_groups=["group-1", "group-2"],
+                        excluded_groups=["group-3", "group-4"],
+                        included_users=["user-1", "user-2"],
+                        excluded_users=["user-3", "user-4"],
+                        included_roles=["role-1", "role-2"],
+                        excluded_roles=["role-3", "role-4"],
+                    ),
+                ),
+                grant_controls=GrantControls(
+                    built_in_controls=[ConditionalAccessGrantControl.BLOCK]
+                ),
+                session_controls=SessionControls(
+                    persistent_browser=PersistentBrowser(
+                        is_enabled=True,
+                        mode="always",
+                    ),
+                    sign_in_frequency=SignInFrequency(
+                        is_enabled=True,
+                        frequency=24,
+                        type=SignInFrequencyType.HOURS,
+                        interval=SignInFrequencyInterval.TIME_BASED,
+                    ),
+                ),
+                state=ConditionalAccessPolicyState.ENABLED_FOR_REPORTING,
+            )
+        }
 
     @patch(
         "prowler.providers.microsoft365.services.entra.entra_service.Entra._get_groups",
