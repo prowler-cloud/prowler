099813b2e5a22bdad75aa4d649bb0aadb42216a9
HugoPBrito <hugopbrit@gmail.com>
2025-03-11 13:51:47 +0100

    fix: validation error

--- tests/providers/microsoft365/services/entra/entra_admin_users_sign_in_frequency_enabled/entra_admin_users_sign_in_frequency_enabled_test.py
+++ tests/providers/microsoft365/services/entra/entra_admin_users_sign_in_frequency_enabled/entra_admin_users_sign_in_frequency_enabled_test.py
@@ -366,6 +366,7 @@ class Test_entra_admin_users_sign_in_frequency_enabled:
                             excluded_roles=[],
                         ),
                     ),
+                    grant_controls=GrantControls(built_in_controls=[]),
                     session_controls=SessionControls(
                         persistent_browser=PersistentBrowser(
                             is_enabled=True, mode="never"
