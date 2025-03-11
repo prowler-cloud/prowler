c88ae32b7fd0c7977c1515ed146e5fe2f0e83161
Hugo Pereira Brito <101209179+HugoPBrito@users.noreply.github.com>
2025-03-11 19:18:33 +0100

    feat(microsoft365): add new check `entra_admin_users_sign_in_frequency_enabled` (#7020)
    Co-authored-by: MrCloudSec <hello@mistercloudsec.com>

--- prowler/providers/microsoft365/services/entra/entra_service.py
+++ prowler/providers/microsoft365/services/entra/entra_service.py
@@ -1,4 +1,5 @@
 from asyncio import gather, get_event_loop
+from enum import Enum
 from typing import List, Optional
 
 from pydantic import BaseModel
@@ -17,18 +18,19 @@ class Entra(Microsoft365Service):
         attributes = loop.run_until_complete(
             gather(
                 self._get_authorization_policy(),
-                self._get_groups(),
+                self._get_conditional_access_policies(),
                 self._get_admin_consent_policy(),
+                self._get_groups(),
             )
         )
 
         self.authorization_policy = attributes[0]
-        self.groups = attributes[1]
+        self.conditional_access_policies = attributes[1]
         self.admin_consent_policy = attributes[2]
+        self.groups = attributes[3]
 
     async def _get_authorization_policy(self):
         logger.info("Entra - Getting authorization policy...")
-
         authorization_policy = None
         try:
             auth_policy = await self.client.policies.authorization_policy.get()
@@ -84,8 +86,174 @@ class Entra(Microsoft365Service):
             logger.error(
                 f"{error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
             )
-
         return authorization_policy
+
+    async def _get_conditional_access_policies(self):
+        logger.info("Entra - Getting conditional access policies...")
+        conditional_access_policies = {}
+        try:
+            conditional_access_policies_list = (
+                await self.client.identity.conditional_access.policies.get()
+            )
+            for policy in conditional_access_policies_list.value:
+                conditional_access_policies[policy.id] = ConditionalAccessPolicy(
+                    id=policy.id,
+                    display_name=policy.display_name,
+                    conditions=Conditions(
+                        application_conditions=ApplicationsConditions(
+                            included_applications=[
+                                application
+                                for application in getattr(
+                                    policy.conditions.applications,
+                                    "include_applications",
+                                    [],
+                                )
+                            ],
+                            excluded_applications=[
+                                application
+                                for application in getattr(
+                                    policy.conditions.applications,
+                                    "exclude_applications",
+                                    [],
+                                )
+                            ],
+                        ),
+                        user_conditions=UsersConditions(
+                            included_groups=[
+                                group
+                                for group in getattr(
+                                    policy.conditions.users,
+                                    "include_groups",
+                                    [],
+                                )
+                            ],
+                            excluded_groups=[
+                                group
+                                for group in getattr(
+                                    policy.conditions.users,
+                                    "exclude_groups",
+                                    [],
+                                )
+                            ],
+                            included_users=[
+                                user
+                                for user in getattr(
+                                    policy.conditions.users,
+                                    "include_users",
+                                    [],
+                                )
+                            ],
+                            excluded_users=[
+                                user
+                                for user in getattr(
+                                    policy.conditions.users,
+                                    "exclude_users",
+                                    [],
+                                )
+                            ],
+                            included_roles=[
+                                role
+                                for role in getattr(
+                                    policy.conditions.users,
+                                    "include_roles",
+                                    [],
+                                )
+                            ],
+                            excluded_roles=[
+                                role
+                                for role in getattr(
+                                    policy.conditions.users,
+                                    "exclude_roles",
+                                    [],
+                                )
+                            ],
+                        ),
+                    ),
+                    grant_controls=GrantControls(
+                        built_in_controls=(
+                            [
+                                ConditionalAccessGrantControl(control.value)
+                                for control in getattr(
+                                    policy.grant_controls, "built_in_controls", {}
+                                )
+                            ]
+                            if policy.grant_controls
+                            else []
+                        )
+                    ),
+                    session_controls=SessionControls(
+                        persistent_browser=PersistentBrowser(
+                            is_enabled=(
+                                policy.session_controls.persistent_browser.is_enabled
+                                if policy.session_controls
+                                and policy.session_controls.persistent_browser
+                                else False
+                            ),
+                            mode=(
+                                policy.session_controls.persistent_browser.mode
+                                if policy.session_controls
+                                and policy.session_controls.persistent_browser
+                                else "always"
+                            ),
+                        ),
+                        sign_in_frequency=SignInFrequency(
+                            is_enabled=(
+                                policy.session_controls.sign_in_frequency.is_enabled
+                                if policy.session_controls
+                                and policy.session_controls.sign_in_frequency
+                                else False
+                            ),
+                            frequency=(
+                                policy.session_controls.sign_in_frequency.value
+                                if policy.session_controls
+                                and policy.session_controls.sign_in_frequency
+                                else None
+                            ),
+                            type=(
+                                SignInFrequencyType(
+                                    policy.session_controls.sign_in_frequency.type
+                                )
+                                if policy.session_controls
+                                and policy.session_controls.sign_in_frequency
+                                and policy.session_controls.sign_in_frequency.type
+                                else None
+                            ),
+                            interval=(
+                                SignInFrequencyInterval(
+                                    policy.session_controls.sign_in_frequency.frequency_interval
+                                )
+                                if policy.session_controls
+                                and policy.session_controls.sign_in_frequency
+                                else None
+                            ),
+                        ),
+                    ),
+                    state=ConditionalAccessPolicyState(
+                        getattr(policy, "state", "disabled")
+                    ),
+                )
+        except Exception as error:
+            logger.error(
+                f"{error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+            )
+        return conditional_access_policies
+
+    async def _get_admin_consent_policy(self):
+        logger.info("Entra - Getting group settings...")
+        admin_consent_policy = None
+        try:
+            policy = await self.client.policies.admin_consent_request_policy.get()
+            admin_consent_policy = AdminConsentPolicy(
+                admin_consent_enabled=policy.is_enabled,
+                notify_reviewers=policy.notify_reviewers,
+                email_reminders_to_reviewers=policy.reminders_enabled,
+                duration_in_days=policy.request_duration_in_days,
+            )
+        except Exception as error:
+            logger.error(
+                f"{error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+            )
+        return admin_consent_policy
 
     async def _get_groups(self):
         logger.info("Entra - Getting groups...")
@@ -107,23 +275,75 @@ class Entra(Microsoft365Service):
             )
         return groups
 
-    async def _get_admin_consent_policy(self):
-        logger.info("Entra - Getting group settings...")
-        admin_consent_policy = None
-        try:
-            policy = await self.client.policies.admin_consent_request_policy.get()
-            admin_consent_policy = AdminConsentPolicy(
-                admin_consent_enabled=policy.is_enabled,
-                notify_reviewers=policy.notify_reviewers,
-                email_reminders_to_reviewers=policy.reminders_enabled,
-                duration_in_days=policy.request_duration_in_days,
-            )
-        except Exception as error:
-            logger.error(
-                f"{error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
-            )
 
-        return admin_consent_policy
+class ConditionalAccessPolicyState(Enum):
+    ENABLED = "enabled"
+    DISABLED = "disabled"
+    ENABLED_FOR_REPORTING = "enabledForReportingButNotEnforced"
+
+
+class ApplicationsConditions(BaseModel):
+    included_applications: List[str]
+    excluded_applications: List[str]
+
+
+class UsersConditions(BaseModel):
+    included_groups: List[str]
+    excluded_groups: List[str]
+    included_users: List[str]
+    excluded_users: List[str]
+    included_roles: List[str]
+    excluded_roles: List[str]
+
+
+class Conditions(BaseModel):
+    application_conditions: Optional[ApplicationsConditions]
+    user_conditions: Optional[UsersConditions]
+
+
+class PersistentBrowser(BaseModel):
+    is_enabled: bool
+    mode: str
+
+
+class SignInFrequencyInterval(Enum):
+    TIME_BASED = "timeBased"
+    EVERY_TIME = "everyTime"
+
+
+class SignInFrequencyType(Enum):
+    HOURS = "hours"
+    DAYS = "days"
+
+
+class SignInFrequency(BaseModel):
+    is_enabled: bool
+    frequency: Optional[int]
+    type: Optional[SignInFrequencyType]
+    interval: Optional[SignInFrequencyInterval]
+
+
+class SessionControls(BaseModel):
+    persistent_browser: PersistentBrowser
+    sign_in_frequency: SignInFrequency
+
+
+class ConditionalAccessGrantControl(Enum):
+    MFA = "mfa"
+    BLOCK = "block"
+
+
+class GrantControls(BaseModel):
+    built_in_controls: List[ConditionalAccessGrantControl]
+
+
+class ConditionalAccessPolicy(BaseModel):
+    id: str
+    display_name: str
+    conditions: Conditions
+    session_controls: SessionControls
+    grant_controls: GrantControls
+    state: ConditionalAccessPolicyState
 
 
 class DefaultUserRolePermissions(BaseModel):
@@ -155,3 +375,21 @@ class AdminConsentPolicy(BaseModel):
     notify_reviewers: bool
     email_reminders_to_reviewers: bool
     duration_in_days: int
+
+
+class AdminRoles(Enum):
+    APPLICATION_ADMINISTRATOR = "9b895d92-2cd3-44c7-9d02-a6ac2d5ea5c3"
+    AUTHENTICATION_ADMINISTRATOR = "c4e39bd9-1100-46d3-8c65-fb160da0071f"
+    BILLING_ADMINISTRATOR = "b0f54661-2d74-4c50-afa3-1ec803f12efe"
+    CLOUD_APPLICATION_ADMINISTRATOR = "158c047a-c907-4556-b7ef-446551a6b5f7"
+    CONDITIONAL_ACCESS_ADMINISTRATOR = "b1be1c3e-b65d-4f19-8427-f6fa0d97feb9"
+    EXCHANGE_ADMINISTRATOR = "29232cdf-9323-42fd-ade2-1d097af3e4de"
+    GLOBAL_ADMINISTRATOR = "62e90394-69f5-4237-9190-012177145e10"
+    GLOBAL_READER = "f2ef992c-3afb-46b9-b7cf-a126ee74c451"
+    HELPDESK_ADMINISTRATOR = "729827e3-9c14-49f7-bb1b-9608f156bbb8"
+    PASSWORD_ADMINISTRATOR = "966707d0-3269-4727-9be2-8c3a10f19b9d"
+    PRIVILEGED_AUTHENTICATION_ADMINISTRATOR = "7be44c8a-adaf-4e2a-84d6-ab2649e08a13"
+    PRIVILEGED_ROLE_ADMINISTRATOR = "e8611ab8-c189-46e8-94e1-60213ab1f814"
+    SECURITY_ADMINISTRATOR = "194ae4cb-b126-40b2-bd5b-6091b380977d"
+    SHAREPOINT_ADMINISTRATOR = "f28a1f50-f6e7-4571-818b-6a12f2af6b6c"
+    USER_ADMINISTRATOR = "fe930be7-5e62-47db-91af-98c3a49a38b1"
