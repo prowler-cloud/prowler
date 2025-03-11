bc60b7e30b6ee104781240658079b694fbadff9d
Daniel Barranquero <danielbo2001@gmail.com>
2025-03-11 12:54:42 +0100

    chore(entra): change status for reporting policies

--- prowler/providers/microsoft365/services/entra/entra_admin_mfa_enabled_for_administrative_roles/entra_admin_mfa_enabled_for_administrative_roles.py
+++ prowler/providers/microsoft365/services/entra/entra_admin_mfa_enabled_for_administrative_roles/entra_admin_mfa_enabled_for_administrative_roles.py
@@ -73,6 +73,9 @@ class entra_admin_mfa_enabled_for_administrative_roles(Check):
                 )
                 report.status = "PASS"
                 report.status_extended = f"Conditional Access Policy '{policy.display_name}' requires MFA for administrative roles."
+                if policy.state == ConditionalAccessPolicyState.ENABLED_FOR_REPORTING:
+                    report.status = "FAIL"
+                    report.status_extended += " (Enabled for reporting, change to enabled so the policy is enforced)."
                 break
 
         findings.append(report)
