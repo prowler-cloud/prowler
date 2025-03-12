0e43504b31721335fdf0cf946a6b7e176539c462
Sergio Garcia <hello@mistercloudsec.com>
2025-03-12 14:44:36 +0100

    fix(cloudwatch): handle None metric alarms (#7205)
    (cherry picked from commit 6564ec1ff57f9da3dbcc093d02aa7da0f212d38e)
    
    # Conflicts:
    #	prowler/providers/aws/services/cloudwatch/cloudwatch_alarm_actions_alarm_state_configured/cloudwatch_alarm_actions_alarm_state_configured.py
    #	prowler/providers/aws/services/cloudwatch/cloudwatch_alarm_actions_enabled/cloudwatch_alarm_actions_enabled.py

--- prowler/providers/aws/services/cloudwatch/cloudwatch_alarm_actions_alarm_state_configured/cloudwatch_alarm_actions_alarm_state_configured.py
+++ prowler/providers/aws/services/cloudwatch/cloudwatch_alarm_actions_alarm_state_configured/cloudwatch_alarm_actions_alarm_state_configured.py
@@ -7,6 +7,7 @@ from prowler.providers.aws.services.cloudwatch.cloudwatch_client import (
 class cloudwatch_alarm_actions_alarm_state_configured(Check):
     def execute(self):
         findings = []
+<<<<<<< HEAD
         for metric_alarm in cloudwatch_client.metric_alarms:
             report = Check_Report_AWS(self.metadata())
             report.region = metric_alarm.region
@@ -19,4 +20,17 @@ class cloudwatch_alarm_actions_alarm_state_configured(Check):
                 report.status = "FAIL"
                 report.status_extended = f"CloudWatch metric alarm {metric_alarm.name} does not have actions configured for the ALARM state."
             findings.append(report)
+=======
+        if cloudwatch_client.metric_alarms is not None:
+            for metric_alarm in cloudwatch_client.metric_alarms:
+                report = Check_Report_AWS(
+                    metadata=self.metadata(), resource=metric_alarm
+                )
+                report.status = "PASS"
+                report.status_extended = f"CloudWatch metric alarm {metric_alarm.name} has actions configured for the ALARM state."
+                if not metric_alarm.alarm_actions:
+                    report.status = "FAIL"
+                    report.status_extended = f"CloudWatch metric alarm {metric_alarm.name} does not have actions configured for the ALARM state."
+                findings.append(report)
+>>>>>>> 6564ec1ff (fix(cloudwatch): handle None metric alarms (#7205))
         return findings
--- prowler/providers/aws/services/cloudwatch/cloudwatch_alarm_actions_enabled/cloudwatch_alarm_actions_enabled.py
+++ prowler/providers/aws/services/cloudwatch/cloudwatch_alarm_actions_enabled/cloudwatch_alarm_actions_enabled.py
@@ -7,6 +7,7 @@ from prowler.providers.aws.services.cloudwatch.cloudwatch_client import (
 class cloudwatch_alarm_actions_enabled(Check):
     def execute(self):
         findings = []
+<<<<<<< HEAD
         for metric_alarm in cloudwatch_client.metric_alarms:
             report = Check_Report_AWS(self.metadata())
             report.region = metric_alarm.region
@@ -21,4 +22,19 @@ class cloudwatch_alarm_actions_enabled(Check):
                 report.status = "FAIL"
                 report.status_extended = f"CloudWatch metric alarm {metric_alarm.name} does not have actions enabled."
             findings.append(report)
+=======
+        if cloudwatch_client.metric_alarms is not None:
+            for metric_alarm in cloudwatch_client.metric_alarms:
+                report = Check_Report_AWS(
+                    metadata=self.metadata(), resource=metric_alarm
+                )
+                report.status = "PASS"
+                report.status_extended = (
+                    f"CloudWatch metric alarm {metric_alarm.name} has actions enabled."
+                )
+                if not metric_alarm.actions_enabled:
+                    report.status = "FAIL"
+                    report.status_extended = f"CloudWatch metric alarm {metric_alarm.name} does not have actions enabled."
+                findings.append(report)
+>>>>>>> 6564ec1ff (fix(cloudwatch): handle None metric alarms (#7205))
         return findings
