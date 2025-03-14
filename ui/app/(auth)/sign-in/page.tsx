c95d003fb5afff550a557c82e39556c35d3f1f57
Pablo Lara <larabjj@gmail.com>
2025-03-14 11:11:49 +0100

    chore: disable social login buttons when env vars are not set

--- ui/app/(auth)/sign-in/page.tsx
+++ ui/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,14 @@
 import { AuthForm } from "@/components/auth/oss";
+import { isGithubOAuthEnabled, isGoogleOAuthEnabled } from "@/lib/helper";
 
 const SignIn = () => {
-  return <AuthForm type="sign-in" />;
+  return (
+    <AuthForm
+      type="sign-in"
+      isGoogleOAuthEnabled={isGoogleOAuthEnabled}
+      isGithubOAuthEnabled={isGithubOAuthEnabled}
+    />
+  );
 };
 
 export default SignIn;
