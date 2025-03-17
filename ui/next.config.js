7523fd5df9c50902dc40a92d80a2fb7f387456e3
Pepe Fagoaga <pepe@prowler.com>
2025-03-17 15:45:54 +0100

    fix: add CSP header

--- ui/next.config.js
+++ ui/next.config.js
@@ -5,12 +5,13 @@ const cspHeader = `
     img-src 'self';
     font-src 'self';
     style-src 'self' 'unsafe-inline';
-    script-src 'self' 'unsafe-inline' https://js.stripe.com;
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com;
     connect-src 'self' https://api.iconify.design https://api.simplesvg.com https://api.unisvg.com https://js.stripe.com;
     frame-src 'self' https://js.stripe.com/;
     frame-ancestors 'none';
     default-src 'self'
 `
+
 
 module.exports = {
   output: "standalone",
