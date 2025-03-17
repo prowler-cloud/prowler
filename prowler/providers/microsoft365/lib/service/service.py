0d6f1cfce5122226adda172d22d0a9b39024d093
HugoPBrito <hugopbrit@gmail.com>
2025-03-17 17:37:57 +0100

    feat: non functional crafter

--- prowler/providers/microsoft365/lib/service/service.py
+++ prowler/providers/microsoft365/lib/service/service.py
@@ -1,3 +1,4 @@
+from msal import ConfidentialClientApplication
 from msgraph import GraphServiceClient
 
 from prowler.providers.microsoft365.microsoft365_provider import Microsoft365Provider
@@ -9,6 +10,27 @@ class Microsoft365Service:
         provider: Microsoft365Provider,
     ):
         self.client = GraphServiceClient(credentials=provider.session)
-
+        self.credentials = provider.session.credentials[0]._credential
+        self.identity = provider.identity
         self.audit_config = provider.audit_config
         self.fixer_config = provider.fixer_config
+
+    def get_headers(self, endpoint: str):
+        """
+        Get headers for a given endpoint to craft requests from services
+        """
+
+        try:
+            app = ConfidentialClientApplication(
+                client_id=self.credentials._client_id,
+                authority=f"https://login.microsoftonline.com/{self.credentials._tenant_id}",
+                client_credential=self.credentials._client_credential,
+            )
+            token_result = app.acquire_token_for_client(scopes=[endpoint])
+
+            return {
+                "Authorization": f"Bearer {token_result['access_token']}",
+                "Content-Type": "application/json",
+            }
+        except Exception as e:
+            raise Exception(f"Error getting headers: {e}")
