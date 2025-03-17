d06f7008f421565ee7da548199788273edb747c1
pedrooot <pedromarting3@gmail.com>
2025-03-17 19:29:46 +0100

    feat(ui): add interrogations

--- ui/components/ui/entities/get-provider-logo.tsx
+++ ui/components/ui/entities/get-provider-logo.tsx
@@ -43,28 +43,28 @@ export const getProviderVideoLink = (providerType: ProviderType) => {
   switch (providerType) {
     case "aws":
       return {
-        text: "How to setup an AWS account",
+        text: "How to setup an AWS account?",
         link: "https://docs.prowler.com/projects/prowler-open-source/en/latest/tutorials/prowler-app/#step-41-aws-credentials",
       };
     case "azure":
       return {
-        text: "How to setup an Azure subscription",
+        text: "How to setup an Azure subscription?",
         link: "https://docs.prowler.com/projects/prowler-open-source/en/latest/tutorials/prowler-app/#step-42-azure-credentials",
       };
     case "gcp":
       return {
-        text: "How to setup a GCP project",
+        text: "How to setup a GCP project?",
         link: "https://docs.prowler.com/projects/prowler-open-source/en/latest/tutorials/prowler-app/#step-43-gcp-credentials",
       };
     case "kubernetes":
       return {
-        text: "How to setup a Kubernetes cluster",
+        text: "How to setup a Kubernetes cluster?",
         link: "https://docs.prowler.com/projects/prowler-open-source/en/latest/tutorials/prowler-app/#step-44-kubernetes-credentials",
       };
     default:
       return {
-        text: "How to setup a provider",
-        link: "https://google.com",
+        text: "How to setup a provider?",
+        link: "https://docs.prowler.com/projects/prowler-open-source/en/latest/tutorials/prowler-app/#step-3-add-a-provider",
       };
   }
 };
