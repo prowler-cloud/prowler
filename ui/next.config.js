d427455db58c88725c5e6603c276273c8e6f1d0a
Pepe Fagoaga <pepe@prowler.com>
2025-03-18 22:34:12 +0545

    chore(security): Configure HTTP Security Headers (#7220)
    Co-authored-by: Pablo Lara <larabjj@gmail.com>

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,5 +1,40 @@
 /** @type {import('next').NextConfig} */
+
+// HTTP Security Headers
+// 'unsafe-eval' is configured under `script-src` because it is required by NextJS for development mode
+const cspHeader = `
+    img-src 'self';
+    font-src 'self';
+    style-src 'self' 'unsafe-inline';
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com;
+    connect-src 'self' https://api.iconify.design https://api.simplesvg.com https://api.unisvg.com https://js.stripe.com;
+    frame-src 'self' https://js.stripe.com/;
+    frame-ancestors 'none';
+    default-src 'self'
+`
+
 module.exports = {
   poweredByHeader: false,  
   output: "standalone",
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
+    ]
+  }
 };
