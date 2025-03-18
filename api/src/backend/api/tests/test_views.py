f0cbcacbe415d0c78028d34a0d9140f086825be7
Pepe Fagoaga <pepe@prowler.com>
2025-03-18 22:29:57 +0545

    chore(security): Add HTTP Security Headers (#7289)

--- api/src/backend/api/tests/test_views.py
+++ api/src/backend/api/tests/test_views.py
@@ -40,6 +40,14 @@ def today_after_n_days(n_days: int) -> str:
     )
 
 
+class TestViewSet:
+    def test_security_headers(self, client):
+        response = client.get("/")
+        assert response.headers["X-Content-Type-Options"] == "nosniff"
+        assert response.headers["X-Frame-Options"] == "DENY"
+        assert response.headers["Referrer-Policy"] == "strict-origin-when-cross-origin"
+
+
 @pytest.mark.django_db
 class TestUserViewSet:
     def test_users_list(self, authenticated_client, create_test_user):
