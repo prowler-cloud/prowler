829906f255b28b5995ad4cefcfbc37d159591618
Daniel Rontome <rontomeromano@gmail.com>
2025-03-20 14:08:58 +0100

    OpenNebula: Created OutputOptions with Kubernetes template

--- prowler/providers/opennebula/models.py
+++ prowler/providers/opennebula/models.py
@@ -1,24 +1,33 @@
 from dataclasses import dataclass
-from typing import Any, Optional
+from pyone import OneServer
+from prowler.providers.common.models import ProviderOutputOptions
+from prowler.config.config import output_file_timestamp
 
 @dataclass
-class OpenNebulaSessionModel:
+class OpenNebulaSession:
     """Class to hold the OpenNebula session information"""
-    client: Any # This should be the client object from the OpenNebula SDK
+    client: OneServer 
     endpoint: str
     username: str
     auth_token: str
 
 @dataclass
-class OpenNebulaIdentityModel:
+class OpenNebulaIdentity:
     """Class to hold the OpenNebula identity information"""
     user_id: str
     user_name: str
-    group_ids: list
-    group_names: list
+    group_id: str
+    group_name: str
 
-@dataclass
-class OpenNebulaOutputOptionsModel:
-    """Class to hold the OpenNebula output options"""
-    endpoint: str
-    user_name: str
+
+class OpenNebulaOutputOptions(ProviderOutputOptions):
+    def __init__(self, arguments, bulk_checks_metadata, identity):
+        # First call ProviderOutputOptions init
+        super().__init__(arguments, bulk_checks_metadata)
+        if (
+            not hasattr(arguments, "output_filename")
+            or arguments.output_filename is None
+        ):
+            self.output_filename = f"prowler-output-{identity.user_name}-{output_file_timestamp}"
+        else:
+            self.output_filename = arguments.output_filename
