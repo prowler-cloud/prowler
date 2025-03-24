3ef6413e6928552574ec5087511c3411eb8d7fed
Jonny <106528116+jonathanbro@users.noreply.github.com>
2025-03-24 10:21:01 +0000

    chore(awslambda): update obsolete lambda runtimes (#7330)
    Co-authored-by: MrCloudSec <hello@mistercloudsec.com>
    (cherry picked from commit 9923def4cb16fab510dd9a9d46d6e52bf2ab5267)
    
    # Conflicts:
    #	prowler/config/config.yaml
    #	prowler/providers/aws/services/awslambda/awslambda_function_using_supported_runtimes/awslambda_function_using_supported_runtimes.py

--- prowler/providers/aws/services/awslambda/awslambda_function_using_supported_runtimes/awslambda_function_using_supported_runtimes.py
+++ prowler/providers/aws/services/awslambda/awslambda_function_using_supported_runtimes/awslambda_function_using_supported_runtimes.py
@@ -1,6 +1,37 @@
 from prowler.lib.check.models import Check, Check_Report_AWS
 from prowler.providers.aws.services.awslambda.awslambda_client import awslambda_client
 
+<<<<<<< HEAD
+=======
+default_obsolete_lambda_runtimes = [
+    "java8",
+    "go1.x",
+    "provided",
+    "python3.6",
+    "python2.7",
+    "python3.7",
+    "python3.8",
+    "nodejs4.3",
+    "nodejs4.3-edge",
+    "nodejs6.10",
+    "nodejs",
+    "nodejs8.10",
+    "nodejs10.x",
+    "nodejs12.x",
+    "nodejs14.x",
+    "nodejs16.x",
+    "dotnet5.0",
+    "dotnet6",
+    "dotnet7",
+    "dotnetcore1.0",
+    "dotnetcore2.0",
+    "dotnetcore2.1",
+    "dotnetcore3.1",
+    "ruby2.5",
+    "ruby2.7",
+]
+
+>>>>>>> 9923def4c (chore(awslambda): update obsolete lambda runtimes (#7330))
 
 class awslambda_function_using_supported_runtimes(Check):
     def execute(self):
