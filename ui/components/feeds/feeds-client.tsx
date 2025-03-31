c3cfab310d2a9475ea0fe1a59a9ad58eb2568d2f
sumit_chaturvedi <chaturvedi.sumit@tftus.com>
2025-03-31 09:28:06 +0530

    srn Updated the logic to show the notification icon

--- ui/components/feeds/feeds-client.tsx
+++ ui/components/feeds/feeds-client.tsx
@@ -22,7 +22,7 @@ interface Feed {
 }
 
 interface FeedsClientProps {
-  initialFeeds?: Feed[]; // Adjust fields based on your data structure
+  initialFeeds?: Feed[];
 }
 
 // TODO: Need to update FeedsClientProps with actual interface when actual RSS data finialized
@@ -30,64 +30,48 @@ export const FeedsClient: React.FC<FeedsClientProps> = ({
   initialFeeds = [],
 }) => {
   const [feed] = useState(initialFeeds);
-  const [isAllowedDomain] = useState(
-    //process.env.NEXT_PUBLIC_IS_CLOUD_ENV === "true",
-    true,
-  );
 
   return (
     <>
-      {isAllowedDomain && (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button
-              variant="outline"
-              className={cn(
-                "relative",
-                "rounded-full",
-                "bg-transparent",
-                "p-2",
-              )}
-            >
-              <Icon size={18} />
-              <span className="absolute right-0 top-0 h-2 w-2 rounded-full bg-red-500"></span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end" forceMount>
-            <h3 className="px-2 text-base font-medium">Feeds</h3>
-            <div className="max-h-48 w-80 overflow-y-auto">
-              {feed.length === 0 ? (
-                <p className="py-4 text-center text-gray-500">
-                  No feeds available
-                </p>
-              ) : (
-                feed.map((item, index) => (
-                  <DropdownMenuItem
-                    key={index}
-                    className="hover:cursor-pointer"
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            className={cn("relative", "rounded-full", "bg-transparent", "p-2")}
+          >
+            <Icon size={18} />
+            <span className="absolute right-0 top-0 h-2 w-2 rounded-full bg-red-500"></span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" forceMount>
+          <h3 className="px-2 text-base font-medium">Feeds</h3>
+          <div className="max-h-48 w-80 overflow-y-auto">
+            {feed.length === 0 ? (
+              <p className="py-4 text-center text-gray-500">
+                No feeds available
+              </p>
+            ) : (
+              feed.map((item, index) => (
+                <DropdownMenuItem key={index} className="hover:cursor-pointer">
+                  <Link
+                    href={item.link}
+                    target="_blank"
+                    className="flex flex-col"
                   >
-                    <Link
-                      href={item.link}
-                      target="_blank"
-                      className="flex flex-col"
-                    >
-                      <h3 className="text-small font-medium leading-none">
-                        {item.title}
-                      </h3>
-                      <p className="text-sm text-gray-500">
-                        {item.description}
-                      </p>
-                      <span className="text-muted-foreground mt-1 text-xs text-gray-400">
-                        {item.lastBuildDate}
-                      </span>
-                    </Link>
-                  </DropdownMenuItem>
-                ))
-              )}
-            </div>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      )}
+                    <h3 className="text-small font-medium leading-none">
+                      {item.title}
+                    </h3>
+                    <p className="text-sm text-gray-500">{item.description}</p>
+                    <span className="text-muted-foreground mt-1 text-xs text-gray-400">
+                      {item.lastBuildDate}
+                    </span>
+                  </Link>
+                </DropdownMenuItem>
+              ))
+            )}
+          </div>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </>
   );
 };
