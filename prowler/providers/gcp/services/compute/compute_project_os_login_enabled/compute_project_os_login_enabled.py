e4d234fe030c0a42a6bfa46afc597030cd427e68
Pedro Martín <pedromarting3@gmail.com>
2025-04-03 17:35:02 +0200

    fix(azure): remove `resource_name` inside the `Check_Report` (#7420)

--- prowler/providers/gcp/services/compute/compute_project_os_login_enabled/compute_project_os_login_enabled.py
+++ prowler/providers/gcp/services/compute/compute_project_os_login_enabled/compute_project_os_login_enabled.py
@@ -8,8 +8,7 @@ class compute_project_os_login_enabled(Check):
         for project in compute_client.compute_projects:
             report = Check_Report_GCP(
                 metadata=self.metadata(),
-                resource=project,
-                resource_name=project.id,
+                resource=compute_client.projects[project.id],
                 project_id=project.id,
                 location=compute_client.region,
             )
