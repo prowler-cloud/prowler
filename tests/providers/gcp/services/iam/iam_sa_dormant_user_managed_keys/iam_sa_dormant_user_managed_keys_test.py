84bda3ec1c124a68669882290a1924533025d0e0
bgdanix <bogdalecu@gmail.com>
2025-04-09 18:03:20 +0300

    fix: re-add the unused user-managed keys and make the period configurable

--- tests/providers/gcp/services/iam/iam_sa_dormant_user_managed_keys/iam_sa_dormant_user_managed_keys_test.py
+++ tests/providers/gcp/services/iam/iam_sa_dormant_user_managed_keys/iam_sa_dormant_user_managed_keys_test.py
@@ -8,7 +8,7 @@ from tests.providers.gcp.gcp_fixtures import (
 )
 
 
-class Test_iam_sa_no_user_managed_keys:
+class Test_iam_sa_user_managed_key_unused:
     def test_iam_no_sa(self):
         iam_client = mock.MagicMock()
 
@@ -18,24 +18,25 @@ class Test_iam_sa_no_user_managed_keys:
                 return_value=set_mocked_gcp_provider(),
             ),
             mock.patch(
-                "prowler.providers.gcp.services.iam.iam_sa_no_user_managed_keys.iam_sa_no_user_managed_keys.iam_client",
+                "prowler.providers.gcp.services.iam.iam_sa_user_managed_key_unused.iam_sa_user_managed_key_unused.iam_client",
                 new=iam_client,
             ),
         ):
-            from prowler.providers.gcp.services.iam.iam_sa_no_user_managed_keys.iam_sa_no_user_managed_keys import (
-                iam_sa_no_user_managed_keys,
+            from prowler.providers.gcp.services.iam.iam_sa_user_managed_key_unused.iam_sa_user_managed_key_unused import (
+                iam_sa_user_managed_key_unused,
             )
 
             iam_client.project_ids = [GCP_PROJECT_ID]
             iam_client.region = GCP_US_CENTER1_LOCATION
             iam_client.service_accounts = []
 
-            check = iam_sa_no_user_managed_keys()
+            check = iam_sa_user_managed_key_unused()
             result = check.execute()
             assert len(result) == 0
 
-    def test_iam_sa_no_keys(self):
+    def test_iam_sa_dormant_no_keys(self):
         iam_client = mock.MagicMock()
+        monitoring_client = mock.MagicMock()
 
         with (
             mock.patch(
@@ -43,12 +44,16 @@ class Test_iam_sa_no_user_managed_keys:
                 return_value=set_mocked_gcp_provider(),
             ),
             mock.patch(
-                "prowler.providers.gcp.services.iam.iam_sa_no_user_managed_keys.iam_sa_no_user_managed_keys.iam_client",
+                "prowler.providers.gcp.services.iam.iam_sa_user_managed_key_unused.iam_sa_user_managed_key_unused.iam_client",
                 new=iam_client,
             ),
+            mock.patch(
+                "prowler.providers.gcp.services.iam.iam_sa_user_managed_key_unused.iam_sa_user_managed_key_unused.monitoring_client",
+                new=monitoring_client,
+            ),
         ):
-            from prowler.providers.gcp.services.iam.iam_sa_no_user_managed_keys.iam_sa_no_user_managed_keys import (
-                iam_sa_no_user_managed_keys,
+            from prowler.providers.gcp.services.iam.iam_sa_user_managed_key_unused.iam_sa_user_managed_key_unused import (
+                iam_sa_user_managed_key_unused,
             )
             from prowler.providers.gcp.services.iam.iam_service import ServiceAccount
 
@@ -66,21 +71,17 @@ class Test_iam_sa_no_user_managed_keys:
                 )
             ]
 
-            check = iam_sa_no_user_managed_keys()
-            result = check.execute()
-            assert len(result) == 1
-            assert result[0].status == "PASS"
-            assert (
-                result[0].status_extended
-                == f"Account {iam_client.service_accounts[0].email} does not have user-managed keys."
+            monitoring_client.sa_keys_metrics = set(
+                ["90c48f61c65cd56224a12ab18e6ee9ca9c3aee7c"]
             )
-            assert result[0].resource_id == iam_client.service_accounts[0].email
-            assert result[0].project_id == GCP_PROJECT_ID
-            assert result[0].location == GCP_US_CENTER1_LOCATION
-            assert result[0].resource_name == iam_client.service_accounts[0].name
 
-    def test_iam_sa_no_user_managed_keys(self):
+            check = iam_sa_user_managed_key_unused()
+            result = check.execute()
+            assert len(result) == 0
+
+    def test_iam_sa_dormant_system_managed_keys(self):
         iam_client = mock.MagicMock()
+        monitoring_client = mock.MagicMock()
 
         with (
             mock.patch(
@@ -88,12 +89,16 @@ class Test_iam_sa_no_user_managed_keys:
                 return_value=set_mocked_gcp_provider(),
             ),
             mock.patch(
-                "prowler.providers.gcp.services.iam.iam_sa_no_user_managed_keys.iam_sa_no_user_managed_keys.iam_client",
+                "prowler.providers.gcp.services.iam.iam_sa_user_managed_key_unused.iam_sa_user_managed_key_unused.iam_client",
                 new=iam_client,
             ),
+            mock.patch(
+                "prowler.providers.gcp.services.iam.iam_sa_user_managed_key_unused.iam_sa_user_managed_key_unused.monitoring_client",
+                new=monitoring_client,
+            ),
         ):
-            from prowler.providers.gcp.services.iam.iam_sa_no_user_managed_keys.iam_sa_no_user_managed_keys import (
-                iam_sa_no_user_managed_keys,
+            from prowler.providers.gcp.services.iam.iam_sa_user_managed_key_unused.iam_sa_user_managed_key_unused import (
+                iam_sa_user_managed_key_unused,
             )
             from prowler.providers.gcp.services.iam.iam_service import (
                 Key,
@@ -110,7 +115,7 @@ class Test_iam_sa_no_user_managed_keys:
                     display_name="My service account",
                     keys=[
                         Key(
-                            name="projects/my-project/serviceAccounts/my-service-account@my-project.iam.gserviceaccount.com/keys/90c48f61c65cd56224a12ab18e6ee9ca9c3aee7c",
+                            name="90c48f61c65cd56224a12ab18e6ee9ca9c3aee7c",
                             origin="GOOGLE_PROVIDED",
                             type="SYSTEM_MANAGED",
                             valid_after=datetime.strptime("2024-07-10", "%Y-%m-%d"),
@@ -122,21 +127,17 @@ class Test_iam_sa_no_user_managed_keys:
                 )
             ]
 
-            check = iam_sa_no_user_managed_keys()
-            result = check.execute()
-            assert len(result) == 1
-            assert result[0].status == "PASS"
-            assert (
-                result[0].status_extended
-                == f"Account {iam_client.service_accounts[0].email} does not have user-managed keys."
+            monitoring_client.sa_keys_metrics = set(
+                ["90c48f61c65cd56224a12ab18e6ee9ca9c3aee7c"]
             )
-            assert result[0].resource_id == iam_client.service_accounts[0].email
-            assert result[0].project_id == GCP_PROJECT_ID
-            assert result[0].location == GCP_US_CENTER1_LOCATION
-            assert result[0].resource_name == iam_client.service_accounts[0].name
 
-    def test_iam_sa_user_managed_keys(self):
+            check = iam_sa_user_managed_key_unused()
+            result = check.execute()
+            assert len(result) == 0
+
+    def test_iam_sa_user_managed_key_unused(self):
         iam_client = mock.MagicMock()
+        monitoring_client = mock.MagicMock()
 
         with (
             mock.patch(
@@ -144,12 +145,16 @@ class Test_iam_sa_no_user_managed_keys:
                 return_value=set_mocked_gcp_provider(),
             ),
             mock.patch(
-                "prowler.providers.gcp.services.iam.iam_sa_no_user_managed_keys.iam_sa_no_user_managed_keys.iam_client",
+                "prowler.providers.gcp.services.iam.iam_sa_user_managed_key_unused.iam_sa_user_managed_key_unused.iam_client",
                 new=iam_client,
             ),
+            mock.patch(
+                "prowler.providers.gcp.services.iam.iam_sa_user_managed_key_unused.iam_sa_user_managed_key_unused.monitoring_client",
+                new=monitoring_client,
+            ),
         ):
-            from prowler.providers.gcp.services.iam.iam_sa_no_user_managed_keys.iam_sa_no_user_managed_keys import (
-                iam_sa_no_user_managed_keys,
+            from prowler.providers.gcp.services.iam.iam_sa_user_managed_key_unused.iam_sa_user_managed_key_unused import (
+                iam_sa_user_managed_key_unused,
             )
             from prowler.providers.gcp.services.iam.iam_service import (
                 Key,
@@ -166,7 +171,7 @@ class Test_iam_sa_no_user_managed_keys:
                     display_name="My service account",
                     keys=[
                         Key(
-                            name="projects/my-project/serviceAccounts/my-service-account@my-project.iam.gserviceaccount.com/keys/90c48f61c65cd56224a12ab18e6ee9ca9c3aee7c",
+                            name="90c48f61c65cd56224a12ab18e6ee9ca9c3aee7c",
                             origin="GOOGLE_PROVIDED",
                             type="USER_MANAGED",
                             valid_after=datetime.strptime("2024-07-10", "%Y-%m-%d"),
@@ -178,21 +183,27 @@ class Test_iam_sa_no_user_managed_keys:
                 )
             ]
 
-            check = iam_sa_no_user_managed_keys()
+            monitoring_client.sa_keys_metrics = set(
+                ["90c48f61c65cd56224a12ab18e6ee9ca9c3aee7c"]
+            )
+            monitoring_client.audit_config = {"max_unused_account_days": 30}
+
+            check = iam_sa_user_managed_key_unused()
             result = check.execute()
             assert len(result) == 1
-            assert result[0].status == "FAIL"
+            assert result[0].status == "PASS"
             assert (
                 result[0].status_extended
-                == f"Account {iam_client.service_accounts[0].email} has user-managed keys."
+                == f"User-managed key {iam_client.service_accounts[0].keys[0].name} for Service Account {iam_client.service_accounts[0].email} was used over the last 30 days."
             )
-            assert result[0].resource_id == iam_client.service_accounts[0].email
+            assert result[0].resource_id == iam_client.service_accounts[0].keys[0].name
             assert result[0].project_id == GCP_PROJECT_ID
             assert result[0].location == GCP_US_CENTER1_LOCATION
-            assert result[0].resource_name == iam_client.service_accounts[0].name
+            assert result[0].resource_name == iam_client.service_accounts[0].email
 
-    def test_iam_sa_mixed_keys(self):
+    def test_iam_sa_dormant_mixed_keys(self):
         iam_client = mock.MagicMock()
+        monitoring_client = mock.MagicMock()
 
         with (
             mock.patch(
@@ -200,12 +211,16 @@ class Test_iam_sa_no_user_managed_keys:
                 return_value=set_mocked_gcp_provider(),
             ),
             mock.patch(
-                "prowler.providers.gcp.services.iam.iam_sa_no_user_managed_keys.iam_sa_no_user_managed_keys.iam_client",
+                "prowler.providers.gcp.services.iam.iam_sa_user_managed_key_unused.iam_sa_user_managed_key_unused.iam_client",
                 new=iam_client,
             ),
+            mock.patch(
+                "prowler.providers.gcp.services.iam.iam_sa_user_managed_key_unused.iam_sa_user_managed_key_unused.monitoring_client",
+                new=monitoring_client,
+            ),
         ):
-            from prowler.providers.gcp.services.iam.iam_sa_no_user_managed_keys.iam_sa_no_user_managed_keys import (
-                iam_sa_no_user_managed_keys,
+            from prowler.providers.gcp.services.iam.iam_sa_user_managed_key_unused.iam_sa_user_managed_key_unused import (
+                iam_sa_user_managed_key_unused,
             )
             from prowler.providers.gcp.services.iam.iam_service import (
                 Key,
@@ -222,14 +237,21 @@ class Test_iam_sa_no_user_managed_keys:
                     display_name="My service account",
                     keys=[
                         Key(
-                            name="projects/my-project/serviceAccounts/my-service-account@my-project.iam.gserviceaccount.com/keys/90c48f61c65cd56224a12ab18e6ee9ca9c3aee7c",
+                            name="90c48f61c65cd56224a12ab18e6ee9ca9c3aee7c",
                             origin="GOOGLE_PROVIDED",
                             type="SYSTEM_MANAGED",
                             valid_after=datetime.strptime("2024-07-10", "%Y-%m-%d"),
                             valid_before=datetime.strptime("9999-12-31", "%Y-%m-%d"),
                         ),
                         Key(
-                            name="projects/my-project/serviceAccounts/my-service-account@my-project.iam.gserviceaccount.com/keys/e5e3800831ac1adc8a5849da7d827b4724b1fce8",
+                            name="e5e3800831ac1adc8a5849da7d827b4724b1fce8",
+                            origin="GOOGLE_PROVIDED",
+                            type="USER_MANAGED",
+                            valid_after=datetime.strptime("2024-07-10", "%Y-%m-%d"),
+                            valid_before=datetime.strptime("9999-12-31", "%Y-%m-%d"),
+                        ),
+                        Key(
+                            name="f8e4771561be5cda9b1267add7006c5143e3a220",
                             origin="GOOGLE_PROVIDED",
                             type="USER_MANAGED",
                             valid_after=datetime.strptime("2024-07-10", "%Y-%m-%d"),
@@ -241,15 +263,30 @@ class Test_iam_sa_no_user_managed_keys:
                 )
             ]
 
-            check = iam_sa_no_user_managed_keys()
+            monitoring_client.sa_keys_metrics = set(
+                ["f8e4771561be5cda9b1267add7006c5143e3a220"]
+            )
+            monitoring_client.audit_config = {"max_unused_account_days": 30}
+
+            check = iam_sa_user_managed_key_unused()
             result = check.execute()
-            assert len(result) == 1
+            assert len(result) == 2
             assert result[0].status == "FAIL"
             assert (
                 result[0].status_extended
-                == f"Account {iam_client.service_accounts[0].email} has user-managed keys."
+                == f"User-managed key {iam_client.service_accounts[0].keys[1].name} for Service Account {iam_client.service_accounts[0].email} was not used over the last 30 days."
             )
-            assert result[0].resource_id == iam_client.service_accounts[0].email
+            assert result[0].resource_id == iam_client.service_accounts[0].keys[1].name
             assert result[0].project_id == GCP_PROJECT_ID
             assert result[0].location == GCP_US_CENTER1_LOCATION
-            assert result[0].resource_name == iam_client.service_accounts[0].name
+            assert result[0].resource_name == iam_client.service_accounts[0].email
+
+            assert result[1].status == "PASS"
+            assert (
+                result[1].status_extended
+                == f"User-managed key {iam_client.service_accounts[0].keys[2].name} for Service Account {iam_client.service_accounts[0].email} was used over the last 30 days."
+            )
+            assert result[1].resource_id == iam_client.service_accounts[0].keys[2].name
+            assert result[1].project_id == GCP_PROJECT_ID
+            assert result[1].location == GCP_US_CENTER1_LOCATION
+            assert result[1].resource_name == iam_client.service_accounts[0].email
