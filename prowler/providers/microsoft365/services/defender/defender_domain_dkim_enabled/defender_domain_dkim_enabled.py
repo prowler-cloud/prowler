22d4dc936c76de47f8692933cbe18de1b905c045
Daniel Barranquero <danielbo2001@gmail.com>
2025-04-09 19:15:17 +0200

    feat(defender): add new check for dkim enabled

---
+++ prowler/providers/microsoft365/services/defender/defender_domain_dkim_enabled/defender_domain_dkim_enabled.py
@@ -0,0 +1,47 @@
+from typing import List
+
+from prowler.lib.check.models import Check, CheckReportMicrosoft365
+from prowler.providers.microsoft365.services.defender.defender_client import (
+    defender_client,
+)
+
+
+class defender_domain_dkim_enabled(Check):
+    """
+    Check if DKIM is enabled for all Exchange Online domains.
+
+    Attributes:
+        metadata: Metadata associated with the check (inherited from Check).
+    """
+
+    def execute(self) -> List[CheckReportMicrosoft365]:
+        """
+        Execute the check to verify if DKIM is enabled for all domains.
+
+        This method checks the DKIM signing configuration for each domain to determine if DKIM is enabled.
+
+        Returns:
+            List[CheckReportMicrosoft365]: A list of reports containing the result of the check.
+        """
+        findings = []
+        for config in defender_client.dkim_configurations:
+            report = CheckReportMicrosoft365(
+                metadata=self.metadata(),
+                resource=config,
+                resource_name="DKIM Configuration",
+                resource_id=config.id,
+            )
+            report.status = "FAIL"
+            report.status_extended = (
+                f"DKIM is not enabled for domain with ID {config.id}."
+            )
+
+            if config.dkim_signing_enabled:
+                report.status = "PASS"
+                report.status_extended = (
+                    f"DKIM is enabled for domain with ID {config.id}."
+                )
+
+            findings.append(report)
+
+        return findings
--- tests/providers/microsoft365/services/defender/defender_service_test.py
+++ tests/providers/microsoft365/services/defender/defender_service_test.py
@@ -4,6 +4,7 @@ from prowler.providers.microsoft365.models import Microsoft365IdentityInfo
 from prowler.providers.microsoft365.services.defender.defender_service import (
     Defender,
     DefenderMalwarePolicy,
+    DkimConfig,
 )
 from tests.providers.microsoft365.microsoft365_fixtures import (
     DOMAIN,
@@ -18,9 +19,20 @@ def mock_defender_get_malware_filter_policy(_):
     ]
 
 
+def mock_defender_get_dkim_config(_):
+    return [
+        DkimConfig(dkim_signing_enabled=True, id="domain1"),
+        DkimConfig(dkim_signing_enabled=False, id="domain2"),
+    ]
+
+
 @patch(
     "prowler.providers.microsoft365.services.defender.defender_service.Defender._get_malware_filter_policy",
     new=mock_defender_get_malware_filter_policy,
+)
+@patch(
+    "prowler.providers.microsoft365.services.defender.defender_service.Defender._get_dkim_config",
+    new=mock_defender_get_dkim_config,
 )
 class Test_Defender_Service:
     def test_get_client(self):
@@ -38,3 +50,11 @@ class Test_Defender_Service:
         assert malware_policies[0].identity == "Policy1"
         assert malware_policies[1].enable_file_filter is True
         assert malware_policies[1].identity == "Policy2"
+
+    def test__get_dkim_config(self):
+        defender_client = Defender(set_mocked_microsoft365_provider())
+        dkim_configs = defender_client.dkim_configurations
+        assert dkim_configs[0].dkim_signing_enabled is True
+        assert dkim_configs[0].id == "domain1"
+        assert dkim_configs[1].dkim_signing_enabled is False
+        assert dkim_configs[1].id == "domain2"
