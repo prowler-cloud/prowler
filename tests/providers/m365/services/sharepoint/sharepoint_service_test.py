52bd48168f460ea23758d61fbefeadd7fc8c6874
Hugo Pereira Brito <101209179+HugoPBrito@users.noreply.github.com>
2025-04-15 19:24:09 +0200

    feat: adapt `Microsoft365` provider to use `PowerShell` (#7331)
    Co-authored-by: MrCloudSec <hello@mistercloudsec.com>

---
+++ tests/providers/m365/services/sharepoint/sharepoint_service_test.py
@@ -0,0 +1,41 @@
+from unittest.mock import patch
+
+from prowler.providers.m365.models import M365IdentityInfo
+from prowler.providers.m365.services.sharepoint.sharepoint_service import (
+    SharePoint,
+    SharePointSettings,
+)
+from tests.providers.m365.m365_fixtures import DOMAIN, set_mocked_m365_provider
+
+
+async def mock_sharepoint_get_settings(_):
+    return SharePointSettings(
+        sharingCapability="ExternalUserAndGuestSharing",
+        sharingAllowedDomainList=["allowed-domain.com"],
+        sharingBlockedDomainList=["blocked-domain.com"],
+        sharingDomainRestrictionMode="allowList",
+        resharingEnabled=False,
+        legacyAuth=True,
+    )
+
+
+@patch(
+    "prowler.providers.m365.services.sharepoint.sharepoint_service.SharePoint._get_settings",
+    new=mock_sharepoint_get_settings,
+)
+class Test_SharePoint_Service:
+    def test_get_client(self):
+        sharepoint_client = SharePoint(
+            set_mocked_m365_provider(identity=M365IdentityInfo(tenant_domain=DOMAIN))
+        )
+        assert sharepoint_client.client.__class__.__name__ == "GraphServiceClient"
+
+    def test_get_settings(self):
+        sharepoint_client = SharePoint(set_mocked_m365_provider())
+        settings = sharepoint_client.settings
+        assert settings.sharingCapability == "ExternalUserAndGuestSharing"
+        assert settings.sharingAllowedDomainList == ["allowed-domain.com"]
+        assert settings.sharingBlockedDomainList == ["blocked-domain.com"]
+        assert settings.sharingDomainRestrictionMode == "allowList"
+        assert settings.resharingEnabled is False
+        assert settings.legacyAuth is True
