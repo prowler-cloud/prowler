4f3496194d5741cedcd93d999631350f6c7f8937
Daniel Barranquero <74871504+danibarranqueroo@users.noreply.github.com>
2025-04-18 18:42:19 +0200

    feat(defender): add new check `defender_antiphishing_policy_configured` (#7453)

--- prowler/providers/m365/services/defender/defender_service.py
+++ prowler/providers/m365/services/defender/defender_service.py
@@ -10,6 +10,8 @@ class Defender(M365Service):
         super().__init__(provider)
         self.powershell.connect_exchange_online()
         self.malware_policies = self._get_malware_filter_policy()
+        self.antiphishing_policies = self._get_antiphising_policy()
+        self.antiphising_rules = self._get_antiphising_rules()
         self.powershell.close()
 
     def _get_malware_filter_policy(self):
@@ -39,9 +41,73 @@ class Defender(M365Service):
             )
         return malware_policies
 
+    def _get_antiphising_policy(self):
+        logger.info("Microsoft365 - Getting Defender antiphishing policy...")
+        antiphishing_policies = {}
+        try:
+            antiphishing_policy = self.powershell.get_antiphishing_policy()
+            if isinstance(antiphishing_policy, dict):
+                antiphishing_policy = [antiphishing_policy]
+            for policy in antiphishing_policy:
+                if policy:
+                    antiphishing_policies[policy.get("Name", "")] = AntiphishingPolicy(
+                        spoof_intelligence=policy.get("EnableSpoofIntelligence", True),
+                        spoof_intelligence_action=policy.get(
+                            "AuthenticationFailAction", ""
+                        ),
+                        dmarc_reject_action=policy.get("DmarcRejectAction", ""),
+                        dmarc_quarantine_action=policy.get("DmarcQuarantineAction", ""),
+                        safety_tips=policy.get("EnableFirstContactSafetyTips", True),
+                        unauthenticated_sender_action=policy.get(
+                            "EnableUnauthenticatedSender", True
+                        ),
+                        show_tag=policy.get("EnableViaTag", True),
+                        honor_dmarc_policy=policy.get("HonorDmarcPolicy", True),
+                        default=policy.get("IsDefault", False),
+                    )
+        except Exception as error:
+            logger.error(
+                f"{error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+            )
+        return antiphishing_policies
+
+    def _get_antiphising_rules(self):
+        logger.info("Microsoft365 - Getting Defender antiphishing rules...")
+        antiphishing_rules = {}
+        try:
+            antiphishing_rule = self.powershell.get_antiphishing_rules()
+            if isinstance(antiphishing_rule, dict):
+                antiphishing_rule = [antiphishing_rule]
+            for rule in antiphishing_rule:
+                if rule:
+                    antiphishing_rules[rule.get("Name", "")] = AntiphishingRule(
+                        state=rule.get("State", ""),
+                    )
+        except Exception as error:
+            logger.error(
+                f"{error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+            )
+        return antiphishing_rules
+
 
 class DefenderMalwarePolicy(BaseModel):
     enable_file_filter: bool
     identity: str
     enable_internal_sender_admin_notifications: bool
     internal_sender_admin_address: str
+
+
+class AntiphishingPolicy(BaseModel):
+    spoof_intelligence: bool
+    spoof_intelligence_action: str
+    dmarc_reject_action: str
+    dmarc_quarantine_action: str
+    safety_tips: bool
+    unauthenticated_sender_action: bool
+    show_tag: bool
+    honor_dmarc_policy: bool
+    default: bool
+
+
+class AntiphishingRule(BaseModel):
+    state: str
--- tests/providers/m365/services/defender/m365_defender_service_test.py
+++ tests/providers/m365/services/defender/m365_defender_service_test.py
@@ -3,6 +3,8 @@ from unittest.mock import patch
 
 from prowler.providers.m365.models import M365IdentityInfo
 from prowler.providers.m365.services.defender.defender_service import (
+    AntiphishingPolicy,
+    AntiphishingRule,
     Defender,
     DefenderMalwarePolicy,
 )
@@ -24,6 +26,44 @@ def mock_defender_get_malware_filter_policy(_):
             internal_sender_admin_address="security@example.com",
         ),
     ]
+
+
+def mock_defender_get_antiphising_policy(_):
+    return {
+        "Policy1": AntiphishingPolicy(
+            spoof_intelligence=True,
+            spoof_intelligence_action="Quarantine",
+            dmarc_reject_action="Reject",
+            dmarc_quarantine_action="Quarantine",
+            safety_tips=True,
+            unauthenticated_sender_action=True,
+            show_tag=True,
+            honor_dmarc_policy=True,
+            default=False,
+        ),
+        "Policy2": AntiphishingPolicy(
+            spoof_intelligence=False,
+            spoof_intelligence_action="None",
+            dmarc_reject_action="None",
+            dmarc_quarantine_action="None",
+            safety_tips=False,
+            unauthenticated_sender_action=False,
+            show_tag=False,
+            honor_dmarc_policy=False,
+            default=True,
+        ),
+    }
+
+
+def mock_defender_get_antiphising_rules(_):
+    return {
+        "Policy1": AntiphishingRule(
+            state="Enabled",
+        ),
+        "Policy2": AntiphishingRule(
+            state="Disabled",
+        ),
+    }
 
 
 class Test_Defender_Service:
@@ -58,8 +98,6 @@ class Test_Defender_Service:
                 )
             )
             malware_policies = defender_client.malware_policies
-            assert malware_policies[0].enable_file_filter is False
-            assert malware_policies[0].identity == "Policy1"
             assert (
                 malware_policies[0].enable_internal_sender_admin_notifications is False
             )
@@ -73,3 +111,69 @@ class Test_Defender_Service:
                 malware_policies[1].internal_sender_admin_address
                 == "security@example.com"
             )
+            defender_client.powershell.close()
+
+    @patch(
+        "prowler.providers.m365.services.defender.defender_service.Defender._get_antiphising_policy",
+        new=mock_defender_get_antiphising_policy,
+    )
+    def test_get_antiphising_policy(self):
+        with (
+            mock.patch(
+                "prowler.providers.m365.lib.powershell.m365_powershell.M365PowerShell.connect_exchange_online"
+            ),
+        ):
+            defender_client = Defender(
+                set_mocked_m365_provider(
+                    identity=M365IdentityInfo(tenant_domain=DOMAIN)
+                )
+            )
+            antiphishing_policies = defender_client.antiphishing_policies
+            assert antiphishing_policies["Policy1"].spoof_intelligence is True
+            assert (
+                antiphishing_policies["Policy1"].spoof_intelligence_action
+                == "Quarantine"
+            )
+            assert antiphishing_policies["Policy1"].dmarc_reject_action == "Reject"
+            assert (
+                antiphishing_policies["Policy1"].dmarc_quarantine_action == "Quarantine"
+            )
+            assert antiphishing_policies["Policy1"].safety_tips is True
+            assert (
+                antiphishing_policies["Policy1"].unauthenticated_sender_action is True
+            )
+            assert antiphishing_policies["Policy1"].show_tag is True
+            assert antiphishing_policies["Policy1"].honor_dmarc_policy is True
+            assert antiphishing_policies["Policy1"].default is False
+            assert antiphishing_policies["Policy2"].spoof_intelligence is False
+            assert antiphishing_policies["Policy2"].spoof_intelligence_action == "None"
+            assert antiphishing_policies["Policy2"].dmarc_reject_action == "None"
+            assert antiphishing_policies["Policy2"].dmarc_quarantine_action == "None"
+            assert antiphishing_policies["Policy2"].safety_tips is False
+            assert (
+                antiphishing_policies["Policy2"].unauthenticated_sender_action is False
+            )
+            assert antiphishing_policies["Policy2"].show_tag is False
+            assert antiphishing_policies["Policy2"].honor_dmarc_policy is False
+            assert antiphishing_policies["Policy2"].default is True
+            defender_client.powershell.close()
+
+    @patch(
+        "prowler.providers.m365.services.defender.defender_service.Defender._get_antiphising_rules",
+        new=mock_defender_get_antiphising_rules,
+    )
+    def test_get_antiphising_rules(self):
+        with (
+            mock.patch(
+                "prowler.providers.m365.lib.powershell.m365_powershell.M365PowerShell.connect_exchange_online"
+            ),
+        ):
+            defender_client = Defender(
+                set_mocked_m365_provider(
+                    identity=M365IdentityInfo(tenant_domain=DOMAIN)
+                )
+            )
+            antiphishing_rules = defender_client.antiphising_rules
+            assert antiphishing_rules["Policy1"].state == "Enabled"
+            assert antiphishing_rules["Policy2"].state == "Disabled"
+            defender_client.powershell.close()
