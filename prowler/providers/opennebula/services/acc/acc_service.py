df77b920b074390a8b55e7e2fabac60277da5c50
Daniel Rontome <rontomeromano@gmail.com>
2025-04-19 21:09:24 +0200

    Add User class to Opennebula service

--- prowler/providers/opennebula/services/acc/acc_service.py
+++ prowler/providers/opennebula/services/acc/acc_service.py
@@ -1,3 +1,4 @@
+from pydantic import BaseModel
 from prowler.providers.opennebula.lib.service.service import OpennebulaService
 from prowler.lib.logger import logger
 import hashlib
@@ -6,7 +7,7 @@ import os
 class ACCService(OpennebulaService):
     def __init__(self, provider):
         super().__init__(provider)
-        self.users = {}
+        self.users : list[User] = []
         self.__get_users__()
 
     def _load_common_password_hashes_(self, file):
@@ -19,22 +20,31 @@ class ACCService(OpennebulaService):
         return user_hash in common_hashes
 
     def __get_users__(self):
-        """Obtener todos los usuarios de OpenNebula"""
-        logger.info("AuthenticationAndAccessControl - Obteniendo usuarios...")
+        """
+        Get users from OpenNebula
+        """
         common_hashes = self._load_common_password_hashes_('10-million-password-list-top-1000000.txt')
         try:
             userpool = self.client.userpool.info()
             for user in userpool.USER:
-                self.users[user.ID] = {
-                    "name": user.NAME,
-                    "password": user.PASSWORD,
-                    "group": user.GNAME,
-                    "auth_driver": user.AUTH_DRIVER,
-                    "enabled": user.ENABLED,
-                    "weak_password": self._is_weak_password_(user.PASSWORD, common_hashes)
-                }
+                self.users.append(User(
+                    id=user.ID,
+                    name=user.NAME,
+                    password=user.PASSWORD,
+                    group=user.GNAME,
+                    auth_driver=user.AUTH_DRIVER,
+                    enabled=user.ENABLED,
+                    weak_password=self._is_weak_password_(user.PASSWORD, common_hashes)
+                ))
         except Exception as error:
             logger.error(f"Error al obtener usuarios: {error}")
 
-        
+class User(BaseModel):
+    id: str
+    name: str
+    password: str
+    group: str
+    auth_driver: str
+    enabled: bool
+    weak_password: bool        
         
