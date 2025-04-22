a3f931029aad71ecaaef26abca3b61a33ce98c30
MrCloudSec <hello@mistercloudsec.com>
2025-04-22 12:24:40 -0400

    chore: revision

--- tests/providers/m365/services/defender/defender_antispam_connection_filter_policy_empty_ip_allowlist/defender_antispam_connection_filter_policy_empty_ip_allowlist_test.py
+++ tests/providers/m365/services/defender/defender_antispam_connection_filter_policy_empty_ip_allowlist/defender_antispam_connection_filter_policy_empty_ip_allowlist_test.py
@@ -3,7 +3,7 @@ from unittest import mock
 from tests.providers.m365.m365_fixtures import DOMAIN, set_mocked_m365_provider
 
 
-class Test_defender_antispam_connection_filter_policy_ip_allow_list_not_used:
+class Test_defender_antispam_connection_filter_policy_empty_ip_allowlist:
     def test_ip_allow_list_not_used(self):
         defender_client = mock.MagicMock()
         defender_client.audited_tenant = "audited_tenant"
@@ -18,12 +18,12 @@ class Test_defender_antispam_connection_filter_policy_ip_allow_list_not_used:
                 "prowler.providers.m365.lib.powershell.m365_powershell.M365PowerShell.connect_exchange_online"
             ),
             mock.patch(
-                "prowler.providers.m365.services.defender.defender_antispam_connection_filter_policy_ip_allow_list_not_used.defender_antispam_connection_filter_policy_ip_allow_list_not_used.defender_client",
+                "prowler.providers.m365.services.defender.defender_antispam_connection_filter_policy_empty_ip_allowlist.defender_antispam_connection_filter_policy_empty_ip_allowlist.defender_client",
                 new=defender_client,
             ),
         ):
-            from prowler.providers.m365.services.defender.defender_antispam_connection_filter_policy_ip_allow_list_not_used.defender_antispam_connection_filter_policy_ip_allow_list_not_used import (
-                defender_antispam_connection_filter_policy_ip_allow_list_not_used,
+            from prowler.providers.m365.services.defender.defender_antispam_connection_filter_policy_empty_ip_allowlist.defender_antispam_connection_filter_policy_empty_ip_allowlist import (
+                defender_antispam_connection_filter_policy_empty_ip_allowlist,
             )
             from prowler.providers.m365.services.defender.defender_service import (
                 ConnectionFilterPolicy,
@@ -34,17 +34,19 @@ class Test_defender_antispam_connection_filter_policy_ip_allow_list_not_used:
                 identity="Default",
             )
 
-            check = defender_antispam_connection_filter_policy_ip_allow_list_not_used()
+            check = defender_antispam_connection_filter_policy_empty_ip_allowlist()
             result = check.execute()
 
             assert len(result) == 1
             assert result[0].status == "PASS"
             assert (
                 result[0].status_extended
-                == "IP Allow List is not used in the Defender connection filter policy Default."
+                == "IP Allowlist is not used in the Antispam Connection Filter Policy Default."
             )
             assert result[0].resource == defender_client.connection_filter_policy.dict()
-            assert result[0].resource_name == "Defender Connection Filter Policy"
+            assert (
+                result[0].resource_name == "Defender Antispam Connection Filter Policy"
+            )
             assert result[0].resource_id == "Default"
             assert result[0].location == "global"
 
@@ -62,12 +64,12 @@ class Test_defender_antispam_connection_filter_policy_ip_allow_list_not_used:
                 "prowler.providers.m365.lib.powershell.m365_powershell.M365PowerShell.connect_exchange_online"
             ),
             mock.patch(
-                "prowler.providers.m365.services.defender.defender_antispam_connection_filter_policy_ip_allow_list_not_used.defender_antispam_connection_filter_policy_ip_allow_list_not_used.defender_client",
+                "prowler.providers.m365.services.defender.defender_antispam_connection_filter_policy_empty_ip_allowlist.defender_antispam_connection_filter_policy_empty_ip_allowlist.defender_client",
                 new=defender_client,
             ),
         ):
-            from prowler.providers.m365.services.defender.defender_antispam_connection_filter_policy_ip_allow_list_not_used.defender_antispam_connection_filter_policy_ip_allow_list_not_used import (
-                defender_antispam_connection_filter_policy_ip_allow_list_not_used,
+            from prowler.providers.m365.services.defender.defender_antispam_connection_filter_policy_empty_ip_allowlist.defender_antispam_connection_filter_policy_empty_ip_allowlist import (
+                defender_antispam_connection_filter_policy_empty_ip_allowlist,
             )
             from prowler.providers.m365.services.defender.defender_service import (
                 ConnectionFilterPolicy,
@@ -78,16 +80,18 @@ class Test_defender_antispam_connection_filter_policy_ip_allow_list_not_used:
                 identity="Default",
             )
 
-            check = defender_antispam_connection_filter_policy_ip_allow_list_not_used()
+            check = defender_antispam_connection_filter_policy_empty_ip_allowlist()
             result = check.execute()
 
             assert len(result) == 1
             assert result[0].status == "FAIL"
             assert result[0].status_extended == (
-                "IP Allow List is used in the Defender connection filter policy Default with IPs: ['192.168.0.1', '10.0.0.5']."
+                "IP Allowlist is used in the Antispam Connection Filter Policy Default with IPs: ['192.168.0.1', '10.0.0.5']."
             )
             assert result[0].resource == defender_client.connection_filter_policy.dict()
-            assert result[0].resource_name == "Defender Connection Filter Policy"
+            assert (
+                result[0].resource_name == "Defender Antispam Connection Filter Policy"
+            )
             assert result[0].resource_id == "Default"
             assert result[0].location == "global"
 
@@ -105,16 +109,16 @@ class Test_defender_antispam_connection_filter_policy_ip_allow_list_not_used:
                 "prowler.providers.m365.lib.powershell.m365_powershell.M365PowerShell.connect_exchange_online"
             ),
             mock.patch(
-                "prowler.providers.m365.services.defender.defender_antispam_connection_filter_policy_ip_allow_list_not_used.defender_antispam_connection_filter_policy_ip_allow_list_not_used.defender_client",
+                "prowler.providers.m365.services.defender.defender_antispam_connection_filter_policy_empty_ip_allowlist.defender_antispam_connection_filter_policy_empty_ip_allowlist.defender_client",
                 new=defender_client,
             ),
         ):
-            from prowler.providers.m365.services.defender.defender_antispam_connection_filter_policy_ip_allow_list_not_used.defender_antispam_connection_filter_policy_ip_allow_list_not_used import (
-                defender_antispam_connection_filter_policy_ip_allow_list_not_used,
+            from prowler.providers.m365.services.defender.defender_antispam_connection_filter_policy_empty_ip_allowlist.defender_antispam_connection_filter_policy_empty_ip_allowlist import (
+                defender_antispam_connection_filter_policy_empty_ip_allowlist,
             )
 
             defender_client.connection_filter_policy = None
 
-            check = defender_antispam_connection_filter_policy_ip_allow_list_not_used()
+            check = defender_antispam_connection_filter_policy_empty_ip_allowlist()
             result = check.execute()
             assert len(result) == 0
