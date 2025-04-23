2054efb0f068de6874f258a987aadc0040ecf063
HugoPBrito <hugopbrit@gmail.com>
2025-04-23 16:52:24 +0200

    Revert "feat: pwsh setup and print credentials comprobation"
    This reverts commit a6dd97b6bca614b5f1f93569ba29c2aa08d3236f.

--- prowler/providers/m365/m365_provider.py
+++ prowler/providers/m365/m365_provider.py
@@ -447,11 +447,8 @@ class M365Provider(Provider):
             f"M365 Region: {Fore.YELLOW}{self.region_config.name}{Style.RESET_ALL}",
             f"M365 Tenant Domain: {Fore.YELLOW}{self._identity.tenant_domain}{Style.RESET_ALL} M365 Tenant ID: {Fore.YELLOW}{self._identity.tenant_id}{Style.RESET_ALL}",
             f"M365 Identity Type: {Fore.YELLOW}{self._identity.identity_type}{Style.RESET_ALL} M365 Identity ID: {Fore.YELLOW}{self._identity.identity_id}{Style.RESET_ALL}",
+            f"M365 User: {Fore.YELLOW}{self.credentials.user}{Style.RESET_ALL}",
         ]
-        if self.credentials and self.credentials.user:
-            report_lines.append(
-                f"M365 User: {Fore.YELLOW}{self.credentials.user}{Style.RESET_ALL}"
-            )
         report_title = (
             f"{Style.BRIGHT}Using the M365 credentials below:{Style.RESET_ALL}"
         )
@@ -703,14 +700,11 @@ class M365Provider(Provider):
             logger.info("M365 provider: Connection to MSGraph successful")
 
             # Set up PowerShell credentials
-            if user and encrypted_password:
-                M365Provider.setup_powershell(
-                    env_auth,
-                    m365_credentials,
-                    provider_id,
-                )
-            else:
-                logger.info("M365 provider: PowerShell authentication not required")
+            M365Provider.setup_powershell(
+                env_auth,
+                m365_credentials,
+                provider_id,
+            )
 
             logger.info("M365 provider: Connection to PowerShell successful")
 
