b14b440059b88efa39792f4aff648b92f7090a1a
pedrooot <pedromarting3@gmail.com>
2025-04-25 13:20:08 +0200

    feat(threatscore): add tests

--- prowler/lib/outputs/compliance/prowler_threatscore/prowler_threatscore_aws.py
+++ prowler/lib/outputs/compliance/prowler_threatscore/prowler_threatscore_aws.py
@@ -37,6 +37,8 @@ class ProwlerThreatScoreAWS(ComplianceOutput):
         """
         for finding in findings:
             # Get the compliance requirements for the finding
+            print(finding.compliance)
+            print(compliance_name)
             finding_requirements = finding.compliance.get(compliance_name, [])
             for requirement in compliance.Requirements:
                 if requirement.Id in finding_requirements:
