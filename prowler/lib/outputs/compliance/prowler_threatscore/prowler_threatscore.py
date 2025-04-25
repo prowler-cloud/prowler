fae996a0267ae8fe9eb25409ba5248989facd35d
pedrooot <pedromarting3@gmail.com>
2025-04-25 11:40:07 +0200

    feat(compliance): change table outputs

--- prowler/lib/outputs/compliance/prowler_threatscore/prowler_threatscore.py
+++ prowler/lib/outputs/compliance/prowler_threatscore/prowler_threatscore.py
@@ -12,22 +12,19 @@ def get_prowler_threatscore_table(
     output_directory: str,
     compliance_overview: bool,
 ):
-    threatscore_compliance_table = {
-        "Pillar": [],
-        "Sub-Pillar": [],
-        "Score": [],
-    }
     pillar_table = {
+        "Provider": [],
         "Pillar": [],
+        "Status": [],
+        "Muted": [],
         "Score": [],
     }
     pass_count = []
     fail_count = []
     muted_count = []
+    pillars = {}
     score_per_pillar = {}
     number_findings_per_pillar = {}
-    score_per_sub_pillar = {}
-    number_findings_per_sub_pillar = {}
     for index, finding in enumerate(findings):
         check = bulk_checks_metadata[finding.check_metadata.CheckID]
         check_compliances = check.Compliance
@@ -35,22 +32,6 @@ def get_prowler_threatscore_table(
             if compliance.Framework == "ProwlerThreatScore":
                 for requirement in compliance.Requirements:
                     for attribute in requirement.Attributes:
-                        sub_pillar = attribute.SubSection
-
-                        if sub_pillar not in score_per_sub_pillar.keys():
-                            score_per_sub_pillar[sub_pillar] = 0
-                            number_findings_per_sub_pillar[sub_pillar] = 0
-                        if finding.status == "FAIL" and not finding.muted:
-                            score_per_sub_pillar[sub_pillar] += attribute.LevelOfRisk
-                            number_findings_per_sub_pillar[sub_pillar] += 1
-                        if finding.muted and index not in muted_count:
-                            muted_count.append(index)
-                        else:
-                            if finding.status == "FAIL" and index not in fail_count:
-                                fail_count.append(index)
-                            elif finding.status == "PASS" and index not in pass_count:
-                                pass_count.append(index)
-
                         pillar = attribute.Section
 
                         if pillar not in score_per_pillar.keys():
@@ -60,59 +41,43 @@ def get_prowler_threatscore_table(
                             score_per_pillar[pillar] += attribute.LevelOfRisk
                             number_findings_per_pillar[pillar] += 1
 
-    # Create Pillar Table
-    for index, finding in enumerate(findings):
-        check = bulk_checks_metadata[finding.check_metadata.CheckID]
-        check_compliances = check.Compliance
-        for compliance in check_compliances:
-            if compliance.Framework == "ProwlerThreatScore":
-                for requirement in compliance.Requirements:
-                    for attribute in requirement.Attributes:
-                        if (
-                            f"{Fore.YELLOW}{attribute.Section}{Style.RESET_ALL}"
-                            not in pillar_table["Pillar"]
-                        ):
-                            pillar_table["Pillar"].append(
-                                f"{Fore.YELLOW}{attribute.Section}{Style.RESET_ALL}"
-                            )
-                            if number_findings_per_pillar[attribute.Section] == 0:
-                                pillar_table["Score"].append(
-                                    f"{orange_color}0{Style.RESET_ALL}"
-                                )
-                            else:
-                                pillar_table["Score"].append(
-                                    f"{orange_color}{score_per_pillar[attribute.Section] / number_findings_per_pillar[attribute.Section]:.2f}/5{Style.RESET_ALL}"
-                                )
+                        if pillar not in pillars:
+                            pillars[pillar] = {"FAIL": 0, "PASS": 0, "Muted": 0}
 
-    # Create Sub-Pillar Table
-    for index, finding in enumerate(findings):
-        check = bulk_checks_metadata[finding.check_metadata.CheckID]
-        check_compliances = check.Compliance
-        for compliance in check_compliances:
-            if compliance.Framework == "ProwlerThreatScore":
-                for requirement in compliance.Requirements:
-                    for attribute in requirement.Attributes:
-                        if (
-                            attribute.SubSection
-                            not in threatscore_compliance_table["Sub-Pillar"]
-                        ):
-                            threatscore_compliance_table["Pillar"].append(
-                                f"{Fore.YELLOW}{attribute.Section}{Style.RESET_ALL}"
-                            )
-                            threatscore_compliance_table["Sub-Pillar"].append(
-                                attribute.SubSection
-                            )
-                            if (
-                                number_findings_per_sub_pillar[attribute.SubSection]
-                                == 0
-                            ):
-                                threatscore_compliance_table["Score"].append(
-                                    f"{orange_color}0{Style.RESET_ALL}"
-                                )
-                            else:
-                                threatscore_compliance_table["Score"].append(
-                                    f"{orange_color}{score_per_sub_pillar[attribute.SubSection] / number_findings_per_sub_pillar[attribute.SubSection]:.2f}/5{Style.RESET_ALL}"
-                                )
+                        if finding.muted:
+                            if index not in muted_count:
+                                muted_count.append(index)
+                                pillars[pillar]["Muted"] += 1
+                        else:
+                            if finding.status == "FAIL" and index not in fail_count:
+                                fail_count.append(index)
+                                pillars[pillar]["FAIL"] += 1
+                            elif finding.status == "PASS" and index not in pass_count:
+                                pass_count.append(index)
+                                pillars[pillar]["PASS"] += 1
+
+    pillars = dict(sorted(pillars.items()))
+    for pillar in pillars:
+        pillar_table["Provider"].append(compliance.Provider)
+        pillar_table["Pillar"].append(pillar)
+        if number_findings_per_pillar[pillar] == 0:
+            pillar_table["Score"].append(f"{Fore.MAGENTA}0{Style.RESET_ALL}")
+        else:
+            pillar_table["Score"].append(
+                f"{Fore.MAGENTA}{score_per_pillar[pillar] / number_findings_per_pillar[pillar]:.2f}/5{Style.RESET_ALL}"
+            )
+        if pillars[pillar]["FAIL"] > 0:
+            pillar_table["Status"].append(
+                f"{Fore.RED}FAIL({pillars[pillar]['FAIL']}){Style.RESET_ALL}"
+            )
+        else:
+            pillar_table["Status"].append(
+                f"{Fore.GREEN}PASS({pillars[pillar]['PASS']}){Style.RESET_ALL}"
+            )
+        pillar_table["Muted"].append(
+            f"{orange_color}{pillars[pillar]['Muted']}{Style.RESET_ALL}"
+        )
+
     if (
         len(fail_count) + len(pass_count) + len(muted_count) > 1
     ):  # If there are no resources, don't print the compliance table
@@ -129,61 +94,18 @@ def get_prowler_threatscore_table(
         ]
         print(tabulate(overview_table, tablefmt="rounded_grid"))
         if not compliance_overview:
-            if len(fail_count) > 0 and any(
-                [
-                    len(threatscore_compliance_table["Pillar"]) > 0,
-                    len(pillar_table["Pillar"]) > 0,
-                ]
-            ):
+            if len(fail_count) > 0 and len(pillar_table["Pillar"]) > 0:
                 print(
                     f"\nFramework {Fore.YELLOW}{compliance_framework.upper()}{Style.RESET_ALL} Results:"
                 )
 
-                if len(threatscore_compliance_table["Pillar"]) > 0:
-                    combined = list(
-                        zip(
-                            threatscore_compliance_table["Sub-Pillar"],
-                            threatscore_compliance_table["Pillar"],
-                            threatscore_compliance_table["Score"],
-                        )
+                print(
+                    tabulate(
+                        pillar_table,
+                        tablefmt="rounded_grid",
+                        headers="keys",
                     )
-                    combined.sort(key=lambda x: x[0])
-                    sub_pillars_sorted, pillars_sorted, scores_sorted = zip(*combined)
-
-                    threatscore_compliance_table["Sub-Pillar"] = list(
-                        sub_pillars_sorted
-                    )
-                    threatscore_compliance_table["Pillar"] = list(pillars_sorted)
-                    threatscore_compliance_table["Score"] = list(scores_sorted)
-
-                    print(
-                        tabulate(
-                            threatscore_compliance_table,
-                            tablefmt="rounded_grid",
-                            headers="keys",
-                        )
-                    )
-
-                if len(pillar_table["Pillar"]) > 0:
-                    combined = list(
-                        zip(
-                            pillar_table["Pillar"],
-                            pillar_table["Score"],
-                        )
-                    )
-                    combined.sort(key=lambda x: x[0])
-                    pillars_sorted, scores_sorted = zip(*combined)
-
-                    pillar_table["Pillar"] = list(pillars_sorted)
-                    pillar_table["Score"] = list(scores_sorted)
-
-                    print(
-                        tabulate(
-                            pillar_table,
-                            tablefmt="rounded_grid",
-                            headers="keys",
-                        )
-                    )
+                )
 
                 print(
                     f"{Style.BRIGHT}{Fore.MAGENTA}\n=== Risk Score Guide ===\nScore ranges from 1 (lowest risk) to 5 (highest risk), indicating the severity of the potential impact.\n{Style.RESET_ALL}"
