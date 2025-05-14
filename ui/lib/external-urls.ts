755a19e2b6469a7281b774abc1562a177c29aa9e
Pepe Fagoaga <pepe@prowler.com>
2025-05-14 09:27:05 +0200

    chore: add external-urls

---
+++ ui/lib/external-urls.ts
@@ -0,0 +1,45 @@
+export const getProviderHelpText = (provider: string) => {
+  switch (provider) {
+    case "aws":
+      return {
+        text: "Need help connecting your AWS account?",
+        link: "https://goto.prowler.com/provider-aws",
+      };
+    case "azure":
+      return {
+        text: "Need help connecting your Azure subscription?",
+        link: "https://goto.prowler.com/provider-azure",
+      };
+    case "m365":
+      return {
+        text: "Need help connecting your Microsoft 365 account?",
+        link: "https://goto.prowler.com/provider-m365",
+      };
+    case "gcp":
+      return {
+        text: "Need help connecting your GCP project?",
+        link: "https://goto.prowler.com/provider-gcp",
+      };
+    case "kubernetes":
+      return {
+        text: "Need help connecting your Kubernetes cluster?",
+        link: "https://goto.prowler.com/provider-k8s",
+      };
+    default:
+      return {
+        text: "How to setup a provider?",
+        link: "https://goto.prowler.com/provider-help",
+      };
+  }
+};
+
+export const getAWSCredentialsTemplateLinks = () => {
+  return {
+    cloudformation:
+      "https://github.com/prowler-cloud/prowler/blob/master/permissions/templates/cloudformation/prowler-scan-role.yml",
+    cloudformationQuickLink:
+      "https://us-east-1.console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/quickcreate?templateURL=https%3A%2F%2Fprowler-cloud-public.s3.eu-west-1.amazonaws.com%2Fpermissions%2Ftemplates%2Faws%2Fcloudformation%2Fprowler-scan-role.yml&stackName=ProwlerScanRole&param_ExternalId=",
+    terraform:
+      "https://github.com/prowler-cloud/prowler/blob/master/permissions/templates/terraform/main.tf",
+  };
+};
