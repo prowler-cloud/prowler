5211701f772f50a9027f75744da10eee0a3fea9c
Andoni A. <14891798+andoniaf@users.noreply.github.com>
2025-05-16 11:00:33 +0200

    feat(repository): add new check `repository_sensitive_data_scanner_enabled`

--- prowler/providers/github/services/repository/repository_service.py
+++ prowler/providers/github/services/repository/repository_service.py
@@ -94,6 +94,18 @@ class Repository(GithubService):
                                 f"{error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
                             )
 
+                    secret_scanning_enabled = False
+                    try:
+                        if repo.security_and_analysis:
+                            secret_scanning_enabled = (
+                                repo.security_and_analysis.secret_scanning.status
+                                == "enabled"
+                            )
+                    except Exception as error:
+                        logger.error(
+                            f"Secret scanning detection error in repo {repo.name}: {error}"
+                        )
+                        secret_scanning_enabled = None
                     repos[repo.id] = Repo(
                         id=repo.id,
                         name=repo.name,
@@ -109,6 +121,7 @@ class Repository(GithubService):
                         default_branch_protection=branch_protection,
                         codeowners_exists=codeowners_exists,
                         require_code_owner_reviews=require_code_owner_reviews,
+                        secret_scanning_enabled=secret_scanning_enabled,
                     )
 
         except Exception as error:
@@ -135,3 +148,4 @@ class Repo(BaseModel):
     approval_count: Optional[int]
     codeowners_exists: Optional[bool]
     require_code_owner_reviews: Optional[bool]
+    secret_scanning_enabled: Optional[bool]
---
+++ tests/providers/github/services/repository/repository_sensitive_data_scanner_enabled/repository_sensitive_data_scanner_enabled_test.py
@@ -0,0 +1,112 @@
+from unittest import mock
+
+from prowler.providers.github.services.repository.repository_service import Repo
+from tests.providers.github.github_fixtures import set_mocked_github_provider
+
+
+class Test_repository_sensitive_data_scanner_enabled:
+    def test_no_repositories(self):
+        repository_client = mock.MagicMock
+        repository_client.repositories = {}
+
+        with (
+            mock.patch(
+                "prowler.providers.common.provider.Provider.get_global_provider",
+                return_value=set_mocked_github_provider(),
+            ),
+            mock.patch(
+                "prowler.providers.github.services.repository.repository_sensitive_data_scanner_enabled.repository_sensitive_data_scanner_enabled.repository_client",
+                new=repository_client,
+            ),
+        ):
+            from prowler.providers.github.services.repository.repository_sensitive_data_scanner_enabled.repository_sensitive_data_scanner_enabled import (
+                repository_sensitive_data_scanner_enabled,
+            )
+
+            check = repository_sensitive_data_scanner_enabled()
+            result = check.execute()
+            assert len(result) == 0
+
+    def test_one_repository_no_secret_scanning(self):
+        repository_client = mock.MagicMock
+        repo_name = "repo1"
+        repository_client.repositories = {
+            1: Repo(
+                id=1,
+                name=repo_name,
+                full_name="account-name/repo1",
+                default_branch="main",
+                private=False,
+                securitymd=True,
+                require_pull_request=False,
+                approval_count=0,
+                secret_scanning_enabled=False,
+            ),
+        }
+
+        with (
+            mock.patch(
+                "prowler.providers.common.provider.Provider.get_global_provider",
+                return_value=set_mocked_github_provider(),
+            ),
+            mock.patch(
+                "prowler.providers.github.services.repository.repository_sensitive_data_scanner_enabled.repository_sensitive_data_scanner_enabled.repository_client",
+                new=repository_client,
+            ),
+        ):
+            from prowler.providers.github.services.repository.repository_sensitive_data_scanner_enabled.repository_sensitive_data_scanner_enabled import (
+                repository_sensitive_data_scanner_enabled,
+            )
+
+            check = repository_sensitive_data_scanner_enabled()
+            result = check.execute()
+            assert len(result) == 1
+            assert result[0].resource_id == 1
+            assert result[0].resource_name == repo_name
+            assert result[0].status == "FAIL"
+            assert (
+                result[0].status_extended
+                == f"Repository {repo_name} does not have secret scanning enabled to detect sensitive data."
+            )
+
+    def test_one_repository_with_secret_scanning(self):
+        repository_client = mock.MagicMock
+        repo_name = "repo2"
+        repository_client.repositories = {
+            2: Repo(
+                id=2,
+                name=repo_name,
+                full_name="account-name/repo2",
+                default_branch="main",
+                private=False,
+                securitymd=True,
+                require_pull_request=False,
+                approval_count=0,
+                secret_scanning_enabled=True,
+            ),
+        }
+
+        with (
+            mock.patch(
+                "prowler.providers.common.provider.Provider.get_global_provider",
+                return_value=set_mocked_github_provider(),
+            ),
+            mock.patch(
+                "prowler.providers.github.services.repository.repository_sensitive_data_scanner_enabled.repository_sensitive_data_scanner_enabled.repository_client",
+                new=repository_client,
+            ),
+        ):
+            from prowler.providers.github.services.repository.repository_sensitive_data_scanner_enabled.repository_sensitive_data_scanner_enabled import (
+                repository_sensitive_data_scanner_enabled,
+            )
+
+            check = repository_sensitive_data_scanner_enabled()
+            result = check.execute()
+            assert len(result) == 1
+            assert result[0].resource_id == 2
+            assert result[0].resource_name == repo_name
+            assert result[0].status == "PASS"
+            assert (
+                result[0].status_extended
+                == f"Repository {repo_name} has secret scanning enabled to detect sensitive data."
+            )
