7385273b435a8cb1b70ee529d11ca8e6a238d4b6
Chandrapal Badshah <12944530+Chan9390@users.noreply.github.com>
2025-05-16 12:23:21 +0530

    Add layout to lighthouse config

---
+++ ui/app/(prowler)/lighthouse/config/layout.tsx
@@ -0,0 +1,19 @@
+import "@/styles/globals.css";
+
+import React from "react";
+
+import { ContentLayout } from "@/components/ui";
+
+interface ChatbotConfigLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function ChatbotConfigLayout({
+  children,
+}: ChatbotConfigLayoutProps) {
+  return (
+    <ContentLayout title="Configure Lighthouse" icon="lucide:settings">
+      {children}
+    </ContentLayout>
+  );
+}
