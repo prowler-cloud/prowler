f74ceefb70f8cbeaa4aebc5e8ac5af3bee568eb5
sumit_chaturvedi <chaturvedi.sumit@tftus.com>
2025-05-16 14:01:24 +0530

    srn change log update

--- ui/actions/scans/scans.ts
+++ ui/actions/scans/scans.ts
@@ -239,7 +239,10 @@ export const getExportsZip = async (scanId: string) => {
     if (!response.ok) {
       const errorData = await response.json();
       // TODO: Updated the getting of error message from `errors[0]?.` to `errors?.` to support currently, but need to be updated in backend to make it consistent
-      throw new Error(errorData?.errors?.detail || "Scan report couldn't be fetched. Please check your download settings or contact support if the problem continues");
+      throw new Error(
+        errorData?.errors?.detail ||
+          "Scan report couldn't be fetched. Please check your download settings or contact support if the problem continues",
+      );
     }
 
     // Get the blob data as an array buffer
@@ -278,7 +281,8 @@ export const getComplianceCsv = async (
       const errorData = await response.json();
       // TODO: Updated the getting of error message from `errors[0]?.` to `errors?.` to support currently, but need to be updated in backend to make it consistent
       throw new Error(
-        errorData?.errors?.detail || "Compliance report couldn't be fetched. Please check your download settings or contact support if the problem continues",
+        errorData?.errors?.detail ||
+          "Compliance report couldn't be fetched. Please check your download settings or contact support if the problem continues",
       );
     }
 
