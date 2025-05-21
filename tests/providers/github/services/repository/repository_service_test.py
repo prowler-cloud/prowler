d036e0054b39980a3e6c4cc2ad76cd0a35c6414c
Andoni Alonso <14891798+andoniaf@users.noreply.github.com>
2025-05-21 16:18:55 +0200

    feat(repository): add new check `repository_default_branch_requires_codeowners_review` (#7753)
    Co-authored-by: Sergio Garcia <hello@mistercloudsec.com>

--- tests/providers/github/services/repository/repository_service_test.py
+++ tests/providers/github/services/repository/repository_service_test.py
@@ -24,6 +24,7 @@ def mock_list_repositories(_):
             status_checks=True,
             approval_count=2,
             codeowners_exists=True,
+            require_code_owner_reviews=True,
             enforce_admins=True,
             delete_branch_on_merge=True,
             conversation_resolution=True,
@@ -62,6 +63,7 @@ class Test_Repository_Service:
         assert repository_service.repositories[1].conversation_resolution
         assert repository_service.repositories[1].approval_count == 2
         assert repository_service.repositories[1].codeowners_exists is True
+        assert repository_service.repositories[1].require_code_owner_reviews is True
 
 
 class Test_Repository_FileExists:
