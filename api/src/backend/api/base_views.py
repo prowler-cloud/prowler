714a03c5334aa7e991b6071bd341197a2c500f20
Víctor Fernández Poyatos <victor@prowler.com>
2025-06-03 16:38:00 +0200

    revert(views): calling order to initial view method (#7921)
    (cherry picked from commit 6e7a32cb516e813b441e0fe2f7766a9ce270a488)

--- api/src/backend/api/base_views.py
+++ api/src/backend/api/base_views.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.db import transaction
 from rest_framework import permissions
 from rest_framework.exceptions import NotAuthenticated
 from rest_framework.filters import SearchFilter
@@ -47,6 +48,8 @@ class BaseViewSet(ModelViewSet):
 
 class BaseRLSViewSet(BaseViewSet):
     def initial(self, request, *args, **kwargs):
+        super().initial(request, *args, **kwargs)
+
         # Ideally, this logic would be in the `.setup()` method but DRF view sets don't call it
         # https://docs.djangoproject.com/en/5.1/ref/class-based-views/base/#django.views.generic.base.View.setup
         if request.auth is None:
@@ -60,8 +63,6 @@ class BaseRLSViewSet(BaseViewSet):
 
         self._rls_cm = rls_transaction(tenant_id)
         self._rls_cm.__enter__()
-
-        super().initial(request, *args, **kwargs)
 
     def finalize_response(self, request, response, *args, **kwargs):
         response = super().finalize_response(request, response, *args, **kwargs)
@@ -80,7 +81,8 @@ class BaseRLSViewSet(BaseViewSet):
 
 class BaseTenantViewset(BaseViewSet):
     def dispatch(self, request, *args, **kwargs):
-        tenant = super().dispatch(request, *args, **kwargs)
+        with transaction.atomic():
+            tenant = super().dispatch(request, *args, **kwargs)
 
         try:
             # If the request is a POST, create the admin role
@@ -115,6 +117,8 @@ class BaseTenantViewset(BaseViewSet):
                 pass  # Tenant might not exist, handle gracefully
 
     def initial(self, request, *args, **kwargs):
+        super().initial(request, *args, **kwargs)
+
         if request.auth is None:
             raise NotAuthenticated
 
@@ -126,8 +130,6 @@ class BaseTenantViewset(BaseViewSet):
 
         self._rls_cm = rls_transaction(value=user_id, parameter=POSTGRES_USER_VAR)
         self._rls_cm.__enter__()
-
-        super().initial(request, *args, **kwargs)
 
     def finalize_response(self, request, response, *args, **kwargs):
         response = super().finalize_response(request, response, *args, **kwargs)
@@ -141,9 +143,11 @@ class BaseTenantViewset(BaseViewSet):
 
 class BaseUserViewset(BaseViewSet):
     def initial(self, request, *args, **kwargs):
+        super().initial(request, *args, **kwargs)
+
         # TODO refactor after improving RLS on users
         if request.stream is not None and request.stream.method == "POST":
-            return super().initial(request, *args, **kwargs)
+            return
         if request.auth is None:
             raise NotAuthenticated
 
@@ -155,8 +159,6 @@ class BaseUserViewset(BaseViewSet):
 
         self._rls_cm = rls_transaction(tenant_id)
         self._rls_cm.__enter__()
-
-        super().initial(request, *args, **kwargs)
 
     def finalize_response(self, request, response, *args, **kwargs):
         response = super().finalize_response(request, response, *args, **kwargs)
