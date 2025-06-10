94d4aec6662cfc6b6d0ec57effd3d86ff75d9fbf
Chandrapal Badshah <12944530+Chan9390@users.noreply.github.com>
2025-06-10 16:41:14 +0530

    fix: update migration file

--- api/src/backend/api/migrations/0031_lighthouseconfig.py
+++ api/src/backend/api/migrations/0031_lighthouseconfig.py
@@ -10,7 +10,7 @@ import api.rls
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("api", "0029_findings_check_index_parent"),
+        ("api", "0030_samlconfigurations"),
     ]
     operations = [
         migrations.CreateModel(
