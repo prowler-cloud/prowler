01f798e7f1eac07d51e67ccac963b01a443eeecd
Drew Kerrigan <drew@prowler.com>
2025-06-16 20:29:04 -0400

    fix: update processors schema to use prowler version

--- api/src/backend/api/v1/serializer_utils/processors.py
+++ api/src/backend/api/v1/serializer_utils/processors.py
@@ -2,6 +2,8 @@ from drf_spectacular.utils import extend_schema_field
 
 from api.v1.serializer_utils.base import YamlOrJsonField
 
+from prowler.lib.mutelist.mutelist import mutelist_schema
+
 
 @extend_schema_field(
     {
@@ -9,93 +11,7 @@ from api.v1.serializer_utils.base import YamlOrJsonField
             {
                 "type": "object",
                 "title": "Mutelist",
-                "properties": {
-                    "Mutelist": {
-                        "properties": {
-                            "Accounts": {
-                                "patternProperties": {
-                                    ".*": {
-                                        "properties": {
-                                            "Checks": {
-                                                "patternProperties": {
-                                                    ".*": {
-                                                        "properties": {
-                                                            "Exceptions": {
-                                                                "properties": {
-                                                                    "Accounts": {
-                                                                        "items": {
-                                                                            "type": "string"
-                                                                        },
-                                                                        "type": "array",
-                                                                    },
-                                                                    "Regions": {
-                                                                        "items": {
-                                                                            "type": "string"
-                                                                        },
-                                                                        "type": "array",
-                                                                    },
-                                                                    "Resources": {
-                                                                        "items": {
-                                                                            "type": "string"
-                                                                        },
-                                                                        "type": "array",
-                                                                    },
-                                                                    "Tags": {
-                                                                        "items": {
-                                                                            "type": "string"
-                                                                        },
-                                                                        "type": "array",
-                                                                    },
-                                                                },
-                                                                "required": [],
-                                                                "type": "object",
-                                                                "additionalProperties": False,
-                                                            },
-                                                            "Regions": {
-                                                                "items": {
-                                                                    "type": "string"
-                                                                },
-                                                                "type": "array",
-                                                            },
-                                                            "Resources": {
-                                                                "items": {
-                                                                    "type": "string"
-                                                                },
-                                                                "type": "array",
-                                                            },
-                                                            "Tags": {
-                                                                "items": {
-                                                                    "type": "string"
-                                                                },
-                                                                "type": "array",
-                                                            },
-                                                        },
-                                                        "required": [
-                                                            "Regions",
-                                                            "Resources",
-                                                        ],
-                                                        "type": "object",
-                                                        "additionalProperties": False,
-                                                    }
-                                                },
-                                                "required": [],
-                                                "type": "object",
-                                            }
-                                        },
-                                        "required": ["Checks"],
-                                        "type": "object",
-                                        "additionalProperties": False,
-                                    }
-                                },
-                                "required": [],
-                                "type": "object",
-                            }
-                        },
-                        "required": ["Accounts"],
-                        "type": "object",
-                        "additionalProperties": False,
-                    }
-                },
+                "properties": {"Mutelist": mutelist_schema},
                 "additionalProperties": False,
             },
         ]
