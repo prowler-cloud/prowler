54d8917410bdaad4239c7609b53b739369dc4003
MrCloudSec <hello@mistercloudsec.com>
2025-06-18 19:02:56 +0800

    chore: revision

--- prowler/lib/outputs/compliance/compliance_output.py
+++ prowler/lib/outputs/compliance/compliance_output.py
@@ -2,7 +2,6 @@ from csv import DictWriter
 from pathlib import Path
 from typing import List
 
-from prowler.config.config import timestamp
 from prowler.lib.check.compliance_models import Compliance
 from prowler.lib.logger import logger
 from prowler.lib.outputs.finding import Finding
