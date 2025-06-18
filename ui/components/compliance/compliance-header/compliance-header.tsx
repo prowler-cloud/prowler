cdc48e8e89e5c2e2d4ad6da3858dec927d4a13eb
alejandrobailo <alejandrobailo94@gmail.com>
2025-06-18 12:43:02 +0200

    fix: compliance layout after merge master

--- ui/components/compliance/compliance-header/compliance-header.tsx
+++ ui/components/compliance/compliance-header/compliance-header.tsx
@@ -58,11 +58,10 @@ export const ComplianceHeader = ({
     <>
       {(showProviders || showSearch) && (
         <>
-          <div className="flex items-center justify-start gap-4">
+          <div className="flex items-start justify-start gap-4">
             {showProviders && <DataCompliance scans={scans} />}
             {showSearch && <FilterControls search />}
           </div>
-          <Spacer y={8} />
         </>
       )}
       {allFilters.length > 0 && (
