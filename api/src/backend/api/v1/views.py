5b1e7bb7f970458e744058197b311af867376213
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2025-06-24 15:55:01 +0200

    fix(saml): avoid IndexError when some attributes are not specified (#8089)

--- api/src/backend/api/v1/views.py
+++ api/src/backend/api/v1/views.py
@@ -509,10 +509,16 @@ class TenantFinishACSView(FinishACSView):
             return response
 
         extra = social_account.extra_data
-        user.first_name = extra.get("firstName", [""])[0]
-        user.last_name = extra.get("lastName", [""])[0]
-        user.company_name = extra.get("organization", [""])[0]
+        user.first_name = (
+            extra.get("firstName", [""])[0] if extra.get("firstName") else ""
+        )
+        user.last_name = extra.get("lastName", [""])[0] if extra.get("lastName") else ""
+        user.company_name = (
+            extra.get("organization", [""])[0] if extra.get("organization") else ""
+        )
         user.name = f"{user.first_name} {user.last_name}".strip()
+        if user.name == "":
+            user.name = "N/A"
         user.save()
 
         email_domain = user.email.split("@")[-1]
@@ -521,7 +527,11 @@ class TenantFinishACSView(FinishACSView):
             .get(email_domain=email_domain)
             .tenant
         )
-        role_name = extra.get("userType", ["saml_default_role"])[0].strip()
+        role_name = (
+            extra.get("userType", ["saml_default_role"])[0].strip()
+            if extra.get("userType")
+            else "saml_default_role"
+        )
         try:
             role = Role.objects.using(MainRouter.admin_db).get(
                 name=role_name, tenant=tenant
