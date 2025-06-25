3bad5f1a853097f8a1acd2700bbb04e581b25f17
alejandrobailo <alejandrobailo94@gmail.com>
2025-06-25 16:48:12 +0200

    feat: new temporal id code implemented

--- ui/app/api/auth/callback/saml/route.ts
+++ ui/app/api/auth/callback/saml/route.ts
@@ -3,22 +3,39 @@
 import { NextResponse } from "next/server";
 
 import { signIn } from "@/auth.config";
-import { baseUrl } from "@/lib/helper";
+import { apiBaseUrl, baseUrl } from "@/lib/helper";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
+  const id = searchParams.get("id");
 
-  const access = searchParams.get("access");
-  const refresh = searchParams.get("refresh");
-
-  if (!access || !refresh) {
+  if (!id) {
     return NextResponse.json(
-      { error: "Access token or refresh token is missing" },
+      { error: "ID parameter is missing" },
       { status: 400 },
     );
   }
 
   try {
+    const response = await fetch(`${apiBaseUrl}/tokens/saml?id=${id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/vnd.api+json",
+        Accept: "application/vnd.api+json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch tokens: ${response.statusText}`);
+    }
+
+    const tokenData = await response.json();
+    const { access, refresh } = tokenData.data;
+
+    if (!access || !refresh) {
+      throw new Error("Tokens not found in response");
+    }
+
     const result = await signIn("social-oauth", {
       accessToken: access,
       refreshToken: refresh,
@@ -33,8 +50,6 @@ export async function GET(req: Request) {
     return NextResponse.redirect(new URL("/", baseUrl));
   } catch (error) {
     console.error("SAML authentication failed:", error);
-    return NextResponse.redirect(
-      new URL("/sign-in?error=SAMLAuthenticationFailed", baseUrl),
-    );
+    return NextResponse.redirect(new URL("/sign-in", baseUrl));
   }
 }
