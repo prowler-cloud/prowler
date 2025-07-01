14ba165a829ecb671166dcaf8ec0b4b4c3961488
Rubén De la Torre Vico <ruben@prowler.com>
2025-07-01 13:52:52 +0200

    test(azure/vm): add service test for LinuxConfiguration mapping in VirtualMachines
    - Ensures correct mapping of disable_password_authentication for Linux VMs.

--- tests/providers/azure/services/vm/vm_service_test.py
+++ tests/providers/azure/services/vm/vm_service_test.py
@@ -5,6 +5,7 @@ from azure.mgmt.compute.models import ManagedDiskParameters, OSDisk, StorageProf
 
 from prowler.providers.azure.services.vm.vm_service import (
     Disk,
+    LinuxConfiguration,
     VirtualMachine,
     VirtualMachines,
 )
@@ -76,6 +77,24 @@ def mock_vm_get_disks(_):
                 resource_name="DiskTest",
                 vms_attached=["managed_by"],
                 encryption_type="EncryptionAtRestWithPlatformKey",
+            )
+        }
+    }
+
+
+def mock_vm_get_virtual_machines_with_linux(_):
+    return {
+        AZURE_SUBSCRIPTION_ID: {
+            "vm_id-linux": VirtualMachine(
+                resource_id="/subscriptions/resource_id_linux",
+                resource_name="LinuxVM",
+                location="location",
+                security_profile=None,
+                extensions=[],
+                storage_profile=None,
+                linux_configuration=LinuxConfiguration(
+                    disable_password_authentication=True
+                ),
             )
         }
     }
@@ -182,3 +201,14 @@ class Test_VirtualMachines_NoneCases:
         assert vm_2.storage_profile.os_disk is None
         assert vm_2.storage_profile.data_disks is None
         assert vm_2.resource_name == "VMWithPartialNone"
+
+
+@patch(
+    "prowler.providers.azure.services.vm.vm_service.VirtualMachines._get_virtual_machines",
+    new=mock_vm_get_virtual_machines_with_linux,
+)
+def test_virtual_machine_with_linux_configuration():
+    virtual_machines = VirtualMachines(set_mocked_azure_provider())
+    vm = virtual_machines.virtual_machines[AZURE_SUBSCRIPTION_ID]["vm_id-linux"]
+    assert vm.linux_configuration is not None
+    assert vm.linux_configuration.disable_password_authentication is True
