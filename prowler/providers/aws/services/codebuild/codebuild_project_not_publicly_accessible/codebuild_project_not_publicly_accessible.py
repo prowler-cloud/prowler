6bda3843aab4b282f2296334292244ed0b775243
MrCloudSec <hello@mistercloudsec.com>
2025-07-02 15:22:57 +0800

    chore: revision

--- prowler/providers/aws/services/codebuild/codebuild_project_not_publicly_accessible/codebuild_project_not_publicly_accessible.py
+++ prowler/providers/aws/services/codebuild/codebuild_project_not_publicly_accessible/codebuild_project_not_publicly_accessible.py
@@ -1,11 +1,10 @@
 from typing import List
 
 from prowler.lib.check.models import Check, Check_Report_AWS
-
 from prowler.providers.aws.services.codebuild.codebuild_client import codebuild_client
 
 
-class codebuild_project_is_not_public(Check):  # type: ignore[misc]
+class codebuild_project_not_publicly_accessible(Check):
     def execute(self) -> List[Check_Report_AWS]:
         findings = []
 
@@ -15,16 +14,12 @@ class codebuild_project_is_not_public(Check):  # type: ignore[misc]
             report.resource_id = project.name
             report.resource_arn = arn
             report.region = project.region
+            report.status = "FAIL"
+            report.status_extended = f"CodeBuild project {project.name} is public."
 
-            if project.project_visibility == "PUBLIC_READ":
-                report.status = "FAILED"
-                report.status_extended = f"CodeBuild project {project.name} is PUBLIC."
-            elif project.project_visibility == "PRIVATE":
+            if project.project_visibility == "PRIVATE":
                 report.status = "PASS"
-                report.status_extended = f"CodeBuild project {project.name} is PRIVATE."
-            else:
-                report.status = "UNKNOWN"
-                report.status_extended = f"CodeBuild project {project.name} has an unknown visibility."
+                report.status_extended = f"CodeBuild project {project.name} is private."
 
             findings.append(report)
 
