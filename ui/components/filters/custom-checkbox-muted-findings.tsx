7a595fd5f90df46b11deb9e39a62c8ef362d543d
alejandrobailo <alejandrobailo94@gmail.com>
2025-07-09 10:54:45 +0200

    feat: wip on exclude mute findings

--- ui/components/filters/custom-checkbox-muted-findings.tsx
+++ ui/components/filters/custom-checkbox-muted-findings.tsx
@@ -1,18 +1,38 @@
+"use client";
+
 import { Checkbox } from "@nextui-org/react";
-import React from "react";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+import { useUrlFilters } from "@/hooks/use-url-filters";
 
 export const CustomCheckboxMutedFindings = () => {
+  const { updateFilter } = useUrlFilters();
+  const searchParams = useSearchParams();
+  const [excludeMuted, setExcludeMuted] = useState(
+    searchParams.get("filter[muted]") === "false",
+  );
+
+  const handleMutedChange = (value: boolean) => {
+    setExcludeMuted(value);
+    updateFilter("muted", value ? "false" : "true");
+  };
+
   return (
-    <Checkbox
-      classNames={{
-        label: "text-small",
-        wrapper: "checkbox-update xl:-mt-8",
-      }}
-      size="md"
-      color="danger"
-      aria-label="Include Mutelist"
-    >
-      Include Mutelist
-    </Checkbox>
+    <div className="flex h-full">
+      <Checkbox
+        classNames={{
+          label: "text-small",
+          wrapper: "checkbox-update",
+        }}
+        size="md"
+        color="primary"
+        aria-label="Include Mutelist"
+        isSelected={excludeMuted}
+        onValueChange={handleMutedChange}
+      >
+        Exclude muted findings
+      </Checkbox>
+    </div>
   );
 };
