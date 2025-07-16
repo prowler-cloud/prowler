5c5d8cd78eaedada044e68d39b9da69f0842530e
MrCloudSec <hello@mistercloudsec.com>
2025-07-16 16:27:22 +0800

    chore: add comment

--- prowler/providers/azure/lib/mutelist/mutelist.py
+++ prowler/providers/azure/lib/mutelist/mutelist.py
@@ -10,13 +10,13 @@ class AzureMutelist(Mutelist):
         subscription_id: str,
     ) -> bool:
         return self.is_muted(
-            subscription_id,
+            subscription_id,  # support Azure Subscription ID in mutelist
             finding.check_metadata.CheckID,
             finding.location,
             finding.resource_name,
             unroll_dict(unroll_tags(finding.resource_tags)),
         ) or self.is_muted(
-            finding.subscription,
+            finding.subscription,  # support Azure Subscription Name in mutelist
             finding.check_metadata.CheckID,
             finding.location,
             finding.resource_name,
