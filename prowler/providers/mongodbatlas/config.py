b5d6fda00d0d9cabf9dcc25854c12a5fb259c5a5
Andoni A. <14891798+andoniaf@users.noreply.github.com>
2025-07-17 16:55:10 +0200

    chore(mongodbatlas): remove variable used only 1 time

--- prowler/providers/mongodbatlas/config.py
+++ prowler/providers/mongodbatlas/config.py
@@ -1,11 +1,2 @@
-from prowler.lib.logger import logger
-
-# MongoDB Atlas Provider Configuration
-
 # Supported encryption providers
 ATLAS_ENCRYPTION_PROVIDERS = ["AWS", "AZURE", "GCP", "NONE"]
-
-# Network access configuration
-ATLAS_OPEN_WORLD_CIDRS = ["0.0.0.0/0", "::/0"]
-
-logger.info("MongoDB Atlas Provider configuration loaded")
