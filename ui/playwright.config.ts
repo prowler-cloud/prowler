7cdf4c3e533f47db9ded6b5ad6602245bb1fb4b0
alejandrobailo <alejandrobailo94@gmail.com>
2025-07-22 13:05:55 +0200

    build: playright env

--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -30,5 +30,9 @@ export default defineConfig({
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
+    env: {
+      NEXT_PUBLIC_API_BASE_URL:
+        process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080/api/v1",
+    },
   },
 });
