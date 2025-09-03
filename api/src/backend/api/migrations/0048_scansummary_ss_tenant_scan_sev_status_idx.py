bdf5072efaa470838f72fb91dda58ba5fdb45f2e
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2025-09-03 12:17:20 +0200

    chore: format

--- api/src/backend/api/migrations/0048_scansummary_ss_tenant_scan_sev_status_idx.py
+++ api/src/backend/api/migrations/0048_scansummary_ss_tenant_scan_sev_status_idx.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("api", "0047_remove_integration_unique_configuration_per_tenant"),
     ]
