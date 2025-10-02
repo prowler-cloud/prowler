c9946679ee7a68ce06e7d35c687340c19ad068d9
Víctor Fernández Poyatos <victor@prowler.com>
2025-10-02 09:57:15 +0200

    chore: add noqa comment on unused variable

--- api/src/backend/api/schema_extensions.py
+++ api/src/backend/api/schema_extensions.py
@@ -6,7 +6,7 @@ class CombinedJWTOrAPIKeyAuthenticationScheme(OpenApiAuthenticationExtension):
     target_class = "api.authentication.CombinedJWTOrAPIKeyAuthentication"
     name = "JWT or API Key"
 
-    def get_security_definition(self, auto_schema: AutoSchema):
+    def get_security_definition(self, auto_schema: AutoSchema):  # noqa: F841
         return {
             "type": "http",
             "scheme": "bearer",
