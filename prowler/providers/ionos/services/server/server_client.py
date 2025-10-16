50efdc9002e4544f0ae48a7dee3ba760d36268d5
HugoPBrito <hugopbrit@gmail.com>
2025-10-16 13:37:55 +0200

    fix: flake8

--- prowler/providers/ionos/services/server/server_client.py
+++ prowler/providers/ionos/services/server/server_client.py
@@ -1,4 +1,4 @@
-from prowler.providers.ionos.services.server.server_service import IonosServer
 from prowler.providers.common.provider import Provider
+from prowler.providers.ionos.services.server.server_service import IonosServer
 
 ionos_server_client = IonosServer(Provider.get_global_provider())
