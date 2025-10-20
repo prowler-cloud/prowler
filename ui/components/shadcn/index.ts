5e85ef5835cae6ef2e6a9082ba68746232409635
Alejandro Bailo <59607668+alejandrobailo@users.noreply.github.com>
2025-10-20 16:49:09 +0200

    feat(ui): new card components and derivates for overview (#8921)
    Co-authored-by: Alan Buscaglia <gentlemanprogramming@gmail.com>

---
+++ ui/components/shadcn/index.ts
@@ -0,0 +1,21 @@
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./card";
+export {
+  ResourceStatsCard,
+  ResourceStatsCardContainer,
+  type ResourceStatsCardContainerProps,
+  ResourceStatsCardContent,
+  type ResourceStatsCardContentProps,
+  ResourceStatsCardDivider,
+  type ResourceStatsCardDividerProps,
+  ResourceStatsCardHeader,
+  type ResourceStatsCardHeaderProps,
+  type ResourceStatsCardProps,
+  type StatItem,
+} from "./resource-stats-card";
