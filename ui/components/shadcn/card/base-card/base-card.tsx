2bbc3446c1868fd513a61c29cae45bf3a9666309
alejandrobailo <alejandrobailo94@gmail.com>
2025-10-21 14:06:20 +0200

    refactor(shadcn): reorganize card components and remove unused exports

--- ui/components/shadcn/card/base-card/base-card.tsx
+++ ui/components/shadcn/card/base-card/base-card.tsx
@@ -1,5 +1,4 @@
 import { cva, type VariantProps } from "class-variance-authority";
-import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
--- ui/components/shadcn/card/card.tsx
+++ ui/components/shadcn/card/card.tsx
@@ -1,5 +1,3 @@
-import * as React from "react";
-
 import { cn } from "@/lib/utils";
 
 function Card({ className, ...props }: React.ComponentProps<"div">) {
