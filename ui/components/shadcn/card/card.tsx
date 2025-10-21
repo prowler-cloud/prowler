66e6bcbe301a5950349669b63581f5b767835392
alejandrobailo <alejandrobailo94@gmail.com>
2025-10-21 14:53:19 +0200

    fix(shadcn/card): refine CardHeader and CardTitle styles for improved layout

--- ui/components/shadcn/card/card.tsx
+++ ui/components/shadcn/card/card.tsx
@@ -18,7 +18,7 @@ function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card-header"
       className={cn(
-        "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-2 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6",
+        "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-2 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6",
         className,
       )}
       {...props}
@@ -30,7 +30,7 @@ function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-title"
-      className={cn("leading-none font-semibold", className)}
+      className={cn("my-2 text-[18px] leading-none", className)}
       {...props}
     />
   );
