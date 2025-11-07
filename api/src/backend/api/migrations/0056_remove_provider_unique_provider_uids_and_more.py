4c9a3514b912168a0d332cd28c7729d9392a7317
rakan-pro <rakan@prowler.com>
2025-11-07 15:06:00 +0300

    refactor(api): rebase migrations

--- api/src/backend/api/migrations/0056_remove_provider_unique_provider_uids_and_more.py
+++ api/src/backend/api/migrations/0056_remove_provider_unique_provider_uids_and_more.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("api", "0054_iac_provider"),
+        ("api", "0055_mongodbatlas_provider"),
     ]
 
     operations = [
