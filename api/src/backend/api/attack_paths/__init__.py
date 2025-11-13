ecee651a1f6bc63c97515a35b48a9862d53a6243
Josema Camacho <josema@prowler.com>
2025-11-13 16:29:57 +0100

    feat(attack-paths): Add Cartography AWS syncy - WIP - Pt. XXII

--- api/src/backend/api/attack_paths/__init__.py
+++ api/src/backend/api/attack_paths/__init__.py
@@ -1,9 +1,13 @@
 from api.attack_paths.query_definitions import (
+    AttackPathsQueryDefinition,
+    AttackPathsQueryParameterDefinition,
     get_queries_for_provider,
     get_query_by_id,
 )
 
 __all__ = [
+    "AttackPathsQueryDefinition",
+    "AttackPathsQueryParameterDefinition",
     "get_queries_for_provider",
     "get_query_by_id",
 ]
