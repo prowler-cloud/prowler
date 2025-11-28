dc852b459501ab1d1c706c117fde6caa5595b601
lydiavilchez <114735608+lydiavilchez@users.noreply.github.com>
2025-11-28 12:21:58 +0100

    feat(gcp-compute): add automatic restart check for VM instances (#9271)

--- prowler/providers/gcp/services/compute/compute_service.py
+++ prowler/providers/gcp/services/compute/compute_service.py
@@ -133,6 +133,15 @@ class Compute(GCPService):
                                     )
                                     for disk in instance.get("disks", [])
                                 ],
+                                automatic_restart=instance.get("scheduling", {}).get(
+                                    "automaticRestart", False
+                                ),
+                                preemptible=instance.get("scheduling", {}).get(
+                                    "preemptible", False
+                                ),
+                                provisioning_model=instance.get("scheduling", {}).get(
+                                    "provisioningModel", "STANDARD"
+                                ),
                                 project_id=project_id,
                             )
                         )
@@ -365,6 +374,9 @@ class Instance(BaseModel):
     service_accounts: list
     ip_forward: bool
     disks_encryption: list
+    automatic_restart: bool = False
+    preemptible: bool = False
+    provisioning_model: str = "STANDARD"
 
 
 class Network(BaseModel):
