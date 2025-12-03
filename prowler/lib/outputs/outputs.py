dbdce98cf29701b59420d788b64871d94865d219
Sergio Garcia <hello@mistercloudsec.com>
2025-12-03 11:47:55 -0500

    feat(alibaba): add Alibaba Cloud provider (#9329)
    Co-authored-by: pedrooot <pedromarting3@gmail.com>
    Co-authored-by: HugoPBrito <hugopbrit@gmail.com>

--- prowler/lib/outputs/outputs.py
+++ prowler/lib/outputs/outputs.py
@@ -30,6 +30,8 @@ def stdout_report(finding, color, verbose, status, fix):
         details = finding.check_metadata.CheckID
     if finding.check_metadata.Provider == "oraclecloud":
         details = finding.region
+    if finding.check_metadata.Provider == "alibabacloud":
+        details = finding.region
 
     if (verbose or fix) and (not status or finding.status in status):
         if finding.muted:
