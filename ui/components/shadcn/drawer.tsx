af684294afe4feb4ef27fd4ffedc5021274f777a
alejandrobailo <alejandrobailo94@gmail.com>
2026-01-12 15:02:05 +0100

    fix: lint issues

--- ui/components/shadcn/drawer.tsx
+++ ui/components/shadcn/drawer.tsx
@@ -5,9 +5,7 @@ import { Drawer as DrawerPrimitive } from "vaul";
 
 import { cn } from "@/lib/utils";
 
-function Drawer({
-  ...props
-}: ComponentProps<typeof DrawerPrimitive.Root>) {
+function Drawer({ ...props }: ComponentProps<typeof DrawerPrimitive.Root>) {
   return <DrawerPrimitive.Root data-slot="drawer" {...props} />;
 }
 
