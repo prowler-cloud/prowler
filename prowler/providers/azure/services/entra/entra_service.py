0b98af9a06d5303ecb3fea50f40afca67850a0e6
HugoPBrito <hugopbrit@gmail.com>
2026-01-13 14:02:26 +0100

    fix: permission error log

--- prowler/providers/azure/services/entra/entra_service.py
+++ prowler/providers/azure/services/entra/entra_service.py
@@ -137,17 +137,9 @@ class Entra(AzureService):
                 ).get()
 
         except Exception as error:
-            if (
-                error.__class__.__name__ == "ODataError"
-                and error.__dict__.get("response_status_code", None) == 403
-            ):
-                logger.error(
-                    "You need 'Reports.Read.All' permission to access user registration details. It only can be granted through Service Principal authentication."
-                )
-            else:
-                logger.error(
-                    f"{error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
-                )
+            logger.error(
+                f"{error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+            )
 
         return registration_details
 
