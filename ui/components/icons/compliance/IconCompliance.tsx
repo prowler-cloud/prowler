32008993c8adec72df8da68fbba02ff69041bfb0
pedrooot <pedromarting3@gmail.com>
2026-02-11 14:02:05 +0100

    feat(compliance): add csa ccm for the aws provider API & UI side

--- ui/components/icons/compliance/IconCompliance.tsx
+++ ui/components/icons/compliance/IconCompliance.tsx
@@ -2,6 +2,7 @@ import AWSLogo from "./aws.svg";
 import C5Logo from "./c5.svg";
 import CCCLogo from "./ccc.svg";
 import CISLogo from "./cis.svg";
+import CSALogo from "./csa.svg";
 import CISALogo from "./cisa.svg";
 import ENSLogo from "./ens.png";
 import FedRAMPLogo from "./fedramp.svg";
@@ -40,6 +41,7 @@ const COMPLIANCE_LOGOS = {
   nis2: NIS2Logo,
   c5: C5Logo,
   ccc: CCCLogo,
+  csa: CSALogo,
 } as const;
 
 export const getComplianceIcon = (complianceTitle: string) => {
--- ui/lib/compliance/compliance-mapper.ts
+++ ui/lib/compliance/compliance-mapper.ts
@@ -3,6 +3,7 @@ import { createElement, ReactNode } from "react";
 import { AWSWellArchitectedCustomDetails } from "@/components/compliance/compliance-custom-details/aws-well-architected-details";
 import { C5CustomDetails } from "@/components/compliance/compliance-custom-details/c5-details";
 import { CCCCustomDetails } from "@/components/compliance/compliance-custom-details/ccc-details";
+import { CSACustomDetails } from "@/components/compliance/compliance-custom-details/csa-details";
 import { CISCustomDetails } from "@/components/compliance/compliance-custom-details/cis-details";
 import { ENSCustomDetails } from "@/components/compliance/compliance-custom-details/ens-details";
 import { GenericCustomDetails } from "@/components/compliance/compliance-custom-details/generic-details";
@@ -32,6 +33,10 @@ import {
   mapComplianceData as mapCCCComplianceData,
   toAccordionItems as toCCCAccordionItems,
 } from "./ccc";
+import {
+  mapComplianceData as mapCSAComplianceData,
+  toAccordionItems as toCSAAccordionItems,
+} from "./csa";
 import {
   mapComplianceData as mapCISComplianceData,
   toAccordionItems as toCISAccordionItems,
@@ -178,6 +183,15 @@ const getComplianceMappers = (): Record<string, ComplianceMapper> => ({
       calculateCategoryHeatmapData(data),
     getDetailsComponent: (requirement: Requirement) =>
       createElement(CCCCustomDetails, { requirement }),
+  },
+  "CSA-CCM": {
+    mapComplianceData: mapCSAComplianceData,
+    toAccordionItems: toCSAAccordionItems,
+    getTopFailedSections,
+    calculateCategoryHeatmapData: (data: Framework[]) =>
+      calculateCategoryHeatmapData(data),
+    getDetailsComponent: (requirement: Requirement) =>
+      createElement(CSACustomDetails, { requirement }),
   },
 });
 
