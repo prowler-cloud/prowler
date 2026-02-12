b8e5ae3b1d47e6708b8e78672f018b5cbdcfbd08
pedrooot <pedromarting3@gmail.com>
2026-02-12 09:33:15 +0100

    feat(compliance): add csa ccm for the gcp provider

--- prowler/lib/outputs/compliance/csa/models.py
+++ prowler/lib/outputs/compliance/csa/models.py
@@ -28,3 +28,32 @@ class AWSCSAModel(BaseModel):
     ResourceName: str
     Framework: str
     Name: str
+
+
+class GCPCSAModel(BaseModel):
+    """
+    GCPCSAModel generates a finding's output in CSV CSA format for GCP.
+    """
+
+    Provider: str
+    Description: str
+    ProjectId: str
+    Location: str
+    AssessmentDate: str
+    Requirements_Id: str
+    Requirements_Description: str
+    Requirements_Name: str
+    Requirements_Attributes_Section: str
+    Requirements_Attributes_CCMLite: str
+    Requirements_Attributes_IaaS: str
+    Requirements_Attributes_PaaS: str
+    Requirements_Attributes_SaaS: str
+    Requirements_Attributes_ScopeApplicability: list[dict]
+    Status: str
+    StatusExtended: str
+    ResourceId: str
+    CheckId: str
+    Muted: bool
+    ResourceName: str
+    Framework: str
+    Name: str
