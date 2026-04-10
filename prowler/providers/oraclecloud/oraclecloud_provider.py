960088de4c3033914128f337345b80852f214b4b
Ronan Chota <ronan.chota@saic.com>
2026-04-10 16:05:44 -0400

    fix issue with single_region

--- prowler/providers/oraclecloud/oraclecloud_provider.py
+++ prowler/providers/oraclecloud/oraclecloud_provider.py
@@ -127,9 +127,10 @@ class OraclecloudProvider(Provider):
 
         logger.info("Initializing OCI provider ...")
 
-        # Check if the configuration is scanning a single region        single_region = None
+        # Check if the configuration is scanning a single region        
+        single_region = None
         if region:
-            single_region = list(region)[0] if len(region) >= 1 else None
+            single_region = list(region)[0] if len(region) == 1 else None
 
         # Setup OCI Session
         logger.info("Setting up OCI session ...")
