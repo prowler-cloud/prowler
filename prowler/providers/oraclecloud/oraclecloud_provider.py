ce58b57845225dd176f2f1b49d932002986d2607
Ronan Chota <ronan.chota@saic.com>
2026-04-17 13:45:27 -0400

    lint

--- prowler/providers/oraclecloud/oraclecloud_provider.py
+++ prowler/providers/oraclecloud/oraclecloud_provider.py
@@ -128,7 +128,7 @@ class OraclecloudProvider(Provider):
 
         logger.info("Initializing OCI provider ...")
 
-        # Check if the configuration is scanning a single region        
+        # Check if the configuration is scanning a single region
         single_region = None
         if region:
             single_region = list(region)[0] if len(region) == 1 else None
