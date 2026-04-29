58d5223edc718866e8970c1625521250755b3f8d
Pepe Fagoaga <pepe@prowler.com>
2026-04-29 12:29:21 +0200

    chore: remove silk from debugging

--- api/src/backend/config/urls.py
+++ api/src/backend/config/urls.py
@@ -1,9 +1,5 @@
-from django.conf import settings
 from django.urls import include, path
 
 urlpatterns = [
     path("api/v1/", include("api.v1.urls")),
 ]
-
-if getattr(settings, "DJANGO_SILK_ENABLED", False):
-    urlpatterns.append(path("silk/", include("silk.urls", namespace="silk")))
