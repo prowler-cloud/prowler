a9c30cb546da0372be9137198daab253d2c81527
alejandrobailo <alejandrobailo94@gmail.com>
2026-05-11 11:03:03 +0200

    docs(docs): add alerts guide

--- ui/lib/external-urls.ts
+++ ui/lib/external-urls.ts
@@ -6,7 +6,7 @@ export const DOCS_URLS = {
     "https://docs.prowler.com/user-guide/tutorials/prowler-app#step-8:-analyze-the-findings",
   AWS_ORGANIZATIONS:
     "https://docs.prowler.com/user-guide/tutorials/prowler-cloud-aws-organizations",
-  ALERTS: "https://docs.prowler.com/user-guide/tutorials/prowler-app", // TODO: Update this URL to the Alerts documentation
+  ALERTS: "https://docs.prowler.com/user-guide/tutorials/prowler-app-alerts",
   ATTACK_PATHS_CUSTOM_QUERIES:
     "https://docs.prowler.com/user-guide/tutorials/prowler-app-attack-paths#writing-custom-opencypher-queries",
 } as const;
