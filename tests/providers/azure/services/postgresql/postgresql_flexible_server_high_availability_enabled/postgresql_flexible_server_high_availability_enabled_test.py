b4a0e5747db49a33cd2608132c85ec33f4cbb377
Daniel Barranquero <danielbo2001@gmail.com>
2026-06-19 11:51:19 +0200

    chore: revision

--- tests/providers/azure/services/postgresql/postgresql_flexible_server_high_availability_enabled/postgresql_flexible_server_high_availability_enabled_test.py
+++ tests/providers/azure/services/postgresql/postgresql_flexible_server_high_availability_enabled/postgresql_flexible_server_high_availability_enabled_test.py
@@ -1,14 +1,41 @@
 from unittest import mock
+from uuid import uuid4
 
+from prowler.providers.azure.services.postgresql.postgresql_service import Server
 from tests.providers.azure.azure_fixtures import (
+    AZURE_SUBSCRIPTION_DISPLAY,
     AZURE_SUBSCRIPTION_ID,
+    AZURE_SUBSCRIPTION_NAME,
     set_mocked_azure_provider,
 )
 
 
+def _make_server(server_id, server_name, high_availability_mode):
+    return Server(
+        id=server_id,
+        name=server_name,
+        resource_group="resource_group",
+        location="eastus",
+        require_secure_transport="ON",
+        active_directory_auth="Enabled",
+        entra_id_admins=[],
+        log_checkpoints="ON",
+        log_connections="ON",
+        log_disconnections="ON",
+        connection_throttling="ON",
+        log_retention_days="3",
+        firewall=[],
+        high_availability_mode=high_availability_mode,
+    )
+
+
 class Test_postgresql_flexible_server_high_availability_enabled:
-    def test_no_subscriptions(self):
+    def test_no_postgresql_flexible_servers(self):
         postgresql_client = mock.MagicMock
+        postgresql_client.subscriptions = {
+            AZURE_SUBSCRIPTION_ID: AZURE_SUBSCRIPTION_NAME
+        }
+        postgresql_client.flexible_servers = {}
 
         with (
             mock.patch(
@@ -23,15 +50,21 @@ class Test_postgresql_flexible_server_high_availability_enabled:
             from prowler.providers.azure.services.postgresql.postgresql_flexible_server_high_availability_enabled.postgresql_flexible_server_high_availability_enabled import (
                 postgresql_flexible_server_high_availability_enabled,
             )
-
-            postgresql_client.flexible_servers = {}
 
             check = postgresql_flexible_server_high_availability_enabled()
             result = check.execute()
             assert len(result) == 0
 
-    def test_pass(self):
+    def test_postgresql_high_availability_disabled(self):
+        server_id = str(uuid4())
+        server_name = "test-server"
         postgresql_client = mock.MagicMock
+        postgresql_client.subscriptions = {
+            AZURE_SUBSCRIPTION_ID: AZURE_SUBSCRIPTION_NAME
+        }
+        postgresql_client.flexible_servers = {
+            AZURE_SUBSCRIPTION_ID: [_make_server(server_id, server_name, "Disabled")]
+        }
 
         with (
             mock.patch(
@@ -46,36 +79,90 @@ class Test_postgresql_flexible_server_high_availability_enabled:
             from prowler.providers.azure.services.postgresql.postgresql_flexible_server_high_availability_enabled.postgresql_flexible_server_high_availability_enabled import (
                 postgresql_flexible_server_high_availability_enabled,
             )
-            from prowler.providers.azure.services.postgresql.postgresql_service import Server
-
-            postgresql_client.flexible_servers = {AZURE_SUBSCRIPTION_ID: [Server(id="/sub/rg/server1", name="test-server", resource_group="rg1", location="eastus", require_secure_transport="ON", active_directory_auth="Enabled", entra_id_admins=[], log_checkpoints="ON", log_connections="ON", log_disconnections="ON", connection_throttling="ON", log_retention_days="3", firewall=[], high_availability_mode="ZoneRedundant")]}
-
-            check = postgresql_flexible_server_high_availability_enabled()
-            result = check.execute()
-            assert len(result) == 1
-            assert result[0].status == "PASS"
-
-    def test_fail(self):
-        postgresql_client = mock.MagicMock
-
-        with (
-            mock.patch(
-                "prowler.providers.common.provider.Provider.get_global_provider",
-                return_value=set_mocked_azure_provider(),
-            ),
-            mock.patch(
-                "prowler.providers.azure.services.postgresql.postgresql_flexible_server_high_availability_enabled.postgresql_flexible_server_high_availability_enabled.postgresql_client",
-                new=postgresql_client,
-            ),
-        ):
-            from prowler.providers.azure.services.postgresql.postgresql_flexible_server_high_availability_enabled.postgresql_flexible_server_high_availability_enabled import (
-                postgresql_flexible_server_high_availability_enabled,
-            )
-            from prowler.providers.azure.services.postgresql.postgresql_service import Server
-
-            postgresql_client.flexible_servers = {AZURE_SUBSCRIPTION_ID: [Server(id="/sub/rg/server1", name="test-server", resource_group="rg1", location="eastus", require_secure_transport="ON", active_directory_auth="Enabled", entra_id_admins=[], log_checkpoints="ON", log_connections="ON", log_disconnections="ON", connection_throttling="ON", log_retention_days="3", firewall=[], high_availability_mode="Disabled")]}
 
             check = postgresql_flexible_server_high_availability_enabled()
             result = check.execute()
             assert len(result) == 1
             assert result[0].status == "FAIL"
+            assert (
+                result[0].status_extended
+                == f"Flexible Postgresql server {server_name} from subscription {AZURE_SUBSCRIPTION_DISPLAY} does not have high availability enabled."
+            )
+            assert result[0].subscription == AZURE_SUBSCRIPTION_ID
+            assert result[0].resource_name == server_name
+            assert result[0].resource_id == server_id
+            assert result[0].location == "eastus"
+
+    def test_postgresql_high_availability_not_set(self):
+        server_id = str(uuid4())
+        server_name = "test-server"
+        postgresql_client = mock.MagicMock
+        postgresql_client.subscriptions = {
+            AZURE_SUBSCRIPTION_ID: AZURE_SUBSCRIPTION_NAME
+        }
+        postgresql_client.flexible_servers = {
+            AZURE_SUBSCRIPTION_ID: [_make_server(server_id, server_name, None)]
+        }
+
+        with (
+            mock.patch(
+                "prowler.providers.common.provider.Provider.get_global_provider",
+                return_value=set_mocked_azure_provider(),
+            ),
+            mock.patch(
+                "prowler.providers.azure.services.postgresql.postgresql_flexible_server_high_availability_enabled.postgresql_flexible_server_high_availability_enabled.postgresql_client",
+                new=postgresql_client,
+            ),
+        ):
+            from prowler.providers.azure.services.postgresql.postgresql_flexible_server_high_availability_enabled.postgresql_flexible_server_high_availability_enabled import (
+                postgresql_flexible_server_high_availability_enabled,
+            )
+
+            check = postgresql_flexible_server_high_availability_enabled()
+            result = check.execute()
+            assert len(result) == 1
+            assert result[0].status == "FAIL"
+            assert (
+                result[0].status_extended
+                == f"Flexible Postgresql server {server_name} from subscription {AZURE_SUBSCRIPTION_DISPLAY} does not have high availability enabled."
+            )
+
+    def test_postgresql_high_availability_enabled(self):
+        server_id = str(uuid4())
+        server_name = "test-server"
+        postgresql_client = mock.MagicMock
+        postgresql_client.subscriptions = {
+            AZURE_SUBSCRIPTION_ID: AZURE_SUBSCRIPTION_NAME
+        }
+        postgresql_client.flexible_servers = {
+            AZURE_SUBSCRIPTION_ID: [
+                _make_server(server_id, server_name, "ZoneRedundant")
+            ]
+        }
+
+        with (
+            mock.patch(
+                "prowler.providers.common.provider.Provider.get_global_provider",
+                return_value=set_mocked_azure_provider(),
+            ),
+            mock.patch(
+                "prowler.providers.azure.services.postgresql.postgresql_flexible_server_high_availability_enabled.postgresql_flexible_server_high_availability_enabled.postgresql_client",
+                new=postgresql_client,
+            ),
+        ):
+            from prowler.providers.azure.services.postgresql.postgresql_flexible_server_high_availability_enabled.postgresql_flexible_server_high_availability_enabled import (
+                postgresql_flexible_server_high_availability_enabled,
+            )
+
+            check = postgresql_flexible_server_high_availability_enabled()
+            result = check.execute()
+            assert len(result) == 1
+            assert result[0].status == "PASS"
+            assert (
+                result[0].status_extended
+                == f"Flexible Postgresql server {server_name} from subscription {AZURE_SUBSCRIPTION_DISPLAY} has high availability enabled."
+            )
+            assert result[0].subscription == AZURE_SUBSCRIPTION_ID
+            assert result[0].resource_name == server_name
+            assert result[0].resource_id == server_id
+            assert result[0].location == "eastus"
