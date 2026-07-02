c2321a2f530adab9d56171793a8c2f25c195cfe7
wbro <80239840234@proton.me>
2026-07-02 17:34:10 -0400

    feat(kubernetes): add check for read-only root filesystem in containers

---
+++ prowler/providers/kubernetes/services/core/core_readonly_root_filesystem_enabled/core_readonly_root_filesystem_enabled.py
@@ -0,0 +1,27 @@
+from prowler.lib.check.models import Check, Check_Report_Kubernetes
+from prowler.providers.kubernetes.services.core.core_client import core_client
+
+
+class core_readonly_root_filesystem_enabled(Check):
+    def execute(self) -> Check_Report_Kubernetes:
+        findings = []
+        for pod in core_client.pods.values():
+            report = Check_Report_Kubernetes(metadata=self.metadata(), resource=pod)
+            report.status = "PASS"
+            report.status_extended = (
+                f"Pod {pod.name} has read-only root filesystem enabled for all containers."
+            )
+
+            for container in pod.containers.values():
+                if (
+                    not container.security_context
+                    or container.security_context.get("read_only_root_filesystem")
+                    is not True
+                ):
+                    report.status = "FAIL"
+                    report.status_extended = f"Pod {pod.name} container {container.name} does not have readOnlyRootFilesystem set to true."
+                    break
+
+            findings.append(report)
+
+        return findings
