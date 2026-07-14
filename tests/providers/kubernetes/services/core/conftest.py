fa9b2c707b0deb67281de17a8d9b66df0a96a32d
Haitao Zheng <h@tzheng.dev>
2026-07-14 13:23:59 +0200

    feat(kubernetes): add hostPath volume check (#11837)
    Co-authored-by: Hugo P.Brito <hugopbrit@gmail.com>

--- tests/providers/kubernetes/services/core/conftest.py
+++ tests/providers/kubernetes/services/core/conftest.py
@@ -32,6 +32,7 @@ def make_pod(
     containers=None,
     init_containers=None,
     ephemeral_containers=None,
+    volumes=None,
     name="test-pod",
     uid="test-pod-uid",
 ):
@@ -53,6 +54,7 @@ def make_pod(
         containers=containers or {},
         init_containers=init_containers or {},
         ephemeral_containers=ephemeral_containers or {},
+        volumes=volumes,
     )
 
 
