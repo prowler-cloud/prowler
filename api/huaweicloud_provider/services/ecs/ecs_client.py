ffa8f0035cd68d93e54a7c930aa4e0b471eaf976
Daniel Barranquero <danielbo2001@gmail.com>
2026-07-21 14:06:28 +0200

    chore(huaweicloud): remove misplaced provider copy from api/

--- api/huaweicloud_provider/services/ecs/ecs_client.py
+++
@@ -1,4 +0,0 @@
-from prowler.providers.huaweicloud.services.ecs.ecs_service import ECS
-from prowler.providers.common.provider import Provider
-
-ecs_client = ECS(Provider.get_global_provider())
--- api/huaweicloud_provider/services/evs/evs_client.py
+++
@@ -1,4 +0,0 @@
-from prowler.providers.huaweicloud.services.evs.evs_service import EVS
-from prowler.providers.common.provider import Provider
-
-evs_client = EVS(Provider.get_global_provider())
