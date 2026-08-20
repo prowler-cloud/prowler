ecd9a271411f2fafb1a6f879646fc2710779f217
DhanushPillay <dhanushpillay28@gmail.com>
2026-08-20 21:41:59 +0530

    feat(azure): add sqlserver_advanced_data_security_enabled check

---
+++ prowler/providers/azure/services/sqlserver/sqlserver_advanced_data_security_enabled/sqlserver_advanced_data_security_enabled.py
@@ -0,0 +1,27 @@
+from prowler.lib.check.models import Check, Check_Report_Azure
+from prowler.providers.azure.services.sqlserver.sqlserver_client import (
+    sqlserver_client,
+)
+
+
+class sqlserver_advanced_data_security_enabled(Check):
+    def execute(self) -> list[Check_Report_Azure]:
+        findings = []
+        for subscription, sql_servers in sqlserver_client.sql_servers.items():
+            subscription_name = sqlserver_client.subscriptions.get(
+                subscription, subscription
+            )
+            for sql_server in sql_servers:
+                if sql_server.security_alert_policies:
+                    report = Check_Report_Azure(
+                        metadata=self.metadata(), resource=sql_server
+                    )
+                    report.subscription = subscription
+                    report.status = "FAIL"
+                    report.status_extended = f"SQL Server {sql_server.name} from subscription {subscription_name} ({subscription}) does not have Advanced Data Security enabled."
+                    if sql_server.security_alert_policies.state == "Enabled":
+                        report.status = "PASS"
+                        report.status_extended = f"SQL Server {sql_server.name} from subscription {subscription_name} ({subscription}) has Advanced Data Security enabled."
+                    findings.append(report)
+
+        return findings
---
+++ tests/providers/azure/services/sqlserver/sqlserver_advanced_data_security_enabled/sqlserver_advanced_data_security_enabled_test.py
@@ -0,0 +1,180 @@
+from unittest import mock
+from uuid import uuid4
+
+from azure.mgmt.sql.models import ServerSecurityAlertPolicy
+
+from prowler.providers.azure.services.sqlserver.sqlserver_service import Server
+from tests.providers.azure.azure_fixtures import (
+    AZURE_SUBSCRIPTION_DISPLAY,
+    AZURE_SUBSCRIPTION_ID,
+    AZURE_SUBSCRIPTION_NAME,
+    set_mocked_azure_provider,
+)
+
+
+class Test_sqlserver_advanced_data_security_enabled:
+    def test_no_sql_servers(self):
+        sqlserver_client = mock.MagicMock
+        sqlserver_client.subscriptions = {
+            AZURE_SUBSCRIPTION_ID: AZURE_SUBSCRIPTION_NAME
+        }
+        sqlserver_client.sql_servers = {}
+
+        with (
+            mock.patch(
+                "prowler.providers.common.provider.Provider.get_global_provider",
+                return_value=set_mocked_azure_provider(),
+            ),
+            mock.patch(
+                "prowler.providers.azure.services.sqlserver.sqlserver_advanced_data_security_enabled.sqlserver_advanced_data_security_enabled.sqlserver_client",
+                new=sqlserver_client,
+            ),
+        ):
+            from prowler.providers.azure.services.sqlserver.sqlserver_advanced_data_security_enabled.sqlserver_advanced_data_security_enabled import (
+                sqlserver_advanced_data_security_enabled,
+            )
+
+            check = sqlserver_advanced_data_security_enabled()
+            result = check.execute()
+            assert len(result) == 0
+
+    def test_sql_servers_no_security_alert_policies(self):
+        sqlserver_client = mock.MagicMock
+        sqlserver_client.subscriptions = {
+            AZURE_SUBSCRIPTION_ID: AZURE_SUBSCRIPTION_NAME
+        }
+        sql_server_name = "SQL Server Name"
+        sql_server_id = str(uuid4())
+        sqlserver_client.sql_servers = {
+            AZURE_SUBSCRIPTION_ID: [
+                Server(
+                    id=sql_server_id,
+                    name=sql_server_name,
+                    public_network_access="",
+                    minimal_tls_version="",
+                    administrators=None,
+                    auditing_policies=[],
+                    firewall_rules=None,
+                    security_alert_policies=None,
+                    location="location",
+                )
+            ]
+        }
+
+        with (
+            mock.patch(
+                "prowler.providers.common.provider.Provider.get_global_provider",
+                return_value=set_mocked_azure_provider(),
+            ),
+            mock.patch(
+                "prowler.providers.azure.services.sqlserver.sqlserver_advanced_data_security_enabled.sqlserver_advanced_data_security_enabled.sqlserver_client",
+                new=sqlserver_client,
+            ),
+        ):
+            from prowler.providers.azure.services.sqlserver.sqlserver_advanced_data_security_enabled.sqlserver_advanced_data_security_enabled import (
+                sqlserver_advanced_data_security_enabled,
+            )
+
+            check = sqlserver_advanced_data_security_enabled()
+            result = check.execute()
+            assert len(result) == 0
+
+    def test_sql_servers_ads_disabled(self):
+        sqlserver_client = mock.MagicMock
+        sqlserver_client.subscriptions = {
+            AZURE_SUBSCRIPTION_ID: AZURE_SUBSCRIPTION_NAME
+        }
+        sql_server_name = "SQL Server Name"
+        sql_server_id = str(uuid4())
+        sqlserver_client.sql_servers = {
+            AZURE_SUBSCRIPTION_ID: [
+                Server(
+                    id=sql_server_id,
+                    name=sql_server_name,
+                    public_network_access="",
+                    minimal_tls_version="",
+                    administrators=None,
+                    auditing_policies=[],
+                    firewall_rules=None,
+                    security_alert_policies=ServerSecurityAlertPolicy(state="Disabled"),
+                    location="location",
+                )
+            ]
+        }
+
+        with (
+            mock.patch(
+                "prowler.providers.common.provider.Provider.get_global_provider",
+                return_value=set_mocked_azure_provider(),
+            ),
+            mock.patch(
+                "prowler.providers.azure.services.sqlserver.sqlserver_advanced_data_security_enabled.sqlserver_advanced_data_security_enabled.sqlserver_client",
+                new=sqlserver_client,
+            ),
+        ):
+            from prowler.providers.azure.services.sqlserver.sqlserver_advanced_data_security_enabled.sqlserver_advanced_data_security_enabled import (
+                sqlserver_advanced_data_security_enabled,
+            )
+
+            check = sqlserver_advanced_data_security_enabled()
+            result = check.execute()
+            assert len(result) == 1
+            assert result[0].status == "FAIL"
+            assert (
+                result[0].status_extended
+                == f"SQL Server {sql_server_name} from subscription {AZURE_SUBSCRIPTION_DISPLAY} does not have Advanced Data Security enabled."
+            )
+            assert result[0].subscription == AZURE_SUBSCRIPTION_ID
+            assert result[0].resource_name == sql_server_name
+            assert result[0].resource_id == sql_server_id
+            assert result[0].location == "location"
+
+    def test_sql_servers_ads_enabled(self):
+        sqlserver_client = mock.MagicMock
+        sqlserver_client.subscriptions = {
+            AZURE_SUBSCRIPTION_ID: AZURE_SUBSCRIPTION_NAME
+        }
+        sql_server_name = "SQL Server Name"
+        sql_server_id = str(uuid4())
+        sqlserver_client.sql_servers = {
+            AZURE_SUBSCRIPTION_ID: [
+                Server(
+                    id=sql_server_id,
+                    name=sql_server_name,
+                    public_network_access="",
+                    minimal_tls_version="",
+                    administrators=None,
+                    auditing_policies=[],
+                    firewall_rules=None,
+                    security_alert_policies=ServerSecurityAlertPolicy(state="Enabled"),
+                    location="location",
+                )
+            ]
+        }
+
+        with (
+            mock.patch(
+                "prowler.providers.common.provider.Provider.get_global_provider",
+                return_value=set_mocked_azure_provider(),
+            ),
+            mock.patch(
+                "prowler.providers.azure.services.sqlserver.sqlserver_advanced_data_security_enabled.sqlserver_advanced_data_security_enabled.sqlserver_client",
+                new=sqlserver_client,
+            ),
+        ):
+            from prowler.providers.azure.services.sqlserver.sqlserver_advanced_data_security_enabled.sqlserver_advanced_data_security_enabled import (
+                sqlserver_advanced_data_security_enabled,
+            )
+
+            check = sqlserver_advanced_data_security_enabled()
+            result = check.execute()
+            assert len(result) == 1
+            assert result[0].status == "PASS"
+            assert (
+                result[0].status_extended
+                == f"SQL Server {sql_server_name} from subscription {AZURE_SUBSCRIPTION_DISPLAY} has Advanced Data Security enabled."
+            )
+            assert result[0].subscription == AZURE_SUBSCRIPTION_ID
+            assert result[0].resource_name == sql_server_name
+            assert result[0].resource_id == sql_server_id
+            assert result[0].location == "location"
