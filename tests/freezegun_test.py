3ca189a52adc4d29bdc9c9886ce27fe54a3464b5
Hugo P.Brito <hugopbrit@gmail.com>
2026-09-01 16:24:53 +0100

    test(sdk): exercise lazy Okta loading under frozen time

--- tests/freezegun_test.py
+++ tests/freezegun_test.py
@@ -1,5 +1,4 @@
 import datetime
-import sys
 
 from freezegun import freeze_time
 
@@ -7,10 +6,12 @@ from freezegun import freeze_time
 def test_freezegun_handles_lazy_okta_models_and_restores_datetime():
     import okta.client  # noqa: F401
 
-    assert callable(sys.modules["okta.models"].__getattr__)
     real_datetime_id = id(datetime.datetime)
 
     with freeze_time("2025-01-01 00:00:00"):
+        from okta.models import NetworkZoneAddress
+
+        assert NetworkZoneAddress.__name__ == "NetworkZoneAddress"
         assert datetime.datetime.now() == datetime.datetime(2025, 1, 1)
 
     assert id(datetime.datetime) == real_datetime_id
